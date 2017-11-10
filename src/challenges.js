2357181fa05debaa23b071e65d3ea56bfad7a87d
Adam Fetters <31798262+adamcodesjava@users.noreply.github.com>
2017-11-10 14:44:30 -0500

    checkMatchingLeaves function in challenge.js - completed

--- src/challenges.js
+++ src/challenges.js
@@ -59,7 +59,26 @@ const reverseStr = str => {
 const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  let val;
+  let flag = true;
+  const checkLeaves = (tree) => {
+    Object.keys(tree).forEach((key) => {
+      if (val === undefined && typeof key !== 'object') {
+        val = tree[key];
+        return undefined;
+      }
+      if (typeof tree[key] === 'object') return checkLeaves(tree[key]);
+      if (tree[key] !== val) {
+        flag = false;
+        return undefined;
+      }
+      return undefined;
+    });
+  };
+  checkLeaves(obj);
+  return flag;
 };
+
 
 const flatten = elements => {
   // Flattens a nested array (the nesting can be to any depth).
