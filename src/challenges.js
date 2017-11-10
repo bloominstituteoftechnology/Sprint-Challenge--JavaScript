88dda698e576891e35f544cb6a8df56b86114a98
Jesse Hood <jessenoob@gmail.com>
2017-11-10 12:55:50 -0500

    solved limitFunctionCallCount

--- src/challenges.js
+++ src/challenges.js
@@ -21,6 +21,14 @@ const map = (elements, cb) => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let counter = n;
+  return (...args) => {
+    if (counter > 0) {
+      counter--;
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 const cacheFunction = cb => {
