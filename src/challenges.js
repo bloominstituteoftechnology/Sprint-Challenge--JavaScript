90c5a1b8681a3060a615b46d018206721a985464
Franz <franzjfernando@hotmail.com>
2017-11-10 11:56:56 -0800

    spent most of the time so far trying to fix npm and get it to run correctly, getting many weird errors and its not running.

--- src/challenges.js
+++ src/challenges.js
@@ -79,7 +79,7 @@ const checkMatchingLeaves = obj => {
 const flatten = elements => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
-  const flattenedArray = reduce(elements, (prev, next) => {
+  const flattenedArray = this.reduce(elements, (prev, next) => {
     if (Array.isArray(next)) return prev.concat(flatten(next));
     return prev.concat(next);
   }, []);
