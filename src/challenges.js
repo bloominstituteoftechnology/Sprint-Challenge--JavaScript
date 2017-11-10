47455014f13bc12b44cdd9037fa28b67639c8c8d
Franz <franzjfernando@hotmail.com>
2017-11-10 11:35:23 -0800

    first commit

--- src/challenges.js
+++ src/challenges.js
@@ -2,19 +2,34 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  // elements.map(element => cb(element));
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const mappedValues = [];
+  for (let i = 0; i < elements.length; i++) {
+    mappedValues.push(cb(elements[i]));
+  }
+  return mappedValues;
 };
+
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let nCount = 0;
+  return (...args) => {
+    if (nCount === n) return null;
+    nCount++;
+    return cb();
+  };
 };
-
 const cacheFunction = cb => {
   // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
@@ -30,16 +45,44 @@ const cacheFunction = cb => {
 const reverseStr = str => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  let newStr = '';
+  for (let i = (str.length - 1); i >= 0; i--) {
+    newStr += str[i];
+  }
+  return newStr;
 };
 
 const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  let tree;
+  let val;
+  let flag = true;
+  const checkLeaves = () => {
+    Object.keys(tree).forEach((leaf) => {
+      if (val === undefined && typeof leaf !== 'object') {
+        val = tree[leaf];
+        return undefined;
+      }
+      if (typeof tree[leaf] === 'object') return checkLeaves(tree[leaf]);
+      if (tree[leaf] !== val) {
+        flag = false;
+        return undefined;
+      }
+      return undefined;
+    });
+  };
+  checkLeaves(obj);
+  return flag;
 };
 
 const flatten = elements => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  const flattenedArray = reduce(elements, (prev, next) => {
+    if (Array.isArray(next)) return prev.concat(flatten(next));
+    return prev.concat(next);
+  }, []);
 };
 
 module.exports = {
