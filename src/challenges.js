a1229ed85148ac10c87911641746015dfd3e8c57
Pat Collins <padjo@mac.com>
2017-11-10 22:08:06 +0000

    finished closures:limitFunctionCallCount

--- src/challenges.js
+++ src/challenges.js
@@ -3,23 +3,32 @@ const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   for (let i = 0; i < elements.length; i++) {
-    cb(elements[i]);
+    cb(elements[i], i);
   }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
-
+  const resArr = [];
   for (let i = 0; i < elements.length; i++) {
-    return cb(elements[i]);
+    resArr[i] = cb(elements[i]);
   }
+  return resArr;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let i = 1;
+  return (...args) => {
+    if (i <= n) {
+      i++;
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 const cacheFunction = cb => {
