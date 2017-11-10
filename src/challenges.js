f6d6de799b81d41739eea00d76ad5fe9c4ea16b4
Pat Collins <padjo@mac.com>
2017-11-10 20:44:26 +0000

    finished for each

--- src/challenges.js
+++ src/challenges.js
@@ -2,11 +2,18 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i]);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+
+  for (let i = 0; i < elements.length; i++) {
+    return cb(elements[i]);
+  }
 };
 
 /* ======================== Closure Practice ============================ */
