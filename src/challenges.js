bfc020182f1322d5f1c9bfbf3f8550bf59a5e17e
Jeff <admin@etisdew.website>
2017-11-10 11:35:06 -0800

    oops forgot the src folder

--- src/challenges.js
+++ src/challenges.js
@@ -1,13 +1,13 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
-  // Iterates over a list of elements, yielding each in turn to the `cb` function.
-  // This only needs to work with arrays.
-};
+  for (let i = 0; i < elements.length; i++) { cb(elements[i]); }
+}; each([1,2,3], (element) => console.log(element));
 
 const map = (elements, cb) => {
-  // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
-  // Return the new array.
-};
+  const _elements = [];
+  for (let i = 0; i < elements.length; i++) { _elements.push(cb(elements[i])); }
+  return _elements;
+}; console.log(map([1,2,3], (element) => element + 100));
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
