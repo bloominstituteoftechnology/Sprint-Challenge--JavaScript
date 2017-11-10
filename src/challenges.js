ffbcd010e809fec556f9ca89da0ea1c281ad35fa
Stephanie <sbowl001@fiu.edu>
2017-11-11 03:00:47 +0800

    each passing

--- src/challenges.js
+++ src/challenges.js
@@ -1,6 +1,9 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
-  // Iterates over a list of elements, yielding each in turn to the `cb` function.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
+    // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
 };
 
