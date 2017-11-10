6a8ad6fa274fe829001a2b82bfd1c7d39595ce8c
Yasin Shuman <email@yasin.io>
2017-11-10 11:28:08 -0800

    used .forEach instead of a for loop - refactor of the each func

--- src/challenges.js
+++ src/challenges.js
@@ -2,9 +2,10 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
-  for (let i = 0; i < elements.length; i++) {
-    cb(elements[i], i);
-  }
+  // for (let i = 0; i < elements.length; i++) {
+  //   cb(elements[i], i);
+// }
+  elements.forEach((elem, index) => cb(elem, index));
 };
 
 const map = (elements, cb) => {
