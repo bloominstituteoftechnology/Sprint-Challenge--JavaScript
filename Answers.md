0061c91973cb5d768e4155f05187545937f440cf
cmoore4os <cm4glos@gmail.com>
2017-11-10 17:13:30 -0500

    initial commit

--- Answers.md
+++ Answers.md
@@ -1,2 +1,23 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 ### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+
+1. A forEach goes through each item in an array and returns something that is done to it without making a new array; map does the same but makes a new array
+
+2. Five type of Javascript data: Strings, Numbers, Objects, Arrays, Maps, Date.
+   - Arrays are special types of Objects
+
+3. Closures are functions that returns a function that retains the access to variables after the main function has exit.
+
+``` javascript
+
+function age(name) {
+  return (age) => {
+    return `${name} is ${age} years old.`
+  } 
+}
+const user1 = age('Consuella');
+const user2 = age('Ryan')
+console.log(user1)
+console.log(user1(45)); // remembers name and knows that this is age
+console.log(user2(31))
+```
--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,31 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const arr = [];
+  for (let i = 0; i < elements.length; i++) {
+    arr.push(cb(elements[i]));
+  }
+  return arr;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    count++;
+    if (count <= n) return cb(...args);
+    return null;
+  };
 };
 
 const cacheFunction = cb => {
