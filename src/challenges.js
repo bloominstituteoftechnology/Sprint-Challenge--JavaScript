01755f6825a9120ba031ff0a151472f01cddcf04
Franz <franzjfernando@hotmail.com>
2017-11-10 17:22:46 -0800

    npm fixed

--- src/challenges.js
+++ src/challenges.js
@@ -37,19 +37,20 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
-  // const cache = {};
-  // // return cached value if it is the same value being called in cb
-  // return () => {
-  //   for (let i = 0; i < cache.length; i++) {
-  //     if (cb === cache[i]) {
-  //       return cache[i];
-  //     }
-  //     // if the object isn't cached, cache it and return it.
-  //     const result = cb;
-  //     cache.push(cb);
-  //     return result;
-  //   }
-  // };
+//   const cache = {};
+//   // return cached value if it is the same value being called in cb
+//   return () => {
+//     for (let i = 0; i < cache.length; i++) {
+//       if (cb === cache[i]) {
+//         return cache[i];
+//       }
+//       // if the object isn't cached, cache it and return it.
+//       const result = cb;
+//       cache.push(cb);
+//       return result;
+//     }
+//   };
+// };
   const cache = {};
   return () => {
     const args = Array.prototype.slice.call(arguments);
@@ -60,8 +61,8 @@ const cacheFunction = cb => {
       return cache[args];
     }
     const result = cb.apply(this, args);
-    cache.push(result);
-    return result;
+    // cache.push(result);
+    return cb(args);
   };
 };
 
@@ -105,10 +106,12 @@ const checkMatchingLeaves = obj => {
 const flatten = elements => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
-  const flattenedArray = this.reduce(elements, (prev, next) => {
-    if (Array.isArray(next)) return prev.concat(flatten(next));
-    return prev.concat(next);
+
+  elements = elements.reduce(function(a, b) {
+    if (Array.isArray(b)) return a.concat(flatten(b));
+    return a.concat(b);
   }, []);
+  return elements;
 };
 
 module.exports = {
