7584b1a623d003e0155ca755b088bcd7917c0a73
Troy Williams <troywilliams@Troys-MacBook-Pro.local>
2017-12-08 11:15:50 -0600

    completed

--- Answers.md
+++ Answers.md
@@ -1,2 +1,63 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 ### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+
+##H2 Describe some of the differences between .forEach & .map.
+forEach - take in an array, peforms some action on it, transforms that data in place.
+
+map does the same except returns a new array with the transformed elements.
+
+##H2 Name five different Types in JavaScript. A Type is something that can represent data. What is so special about Arrays?
+1. string - anything surrounded by quotes "words" "453"
+2. Numbers - integers, floats.
+3. objects - key value pairing the can be used to reference related data;
+object = {
+    name: 'troy',
+    age: '27',
+}
+values can be accesed with dot or bracket notation.
+object.name => 'troy';
+object['name'] => 'troy';
+4. undefined;
+5. null;
+6. Arrays - they are special because they are Ryan's favorite. Stores any number of elements in a group surrounded by [];
+Actually special because they can be iterated through the find or manipulate data. has a bunch of built in methods. forEach. Map length pop push.
+
+
+##H2What is closure? Can you code out a quick example of a closure?
+
+Closure is a local variable created through a function and scope.
+```
+
+let global = 'hi';
+
+const hello = () => {
+    const local = 'bye'
+
+
+    //console.log(global) => 'hi';
+    //console.log(local) => 'bye';
+}
+
+//console.log(global) => 'hi';
+//console.log(local) => undefined
+```
+the variable local only exsits inside the hello function and cannot be accessed outside of it so when it is tryig to be logged outside of the hello function, it can't not be found.
+
+the global variable is able to be logged inside of hello because functions inhierate the closure and values from anything above it.
+
+
+
+##H2 Describe the four rules of the 'this' keyword. No need to provide examples about it this time :)
+
+1. window/ global onclick= console.log(this). log the entire window object;
+2  what ever is to the left to of the method when calling a object. ==>object.method(this)
+3 when using JS native methods .bind .call. .apply
+4. When used with a cronstructor, this will refer to this specific instance created. not the constructor.
+
+
+
+
+
+
+
+
--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,33 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
-};
+  for (let i = 0; i < elements.length; i++){
+    cb(elements[i], i)
+  }
+}
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  let newArr = [];
+  for (let i = 0; i < elements.length; i++){
+    newArr.push(cb(elements[i]));
+  }
+  return newArr;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    if (count >= n){
+      return null;
+    }
+    count++;
+    return cb(...args);
+  }
 };
 
 const cacheFunction = cb => {
@@ -22,6 +38,15 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  let cache = {};
+  return (...args) => {
+    const key = args;
+    if (key in cache){
+      return cache.key;
+    }
+    cache[key] = cb(key);
+    return cache[key];
+  }
 };
 
 /* eslint-enable no-unused-vars */
@@ -30,16 +55,46 @@ const cacheFunction = cb => {
 const reverseStr = str => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  if (str.length === 0) {
+    return "";
+  }
+  return str[str.length - 1] + reverseStr(str.substring(0, str.length - 1));
 };
 
 const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  let tester;
+  let flag = true;
+  const checkLeaves = (object) => {
+    Object.keys(object).forEach((elem) => {
+      if (tester === undefined && typeof elem !== 'object'){
+        tester = object[elem]
+      }
+      if (typeof object[elem] === 'object'){
+        return checkLeaves(object[elem]);
+      }
+      if (object[elem] !== tester){
+        flag = false;
+      }
+    });
+  };
+  checkLeaves(obj);
+  return flag;
 };
 
 const flatten = elements => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  let resultArr = [];
+  elements.forEach((elem) => {
+    if (Array.isArray(elem)){
+      resultArr = resultArr.concat(flatten(elem));
+    } else {
+      resultArr.push(elem);
+    }
+  })
+  return resultArr;
 };
 
 module.exports = {
