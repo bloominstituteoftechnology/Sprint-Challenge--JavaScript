c233a64a90e23f00196b8ad8d1a68342dddf6d50
Jessica Ju-Yuan Liu <Airistotle@Airistotle.local>
2017-12-08 18:55:01 -0500

    Jessica - Challenges

--- Answers.md
+++ Answers.md
@@ -1,2 +1,19 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 ### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+
+1. forEach iterates over each element in an array and applies the designated function to each element.
+    A new array is not returned. map transforms each element in an array and returns a new array with the same
+    number of indexes as the original array. 
+    
+2. Boolean, Number, String, Null, and Undefined. Arrays are objects that can hold multiple elements of varying
+    data types. One array can only hold one data type, i.e. string, number, although it has the capability of
+    holding different types of data in general. There's a lot of flexibility that comes with arrays and handling
+    large amount of data. 
+
+3. A closure allows the local scope of a function to see outside of its scope, such as accessing a variable
+    outside of the local scope. 
+
+4. * This can be used with a function that is in the global scope.
+   * This can be used with the dot operator.
+   * This can be used with a constructor and refers to the object that was created.
+   * This can also be explicitly defined with .call() or .apply().
--- src/challenges.js
+++ src/challenges.js
@@ -1,45 +1,77 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
-  // Iterates over a list of elements, yielding each in turn to the `cb` function.
-  // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
-  // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
-  // Return the new array.
+  const newArray = [];
+  each(elements, (item, index) => {
+    newArray.push(cb(item, index));
+  });
+
+  return newArray;
 };
 
 /* ======================== Closure Practice ============================ */
+
 const limitFunctionCallCount = (cb, n) => {
-  // Should return a function that invokes `cb`.
-  // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    if (n === count) {
+      return null;
+    }
+    count++;
+    return cb(...args);
+  };
 };
 
+// -- Fix syntax
 const cacheFunction = cb => {
-  // Should return a funciton that invokes `cb`.
-  // A cache (object) should be kept in closure scope.
-  // The cache should keep track of all arguments have been used to invoke this function.
-  // If the returned function is invoked with arguments that it has already seen
-  // then it should return the cached result and not invoke `cb` again.
-  // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return input => {
+    if (Object.prototype.hasOwnProperty.call(cache, input)) return cache[input];
+    cache[input] = cb(input);
+    return cache[input];
+  };
 };
 
 /* eslint-enable no-unused-vars */
 
 /* ======================== Recursion Practice ============================ */
 const reverseStr = str => {
-  // reverse str takes in a string and returns that string in reversed order
-  // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  if (str === '') return '';
+  return reverseStr(str.substr(1)) + str.charAt(0);
 };
 
+// Make beautiful!
 const checkMatchingLeaves = obj => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
+  const index = 0;
+  const objArray = Object.values(obj);
+
+  const testFunction = (arr, i) => {
+    while (i < arr.length - 1) {
+      if (arr[i] !== arr[++i]) {
+        return false;
+      }
+      return testFunction(arr, i++);
+    }
+    return true;
+  };
+
+  return testFunction(objArray, index);
 };
 
 const flatten = elements => {
-  // Flattens a nested array (the nesting can be to any depth).
-  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  let outPut = [];
+  each(elements, num => {
+    if (Array.isArray(num)) outPut = outPut.concat(flatten(num));
+    else {
+      outPut.push(num);
+    }
+  });
+  return outPut;
 };
 
 module.exports = {
