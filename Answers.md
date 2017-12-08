45339ec7b38eaa203dc2edc56bbd30df7bde2862
Faiza Husain <faizahusain@Faizas-MacBook-Pro.local>
2017-12-08 10:44:25 -0800

    Changes made

--- Answers.md
+++ Answers.md
@@ -1,2 +1,40 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 ### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+1. Describe some of the differences between .forEach and .map.
+
+    .forEach is an array method that is like a for loop in that it iterates over each value/element inside the array. .forEach is easier to read because you can just put in the function inside the parentheses and the function's parameters. In other words, if you use a forEach on an array, it will call on the function to go over each value/element individually in the array. 
+
+2. Name five different Types in JavaScript. A Type is something that can represent data. What is so special about Arrays?
+
+    Strings, Booleans, Null, Undefined, and Number are the 5 data types in JavaScript. Arrays are special because they are the only object that can take all of these data types. 
+    Example: array = [1 , "two", true, null]; and I can get undefined if I try to call array[4] because there is no index 4 inside the array.
+
+3. What is closure? Can you code out a quick example of a closure?
+
+    In nested functions, a closure in an inner function can go outside of its own function and access the variables in the outer (enclosing) function. The closure can access three variable chains: its own, the variables of the enclosing function, and the global scope variables located outside of the outer function.
+
+    Example: 
+
+    function showName (firstName, lastName) {
+        let  nameIntro = "Your name is ";
+    
+    function fullName () {
+        return nameIntro + firstName + " " + lastName + "."; 
+    }
+    return fullName();
+    }
+
+    showName('Faiza', 'Husain'); //Your name is Faiza Husain.
+
+4. Describe the four rules of the 'this' keyword. No need to provide examples about it this time :)
+
+    'Global object' --> refers to the global scope variables outside of any function.
+    
+    'Implicit binding' --> when you 'bind' a variable/scope to the this keyword so that when this 
+                           is invoked, it refers to the variable bound to it. So if a this keyword 
+                           is called with in a function, it will refer to the variable inside that function.
+    
+    'new' keyword --> is when we create a new function/context for the this keyword.
+
+    'Explicit binding' --> is when we explicitly bind a context to the function with a .call
+                           or .apply method.
--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,32 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 1; i < elements.length - 1; i++) {
+    cb(elements[i]);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const arr = [];
+  each(elements, (item, index) => {
+    arr.push(cb(item, index));
+  });
+  return arr;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    console.log(...args);
+    if (n === count) return null;
+    count++;
+    return cb(...args);
+  };
 };
 
 const cacheFunction = cb => {
@@ -22,6 +37,12 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return input => {
+    if ({}.hasOwnPorperty.call(cache, input)) return cache[input];
+    cache[input] = cb(input);
+    return cache[input];
+  };
 };
 
 /* eslint-enable no-unused-vars */
@@ -30,17 +51,44 @@ const cacheFunction = cb => {
 const reverseStr = str => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  if (str === '') {
+    return '';
+  } else {
+    return reverseStr(str.substr(1)) + str.charAt(0);
+  }
 };
 
 const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  tree = '' || {};
+  return Object.keys(object).every(function (leaves) {
+    if (object[leaves] && typeof object[leaves] === 'object') {
+      return checke(object[leaves], tree);
+    }
+    if (!(value in value)) {
+      value.value = object[leaves];
+      return true;
+    }
+    return object[leaves] === tree.tree;
+  });
 };
 
 const flatten = elements => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  const outPut = [];
+  each(elements, num => {
+    if (Array.isArray(num)) {
+      flatten(num);
+    } else {
+      outPut.push(num);
+    }
+  });
+  return outPut;
 };
+const myArr = [1, 2, 3, [[4]]];
+flatten(myArr);
 
 module.exports = {
   each,
