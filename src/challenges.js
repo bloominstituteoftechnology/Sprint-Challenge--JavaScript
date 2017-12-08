e6fc563b339acef21d49358733dedfe4715d7cbf
Samuel Cha <sunchipssam@gmail.com>
2017-12-08 08:28:04 -0800

    Sprint-Challenge-JavaScript - limitFunctionCallCount passed

--- src/challenges.js
+++ src/challenges.js
@@ -1,15 +1,11 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
-  // Iterates over a list of elements, yielding each in turn to the `cb` function.
-  // This only needs to work with arrays.
   for (let i = 0; i < elements.length; i++) {
     cb(elements[i], i);
   }
 };
 
 const map = (elements, cb) => {
-  // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
-  // Return the new array.
   const retArr = [];
   each(elements, (element, i) => {
     retArr[i] = cb(element);
@@ -21,6 +17,14 @@ const map = (elements, cb) => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    if (count < n) {
+      count++;
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 const cacheFunction = cb => {
