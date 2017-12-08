18be729f54d036f5e14657c02976ef24b47cfb4b
LordOrbnauticus <haywood.d.johnson@gmail.com>
2017-12-08 12:51:09 -0600

    Finished Sprint Challenges

--- Answers.md
+++ Answers.md
@@ -1,2 +1,15 @@
-# Your responses to the short answer questions should be laid out here using Mark Down.
-### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+1. Describe some of the differences between .forEach & .map.
+⋅⋅* The biggest difference between the two is .map()does not affect the list items. It returns a new list with the contents transformed. .foreach() iterates over a list and applies side effects to it's items.
+2.  Name five different Types in JavaScript. A Type is something that can represent data. What is so special about Arrays?
+⋅⋅1. Boolean
+⋅⋅2. String
+⋅⋅3. Null
+⋅⋅4. Number
+⋅⋅5. Undefined
+3. What is closure? Can you code out a quick example of a closure?
+⋅⋅* closure is when a functio reaches outside it's own scope to use another variable
+4. Describe the four rules of the 'this' keyword. No need to provide examples about it this time :)
+⋅⋅1. If new is called, 'this' becomes a brand new object.
+⋅⋅2.  If apply, call, or bind are used to call a function,'this' inside the function is the passed argument.
+⋅⋅3. If a function is called as a method, 'this' is the object left of the dot.
+⋅⋅4.if a function is envoked without any conditions present, 'this' is the global object/window object.
--- src/challenges.js
+++ src/challenges.js
@@ -1,21 +1,45 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
 };
 
 const map = (elements, cb) => {
+  const newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArr.push(cb(elements[i]));
+  }
+  return newArr;
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
+  let count = 0;
+  return (...args) => {
+    if (count === n) return null;
+    count++;
+    return cb(...args);
+  };
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
 };
 
 const cacheFunction = cb => {
+  let cache = {};
+  return (...arg) => {
+    if (arg in cache) {
+      cache = arg;
+    } else {
+      cache[arg] = cb(...arg);
+      cache = cache[arg];
+    }
+    return cache;
+  };
   // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
@@ -28,16 +52,39 @@ const cacheFunction = cb => {
 
 /* ======================== Recursion Practice ============================ */
 const reverseStr = str => {
+  return str.split('').reverse().join('');
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
 };
 
 const checkMatchingLeaves = obj => {
+  const index = 0;
+  const objArr = Object.values(obj);
+
+  const testFunct = (arr, i) => {
+    while (i < arr.length - 1) {
+      if (arr[i] !== arr[++i]) {
+        return false;
+      }
+      return testFunct(arr, i);
+    }
+    return true;
+  };
+  return testFunct(objArr, index);
   // return true if every property on `obj` is the same
   // otherwise return false
 };
 
 const flatten = elements => {
+  let out = [];
+  each(elements, num => {
+    if (Array.isArray(num)) {
+      out = out.concat(flatten(num));
+    } else {
+      out.push(num);
+    }
+  });
+  return out;
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 };
