239c785022ec929dd5af4094b77297dcdf986dc8
Johnathan Justinn <jjustinn85@gmail.com>
2017-12-08 14:03:42 -0500

    pass all test but 1

--- src/challenges.js
+++ src/challenges.js
@@ -37,6 +37,13 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return obj => {
+    if (!(obj in cache)) {
+      cache[obj] = cb(obj);
+    }
+    return cache[obj];
+  };
 };
 
 /* eslint-enable no-unused-vars */
@@ -45,16 +52,46 @@ const cacheFunction = cb => {
 const reverseStr = str => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  if (str.length <= 0) {
+    return str;
+  }
+  return str.split('').reverse().join('');
 };
 
 const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  let check;
+  let flag = true;
+  const checkProp = object => {
+    Object.keys(object).forEach(element => {
+      if (check === undefined && typeof element !== 'object') {
+        return check === object[element];
+      }
+      if (typeof object[element] === 'object') {
+        return checkProp(object[element]);
+      }
+      if (object[element] !== check) {
+        flag = false;
+      }
+    });
+  };
+  checkProp(obj);
+  return flag;
 };
 
 const flatten = elements => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  let nowFlat = [];
+  each(elements, num => {
+    if (Array.isArray(num)) {
+      nowFlat = nowFlat.concat(flatten(num));
+    } else {
+      nowFlat.push(num);
+    }
+  });
+  return nowFlat;
 };
 
 module.exports = {
