9f03a2ecce31676a8058bc9a3f41572107ec09f8
unknown <brcampbell001@outlook.com>
2017-12-08 10:34:39 -0600

    each Passed

--- src/challenges.js
+++ src/challenges.js
@@ -1,7 +1,9 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
-  // Iterates over a list of elements, yielding each in turn to the `cb` function.
-  // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
+  return elements;
 };
 
 const map = (elements, cb) => {
