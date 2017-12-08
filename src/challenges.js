26e5f685c76e954d9dc1cbb4f2fe0194b6d91442
Samuel Cha <sunchipssam@gmail.com>
2017-12-08 08:22:09 -0800

    Sprint-Challenge-JavaScript - map test passed

--- src/challenges.js
+++ src/challenges.js
@@ -10,6 +10,11 @@ const each = (elements, cb) => {
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const retArr = [];
+  each(elements, (element, i) => {
+    retArr[i] = cb(element);
+  })
+  return retArr;
 };
 
 /* ======================== Closure Practice ============================ */
