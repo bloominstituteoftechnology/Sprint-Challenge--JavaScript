72551233396e13e8f4d74478f9570cf3da86266f
Samuel Cha <sunchipssam@gmail.com>
2017-12-08 08:37:06 -0800

    Sprint-Challenge-JavaScript - cacheFunction test passed

--- src/challenges.js
+++ src/challenges.js
@@ -15,8 +15,6 @@ const map = (elements, cb) => {
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
-  // Should return a function that invokes `cb`.
-  // The returned function should only allow `cb` to be invoked `n` times.
   let count = 0;
   return (...args) => {
     if (count < n) {
@@ -28,12 +26,16 @@ const limitFunctionCallCount = (cb, n) => {
 };
 
 const cacheFunction = cb => {
-  // Should return a funciton that invokes `cb`.
-  // A cache (object) should be kept in closure scope.
-  // The cache should keep track of all arguments have been used to invoke this function.
-  // If the returned function is invoked with arguments that it has already seen
-  // then it should return the cached result and not invoke `cb` again.
-  // `cb` should only ever be invoked once for a given set of arguments.
+  const cacheObj = {};
+  return (...args) => {
+    const key = args;
+    if (!(key in cacheObj)) {
+      const cacheVal = cb(...args);
+      cacheObj[key] = cacheVal;
+      return cacheVal;
+    }
+    return cacheObj.key;
+  };
 };
 
 /* eslint-enable no-unused-vars */
