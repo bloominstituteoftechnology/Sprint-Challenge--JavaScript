f7ecfe9a485cd0d6909935727b33abd83633e8ce
Dixie Korley <dixiekorley@gmail.com>
2018-01-12 13:43:38 -0500

    Additional answers. Check for last problem.

--- src/challenges.js
+++ src/challenges.js
@@ -1,22 +1,46 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
 };
 
 const map = (elements, cb) => {
+  const array = [];
+  for (let i = 0; i < elements.length; i++) {
+    cb(array.push(elements[i] * 2));
+  }
+  return array;
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
+  let count = 0;
+  return (...args) => {
+    if (count < n) {
+      count++;
+      return cb(...args);
+    }
+    return null;
+  };
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
 };
 
 const cacheFunction = cb => {
-  // Should return a funciton that invokes `cb`.
+  const cache = {};
+  return input => {
+    if (Object.prototype.hasOwnProperty.call(cache, input)) {
+      return cache[input];
+    }
+    cache[input] = cb(input);
+    return cache[input];
+  };
+  // Should return a function that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
   // If the returned function is invoked with arguments that it has already seen
@@ -28,16 +52,35 @@ const cacheFunction = cb => {
 
 /* ======================== Recursion Practice ============================ */
 const reverseStr = str => {
+  return str.split('').reverse().join('');
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
 };
 
 const checkMatchingLeaves = obj => {
+  const array = [];
+  const nestedObject = item => {
+    Object.keys(item).forEach(value => {
+      if (typeof item[value] === 'object') {
+        nestedObject(item[value]);
+      } array.push(item[value]);
+    });
+  };
+  nestedObject(obj);
+
+  for (let i = 1; i < array.length; i++) {
+    const start = array[0];
+    if (array[i] !== start) return false;
+  } return true;
   // return true if every property on `obj` is the same
   // otherwise return false
 };
 
 const flatten = elements => {
+  for (let i = 0; i < elements.length; i++) {
+    elements = elements.reduce((flat, next) => flat.concat(next), []);
+  }
+  return elements;
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 };
