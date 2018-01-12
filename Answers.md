b7cc40098c24246fdb53e1f05d3fecfc5c622611
Streltz <35007289+Streltz@users.noreply.github.com>
2018-01-12 14:07:07 -0500

    Answers.md

--- Answers.md
+++ Answers.md
@@ -1,2 +1,24 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 ### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+
+1. Map transforms each item in the array and returns a new array of transformed items with the same size of the old array. forEach is useful in just interating over the items in an array and manimpulating the items within the array. forEach iterates over a list and applies some manipulation of the members of that list. map iterates over a list and applies an operation to the members of that list and returns another list of the same size with the transformed values.
+
+2. Five different JavaScript data types are strings, numbers, Boolean, objects and arrays. The special thing about arrays are that the elements within an array don't need to be the same data type and a single array can hold multimple values of different data types.
+
+3. A closure is an inner function that has access to the outer function's variables. The closure has three scope chains: it has access to its own scope (vaiable defined between its curly brackets), it has access to the outer function's varaible and it has access to the global variables.  
+Example:
+    const limitFunction = (cb, n)
+      let callC = 0;
+  return (...args) => {
+    if (callC === n) return null;
+    callC++;
+    return cb(...args);
+  };
+};
+
+4. The four rules of 'this' in JavaScript are:
+    1- Whenever a function is contained in the global scope, the value of this inside of that function will be the window object
+    2- Whenever a function is called by a preceding dot, the object before that do is this
+    3- Whenever a constructor function is used, this refers to the specific instance of the object that is created and returned by the constructor function.
+    4- Whenever JavaScript's call or apply method is used, this is explicitly defined.
+}
--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,8 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
 };
@@ -7,12 +10,23 @@ const each = (elements, cb) => {
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const mappedArr = [];
+  each(elements, item => (mappedArr.push(cb(item))));
+  return mappedArr = []
 };
 
 /* ======================== Closure Practice ============================ */
-const limitFunctionCallCount = (cb, n) => {
+const limitFunctilsonCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  const cbCalLimit = () => {
+    if (count < n) {
+      cb();
+      count++;
+    }
+  };
+  return cbCalLimit;
 };
 
 const cacheFunction = cb => {
@@ -22,6 +36,12 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (input) => {
+    if (Object.prototype.hasOwnProperty.call(cache, input)) return cache[input];
+    cache[input] = cb(input);
+    return cache[input];
+  };
 };
 
 /* eslint-enable no-unused-vars */
@@ -30,17 +50,27 @@ const cacheFunction = cb => {
 const reverseStr = str => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
-};
+let newString = '';
+for (let i = str.length -1; i >= 0; i--) {
+  newString += str[i];
+  }
+  return newString
+}
 
 const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
 };
 
-const flatten = elements => {
+const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
-};
+  const flattenedArr = reduce(elements, (memo, item) => {
+    if (Array.isArray(item)) return memo.concat(flatten(item));
+    return memo.concat(item);
+   [];
+  return flattenedArr;
+}
 
 module.exports = {
   each,
