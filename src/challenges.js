a76e89eaed7db437a50fd9e6ea2b64b5f272ccc9
Igor Yermak <igor.yermak@gmail.com>
2018-01-12 14:15:02 -0500

    Sprint Challenge
    All challenges done except for matchingLeaves

--- src/challenges.js
+++ src/challenges.js
@@ -1,53 +1,87 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
+  for (let i = 0; i < elements.length; i++) {/*initiate for loop */
+    cb(elements[i]);
+  }
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
-};
 
-const map = (elements, cb) => {
+
+  const map = (elements, cb) => {
+    let newArr = [];
+    for (let i = 0; i < elements.length; i++)
+      newArr.push(cb(elements));
+  };
+  return newArr;
+
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
-};
 
-/* ======================== Closure Practice ============================ */
-const limitFunctionCallCount = (cb, n) => {
-  // Should return a function that invokes `cb`.
-  // The returned function should only allow `cb` to be invoked `n` times.
-};
+  /* ======================== Closure Practice ============================ */
+  const limitFunctionCallCount = (cb, n) => {
+    let times = 0;
+    return (...args) => {
+      times++;
+      if (times <= n) {
+        return cb();
+      }
+      // Should return a function that invokes `cb`.
+      // The returned function should only allow `cb` to be invoked `n` times.
+    };
 
-const cacheFunction = cb => {
-  // Should return a funciton that invokes `cb`.
-  // A cache (object) should be kept in closure scope.
-  // The cache should keep track of all arguments have been used to invoke this function.
-  // If the returned function is invoked with arguments that it has already seen
-  // then it should return the cached result and not invoke `cb` again.
-  // `cb` should only ever be invoked once for a given set of arguments.
-};
+    const cacheFunction = cb => {
+      return (input) => {
+        if (Object.prototype.hasOwnProperty.call(cache, input)) {
+          return cache[input];
+        }
+        cache[input] = cb(input);
+        return cache[input];
+        // Should return a funciton that invokes `cb`.
+        // A cache (object) should be kept in closure scope.
+        // The cache should keep track of all arguments have been used to invoke this function.
+        // If the returned function is invoked with arguments that it has already seen
+        // then it should return the cached result and not invoke `cb` again.
+        // `cb` should only ever be invoked once for a given set of arguments.
+      };
 
-/* eslint-enable no-unused-vars */
+      /* eslint-enable no-unused-vars */
 
-/* ======================== Recursion Practice ============================ */
-const reverseStr = str => {
-  // reverse str takes in a string and returns that string in reversed order
-  // The only difference between the way you've solved this before and now is that you need to do it recursivley!
-};
+      /* ======================== Recursion Practice ============================ */
+      const reverseStr = str => {
 
-const checkMatchingLeaves = obj => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
-};
+        if (!str.length) {
+          return '';
+        } else {
+          return str.split('').reverse().join('');
+        }
+        // reverse str takes in a string and returns that string in reversed order
+        // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+      };
 
-const flatten = elements => {
-  // Flattens a nested array (the nesting can be to any depth).
-  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
-};
+      // return true if every property on `obj` is the same
+      // otherwise return false
+      const checkMatchingLeaves = obj => {
+        let check = 0;
 
-module.exports = {
-  each,
-  map,
-  limitFunctionCallCount,
-  cacheFunction,
-  reverseStr,
-  checkMatchingLeaves,
-  flatten,
-};
+
+
+
+      };
+
+      const flatten = elements => {
+        for (let i = 0; i < elements.length; i++) {
+          elements = elements.reduce((flat, arr) => flat.concat(arr), []);
+        } return elements;
+        // Flattens a nested array (the nesting can be to any depth).
+        // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+      };
+
+      module.exports = {
+        each,
+        map,
+        limitFunctionCallCount,
+        cacheFunction,
+        reverseStr,
+        checkMatchingLeaves,
+        flatten,
+      };
