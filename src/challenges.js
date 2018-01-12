69e35ce60c475b85de5a04dab153dc62b8d99e46
Giraud Julemis <giraud.julemis@gmail.com>
2018-01-12 12:37:26 -0500

    added cacheFunction

--- src/challenges.js
+++ src/challenges.js
@@ -38,6 +38,14 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (...input) => {
+    if (Object.prototype.hasOwnProperty.call(cache, input)) {
+      return cache[input];
+    }
+    cache[input] = cb(...input);
+    return cache[input];
+  };
 };
 
 /* eslint-enable no-unused-vars */
