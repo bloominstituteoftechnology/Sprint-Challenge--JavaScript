5bd26e9c5001e3e66881effa6948cd547ac3384e
jonbrunt <jbrunt.mobile@gmail.com>
2018-01-12 12:04:51 -0600

    Test 3 passed

--- src/challenges.js
+++ src/challenges.js
@@ -21,6 +21,12 @@ const map = (elements, cb) => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    if (count === n) return null;
+    count += 1;
+    return cb(...args);
+  }
 };
 
 const cacheFunction = cb => {
