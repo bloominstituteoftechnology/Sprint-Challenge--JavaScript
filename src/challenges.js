a0519a2ff113622a01d53ef2001b1e9fa296f22c
Res Yeg <shaqk702@gmail.com>
2018-01-12 10:02:59 -0800

    Completed Closure problems

--- src/challenges.js
+++ src/challenges.js
@@ -21,7 +21,12 @@ const map = (elements, cb) => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
-  
+  let callCount = 0;
+  return (...args) => {
+    if (callCount === n) return null;
+    callCount++;
+    return cb(...args);
+  };
 };
 
 const cacheFunction = cb => {
@@ -31,6 +36,12 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (input) => {
+    if (Object.prototype.hasOwnProperty.call(cache, input)) return cache[input];
+    cache[input] = cb(input);
+    return cache[input];
+  };
 };
 
 /* eslint-enable no-unused-vars */
