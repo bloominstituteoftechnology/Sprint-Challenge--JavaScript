8dda182e9210efae0ea15684600fe842aef736ab
Lokesh Patel <Lokesh@lokeshs-mbp.attlocal.net>
2018-01-12 12:41:43 -0600

    Initial commit

--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,33 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const arr = [];
+  for (let i = 0; i < elements.length; i++) {
+    arr[i] = cb(elements[i]);
+  }
+  return arr;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    if (count < n) {
+      count++;
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 const cacheFunction = cb => {
@@ -22,6 +38,14 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+
+  return args => {
+    if (!(args in cache)) {
+      cache[args] = cb(args);
+    }
+    return cache[args];
+  };
 };
 
 /* eslint-enable no-unused-vars */
@@ -30,16 +54,35 @@ const cacheFunction = cb => {
 const reverseStr = str => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  if (str === '') {
+    return '';
+  } else {
+    return reverseStr(str.substr(1)) + str.charAt(0);
+  }
 };
 
 const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
+ let num = 0;
+ const values = Object.values(obj);
+ const place = values[0];
+ values.forEach((x) => {
+  if(x == place) {
+    return num += 0;
+  }
+   return num += 1;
+ });
+ return (num > 0) ? false : true;
+
 };
 
 const flatten = elements => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+ return elements.reduce((flat, toFlatten) => {
+   return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
+ }, []);
 };
 
 module.exports = {
