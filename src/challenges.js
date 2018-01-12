53a4687f84046216c7b56ac3145b94dda533b44e
Giraud Julemis <giraud.julemis@gmail.com>
2018-01-12 12:25:37 -0500

    solved map function

--- src/challenges.js
+++ src/challenges.js
@@ -10,6 +10,11 @@ const each = (elements, cb) => {
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const newArray = [];
+  elements.forEach(item => {
+    newArray.push(cb(item));
+  });
+  return newArray;
 };
 
 /* ======================== Closure Practice ============================ */
