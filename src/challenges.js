0f60f82fd45246f6cc3b557301a66c5541e226dc
Aaron <aarondburk@gmail.com>
2018-01-12 12:35:45 -0600

    passed one of the cachedFunction tests

--- src/challenges.js
+++ src/challenges.js
@@ -38,6 +38,10 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  return (...args) => {
+
+  };
+  return cb(...args);
 };
 
 /* eslint-enable no-unused-vars */
