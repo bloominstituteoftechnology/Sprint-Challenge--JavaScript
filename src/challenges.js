a5e5fa4f014553b90d2e5959584b78c777abb51f
Amanda Phillips <amanda@phillipsdev.org>
2018-01-12 11:20:59 -0600

    Some passing tests

--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,34 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    return cb(elements[i]);
+  }
 };
+//-----------------------------
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const mappedValues = [];
+  for (let i = 0; i < elements.length; i++) {
+    mappedValues.push(cb(elements[i]));
+  }
+  return mappedValues;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let i = 0;
+  return (...args) => {
+    i++;
+    if (i <= n) {
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 const cacheFunction = cb => {
@@ -30,6 +47,9 @@ const cacheFunction = cb => {
 const reverseStr = str => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  // return str.split('').reverse().join('');
+  if (str.length <= 1) return str;
+  return reverseStr(str.substring(1)) + str.charAt(0);
 };
 
 const checkMatchingLeaves = obj => {
@@ -40,7 +60,13 @@ const checkMatchingLeaves = obj => {
 const flatten = elements => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  const flatArr = elements.reduce(elements, (x, item) => {
+    if (Array.isArray(item)) return x.push(flatten(item));
+    return x.push(item);
+  }, []);
+  return flatArr;
 };
+//---------------------------------
 
 module.exports = {
   each,
