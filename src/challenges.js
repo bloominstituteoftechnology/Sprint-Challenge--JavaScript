cd1278a7dd30b1237ae2e76ef717004fca5c45ac
Aaron <aarondburk@gmail.com>
2018-01-12 12:15:26 -0600

    Completed the limitFunctionCallCount challenge

--- src/challenges.js
+++ src/challenges.js
@@ -21,6 +21,14 @@ const map = (elements, cb) => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let i = 0;
+  return (...args) => {
+    i++;
+    if (i <= n) {
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 const cacheFunction = cb => {
