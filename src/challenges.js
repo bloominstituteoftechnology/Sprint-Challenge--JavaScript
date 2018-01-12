c9e4f9a6615621838410b5d76d8ab47750afbfc1
Richard Reis <richardreeze1@gmail.com>
2018-01-12 08:58:04 -0800

    Finished exercise #4 (const limitFunctionCallCount)

--- src/challenges.js
+++ src/challenges.js
@@ -32,12 +32,20 @@ const limitFunctionCallCount = (cb, n) => {
 };
 
 const cacheFunction = cb => {
-  // Should return a funciton that invokes `cb`.
+  // Should return a function that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (input) => {
+    if (Object.prototype.hasOwnProperty.call(cache, input)){
+      return cache[input];
+    }
+    cache[input] = cb(input);
+    return cache[input];
+  };
 };
 
 /* eslint-enable no-unused-vars */
