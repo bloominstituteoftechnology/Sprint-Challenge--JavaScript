74ca02b7bd8bb035bd3b1fea65ed54e7d9f02967
Christopher Beards <beardscm@gmail.com>
2018-01-12 12:10:40 -0500

    First Passing Test

--- src/challenges.js
+++ src/challenges.js
@@ -2,11 +2,18 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  let newArr = [];
+  for (let i = 0; i <= elements.length; i++) {
+    newArr.push(cb(elements[i]));
+  }
 };
 
 /* ======================== Closure Practice ============================ */
