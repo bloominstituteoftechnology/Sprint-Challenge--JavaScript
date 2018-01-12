61fe440cbe231b5fae7657e5ee3ea48c15eaa33e
Giraud Julemis <giraud.julemis@gmail.com>
2018-01-12 13:54:20 -0500

    added checkMatchingLeaves

--- src/challenges.js
+++ src/challenges.js
@@ -63,6 +63,26 @@ const reverseStr = str => {
 const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  const destruct = input => {
+    if (!Object.is(Object(input), input)) {
+      return input;
+    }
+    const keys = Object.keys(input);
+    const first = input[keys[0]];
+    return Object.is(Object(first), first) ? destruct(first) : first;
+  };
+  const keys = Object.keys(obj);
+  const first = obj[keys[0]];
+  for (let i = 0; i < keys.length; i++) {
+    const current = obj[keys[i]];
+    if (Object.is(Object(current), current) && !checkMatchingLeaves(current)) {
+      return false;
+    }
+    if (!Object.is(destruct(current), destruct(first))) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const flatten = elements => {
