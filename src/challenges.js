1f6f403c51c5c2251fb4aeec349de860e1d4863c
rcs784 <rcstinson@hotmail.com>
2018-01-12 12:50:08 -0500

    complete sprint challenge 1/12

--- src/challenges.js
+++ src/challenges.js
@@ -1,45 +1,73 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
-  // Iterates over a list of elements, yielding each in turn to the `cb` function.
-  // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
-  // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
-  // Return the new array.
+  const newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArr[i] = cb(elements[i], i);
+  }
+  return newArr;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
-  // Should return a function that invokes `cb`.
-  // The returned function should only allow `cb` to be invoked `n` times.
+  let timesInvoked = 0;
+  return (...args) => {
+    if (timesInvoked < n) {
+      timesInvoked++;
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 const cacheFunction = cb => {
-  // Should return a funciton that invokes `cb`.
-  // A cache (object) should be kept in closure scope.
-  // The cache should keep track of all arguments have been used to invoke this function.
-  // If the returned function is invoked with arguments that it has already seen
-  // then it should return the cached result and not invoke `cb` again.
-  // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (...args) => {
+    if (Object.prototype.hasOwnProperty.call(cache, args)) return cache[args];
+    cache[args] = cb(args);
+    return cache[args];
+  };
 };
 
 /* eslint-enable no-unused-vars */
 
 /* ======================== Recursion Practice ============================ */
 const reverseStr = str => {
-  // reverse str takes in a string and returns that string in reversed order
-  // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  return str.length < 2 ? str : reverseStr(str.slice(1)).concat(str[0]);
 };
 
 const checkMatchingLeaves = obj => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
+  const simplify = input => {
+    if (Object(input) !== input) return input;
+    const keys = Object.keys(input);
+    const firstVal = input[keys[0]];
+    return (Object(firstVal) === firstVal) ? simplify(firstVal) : firstVal;
+  };
+  const keys = Object.keys(obj);
+  const firstVal = obj[keys[0]];
+  for (let i = 0; i < keys.length; i++) {
+    const currentVal = obj[keys[i]];
+    if (Object(currentVal) === currentVal && !checkMatchingLeaves(currentVal)) return false;
+    if (simplify(currentVal) !== simplify(firstVal)) return false;
+  }
+  return true;
 };
 
 const flatten = elements => {
-  // Flattens a nested array (the nesting can be to any depth).
-  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  let newArr = [];
+  elements.forEach(el => {
+    if (Array.isArray(el)) {
+      newArr = newArr.concat(flatten(el));
+    } else {
+      newArr.push(el);
+    }
+  });
+  return newArr;
 };
 
 module.exports = {
