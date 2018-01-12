b8a18e15794eb3fd502d4661d6b260e8e60ba5c9
Giraud Julemis <giraud.julemis@gmail.com>
2018-01-12 12:21:24 -0500

    solved each function

--- src/challenges.js
+++ src/challenges.js
@@ -2,6 +2,9 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  return elements.forEach((item, i) => {
+    cb(item, i);
+  });
 };
 
 const map = (elements, cb) => {
