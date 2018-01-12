8b645c625b9702192cc581b2e94b8e827259ed73
Nate152 <35231160+Nate152@users.noreply.github.com>
2018-01-12 11:43:40 -0500

    Commit1Challenge

--- src/challenges.js
+++ src/challenges.js
@@ -1,21 +1,43 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
 };
 
 const map = (elements, cb) => {
+  const newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArr[i] = cb(elements[i]);
+  }
+  return newArr;
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
+  let i = 0;
+  return (...args) => {
+    if (i < n) {
+      i++;
+      return cb(...args);
+    }
+    return null;
+  };
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
 };
 
 const cacheFunction = cb => {
+  const cache = {};
+  return input => {
+    if (Object.prototype.hasOwnProperty.call(cache, input)) return cache[input];
+    cache[input] = cb(input);
+    return cache[input];
+  };
   // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
@@ -28,6 +50,7 @@ const cacheFunction = cb => {
 
 /* ======================== Recursion Practice ============================ */
 const reverseStr = str => {
+  return str.split('').reverse().join('');
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
 };
