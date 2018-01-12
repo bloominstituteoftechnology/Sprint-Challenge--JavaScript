08f2f25791cf7ec387ab0f85f29ac9b9a04ae79f
jonbrunt <jbrunt.mobile@gmail.com>
2018-01-12 11:52:35 -0600

    Test 1 passed

--- src/challenges.js
+++ src/challenges.js
@@ -2,6 +2,9 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i += 1) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
