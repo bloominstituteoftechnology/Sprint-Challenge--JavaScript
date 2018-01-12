49bdc303645b6581abe45c5358500775250657a6
Kevin <16568166+sirkevinc@users.noreply.github.com>
2018-01-12 13:42:52 -0500

    Latest progress on challenges.js

--- src/challenges.js
+++ src/challenges.js
@@ -75,7 +75,6 @@ const checkMatchingLeaves = obj => {
 const flatten = elements => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
-  //base case: 
 };
 
 module.exports = {
