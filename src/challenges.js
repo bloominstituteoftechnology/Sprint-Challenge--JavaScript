357d495583894559fe6cff939d0b7ac27aecfb2d
temp <temp@Debis-Home-Mac.local>
2018-01-12 12:43:01 -0500

    halfway done

--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,26 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  return elements.forEach((units, i) => cb(units, i));
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  return elements.map(units => cb(units));
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let counter = 0;
+  return (...args) => {
+    if (counter < n) {
+      counter++;
+      return cb(...args);
+    } return null;
+  }
 };
 
 const cacheFunction = cb => {
@@ -22,6 +31,25 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  let i = 0;
+  let duplicate = 0;
+
+  return (...args) => {
+    duplicate = Object.keys(cache).forEach(key => {
+      if (cache[key] === args) {
+        return ++duplicate;
+      } else return duplicate;
+    });
+    if (duplicate > 0) {
+      cache[i] = args;
+      i++;
+      cb(...args);
+    } else {
+      duplicate = 0;
+      return cache[i];
+    }
+  }
 };
 
 /* eslint-enable no-unused-vars */
