5d2f4244e5eab1f1060382f7e8b5687000498b4b
Enjoyli Harrell <34499287+quietkittie@users.noreply.github.com>
2018-01-20 11:19:32 -0800

    Complete ReverseString

--- src/challenges.js
+++ src/challenges.js
@@ -2,20 +2,23 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
-  for (let i = 1; i < elements.length; i++) {
+  for (let i = 0; i < elements.length; i++) {
     cb(elements[i]);
   }
 };
 
+
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+
 };
 
 const cacheFunction = cb => {
@@ -25,6 +28,7 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+
 };
 
 /* eslint-enable no-unused-vars */
@@ -33,16 +37,20 @@ const cacheFunction = cb => {
 const reverseStr = str => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  if (str.length <= 1) return str;
+  return reverseStr(str.substring(1)) + str.charAt(0);
 };
 
 const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
+
 };
 
 const flatten = elements => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+
 };
 
 module.exports = {
