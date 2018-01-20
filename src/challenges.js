11a32d8f91b19c4a1623392bd00288dfed66a583
Lisa Cee <three_nineteen@yahoo.com>
2018-01-20 11:37:21 -0800

    still 15/19

--- src/challenges.js
+++ src/challenges.js
@@ -27,12 +27,23 @@ const limitFunctionCallCount = (cb, n) => {
 };
 
 const cacheFunction = cb => {
-  // Should return a funciton that invokes `cb`.
+  // Should return a function that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const memoized = x => {
+    const cache = {};
+    function callCb() {
+      if (x in cache) {
+        return cache[x];
+      }
+      cb(x);
+      cache[x] = cb(x);
+    }
+    return callCb();
+  };
 };
 
 /* eslint-enable no-unused-vars */
