455e78a14afba925b25217efffbd9bce9142365b
Stephanie <sbowl001@fiu.edu>
2018-01-21 03:07:45 +0800

    some solved

--- src/challenges.js
+++ src/challenges.js
@@ -1,19 +1,35 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
-  // Iterates over a list of elements, yielding each in turn to the `cb` function.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
+    // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
 };
 
 const map = (elements, cb) => {
+  const newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArr.push(cb(elements[i]));
+  } return newArr;
+};
+
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
-};
+
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
-  // Should return a function that invokes `cb`.
-  // The returned function should only allow `cb` to be invoked `n` times.
+  let callCount = 0;
+  return (...args) => {
+    if (callCount === n) return null;
+    callCount++;
+    return cb(...args);
+  };
 };
+// need review on args
+  // The returned function should only allow `cb` to be invoked `n` times.
+
 
 const cacheFunction = cb => {
   // Should return a funciton that invokes `cb`.
@@ -28,7 +44,7 @@ const cacheFunction = cb => {
 
 /* ======================== Recursion Practice ============================ */
 const reverseStr = str => {
-  // reverse str takes in a string and returns that string in reversed order
+  return str.split('').reverse().join('');// reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
 };
 
