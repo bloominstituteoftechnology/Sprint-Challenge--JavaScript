341ab0ae7368114e6824a53d7ddaacfd12a40225
unknown <alexisreyes90@gmail.com>
2018-01-20 14:04:34 -0500

    Alexis Reyes: partial sprint challenge update

--- src/challenges.js
+++ src/challenges.js
@@ -1,5 +1,8 @@
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
+  for(let i = 0; i < elements.length; i++) {
+    cb(elements[i]);// passes each element in arr to cb
+  }
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
 };
