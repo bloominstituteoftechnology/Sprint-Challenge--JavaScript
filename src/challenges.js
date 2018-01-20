fe68fa526b188b63de9ab0575b05651a6505cb8f
jnandez0586 <jnandez0586@gmail.com>
2018-01-20 11:39:50 -0600

    Jorge Hernandez: First commit

--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,30 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i]);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const arr = [];
+  for (let i = 0; i < elements.length; i++) {
+    arr.push(cb(elements[i]));
+  }
+  return arr;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  return function () {
+    for (let i = 0; i <= n; i++) {
+      cb();
+    }
+  };
 };
 
 const cacheFunction = cb => {
@@ -22,6 +35,7 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+
 };
 
 /* eslint-enable no-unused-vars */
@@ -30,6 +44,7 @@ const cacheFunction = cb => {
 const reverseStr = str => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+
 };
 
 const checkMatchingLeaves = obj => {
