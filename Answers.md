800a5f7232f16c8c8e42187103ae71d55d3ec879
John Coronel <john.m.coronel@gmail.com>
2018-01-27 08:59:11 -0800

    first commit

--- Answers.md
+++ Answers.md
@@ -1,2 +1,19 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 ### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+1. 
+.forEach - iterates over a array, modifying it in the process
+   .map - returns a new array which does some operation on the original arrays elements
+2.
+ Object,arrays,strings, integers, booleans
+Arrays are a collection of other types, also possibly other arrays.
+
+3. 
+ A closure is the added scope of an outer fuction/scope
+  For Example
+ ```
+ function counter = () => {
+     count = 0;
+     function increment () {
+         count++;
+     }
+ }
--- src/challenges.js
+++ src/challenges.js
@@ -2,27 +2,60 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const mappedArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    mappedArray.push(cb(elements[i]));
+  }
+  return mappedArray;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  const cbfunction = (...args) => {
+    if (count < n) {
+      count++;
+      cb(...args);
+    }
+    return null;
+  };
+  return cbfunction;
 };
 
 const cacheFunction = cb => {
+  const cache = {
+
+  };
+  const funct = (...args) => {
+    let str = '';
+    for (let i = 0; i < args.length; i++) {
+      str += args[i].tostring();
+    }
+    if (cache.str !== undefined) {
+      return cache.str;
+    }
+    cache.str = cb(...args);
+    cb(...args);
+  };
+  return funct;
+};
   // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
-};
+
 
 /* eslint-enable no-unused-vars */
 
@@ -30,16 +63,33 @@ const cacheFunction = cb => {
 const reverseStr = str => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  if (str === '') {
+    return '';
+  }
+  return reverseStr(str.substr(1)) + str[0];
 };
 
 const checkMatchingLeaves = obj => {
+  const matched = false;
+  const values = Object.values(obj);
   // return true if every property on `obj` is the same
   // otherwise return false
 };
 
 const flatten = elements => {
+  const flat = [];
+  elements.forEach(item => {
+    if (Array.isArray(item)) {
+      flat.push(...flatten(item));
+    } else {
+      flat.push(item);
+    }
+  }
+);
+
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  return flat;
 };
 
 module.exports = {
