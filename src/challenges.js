4f7be4a8bb39967cbfe51778fa449f3fbeb2528a
Iqra Javed <iqrajaved@Iqras-MacBook-Air.local>
2018-02-23 15:31:40 -0600

    completed each & map

--- src/challenges.js
+++ src/challenges.js
@@ -2,20 +2,37 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    const newVal = cb(elements[i]);
+    newArr.push(newVal);
+  }
+  return newArr;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let callLimit = n;
+  let counter = 0;
+  return () => {
+    for (let i = 0; i <= n; i++){
+    cb();
+    counter++;
+    }
+  }
 };
 
-const cacheFunction = cb => {
+const cacheFunction = (cb) => {
   // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
@@ -27,17 +44,17 @@ const cacheFunction = cb => {
 /* eslint-enable no-unused-vars */
 
 /* ======================== Recursion Practice ============================ */
-const reverseStr = str => {
+const reverseStr = (str) => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
 };
 
-const checkMatchingLeaves = obj => {
+const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
 };
 
-const flatten = elements => {
+const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 };
