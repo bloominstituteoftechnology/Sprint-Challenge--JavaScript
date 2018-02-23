e222b053dea4f473739a2277326dd774819e927d
joshuacoyne <josh.j.coyne@gmail.com>
2018-02-23 18:52:25 -0500

    JavaScript Sprint Challenge--Not complete

--- Answers.md
+++ Answers.md
@@ -1,2 +1,9 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 ### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+1. .forEach iterates through each item of an array and can access every elemnt. .map iterates over the array and can preform an operation with each element that it stores in a new array which is then returned. You would use .forEach to iterate over an array and you would use .map to iterate over an array and make changes to each element.
+2.Five types in JavaScript are Object, function, string, number and boolean. Arrays are a special type of object that is a sequential list. The key values are sequential numbers. Arrays also have a set of functions on the array prototype that allow you to operate on an array in specific ways.
+3.I can't really explain closure.
+4.The four rules of 'this' are as follows:
+    1. If this is in the global scope it refers to the global object. The window in chrome or the consosole in Node for example.
+    2.It implicitly refers to the object that it is being called on if it is being called from an object.
+    3. It explicitly can refer to a specific object using call or apply.
--- src/challenges.js
+++ src/challenges.js
@@ -2,42 +2,81 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArr.push(cb(elements[i]));
+  }
+  return newArr;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    if (count !== n) {
+      count++;
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
-const cacheFunction = cb => {
+const cacheFunction = (cb) => {
   // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (...args) => {
+    //  if (cache[args] !== undefined) return cache[args];
+    if (Object.hasOwnProperty.call(cache, args)) return cache[args];
+    cache[args] = cb(...args);
+    return cache[args];
+  };
 };
 
 /* eslint-enable no-unused-vars */
 
 /* ======================== Recursion Practice ============================ */
-const reverseStr = str => {
+const reverseStr = (str) => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
 };
 
-const checkMatchingLeaves = obj => {
+const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  const arr = Object.values(obj);
+  let chk = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] !== 'object') {
+      chk = arr[i];
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === 'object') {
+      arr[i].newprop = chk;
+      if (!checkMatchingLeaves(arr[i])) return false;
+    } else if (chk !== arr[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
-const flatten = elements => {
+const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 };
