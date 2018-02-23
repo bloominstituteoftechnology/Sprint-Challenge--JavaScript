1705a18e85244b25e146336e4bcac8c9811eb3ce
Khepf <jeremy1975@gmail.com>
2018-02-23 18:58:27 -0500

    did not finish closure or recursion

--- src/challenges.js
+++ src/challenges.js
@@ -18,15 +18,27 @@ const map = (elements, cb) => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let callCount = 0;
+  return (...args) => {
+    if (callCount === n) {
+      return null;
+    }
+    callCount++;
+    return cb(...args);
+  };
 };
 
 const cacheFunction = (cb) => {
-  // Should return a funciton that invokes `cb`.
+  // Should return a function that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  return (...args) => {
+    let cache = {};
+    return cb(...args);
+  };
 };
 
 /* eslint-enable no-unused-vars */
@@ -35,6 +47,13 @@ const cacheFunction = (cb) => {
 const reverseStr = (str) => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+    const recReverse = recStr => {
+      if (recStr.length === str.length) {
+        return recStr;
+        recStr = str.substring(str.length -1, str.length);
+        recReverse(--recStr);
+      }
+    }
 };
 
 const checkMatchingLeaves = (obj) => {
