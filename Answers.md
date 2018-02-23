aff9bf7ac2bfd236668d4e175418592ac473033c
mgchmiel <michaelchmielniak@yahoo.com>
2018-02-23 17:59:39 -0500

    Michal Chmielniak

--- Answers.md
+++ Answers.md
@@ -1,2 +1,22 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 ### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+1. Some of the differences between forEach and map include the fact while the forEach does not return anything while returns values and a new array. Which means forEach alters the inital array while map leaves it alone performs the function and creates the new array.
+2. Five different types in javascript are:
+boolean,
+null,
+number,
+string,
+symbol.
+Arrays are special since they store multiple values in a single variable.
+3. Closure is a function inside another function which has the ability to utilize the variables of the outter function. 
+function school (major,year) {
+    let intro = "I'm ";
+    function introduction() {
+        return intro + major + " " + year;
+    }
+    return introduction();
+}
+4. a. Whenever a fumction is containted globally the value will be withing hte function as the window object.
+    b.When a function is called with a preceding dot, the object before the dot is this.
+    c. When constructors are used this is refered to when the constructor was created and will be returned by the constructor. 
+    d. When call or apply are utilized this is explicitly defined. 
--- src/challenges.js
+++ src/challenges.js
@@ -2,44 +2,103 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    (cb(elements[i], i));
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const arr = [];
+  for (let i = 0; i < elements.length; i++) {
+    arr.push(cb(elements[i]));
+  }
+  return arr;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  function Limit(...args) {
+    if (count < n) {
+      count++;
+      return cb(...args);
+    }
+    return null;
+  }
+  return Limit;
 };
 
-const cacheFunction = cb => {
+const cacheFunction = (cb) => {
   // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const obj = {};
+  return (cache) => {
+    if (Object.hasOwnProperty.call(obj, cache)) {
+      return obj[cache];
+    }
+    obj[cache] = cb(cache);
+    return obj[cache];
+  };
 };
 
 /* eslint-enable no-unused-vars */
 
 /* ======================== Recursion Practice ============================ */
-const reverseStr = str => {
+const reverseStr = (str) => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  if (str === '') {
+    return '';
+  }
+  const reverse = reverseStr(str.substr(1)) + str.charAt(0);
+  return reverse;
 };
 
-const checkMatchingLeaves = obj => {
+const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  let result = true;
+  const leaves = [];
+  const recur = (objGiven) => {
+    Object.keys(objGiven).forEach((key) => {
+      if (typeof objGiven[key] === 'number' || typeof objGiven[key] === 'string') {
+        leaves.push(objGiven[key]);
+      }
+      if (typeof objGiven[key] === 'object') {
+        recur(objGiven[key]);
+      }
+    });
+  };
+  recur(obj);
+  leaves.reduce((memo, i) => {
+    if (memo !== i) {
+      result = false;
+    }
+    return i;
+  });
+  return result;
 };
 
-const flatten = elements => {
+const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  let arr = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (Array.isArray(elements[i])) {
+      arr = arr.concat(flatten(elements[i]));
+    } else {
+      arr.push(elements[i]);
+    }
+  }
+  return arr;
 };
 
 module.exports = {
