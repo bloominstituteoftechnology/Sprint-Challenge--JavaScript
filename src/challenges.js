d98eba8be059f80cc5450d0ba296083e95c64f24
Juber Ahmed <a.juber@hotmail.com>
2018-02-23 16:54:26 -0500

    Completed callback function: limitFunctionCallCount

--- src/challenges.js
+++ src/challenges.js
@@ -21,6 +21,14 @@ const map = (elements, cb) => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let callCount = 0;
+  return (...args) => {
+    if (callCount === n) {
+      return null;
+    }
+    callCount++;
+    return cb(...args);
+  };
 };
 
 const cacheFunction = (cb) => {
