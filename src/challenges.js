b56c2d5cab38a016c454fc613f838f9774e1df2f
hrehman000@citymail.cuny.edu <hrehman000@citymail.cuny.edu>
2018-02-23 18:58:54 -0500

    Habib-ur-Rehman SprintChallenge-JavaScript

--- src/challenges.js
+++ src/challenges.js
@@ -36,10 +36,10 @@ const cacheFunction = (cb) => {
   // `cb` should only ever be invoked once for a given set of arguments.
   const cache = {};
   return (obj) => {
-    if (Object.prototype.hasOwnProperty.call(cache,obj)) return cache[obj];
+    if (Object.prototype.hasOwnProperty.call(cache, obj)) return cache[obj];
     cache[obj] = cb(obj);
     return cache[obj];
-  }
+  };
 };
 
 /* eslint-enable no-unused-vars */
@@ -48,16 +48,39 @@ const cacheFunction = (cb) => {
 const reverseStr = (str) => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  if (str === '') return '';
+  return reverseStr(str.substr(1)) + str[0];
 };
 
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  const property = [];
+  function checkLeaf(object) {
+    Object.keys(object).forEach((key) => {
+      if (typeof object[key] === 'object') return checkLeaf(object[key]);
+      property.push(object[key]);
+    });
+  }
+  checkLeaf(obj);
+  return property.every(leaf => property[0] === leaf);
 };
 
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  const newArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (Array.isArray(elements[i])) flatten(elements[i]).forEach(element => newArray.push(element));
+    else newArray.push(elements[i]);
+  }
+  return newArray;
+  // if (Array.isArray(elements)) return [elements];
+  // const array = [];
+  // for (let i = 0; i < elements.length; i++) {
+  //   array = array.concat(flatteqn(elements[i]));
+  // }
+  // return array;
 };
 
 module.exports = {
