49682910e209822b29c00dcec4bb99b0ce079f62
unknown <adfaris@gmail.com>
2018-02-23 17:01:35 -0500

    Attempted few Questions

--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,27 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+ for(let i = 0; i < elements.length; i++) {
+   cb(elements[i]);
+ }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const arr = elements.map(cb);
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let callCount = 0;
+  return (...args) => {
+    if (callCount === n) return null;
+    callCount++;
+    return cb(...args);
+  };
 };
 
 const cacheFunction = cb => {
@@ -22,6 +32,14 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  let memo = {};
+  return (args) => {
+    if (memo === args) {
+      return memo;
+    }
+    memo = args;
+    return cb(args);
+  };
 };
 
 /* eslint-enable no-unused-vars */
