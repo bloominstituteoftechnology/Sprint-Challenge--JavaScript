70406e77bec49338690d9a5c56b2d8bd3977d185
Joshua Goode <jgoode.business@gmail.com>
2018-02-23 17:11:34 -0500

    Completed limitFunctionCallCount within Closure Practice

--- src/challenges.js
+++ src/challenges.js
@@ -21,6 +21,14 @@ const map = (elements, cb) => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let limitCount = 0;
+  return (...args) => {
+    if (limitCount < n) {
+      ++limitCount;
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 const cacheFunction = (cb) => {
