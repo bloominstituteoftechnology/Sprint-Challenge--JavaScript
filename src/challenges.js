531224e54198b9e4c9e2b284f973e65a5e729175
Frank Faustino <nawzyah@gmail.com>
2018-02-23 16:04:41 -0800

    ✨ Completed — Frank Faustino ✨

--- src/challenges.js
+++ src/challenges.js
@@ -1,46 +1,63 @@
 /* ======================== CallBacks Practice ============================ */
-const each = (elements, cb) => {
-  // Iterates over a list of elements, yielding each in turn to the `cb` function.
-  // This only needs to work with arrays.
+const each = (arr, cb) => {
+  for (let i = 0; i < arr.length; i++) {
+    cb(arr[i], i);
+  }
 };
 
-const map = (elements, cb) => {
-  // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
-  // Return the new array.
+const map = (arr, cb) => {
+  const output = [];
+  each(arr, item => output.push(cb(item)));
+  return output;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
-  // Should return a function that invokes `cb`.
-  // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    if (count === n) return null;
+    count++;
+    return cb(...args);
+  };
 };
 
-const cacheFunction = cb => {
-  // Should return a funciton that invokes `cb`.
-  // A cache (object) should be kept in closure scope.
-  // The cache should keep track of all arguments have been used to invoke this function.
-  // If the returned function is invoked with arguments that it has already seen
-  // then it should return the cached result and not invoke `cb` again.
-  // `cb` should only ever be invoked once for a given set of arguments.
+const cacheFunction = (cb) => {
+  const cache = {};
+  return (arg) => {
+    if (arg in cache) return cache[arg];
+    return cache[arg] = cb(arg);
+  };
 };
 
 /* eslint-enable no-unused-vars */
 
 /* ======================== Recursion Practice ============================ */
-const reverseStr = str => {
-  // reverse str takes in a string and returns that string in reversed order
-  // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+const reverseStr = (str) => {
+  if (str.length === 0) return '';
+  return reverseStr(str.substr(1, str.length)) + str[0];
 };
 
-const checkMatchingLeaves = obj => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
+const checkMatchingLeaves = (obj) => {
+  let value;
+  let flag = true;
+
+  const checkLeaves = (object) => {
+    Object.keys(object).forEach((key) => {
+      if (value === undefined && typeof object[key] !== 'object') return value = object[key];
+      if (typeof object[key] === 'object') return checkLeaves(object[key]);
+      if (object[key] !== value) return flag = false;
+      return;
+    });
+  };
+  checkLeaves(obj);
+  return flag;
 };
 
-const flatten = elements => {
-  // Flattens a nested array (the nesting can be to any depth).
-  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
-};
+
+const flatten = arr => arr.reduce((a, b) => {
+  if (Array.isArray(b)) return a.concat(flatten(b));
+  return a.concat(b);
+}, []);
 
 module.exports = {
   each,
