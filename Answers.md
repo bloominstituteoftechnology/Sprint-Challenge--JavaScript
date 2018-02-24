87bccb93b846cc75a791c753fb3c9785e3584a3a
nourmhassan <nour-pop1@hotmail.com>
2018-02-23 16:02:41 -0800

    nour hassan sprint-challenge

--- Answers.md
+++ Answers.md
@@ -1,2 +1,11 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 ### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+1. they both go throw  each array elemnts..  .map return new array  .forEach doesn't retun array ( undefind)
+2.Boolean -number - string - object
+3.is a somthing always looking in the function name in whole code!
+const myCat = 'Lucy';
+function exposeLucy (){
+	console.log(myCat);___> //Lucy
+}
+exposeLucy();
+4.when we have long function THIS help to be window obj -
--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,31 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for ( let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const newArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArray[i] = cb(elements[i]);
+  }
+  return newArray;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+ let callCount = 0;
+  return () => {
+   if (callCount === n){
+    return null;
+  }
+   }
 };
 
 const cacheFunction = cb => {
