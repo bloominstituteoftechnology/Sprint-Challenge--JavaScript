b09416d3d09e7e542a3154e7a45d0734e44d2339
Abraham Andujo <foxtara00@gmail.com>
2018-02-25 15:50:55 -0800

    wip abe accidently forgot to submit changes

--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,33 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const newArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArray[i] = cb(elements[i]);
+  }
+  return newArray;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let limit = 0;
+  return (...args) => {
+    if (limit <= n) {
+      limit++;
+      return cb(...args);
+    }
+    return null;
+  };
 };
 
 const cacheFunction = cb => {
@@ -22,14 +38,26 @@ const cacheFunction = cb => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (...args) => {
+    const keys = Object.keys(cache);
+    const aString = args.toString();
+    if (keys.indexOf(aString) !== -1) {
+      return cache[aString];
+    }
+    const result = cb(...args);
+    cache[aString] = result;
+    return result;
+  };
 };
 
 /* eslint-enable no-unused-vars */
 
 /* ======================== Recursion Practice ============================ */
 const reverseStr = str => {
-  // reverse str takes in a string and returns that string in reversed order
+  // reverse str takes in a aString and returns that aString in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+
 };
 
 const checkMatchingLeaves = obj => {
