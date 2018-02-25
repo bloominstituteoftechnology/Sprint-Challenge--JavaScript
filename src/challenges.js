ea1e03cbd073ea2c86d4615cc7edebd9cfc7b1a1
Jonathan Reeves <jonathanedwardreeves1988@gmail.com>
2018-02-24 19:55:14 -0600

    WIP Sprint Challenge Jonathan Reeves

--- src/challenges.js
+++ src/challenges.js
@@ -2,17 +2,31 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
   // Return the new array.
+  const mappedArray = [];
+  each(elements, item => (mappedArr.push(cb(item))));
+  return mappedArray;
 };
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    if (count === n) {
+      return null;
+    }
+    count++;
+    return cb(...args);
+  }
 };
 
 const cacheFunction = (cb) => {
@@ -22,6 +36,14 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = [];
+  return (...args) => {
+    if (cache.filter(argList => JSON.stringify(argList ===JSON.stringify(args)).length === 0) {
+      cache.push(args);
+      return cb(args);
+    }
+    return null;
+  };
 };
 
 /* eslint-enable no-unused-vars */
@@ -40,6 +62,15 @@ const checkMatchingLeaves = (obj) => {
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  const newArray = [];
+  for (let i = 0; i < elements.lenght, i++) {
+    if (Array.isArray(elements[i])){
+      flatten(elements[i]).forEach(el => newArray.push(el));
+    } else {
+      newArray.push(elements[i]);
+    }
+  }
+  return newArray;
 };
 
 module.exports = {
