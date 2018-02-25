0873e6f9cf9ed0109e51c31f5b257abd15443346
Robert Hogan <rhogan29@outlook.com>
2018-02-25 00:40:11 -0500

    Robert Hogan. WIP. Questions answered, and a couple of problems completed. Not a very good unserstanding of this subject just yet.

--- src/challenges.js
+++ src/challenges.js
@@ -36,13 +36,6 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
-  const cache = {};
-  return (input) => {
-    if (input in cache) return cache[input];
-    cache[input] = cb(input);
-    return cache[input];
-  };
-};
 
 /* eslint-enable no-unused-vars */
 
