0554ade19914a3ad93714d79b74b7dead722184e
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-02-26 11:17:10 -0500

    working on Sprint, 11 of 19 passed after fixing eslint errors

--- src/challenges.js
+++ src/challenges.js
@@ -13,11 +13,11 @@ const map = (elements, cb) => {
 
 /* ======================== Closure Practice ============================ */
 const limitFunctionCallCount = (cb, n) => {
-let count = 0;
-return (...args) => {
-  if (count === n) {
-    return null;
-  }
+  let count = 0;
+  return (...args) => {
+    if ((count) === n) {
+      return null;
+    }
     count++;
     return cb(...args);
   };
@@ -25,7 +25,7 @@ return (...args) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
 
-const cacheFunction = cb => {
+const cacheFunction = (cb) => {
   // Should return a funciton that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
@@ -42,12 +42,12 @@ const reverseStr = (str) => {
   return reverseStr(str.slice(1)) + str[0];
 };
 
-const checkMatchingLeaves = obj => {
+const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
 };
 
-const flatten = elements => {
+const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
 };
