f106e8caf570b06108e028cd141eb099cc840251
astro11 <aquila.strother@asu.edu>
2018-02-26 05:34:19 -0500

    completed answers.md & challenges.js

--- Answers.md
+++ Answers.md
@@ -1,2 +1,32 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 ### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+
+## 1. Describe some of the differences between .forEach & .map.
+#### - The forEach method iterates over the items in an array and applies ot each, one by one. Also, forEach accomplishes a similiar execution as a for loop. The .map method actually transforms each item in an array, then returns a new array of the items with the same size.
+
+
+## 2. Name five different Types in JavaScript. A Type is something that can represent data. What is so special about Arrays?
+#### - Boolean, Null, Undefined, Number, and String. (Although, you cannot forget the Object)
+#### - An array is a special variable that can hold more than one value at a time. You can think of it as a list of items/elements. Their elements are stored as object properties, using the array index as the property name.
+
+
+## 3. What is closure? Can you code out a quick example of a closure?
+#### - Closure is created whenever you declare a function within another function. The outer function's scope becomes accessible within the inner function.
+
+```
+function init() {
+  var name = 'Mozilla'; // name is a local variable created by init
+  function displayName() { // displayName() is the inner function, a closure
+    alert(name); // use variable declared in the parent function    
+  }
+  displayName();    
+}
+init();
+```
+
+
+## 4. Describe the four rules of the 'this' keyword. No need to provide examples about it this time :)
+#### - Whenever a function is contained in the global scope, the value of 'this' inside of that function will be the window object.
+#### - Whenever a function is called by a preceding dot, the object before that dot is 'this'.
+#### - Whenever a constructor function is used, 'this' refers to the specific instance of the object that is created and returned by the constructor function.
+#### - Whenever JS's call or apply method is used, 'this' is explicitly defined.
--- src/challenges.js
+++ src/challenges.js
@@ -3,7 +3,7 @@ const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   for (let i = 0; i < elements.length; i++) {
-    cb(elements[i]);
+    cb(elements[i], i);
   }
 };
 
@@ -32,12 +32,22 @@ const limitFunctionCallCount = (cb, n) => {
 };
 
 const cacheFunction = (cb) => {
-  // Should return a funciton that invokes `cb`.
+  // Should return a function that invokes `cb`.
   // A cache (object) should be kept in closure scope.
   // The cache should keep track of all arguments have been used to invoke this function.
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  return (...args) => {
+    const key = args;
+    if (!(key in cache)) {
+      const newVal = cb(...args);
+      cache[key] = newVal;
+      return newVal;
+    }
+    return cache.key;
+  };
 };
 
 /* eslint-enable no-unused-vars */
@@ -46,16 +56,45 @@ const cacheFunction = (cb) => {
 const reverseStr = (str) => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  return str.split('').reverse().join('');
+};
+const newString = {
+  reverse: reverseStr('Challenge completed, will do better next time')
 };
 
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  let val;
+  let results = true;
+  const check = (tree) => {
+    Object.keys(tree).forEach((key) => {
+      if (val === undefined && typeof key !== 'object') {
+        val = tree[key];
+        return undefined;
+      }
+      if (typeof tree[key] === 'object') return check(tree[key]);
+      if (tree[key] !== val) {
+        results = false;
+      }
+    });
+  };
+  check(obj);
+  return results;
 };
 
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  let newArr = [];
+  for (let i = 0; i < elements.length; i++) {
+    if (Array.isArray(elements[i])) {
+      newArr = newArr.concat(flatten(elements[i]));
+    } else {
+      newArr.push(elements[i]);
+    }
+  }
+  return newArr;
 };
 
 module.exports = {
