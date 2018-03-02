f0c72fb8cafb9c2899dbb1eb377201602954c3be
unknown <anderson56ak@gmail.com>
2018-03-02 14:22:15 -0600

    A.AndersonSprintChall

--- src/challenges.js
+++ src/challenges.js
@@ -21,6 +21,14 @@ const map = (elements, cb) => {
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let limitCount = 0;
+  return () => {
+    if (limitCount === n) {
+      return null;
+    }
+    limitCount++;
+    return cb();
+  };
 };
 
 const cacheFunction = (cb) => {
@@ -30,6 +38,15 @@ const cacheFunction = (cb) => {
   // If the returned function is invoked with arguments that it has already seen
   // then it should return the cached result and not invoke `cb` again.
   // `cb` should only ever be invoked once for a given set of arguments.
+  const cache = {};
+  const i = (...args) => {
+    if (cb in cache) {
+      return cache[cb];
+    }
+    cache[cb] = cb(...args);
+    return cb(...args);
+  };
+  return i;
 };
 
 /* eslint-enable no-unused-vars */
@@ -38,16 +55,43 @@ const cacheFunction = (cb) => {
 const reverseStr = (str) => {
   // reverse str takes in a string and returns that string in reversed order
   // The only difference between the way you've solved this before and now is that you need to do it recursivley!
+  const reverse = '';
+  if (str !== '') {
+    return Array.prototype.reverse(str.substr(1)) + str.charAt();
+  }
+  return reverse;
 };
 
 const checkMatchingLeaves = (obj) => {
   // return true if every property on `obj` is the same
   // otherwise return false
+  const arr = [];
+  const recurse = (someObj) => {
+    const keys = Object.keys(someObj);
+    keys.forEach((item) => {
+      if (typeof someObj[item] === 'object') {
+        recurse(someObj[item]);
+      } else {
+        arr.push(someObj[item]);
+      }
+    });
+  };
+  recurse(obj);
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[0] !== arr[i]) {
+      return false;
+    }
+  }
+  return true;
 };
 
 const flatten = (elements) => {
   // Flattens a nested array (the nesting can be to any depth).
   // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
+  while (elements.find(value => Array.isArray(value))) {
+    elements = Array.prototype.concat(...elements);
+  }
+  return elements;
 };
 
 module.exports = {
