fc3931c8a5c510ffb46599dde3d9bf16cbe9d24c
Jared Cuffe <jcuffe@gmail.com>
2018-03-23 13:09:54 -0700

    Clean starting file for future students

--- src/challenges.js
+++ src/challenges.js
@@ -31,21 +31,16 @@ const limitFunctionCallCount = (cb, n) => {
 
 // Create a CuboidMaker constructor function that accepts properties for length, width, and height
 const CuboidMaker = function CuboidMaker(options) {
-  this.height = options.height;
-  this.width = options.width;
-  this.length = options.length;
 };
 
 // Create a seperate function property of CuboidMaker that returns the volume of a given cuboid's length, width, and height
 // Formula for cuboid volume: length * width * height
 CuboidMaker.prototype.volume = function volume() {
-  return this.height * this.length * this.width;
 };
 
 // Create a seperate function property of CuboidMaker that returns the surface area of a given cuboid's length, width, and height. 
 // Formula for cuboid surface area of a cube: 2(length * width + length * height + width * height)
 CuboidMaker.prototype.surfaceArea = function surfaceArea() {
-  return 2 * (this.length * this.height + this.width * this.length + this.width * this.height);
 };
 
 /* ======================== Class Practice ============================ */
@@ -54,28 +49,19 @@ CuboidMaker.prototype.surfaceArea = function surfaceArea() {
 class Cuboid {
   constructor(options) {
     // Implement this constructor
-    this.height = options.height;
-    this.length = options.length;
-    this.width = options.width;
   }
   volume() {
     // Implement this function
-    return this.height * this.length * this.width;
   }
   surfaceArea() {
     // Implement this function
-    return 2 * (this.length * this.height + this.width * this.length + this.width * this.height);
   }
 }
 // Task 2: Create a new class called Cube. Extend the Cube class with the CuboidMaker class.
 // Create two new methods on the Cube class to calculate the volume and surface area of a cube given the same values passed in from CuboidMaker.
-class Cube extends Cuboid{ // Be sure to use `extends` to extend the Cuboid class
+class Cube { // Be sure to use `extends` to extend the Cuboid class
   constructor(options) {
     // Use the parent constructor
-    super(options);
-  }
-  isCube() {
-    return 'We have a cube!';
   }
 }
 
@@ -85,11 +71,10 @@ class Cube extends Cuboid{ // Be sure to use `extends` to extend the Cuboid clas
 // Challenge 1: Go back to your prototype CuboidMaker and extend Cube using psuedo-classical inheritance to achiveve the same results you built using the ES6 class syntax
 const CubeMaker = function CubeMaker(options) {
   // Call the parent constructor
-  CuboidMaker.call(this, options);
 };
 
 // Set up inheritance by linking the prototype to its parent.
-CubeMaker.prototype = Object.create(CuboidMaker.prototype);
+// CubeMaker.prototype = null;
 
 // Challenge 2: Go back to your class Cube and add the following property: isCube.
 // Create a method inside of Cube that checks for isCube and if it's true, returns a string 'We have a cube!';
@@ -99,7 +84,6 @@ const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
 };
-
 
 module.exports = {
   each,
