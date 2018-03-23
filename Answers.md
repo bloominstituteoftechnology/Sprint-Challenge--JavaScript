b8e44c8e5084dd6979ee72b1c42a0cba2760ae97
jaivonmassena <jaivonmassena@gmail.com>
2018-03-23 13:54:44 -0400

    answers and commit completed

--- Answers.md
+++ Answers.md
@@ -1,2 +1,11 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 ### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+
+1. `.forEach` executes a function on every element in an array and `map` creates a new array on using a function.  
+2. `Strings`, `Booleans`, `Number`, `Object`, `Null`
+3. A function that has access to an outer variable.
+  let i = 0
+  function cl(){
+    return i + 1;
+  }
+4. `implicit binding` (`this` left of the dot), `window binding`(`this` isn't declared so it refers to window object), expliciting binding (this is binded using call, apply and bind), `new binding`(uses new key to bind object to this).
--- src/challenges.js
+++ src/challenges.js
@@ -5,11 +5,23 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for(let i = 0; i < elements.length; i++){
+     cb(elements[i], i);
+   }
 };
 
 const map = (elements, cb) => {
-  // Produces a new array of values by mapping each value in list through a transformation function.
-  // Return the new array.
+    // Produces a new array of values by mapping each value in list through a transformation function.
+    // Return the new array.
+  // const newArray = [];
+  // for(let i = 0; elements.length; i++){
+  //   newArray.push(cb(elements[i]));
+  // }
+  const newArray = [];
+  for (let i = 0; i < elements.length; i++) {
+   newArray.push(cb(elements[i]));
+  }
+  return newArray
 };
 
 /* ======================== Closure Practice ============================ */
@@ -19,58 +31,110 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let inc = 0;
+  const newCounter = () => {
+    return inc +=1;
+  };
+  return newCounter();
 };
 
+//
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  count = 0;
+  return (...args) => {
+    if (count === n) return null;
+    count++;
+    return cb(...args);
+  }
 };
 
 /* ======================== Prototype Practice ============================ */
 
 // ***Prototypes do NOT have test cases built for them.  You must use the console logs provided at the end of this section.***
 
-// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. 
+// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides.
 
 // Create a CuboidMaker constructor function that accepts properties for length, width, and height
 
 // Create a seperate function property of CuboidMaker that returns the volume of a given cuboid's length, width, and height
 // Formula for cuboid volume: length * width * height
 
-// Create a seperate function property of CuboidMaker that returns the surface area of a given cuboid's length, width, and height. 
+// Create a seperate function property of CuboidMaker that returns the surface area of a given cuboid's length, width, and height.
 // Formula for cuboid surface area of a cube: 2(length * width + length * height + width * height)
 
-// Create a cuboid object that inherits from CuboidMaker. 
+// Create a cuboid object that inherits from CuboidMaker.
 // The cuboid object must contain keys for length, width, and height.
 
-// To test your formulas, pass these key/value pairs into your constructor: length: 4, width: 5, and height: 5. When running your logs, you should get Volume: 100 with a Surface Area of 130. 
+// To test your formulas, pass these key/value pairs into your constructor: length: 4, width: 5, and height: 5. When running your logs, you should get Volume: 100 with a Surface Area of 130.
+function CuboidMaker(stuff){
+  this.length = stuff.length;
+  this.width = stuff.width;
+  this.height  = stuff.height;
+}
+
+CuboidMaker.prototype.volume = function() {
+  return this.length * this.width * this.height;
+}
+CuboidMaker.prototype.surfaceArea = function() {
+  return 2 *(this.length * this.width + this.length * this.height + this.width * this.height);
+}
+const cuboid = new CuboidMaker({length: 4, width: 5, height: 5});
 
 // Use these logs to test your results:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 /* ======================== Class Practice ============================ */
 
 // ***Class Practice does NOT have test cases built.  You must use the console logs provided at the end of this section.***
 
 // Task 1: Copy and paste your prototype CuboidMaker here and proceed to convert it into ES6 Class syntax
+class CuboidMaker2{
+  constructor(stuff){
+    this.length = stuff.length;
+    this.width = stuff.width;
+    this.height  = stuff.height;
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea() {
+    return 2 *(this.length * this.width + this.length * this.height + this.width * this.height);
+  }
+};
 
 // Task 2: Create a new class called Cube. Extend the Cube class with the CuboidMaker class.
 
+class Cube extends CuboidMaker2{
+  constructor(stuff){
+    super(stuff);
+  }
+  wat(){
+    return("these methods already exists");
+  }
+  surfaceArea(){
+    return 6 * (this.length * this.width);
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+}
 // Create two new methods on the Cube class to calculate the volume and surface area of a cube given the same values passed in from CuboidMaker.
 
 // The volume of a cube is: length * width * height
 // The surface area of a cube is: 6 * (length + width)
+const cube = new Cube({length: 2, width: 2, height: 2})
+// Create a new cube object that has equal values for length, width, and height
 
-// Create a new cube object that has equal values for length, width, and height 
-
-// To test your formulas, pass these key/value pairs into your constructor: length: 2, width: 2, and height: 2. You should get Volume: 8 with a Surface Area of 24. 
+// To test your formulas, pass these key/value pairs into your constructor: length: 2, width: 2, and height: 2. You should get Volume: 8 with a Surface Area of 24.
 
 // Use these logs to test your results:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
-// console.log(cube.volume()); // 8
-// console.log(cube.surfaceArea()); // 24
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+console.log(cube.volume()); // 8
+console.log(cube.surfaceArea()); // 24
 
 /* ======================== Stretch Challenges ============================ */
 
