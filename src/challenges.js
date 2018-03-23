ab3665bdff530ef86fe7b34d9ed515984839e58e
faulkenfett11 <36997363+faulkenfett11@users.noreply.github.com>
2018-03-23 10:49:27 -0700

    finished class

--- src/challenges.js
+++ src/challenges.js
@@ -68,32 +68,34 @@ const limitFunctionCallCount = (cb, n) => {
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 
-function CuboidMaker(dimension) {
-  this.length = dimension.length;
-  this.width = dimension.width;
-  this.height = dimension.height;
-}
+// PROTOTYPE ======================================================================================
 
-CuboidMaker.prototype.volume = function() {
-  return this.length * this.width * this.height;
-};
+// function CuboidMaker(dimension) {
+//   this.length = dimension.length;
+//   this.width = dimension.width;
+//   this.height = dimension.height;
+// }
 
-CuboidMaker.prototype.surfaceArea = function() {
-  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
-};
+// CuboidMaker.prototype.volume = function() {
+//   return this.length * this.width * this.height;
+// };
 
-function CuboidObject(cuboidDimensions) {
-  CuboidMaker.call(this, cuboidDimensions);
-}
+// CuboidMaker.prototype.surfaceArea = function() {
+//   return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+// };
 
-let cuboid = new CuboidMaker({
-  length: 4,
-  width: 5,
-  height: 5,
-});
+// function CuboidObject(cuboidDimensions) {
+//   CuboidMaker.call(this, cuboidDimensions);
+// }
 
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+// let cuboid = new CuboidMaker({
+//   length: 4,
+//   width: 5,
+//   height: 5,
+// });
+
+// console.log(cuboid.volume()); // 100
+// console.log(cuboid.surfaceArea()); // 130
 
 /* ======================== Class Practice ============================ */
 
@@ -117,6 +119,62 @@ console.log(cuboid.surfaceArea()); // 130
 // console.log(cuboid.surfaceArea()); // 130
 // console.log(cube.volume()); // 8
 // console.log(cube.surfaceArea()); // 24
+
+// CLASS =============================================================================================
+
+class CuboidMaker {
+  constructor(dimension) {
+  this.length = dimension.length;
+  this.width = dimension.width;
+  this.height = dimension.height;
+}
+
+volume() {
+  return this.length * this.width * this.height;
+}
+
+surfaceArea() {
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+}
+}
+
+class CuboidObject extends CuboidMaker {
+  constructor(cuboidDimensions) {
+    super(cuboidDimensions);
+  }
+}
+
+class Cube extends CuboidMaker {
+  constructor(cubeDimensions) {
+    super(cubeDimensions)
+  }
+  
+  volume() {
+    return this.length * this.width * this.height;
+  }
+  
+  surfaceArea() {
+    return 2 * (this.length * this.width);
+  }
+}
+
+
+let cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5,
+});
+
+let cube = new CuboidMaker({
+  length: 2,
+  width: 2,
+  height: 2,
+});
+
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+console.log(cube.volume()); // 8
+console.log(cube.surfaceArea()); // 24
 
 /* ======================== Stretch Challenges ============================ */
 
