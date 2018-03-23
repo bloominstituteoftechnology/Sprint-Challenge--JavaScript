026a723235366d3ac2eae82a9dc69b79f4682efb
Devon Bacon <devon.bacon@gmail.com>
2018-03-23 09:32:06 -0700

    Completed stretch goal part 3

--- src/challenges.js
+++ src/challenges.js
@@ -179,9 +179,24 @@ const cube = new Cube(2)
 // console.log(cube.checkIfCube());  // "We have a cube!"
 
 // Challenge 3: Recursion
-const checkMatchingLeaves = obj => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
+const checkMatchingLeaves = (obj, arr) => { // Pass in array so as not to reset it with recursion
+  // Set up new array
+  arr = arr || [];
+  // Store reference to object values array
+  const objVals = Object.values(obj);
+
+  // Loop through items in values array
+  for (let i = 0; i < objVals.length; i++) {
+    if (typeof objVals[i] === 'object') { // If item is an object then call recursion
+      checkMatchingLeaves(objVals[i], arr);
+    } else { // Else store value in new array
+      arr.push(objVals[i]);
+    }
+  }
+  // Convert arr to set to elimintate duplicates
+  // Convert back to array and check length
+  // If length is 1 then all values were identical
+  return Array.from(new Set(arr)).length === 1;
 };
 
 
