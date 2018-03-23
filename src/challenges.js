ca33dbef620e805aa347e4dc2080aa67f15baa94
M. Margo Nikolaisen <mm.nikolaisen@gmail.com>
2018-03-23 14:58:56 -0600

    JavaScript Sprint Challenge Best Effort

--- src/challenges.js
+++ src/challenges.js
@@ -54,88 +54,115 @@ const limitFunctionCallCount = (cb, n) => {
 // The cuboid object must contain keys for length, width, and height.
 
 // To test your formulas, pass these key/value pairs into your constructor: length: 4, width: 5, and height: 5. When running your logs, you should get Volume: 100 with a Surface Area of 130. 
-  function CuboidMaker(values) {
-    this.length = values.length;
-    this.width = values.width;
-    this.height = values.height; 
-  }
-  
-  CuboidMaker.prototype.volume = function() {
-    return `${this.length} * ${this.width} * ${this.height}`
-  }
-
-  CuboidMaker.prototype.surfaceArea = function() {
-    return 2 * `${this.length} * `
-  }
-
-// Use these logs to test your results:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
-
-/* ======================== Class Practice ============================ */
-
-// ***Class Practice does NOT have test cases built.  You must use the console logs provided at the end of this section.***
-
-// Task 1: Copy and paste your prototype CuboidMaker here and proceed to convert it into ES6 Class syntax
-
-// Task 2: Create a new class called Cube. Extend the Cube class with the CuboidMaker class.
-
-// Create two new methods on the Cube class to calculate the volume and surface area of a cube given the same values passed in from CuboidMaker.
-
-// The volume of a cube is: length * width * height
-// The surface area of a cube is: 6 * (length + width)
-
-// Create a new cube object that has equal values for length, width, and height 
-
-// To test your formulas, pass these key/value pairs into your constructor: length: 2, width: 2, and height: 2. You should get Volume: 8 with a Surface Area of 24. 
-
-
-class CuboidMaker (length, Width, Height) {
-  this.length = length;
-  this.width = width;
-  this.height = height; 
+function CuboidMaker(values) {
+  this.length = values.length;
+  this.width = values.width;
+  this.height = values.height;
 }
 
-class Cube extends CuboidMaker {
-constructor(cubeValues)
+CuboidMaker.prototype.volume = function () {
+  return `Volume: ${this.length * this.width * this.height}`;
 }
-// Use these logs to test your results:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
-// console.log(cube.volume()); // 8
-// console.log(cube.surfaceArea()); // 24
 
-/* ======================== Stretch Challenges ============================ */
+CuboidMaker.prototype.surfaceArea = function () {
+  return `SurfaceArea: ${2 * (this.length * this.width + this.length * this.height + this.width * this.height)}`;
 
+  const cuboid = new CuboidMaker({
+    length: 4,
+    width: 5,
+    height: 5
+  });
 
-// Challenge 1: Go back to your prototype CuboidMaker and extend Cube using psuedo-classical inheritance to achiveve the same results you built using the ES6 class syntax
+  // Use these logs to test your results:
+  console.log(cuboid.volume()); // 100
+  console.log(cuboid.surfaceArea()); // 130
 
-// Use these logs to test your results:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
-// console.log(cube.volume()); // 8
-// console.log(cube.surfaceArea()); // 24
+  /* ======================== Class Practice ============================ */
 
-// Challenge 2: Go back to your class Cube and add the following property: isCube.
-// Create a method inside of Cube that checks for isCube and if it's true, returns a string 'We have a cube!';
+  // ***Class Practice does NOT have test cases built.  You must use the console logs provided at the end of this section.***
 
-// Use these logs to test your results:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
-// console.log(cube.volume()); // 8
-// console.log(cube.surfaceArea()); // 24
-// console.log(cube.checkIfCube());  // "We have a cube!"
+  // Task 1: Copy and paste your prototype CuboidMaker here and proceed to convert it into ES6 Class syntax
 
-// Challenge 3: Recursion
-const checkMatchingLeaves = obj => {
-  // return true if every property on `obj` is the same
-  // otherwise return false
-};
+  // Task 2: Create a new class called Cube. Extend the Cube class with the CuboidMaker class.
+
+  // Create two new methods on the Cube class to calculate the volume and surface area of a cube given the same values passed in from CuboidMaker.
+
+  // The volume of a cube is: length * width * height
+  // The surface area of a cube is: 6 * (length + width)
+
+  // Create a new cube object that has equal values for length, width, and height 
+
+  // To test your formulas, pass these key/value pairs into your constructor: length: 2, width: 2, and height: 2. You should get Volume: 8 with a Surface Area of 24. 
 
 
-module.exports = {
-  each,
-  map,
-  limitFunctionCallCount,
-  checkMatchingLeaves,
-};
+  class CuboidMaker {
+    constructor(values) {
+      this.length = values.length;
+      this.width = values.width;
+      this.height = values.height;
+    }
+
+    volume() {
+      return `Volume: ${this.length * this.width * this.height}`;
+    }
+    surfaceArea() {
+      return `SurfaceArea: ${2 * (this.length * this.width + this.length * this.height + this.width * this.height)}`;
+    }
+
+  }
+
+  class Cube extends CuboidMaker {
+    constructor(cubeValues) {
+      super(CuboidValues);
+      this.isCube = cubeValues.isCube;
+    }
+    checkIfCube() {
+      if (this.isCube) {
+        return `We have a cube!`;
+      }
+
+      return surfaceArea;
+    }
+
+  }
+  // Use these logs to test your results:
+  console.log(cuboid.volume()); // 100
+  console.log(cuboid.surfaceArea()); // 130
+  console.log(cube.volume()); // 8
+  console.log(cube.surfaceArea()); // 24
+
+  /* ======================== Stretch Challenges ============================ */
+
+
+  // Challenge 1: Go back to your prototype CuboidMaker and extend Cube using psuedo-classical inheritance to achiveve the same results you built using the ES6 class syntax
+
+  // Use these logs to test your results:
+  // console.log(cuboid.volume()); // 100
+  // console.log(cuboid.surfaceArea()); // 130
+  // console.log(cube.volume()); // 8
+  // console.log(cube.surfaceArea()); // 24
+
+  // Challenge 2: Go back to your class Cube and add the following property: isCube.
+  // Create a method inside of Cube that checks for isCube and if it's true, returns a string 'We have a cube!';
+
+  // Use these logs to test your results:
+  // console.log(cuboid.volume()); // 100
+  // console.log(cuboid.surfaceArea()); // 130
+  // console.log(cube.volume()); // 8
+  // console.log(cube.surfaceArea()); // 24
+  // console.log(cube.checkIfCube());  // "We have a cube!"
+
+  // Challenge 3: Recursion
+  const checkMatchingLeaves = obj => {
+    // return true if every property on `obj` is the same
+    // otherwise return false
+  };
+
+
+  module.exports = {
+    each,
+    map,
+    limitFunctionCallCount,
+    checkMatchingLeaves,
+  }
+}
