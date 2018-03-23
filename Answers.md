b267b563ee49029fe1f06c8df8833d6ece5e1a0d
Henry K <mykim1891@hotmail.com>
2018-03-23 12:19:38 -0700

    Completed all but the very last one (Challenge 3)

--- Answers.md
+++ Answers.md
@@ -1,2 +1,20 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 ### For help with markdown syntax [Go here](https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet)
+
+1. forEach executes a provided function once for each element of the array, while map function creates a new, separate array with the results of calling a provided function on every elements in the calling array.
+
+2. Boolean, numbers, strings, objects, and arrays. Arrays are special because the data elements are indexed, meaning they can be referred to a specific number.
+
+3. Closure is a way to make a function-related codes to be separated (private) from the global window (default environment) of your overall code.
+// global
+let x = 1;
+    // closed
+    function multiplyBy2 {
+        return x *= 2
+    }
+
+4. The 4 rules of 'this' keywords are: 
+    i) Window/Global Object Binding, which in global scope, 'this' will call the value of window/console Object,
+    ii) Implicit Binding, meaning whenever a function is called by a preceding dot, the object before that dot is the one that becomes 'this',
+    iii) New Binding, meaning whenever a constructor function is used, 'this' will refer to the specific object created and returned by the constructor function.
+    iv) Explicit Binding, Whenever whenever call, apply, or bind is used, 'this' is explicitly defined.
--- src/challenges.js
+++ src/challenges.js
@@ -5,11 +5,19 @@
 const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
 };
 
 const map = (elements, cb) => {
   // Produces a new array of values by mapping each value in list through a transformation function.
   // Return the new array.
+  const newArray = [];
+  for (let i = 0; i < elements.length; i++) {
+    newArray.push(cb(elements[i]));
+  }
+  return newArray;
 };
 
 /* ======================== Closure Practice ============================ */
@@ -19,11 +27,24 @@ const counter = () => {
   // Example: const newCounter = counter();
   // newCounter(); // 1
   // newCounter(); // 2
+  let count = 0;
+  return () => {
+    count++;
+    return count;
+  }
 };
 
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
+  let count = 0;
+  return (...args) => {
+    if (count < n) {
+      count++;
+      return cb(...args);
+    }
+    return null;
+  }
 };
 
 /* ======================== Prototype Practice ============================ */
@@ -46,6 +67,24 @@ const limitFunctionCallCount = (cb, n) => {
 // To test your formulas, pass these key/value pairs into your constructor: length: 4, width: 5, and height: 5. When running your logs, you should get Volume: 100 with a Surface Area of 130. 
 
 // Use these logs to test your results:
+
+// function CuboidMaker(options) {
+//   this.length = options.length;
+//   this.width = options.width;
+//   this.height = options.height;
+// }
+// CuboidMaker.prototype.volume = function() {
+//   return this.length * this.width * this.height;
+// };
+// CuboidMaker.prototype.surfaceArea = function() {
+//   return 2*(this.length * this.width + this.length * this.height + this.width * this.height);
+// };
+
+// const cuboid = new CuboidMaker({
+//   length: 4,
+//   width: 5,
+//   height: 5
+// });
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 
@@ -67,6 +106,42 @@ const limitFunctionCallCount = (cb, n) => {
 // To test your formulas, pass these key/value pairs into your constructor: length: 2, width: 2, and height: 2. You should get Volume: 8 with a Surface Area of 24. 
 
 // Use these logs to test your results:
+
+// class CuboidMaker {
+//   constructor(options) {
+//   this.length = options.length;
+//   this.width = options.width;
+//   this.height = options.height;
+//   }
+//   volume() {
+//     return this.length * this.width * this.height;
+//   }
+//   surfaceArea() {
+//     return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+//   }
+// }
+
+// class Cube extends CuboidMaker{
+//   constructor(optionsCube) {
+//     super(optionsCube);    
+//   }
+//   surfaceArea() {
+//     return 6 * (this.length * this.width);
+//   }
+// }
+
+// const cuboid = new CuboidMaker({
+//   length: 4,
+//   width: 5,
+//   height: 5
+// });
+
+// const cube = new Cube({
+//   length: 2,
+//   width: 2,
+//   height: 2
+// });
+
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 // console.log(cube.volume()); // 8
@@ -75,9 +150,43 @@ const limitFunctionCallCount = (cb, n) => {
 /* ======================== Stretch Challenges ============================ */
 
 
-// Challenge 1: Go back to your prototype CuboidMaker and extend Cube using psuedo-classical inheritance to achiveve the same results you built using the ES6 class syntax
+// Challenge 1: Go back to your prototype CuboidMaker and extend cube using psuedo-classical inheritance to achiveve the same results you built using the ES6 class syntax
 
 // Use these logs to test your results:
+
+// function CuboidMaker(options) {
+//   this.length = options.length;
+//   this.width = options.width;
+//   this.height = options.height;
+// }
+// CuboidMaker.prototype.volume = function() {
+//   return this.length * this.width * this.height;
+// };
+// CuboidMaker.prototype.surfaceArea = function() {
+//   return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+// };
+
+// function Cube(optionsCube) {
+//   CuboidMaker.call(this, optionsCube);
+// }
+// Cube.prototype = Object.create(CuboidMaker.prototype);
+
+// Cube.prototype.surfaceArea = function() {
+//   return 6 * (this.length * this.width);
+// }
+
+// const cuboid = new CuboidMaker({
+//   length: 4,
+//   width: 5,
+//   height: 5
+// });
+
+// const cube = new Cube({
+//   length: 2,
+//   width: 2,
+//   height: 2
+// });
+
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 // console.log(cube.volume()); // 8
@@ -86,18 +195,67 @@ const limitFunctionCallCount = (cb, n) => {
 // Challenge 2: Go back to your class Cube and add the following property: isCube.
 // Create a method inside of Cube that checks for isCube and if it's true, returns a string 'We have a cube!';
 
+class CuboidMaker {
+  constructor(options) {
+  this.length = options.length;
+  this.width = options.width;
+  this.height = options.height;
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
+}
+
+class Cube extends CuboidMaker{
+  constructor(optionsCube) {
+    super(optionsCube);    
+    this.isCube = optionsCube.isCube
+  }
+  surfaceArea() {
+    return 6 * (this.length * this.width);
+  }
+  checkIfCube() {
+    if(this.isCube) {
+      return 'We have a cube!';
+    }
+  }
+}
+
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+});
+
+const cube = new Cube({
+  length: 2,
+  width: 2,
+  height: 2,
+  isCube: true
+});
+
 // Use these logs to test your results:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
-// console.log(cube.volume()); // 8
-// console.log(cube.surfaceArea()); // 24
-// console.log(cube.checkIfCube());  // "We have a cube!"
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+console.log(cube.volume()); // 8
+console.log(cube.surfaceArea()); // 24
+console.log(cube.checkIfCube());  // "We have a cube!"
 
 // Challenge 3: Recursion
 const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
-};
+  for (let i = 0; i < obj.length; i++) {
+    if (obj[i] === obj[i + 1]  ) {
+      ++i;
+      return checkMatchingLeaves();
+    }
+  }
+}
+
 
 
 module.exports = {
