7811a31930a839c401964f8a6451f3906671932e
MINE <Mine@MINEs-MacBook-Pro-2.local>
2018-03-23 11:10:03 -0700

    Callback each

--- src/challenges.js
+++ src/challenges.js
@@ -3,11 +3,15 @@
 
 /* ======================== CallBacks Practice ============================ */
 const each = (elements, cb) => {
+  for (let i = 0; i < elements.length; i++) {
+    cb(elements[i], i);
+  }
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
 };
 
 const map = (elements, cb) => {
+
   // Produces a new array of values by mapping each value in list through a transformation function.
   // Return the new array.
 };
