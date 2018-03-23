8d432dda7a43be048d5250718c5b29d0002a7bf3
Josh Knell <bigknell@gmail.com>
2018-03-23 00:27:15 -0600

    Updates to sprint challenge
    - Added prototypes
    - Classes
    - Stretch problems for prototypes and classes

--- src/challenges.js
+++ src/challenges.js
@@ -5,49 +5,101 @@ const each = (elements, cb) => {
 };
 
 const map = (elements, cb) => {
-  // Produces a new array of values by mapping each value in list through a transformation function (iteratee).
+  // Produces a new array of values by mapping each value in list through a transformation function.
   // Return the new array.
 };
 
 /* ======================== Closure Practice ============================ */
+// No test needed here, just run the newCounter(); and make sure it's counting up
+const counter = () => {
+  // Return a function that when invoked increments and returns a counter variable.
+  // Example: const newCounter = counter();
+  // newCounter(); // 1
+  // newCounter(); // 2
+};
+
 const limitFunctionCallCount = (cb, n) => {
   // Should return a function that invokes `cb`.
   // The returned function should only allow `cb` to be invoked `n` times.
 };
 
-const cacheFunction = cb => {
-  // Should return a funciton that invokes `cb`.
-  // A cache (object) should be kept in closure scope.
-  // The cache should keep track of all arguments have been used to invoke this function.
-  // If the returned function is invoked with arguments that it has already seen
-  // then it should return the cached result and not invoke `cb` again.
-  // `cb` should only ever be invoked once for a given set of arguments.
-};
+/* ======================== Prototype Practice ============================ */
 
-/* eslint-enable no-unused-vars */
+// ***Prototypes do NOT have test cases built for them.  You must use the console logs provided at the end of this section.***
 
-/* ======================== Recursion Practice ============================ */
-const reverseStr = str => {
-  // reverse str takes in a string and returns that string in reversed order
-  // The only difference between the way you've solved this before and now is that you need to do it recursivley!
-};
+// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. 
 
+// Create a CuboidMaker constructor function that accepts properties for length, width, and height
+
+// Create a seperate function property of CuboidMaker that returns the volume of a given cuboid's length, width, and height
+// Formula for cuboid volume: length * width * height
+
+// Create a seperate function property of CuboidMaker that returns the surface area of a given cuboid's length, width, and height. 
+// Formula for cuboid surface area of a cube: 2(length * width + length * height + width * height)
+
+// Create a cuboid object that inherits from CuboidMaker. 
+// The cuboid object must contain keys for length, width, and height.
+
+// To test your formulas, pass these key/value pairs into your constructor: length: 4, width: 5, and height: 5. When running your logs, you should get Volume: 100 with a Surface Area of 130. 
+
+// Use these logs to test your results:
+// console.log(cuboid.volume()); // 100
+// console.log(cuboid.surfaceArea()); // 130
+
+/* ======================== Class Practice ============================ */
+
+// ***Class Practice does NOT have test cases built.  You must use the console logs provided at the end of this section.***
+
+// Task 1: Copy and paste your prototype CuboidMaker here and proceed to convert it into ES6 Class syntax
+
+// Task 2: Create a new class called Cube. Extend the Cube class with the CuboidMaker class.
+
+// Create two new methods on the Cube class to calculate the volume and surface area of a cube given the same values passed in from CuboidMaker.
+
+// The volume of a cube is: length * width * height
+// The surface area of a cube is: 6 * (length + width)
+
+// Create a new cube object that has equal values for length, width, and height 
+
+// To test your formulas, pass these key/value pairs into your constructor: length: 2, width: 2, and height: 2. You should get Volume: 8 with a Surface Area of 24. 
+
+// Use these logs to test your results:
+// console.log(cuboid.volume()); // 100
+// console.log(cuboid.surfaceArea()); // 130
+// console.log(cube.volume()); // 8
+// console.log(cube.surfaceArea()); // 24
+
+/* ======================== Stretch Challenges ============================ */
+
+
+// Challenge 1: Go back to your prototype CuboidMaker and extend Cube using psuedo-classical inheritance to achiveve the same results you built using the ES6 class syntax
+
+// Use these logs to test your results:
+// console.log(cuboid.volume()); // 100
+// console.log(cuboid.surfaceArea()); // 130
+// console.log(cube.volume()); // 8
+// console.log(cube.surfaceArea()); // 24
+
+// Challenge 2: Go back to your class Cube and add the following property: isCube.
+// Create a method inside of Cube that checks for isCube and if it's true, returns a string 'We have a cube!';
+
+// Use these logs to test your results:
+// console.log(cuboid.volume()); // 100
+// console.log(cuboid.surfaceArea()); // 130
+// console.log(cube.volume()); // 8
+// console.log(cube.surfaceArea()); // 24
+// console.log(cube.checkIfCube());  // "We have a cube!"
+
+// Challenge 3: Recursion
 const checkMatchingLeaves = obj => {
   // return true if every property on `obj` is the same
   // otherwise return false
 };
 
-const flatten = elements => {
-  // Flattens a nested array (the nesting can be to any depth).
-  // Example: flatten([1, [2], [3, [[4]]]]); => [1, 2, 3, 4];
-};
 
 module.exports = {
   each,
   map,
   limitFunctionCallCount,
-  cacheFunction,
-  reverseStr,
   checkMatchingLeaves,
-  flatten,
 };
