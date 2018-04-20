ebc526e289d2a95f8523a4ed811992dbafbd028e
Thuy Pham <tpham045@hotmail.com>
2018-04-20 08:28:01 -0700

    feat: functions.js completed

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,25 +6,40 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(param1, param2, cb) {
+  return cb(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(a, b) {
+  console.log(a+b)
+  return a+b;
+}
 
+function multiply(a,b) {
+  console.log(a*b)
+  return a*b;
+}
+
+function greeting(a,b) {
+  console.log(`Hello ${a} ${b}, nice to meet you!`);
+  return `Hello ${a} ${b}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation: `nestedFunction` function is inside `myFunction` 
 
 // Question 2: Given the example below, what scope is the external variable in?
 
@@ -32,6 +47,7 @@ let external = "I'm outside!";
 
 function myFunction() {
   let internal = "Hello! I'm inside the function";
+  
   console.log(external);
 
   function nestedFunction() {
@@ -40,3 +56,5 @@ function myFunction() {
   nestedFunction();
 }
 myFunction();
+
+// `external` vairable is outside of `myFunction`. It is global scope. It can be used anywhere. If it is reassigned anywhere, its value will change. 
