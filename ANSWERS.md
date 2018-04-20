8707e75b87963653e9fd56488508c1b955de6a7c
Obo Agboghidi <oagboghi2@gmail.com>
2018-04-20 10:04:19 -0700

    Obo Agboghidi First Commit

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,6 +1,11 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 1. Describe the biggest difference between `.forEach` & `.map`.
+- forEach iterates through an array, while map iterates through an array and returns a new array once done.
 2. What is the difference between a function and a method?
+- A method is a function inside of a object.
 3. What is closure?
+- CLosure is when a function uses data from outside of it's local scope
 4. Describe the four rules of the 'this' keyword.
+- 
 5. Why do we need super() in an extended class?
+- Super copies and grants the ability of a object to use the properties and methods from which it inherited from
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,12 +7,37 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
+const tyrannosaurus = {
+  "diet": "carnivorous",
+  "weight": " 7000kg",
+  "length": "12m",
+  "period": "Late Cretaceious",
+  "roar" : function () {
+    return "RAWERSRARARWERSARARARRRR!"
+  }
+
+}
+
+
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stegosaurus = {
+  "diet": "herbivorous",
+  "weight": " 2000kg",
+  "length": "9m",
+  "period": "Late Jurassic"
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+const velociraptor = {
+  "diet": "carnivorous",
+  "weight": " 15kg",
+  "length": "1.8m",
+  "period": "Late Cretaceious"
+}
 
 // Using your dinosaur objects, log answers to these questions:
+
 
 // How much did tyrannosaurus weigh?
 console.log(tyrannosaurus.weight);
@@ -46,6 +71,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for(var i = 0; i < graduates.length; i++){
+  universities.push(graduates[i]["university"])
+  universities.sort();
+}
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -55,11 +84,25 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for(var i = 0; i < graduates.length; i++){
+  var info = graduates["first_name"] + graduates["email"]
+  contactInfo.push(graduates[i]["first_name"]+ " " + graduates[i]["email"])
+  //contactInfo.push(graduates[i]["email"])
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for(var i = 0; i < graduates.length; i++){
+  let spl = graduates[i]["university"].split("",3).join('')
+  console.log(spl);
+  if(spl === "Uni"){
+    uni.push(graduates[i]["university"])
+    
+  }
+
+}
 console.log(uni);
 
 
