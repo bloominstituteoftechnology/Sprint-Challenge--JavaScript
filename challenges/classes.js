4e86099d3bad61ad2cf0b77c56faad16bee2a5a9
mister-corn <code@ronaldlibago.com>
2018-04-20 06:32:15 -0700

    T+ 1hr26min: `classes.js` Complete sans Stretch.

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,65 @@
 // Copy and paste your prototype in here and refactor into class syntax.
+/* ===== Prototype Practice ===== */
 
+// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge.
+
+/* == Step 1: Base Constructor ==
+  Create a constructor function named CuboidMaker that accepts properties for length, width, and height
+*/
+
+// function Cuboid(length, width, height) {
+  // this.length = length;
+  // this.width = width;
+  // this.height = height;
+// }
+
+class Cuboid {
+  constructor(length, width, height) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
+}
+
+/* == Step 2: Volume Method ==
+  Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
+  
+  Formula for cuboid volume: length * width * height
+*/
+
+// Cuboid.prototype.volume = function() {
+//   return this.length * this.width * this.height;
+// }
+
+// Look at Step 1 for the answer!!!
+
+/* == Step 3: Surface Area Method ==
+  Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
+
+  Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
+*/
+
+// Cuboid.prototype.surfaceArea = function() {
+//   return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+// }
+
+// Look at Step 1 for the answer!!!
+
+/* == Step 4: Create a new object that uses CuboidMaker ==
+  Create a COMPANION CUBE object that uses the new keyword to use our CuboidMaker constructor
+  Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
+*/
+
+const companionCube = new Cuboid(4, 5, 5);
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(companionCube.volume()); // 100
+console.log(companionCube.surfaceArea()); // 130
 
 /* Stretch Task:
 
