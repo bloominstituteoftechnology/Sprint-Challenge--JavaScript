436dbe02784888dd3074bed934d91e0100e6be78
mr-burton <taylormade800@gmail.com>
2018-04-20 09:39:01 -0700

    working on closures

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,5 +1,9 @@
 // ==== Callbacks ====  
 
+const london = function (cb) {
+  console.log(cb);
+}
+
 
 /* Step 1: Create a callback function
   * Create a function named consume that can take 3 parameters.
@@ -7,14 +11,30 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(num1, num2, cb) {
+  return cb(num1 * num2);
+}
+consume(12, 2, london);
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add() {
+  return cb();
+}
+consume(36, 14, london);
 
+function multiply() {
+  return cb()
+}
+consume(12, 12, london);
+
+function greeting() {
+  return cb();
+}
+consume(uk, england, london)
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
 //consume(2,2,add); // 4
