53773f320ce38f6ad5f3465e1a0227260d199553
MacBookPro <macbookpro@MacBookPros-MacBook-Pro.local>
2018-04-20 12:36:42 -0500

    files prototypes.js and classes.js complete

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,37 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMaker {
+  constructor(properties){
+    this.length = properties.length;
+    this.width = properties.width;
+    this.height = properties.height;
+}
+  volume() {
+    return (this.length * this.width * this.height)
+  };
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+  };
+}
+
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+});
+
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+
+/* log results
+Info: Start process (12:32:56)
+100
+130
+Info: End process (12:32:56)*/
+
 
 /* Stretch Task:
 
