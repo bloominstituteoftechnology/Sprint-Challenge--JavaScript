fc537ae16df87581326f80c79c05c07bf1ebfafd
Brandon Benefield <bsquared18@gmail.com>
2018-04-20 12:45:44 -0400

    prototypes.js: complete step 1

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,6 +5,11 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
+function CuboidMaker(props) {
+  this.height = props.height;
+  this.length = props.length;
+  this.width  = props.width;
+}
 
 
 /* == Step 2: Volume Method ==
