a3f2debd86c86afac88bd7fd20738a8215f868d1
mr-burton <taylormade800@gmail.com>
2018-04-20 10:29:15 -0700

    arrays toLowerCase completed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -117,7 +117,7 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 let lowerCase = [];
-lowerCase = zooAnimals.map(zooAnimals => zooAnimals.animal_name);
+lowerCase = zooAnimals.map(zooAnimals => zooAnimals.animal_name.toLowerCase());
 console.log(lowerCase); 
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
