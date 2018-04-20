1bc178a47328a49fa1466924ae4edceb2b5ca6b9
Brandon Benefield <bsquared18@gmail.com>
2018-04-20 12:47:37 -0400

    prototypes.js: complete step 3

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -27,6 +27,9 @@ CuboidMaker.prototype.volume = function() {
 
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
+CuboidMaker.prototype.surfArea = function() {
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+}
 
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
