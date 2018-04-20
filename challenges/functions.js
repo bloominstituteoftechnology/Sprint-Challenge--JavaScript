de127540780bfdb601ace2979de66fe2b0c85677
maxwashington <mnwashington18@gmail.com>
2018-04-20 11:11:36 -0500

    -Functions Complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,9 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function addstuff(x, y, cb){
+  return cb(x+y);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,19 +16,38 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function consume(x, y, cb){
+  return cb(x, y);
+}
+function add(parma1, parma2){
+  return (parma1 + parma2);
+}
+
+function multiply(parma1, parma2){
+  return (parma1 * parma2);
+}
+
+function greeting(firstname, lastname){
+  return (`Hello ${firstname} ${lastname}, nice to meet you!`);
+}
+const m = "Max";
+const w = "Washington"
+const hello = greeting(m, w);
+console.log(hello)
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)) // 4
+console.log(consume(10,16,multiply)) // 160
+console.log(consume("Mary","Poppins", greeting))// Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation: The extrnal variable is being called even though its outside the local scope of myFunction
 
-// Question 2: Given the example below, what scope is the external variable in?
+// Question 2: Given the example below, what scope is the external variable in? 
+//Global?
 
 let external = "I'm outside!";
 
