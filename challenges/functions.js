c2aee254f08cd75c9a84e8cceda0278540493d7a
AmyShackles <amyshackles@gmail.com>
2018-04-20 09:02:03 -0700

    Finished functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,27 +6,40 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume (a, b, cb) {
+  return cb(a, b)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
-
+function add(a, b) {
+  return a + b;
+}
+function multiply (a, b) {
+  return a * b;
+}
+function greeting (firstName, lastName) {
+  return `Hello, ${firstName} ${lastName}, nice to meet you!`
+}
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
-
+consume(2,2,add); // 4
+console.log(consume(2,2,add))
+consume(10,16,multiply); // 160
+console.log(consume(10,16,multiply))
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume("Mary", "Poppins", greeting))
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
 // Explanation: 
+// The example below is a closure for two reasons.  One, the function can access variables from outside of the function (the global variale).  Two, the inner function has access to the outer function's variables.
 
 // Question 2: Given the example below, what scope is the external variable in?
+//The external variable has global scope.
 
 let external = "I'm outside!";
 
@@ -37,6 +50,6 @@ function myFunction() {
   function nestedFunction() {
     console.log(internal);
   };
-  nestedFunction();
+  console.log(nestedFunction())
 }
-myFunction();
+console.log(myFunction())
