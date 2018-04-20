27b6b60e4bcef4e18d8e9fc7957e9aa4ca652800
jaklundun <govi.escudero@gmail.com>
2018-04-20 12:06:58 -0700

    Completed Stetch Task for classes.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -36,6 +36,8 @@ class CuboidMaker  {
 
 const cuboid = new CuboidMaker (4, 5, 5);
 
+console.log(cuboid)
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
@@ -45,3 +47,27 @@ console.log(cuboid.surfaceArea()); // 130
   Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods as well.  Create a new cube object and log out the results of your new cube.
 
 */
+
+class CubeMaker extends CuboidMaker {
+  constructor(length, width, height) {
+    super(length, width, height);
+  }
+
+  volume () {
+      return this.length * this.length * this.length
+  }
+
+  surfaceArea () {
+     return 6 * (this.length * this.length)
+  }
+
+}
+
+let cube = new CubeMaker(8, 10, 10);
+
+console.log(cube)
+
+console.log(cube.volume())
+
+console.log(cube.surfaceArea())
+
