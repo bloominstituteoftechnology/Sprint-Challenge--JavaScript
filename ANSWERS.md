94cd65622efaeeaf2c6cca870b11a76f6c53afea
fireinjun <fireinjun291@gmail.com>
2018-04-20 12:52:06 -0400

    Sailing pretty smoothly, more corrections to the files. Abandoned the ANSWERS.MD  for now to focus on the coding.

--- ANSWERS.md
+++ ANSWERS.md
@@ -2,8 +2,11 @@
 
 
 1. Describe the biggest difference between `.forEach` & `.map`.
-The `.forEach` method executes a provided function once for each array element. Where the `.map` method creates a new array with the results of calling a provided function on every element in the calling array.
-`.foreach` just goes at the array, no copy. High risk. `.map`, makes a new array that it then messes with.
+MDN states:
+* 'The `.forEach` method executes a provided function once for each array element.'
+* 'The `.map` method creates a new array with the results of calling a provided function on every element in the calling array.'
+
+So basically what that means is: `.foreach` just goes at the array, no copy. High risk. `.map`, makes a new array that it then messes with.
 
 2. What is the difference between a function and a method?
 
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,11 +6,53 @@
   object name, diet, weight, length, period
 */
 
+
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
+
+const tyrannosaurus = ({
+  diet: `carnivorous`,
+  weight: `${7000}kg`,
+  length: `${12}m`,
+  period: `Late Cretaceious`,
+  roar: function(obj) {
+    return `${'RAWERSRARARWERSARARARRRR!'}`,
+  }
+})
+
+
+
+//uncomment below to test.
+//console.log(tyrannosaurus)
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stegosaurus = ({
+  diet: `herbivore`,
+  weight: `${2000}kg`,
+  length: `${9}m`,
+  period: `Late Jurassic`
+})
+
+
+//uncomment below to test.
+//console.log(stegosaurus)
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+
+const velociraptor = ({
+  diet: `carnivorous`,
+  weight: `${15}kg`,
+  length: `${1.8}m`,
+  period: `Late Cretaceious`
+})
+
+
+//uncomment below to test.
+//console.log(velociraptor)
+
+
+
 
 // Using your dinosaur objects, log answers to these questions:
 
@@ -31,16 +73,67 @@ console.log(tyrannosaurus.roar());
 
 // Given an array of college graduates.  Complete the following requests without using any array methods like .forEach(), .map(), .reduce(), .filter()
 
-const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
-{"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
-{"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
-{"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
-{"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
-{"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
-{"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
-{"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
-{"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
-{"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
+const graduates = [{
+    "id": 1,
+    "first_name": "Cynde",
+    "university": "Missouri Southern State College",
+    "email": "ctorry0@macromedia.com"
+  },
+  {
+    "id": 2,
+    "first_name": "Saundra",
+    "university": "The School of the Art Institute of Chicago",
+    "email": "swhal1@state.gov"
+  },
+  {
+    "id": 3,
+    "first_name": "Lambert",
+    "university": "Marian College",
+    "email": "lparham2@techcrunch.com"
+  },
+  {
+    "id": 4,
+    "first_name": "Modestine",
+    "university": "International Medical & Technological University",
+    "email": "mdolder3@symantec.com"
+  },
+  {
+    "id": 5,
+    "first_name": "Chick",
+    "university": "Sultan Salahuddin Abdul Aziz Shah Polytechnic",
+    "email": "camorts4@google.com.au"
+  },
+  {
+    "id": 6,
+    "first_name": "Jakob",
+    "university": "Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft",
+    "email": "jharken5@spiegel.de"
+  },
+  {
+    "id": 7,
+    "first_name": "Robbi",
+    "university": "Salem University",
+    "email": "rbrister6@redcross.org"
+  },
+  {
+    "id": 8,
+    "first_name": "Colline",
+    "university": "Coastal Carolina University",
+    "email": "cbrosh7@alibaba.com"
+  },
+  {
+    "id": 9,
+    "first_name": "Michail",
+    "university": "Universidad Católica de Ávila",
+    "email": "mrome8@shinystat.com"
+  },
+  {
+    "id": 10,
+    "first_name": "Hube",
+    "university": "Universitat Rovira I Virgili Tarragona",
+    "email": "hlethbrig9@foxnews.com"
+  }
+]
 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
@@ -67,20 +160,71 @@ console.log(uni);
 
 // Given this zoo data from around the United States, follow the instructions below
 
-zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":"Canis aureus","state":"Kentucky"},
-{"animal_name":"Screamer, southern","population":1,"scientific_name":"Chauna torquata","state":"Alabama"},
-{"animal_name":"White spoonbill","population":8,"scientific_name":"Platalea leucordia","state":"Georgia"},
-{"animal_name":"White-cheeked pintail","population":1,"scientific_name":"Anas bahamensis","state":"Oregon"},
-{"animal_name":"Black-backed jackal","population":2,"scientific_name":"Canis mesomelas","state":"Washington"},
-{"animal_name":"Brolga crane","population":9,"scientific_name":"Grus rubicundus","state":"New Mexico"},
-{"animal_name":"Common melba finch","population":5,"scientific_name":"Pytilia melba","state":"Pennsylvania"},
-{"animal_name":"Pampa gray fox","population":10,"scientific_name":"Pseudalopex gymnocercus","state":"Connecticut"},
-{"animal_name":"Hawk-eagle, crowned","population":10,"scientific_name":"Spizaetus coronatus","state":"Florida"},
-{"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
+zooAnimals = [{
+    "animal_name": "Jackal, asiatic",
+    "population": 5,
+    "scientific_name": "Canis aureus",
+    "state": "Kentucky"
+  },
+  {
+    "animal_name": "Screamer, southern",
+    "population": 1,
+    "scientific_name": "Chauna torquata",
+    "state": "Alabama"
+  },
+  {
+    "animal_name": "White spoonbill",
+    "population": 8,
+    "scientific_name": "Platalea leucordia",
+    "state": "Georgia"
+  },
+  {
+    "animal_name": "White-cheeked pintail",
+    "population": 1,
+    "scientific_name": "Anas bahamensis",
+    "state": "Oregon"
+  },
+  {
+    "animal_name": "Black-backed jackal",
+    "population": 2,
+    "scientific_name": "Canis mesomelas",
+    "state": "Washington"
+  },
+  {
+    "animal_name": "Brolga crane",
+    "population": 9,
+    "scientific_name": "Grus rubicundus",
+    "state": "New Mexico"
+  },
+  {
+    "animal_name": "Common melba finch",
+    "population": 5,
+    "scientific_name": "Pytilia melba",
+    "state": "Pennsylvania"
+  },
+  {
+    "animal_name": "Pampa gray fox",
+    "population": 10,
+    "scientific_name": "Pseudalopex gymnocercus",
+    "state": "Connecticut"
+  },
+  {
+    "animal_name": "Hawk-eagle, crowned",
+    "population": 10,
+    "scientific_name": "Spizaetus coronatus",
+    "state": "Florida"
+  },
+  {
+    "animal_name": "Australian pelican",
+    "population": 5,
+    "scientific_name": "Pelecanus conspicillatus",
+    "state": "West Virginia"
+  }
+];
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 let lowerCase = [];
-console.log(lowerCase); 
+console.log(lowerCase);
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
 let populationTotal = [];
