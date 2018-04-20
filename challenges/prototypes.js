5d35292fdda4132d3babe73e496eb754ed6dfe90
Fausto Wladimir Fraga <wladimir917@hotmail.com>
2018-04-20 14:28:25 -0400

    prototypes.js done

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -1,10 +1,18 @@
 /* ===== Prototype Practice ===== */
 
-// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge.
+// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. 
+// Cuboids are similar to cubes but do not have even sides. 
+// Follow the steps in order to accomplish this challenge.
 
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
+
+function CuboidMaker(properties) {
+  this.length = properties.length;
+  this.width = properties.width;
+  this.height = properties.height;
+}
 
 
 /* == Step 2: Volume Method ==
@@ -12,6 +20,9 @@
   
   Formula for cuboid volume: length * width * height
 */
+CuboidMaker.prototype.volume = function() {
+  return this.length * this.width * this.height;
+}
 
 
 /* == Step 3: Surface Area Method ==
@@ -20,14 +31,26 @@
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
+CuboidMaker.prototype.area = function() {
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+}
+
+
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
+cuboid = new CuboidMaker( {
+  length: 4, 
+  width: 5,
+  height: 5,
+});
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 
-
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
