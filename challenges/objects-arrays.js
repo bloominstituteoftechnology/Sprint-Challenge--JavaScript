c69ff1c252120b159e57415299a16fac26b65f95
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-04-20 10:32:11 -0700

    change array methods to forEach in top section

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -67,7 +67,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = graduates.map(student => student.university).sort();
+// const universities = graduates.map(student => student.university).sort();
+const universities = [];
+graduates.forEach(grad => {universities.push(grad.university)});
+universities.sort()
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -76,13 +79,17 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = graduates.map(student => `${student.first_name} ${student.email}`);
+// const contactInfo = graduates.map(student => `${student.first_name} ${student.email}`);
+const contactInfo = [];
+graduates.forEach(grad => {contactInfo.push(`${grad.first_name} ${grad.email}`)});
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name.
 // Create a new array called uni that contains them all. Log the result. */
-const uni = graduates.filter(student => student.university.includes('Uni')).map(student => student.university);
+// const uni = graduates.filter(student => student.university.includes('Uni')).map(student => student.university);
+const uni = [];
+graduates.forEach(grad => {grad.university.includes('Uni') ? uni.push(grad.university) : false});
 console.log(uni);
 
 
