7434ed2c206cc854fb522a8437b55af628f50945
dlinz7 <linzdiegoa@gmail.com>
2018-04-20 12:09:23 -0500

    Prototypes Complete

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,6 +5,11 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
+function CuboidMaker(props){
+  this.length = props.length;
+  this.width = props.width; 
+  this.height = props.height;
+}
 
 
 /* == Step 2: Volume Method ==
@@ -12,6 +17,9 @@
   
   Formula for cuboid volume: length * width * height
 */
+CuboidMaker.prototype.volume = function(){
+  return (this.length * this.width * this.height);
+}
 
 
 /* == Step 3: Surface Area Method ==
@@ -19,6 +27,9 @@
 
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
+CuboidMaker.prototype.surfaceArea = function(){
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+}
 
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
@@ -26,8 +37,13 @@
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
-// Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+})
 
+// Test your volume and surfaceArea methods by uncommenting the logs below:
+ console.log(cuboid.volume()); // 100
+ console.log(cuboid.surfaceArea()); // 130
 
