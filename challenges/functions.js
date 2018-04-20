915f6215ce954dbc1d5f98c13870948c2e3e9958
Brandon Benefield <bsquared18@gmail.com>
2018-04-20 12:32:40 -0400

    functions.js: complete step 1

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,9 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
+function consume(a, b, cb) {
+  return cb(a, b);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
