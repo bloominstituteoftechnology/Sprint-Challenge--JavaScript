89776492a1204cbac2cd92024bd516f323f88e54
masontmorrow <masontmorrow@gmail.com>
2018-04-20 18:55:18 +0200

    Finished Functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,7 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
+let consume = (a, b, cb) => cb(a, b);
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -14,19 +15,29 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+let add = (a, b) => a + b;
+let multiply = (a, b) => a * b;
+let greeting = (first, last) => `Hello ${first} ${last}, nice to meet you!`
+
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
 // Explanation: 
+/*
+The example is a Closure because the function nestedFunction uses the internal variable that is set in its parent function, and also because myFunction uses a global variable `external` inside its own scope.
+*/
 
 // Question 2: Given the example below, what scope is the external variable in?
+/*
+Lol, global.
+*/
 
 let external = "I'm outside!";
 
