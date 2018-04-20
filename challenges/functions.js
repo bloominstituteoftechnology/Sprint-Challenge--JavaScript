3a21192933af03cb2027270b855181ec200ea264
Sam Park <sam.park.email@gmail.com>
2018-04-20 13:22:28 -0400

    finished

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,26 +7,39 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(any1, any2, cb) {
+  return cb(any1, any2)
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(a, b) {
+  return a + b;
+};
+function multiply(a, b) {
+  return a * b;
+};
+function greeting(first, last) {
+  return `Hello ${first} ${last}, nice to meet you!`;
+}
 
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
 // Explanation: 
-
-// Question 2: Given the example below, what scope is the external variable in?
+// because it is an enclosed function that is referencing its surrounding state(lexical environment). You have access to an outer functions scope from an inner function.
+// Question 2: Given the example below, what scope is the external variable in? Global
 
 let external = "I'm outside!";
 
