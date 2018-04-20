1cc387dde9426118648e67fedbf99ce9cbe9c44e
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-04-20 09:59:16 -0700

    finished functions w/ errors, moved on for time

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(x, y, cb) {
+  return (x, y, cb);          // return cb(x, y); broke stuff, the callback function doesn't work.
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,17 +17,29 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+let add = (x, y) => {
+  return cb(x+y);
+};
+
+let multiply = (x, y) => {
+  return cb(x*y);
+};
+
+
+let greeting = (first_name, last_name) => {
+  return cb(`Hello ${first_name} ${last_name}, nice to meet you!`);
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation: The inside function does not have access to the external variable.
 
 // Question 2: Given the example below, what scope is the external variable in?
 
@@ -40,3 +55,5 @@ function myFunction() {
   nestedFunction();
 }
 myFunction();
+
+// Answer: Window/Global 
