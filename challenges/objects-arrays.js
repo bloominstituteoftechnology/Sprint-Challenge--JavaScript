afaab3e858c6dc7d1e1878df40f8c967d8b0c0ef
jesuarva <jean.ariza@jesuarva.com>
2018-04-20 23:04:37 +0200

    REFACTOR Array method to for-loops

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -63,7 +63,12 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = graduates.map( (student) => student.university ).sort();
+let universities = []
+// let universities = graduates.map( (student) => student.university ).sort();
+for ( student of graduates) {
+  universities.push(student.university);
+}
+universities.sort();
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -72,13 +77,26 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = graduates.map( student => `${student.first_name} ${student.email}` );
+const contactInfo = []
+// const contactInfo = graduates.map( student => `${student.first_name} ${student.email}` );
+console.log(contactInfo);
+for ( student of graduates ) {
+  contactInfo.push(`${student.first_name} ${student.email}`);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = graduates.filter( (student) => student.university.includes('Uni') )
-                      .map( student => student.university );
+const uni = [];
+// const uni = graduates.filter( (student) => student.university.includes('Uni') )
+//                       .map( student => student.university );
+const temp = [];
+for ( student of graduates ){
+  student.university.includes('Uni') ? temp.push(student) : null ;
+}
+for ( student of temp ) {
+  uni.push(student.university)
+}
 console.log(uni);
 
 
