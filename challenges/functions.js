992f1dfcdd45403ee5278c7390f0b2098ce9a00c
Cassandra Lamendola <cass.lamendola@gmail.com>
2018-04-20 09:33:07 -0700

    complete function challenges

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,27 +6,38 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
+consume = (param1, param2, cb) => {return cb(param1, param2)}
 
+eat = (substance, quantity) => {return `Eat ${quantity} ${substance}!`}
+
+console.log(consume('strawberries', 5, eat));
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+add = (num1, num2) => {return num1 + num2}
 
+multiply = (num1, num2) => {return num1 * num2}
+
+greeting = (first, last) => {return `Hello ${first} ${last}, nice to meet you!`}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation: The variable, external, is being called within the scope of myFunction (outside its own scope)
+// and internal is being called within the scope of nestedFunction (outside its own scope).
 
 // Question 2: Given the example below, what scope is the external variable in?
+
+// external is in the global scope
 
 let external = "I'm outside!";
 
