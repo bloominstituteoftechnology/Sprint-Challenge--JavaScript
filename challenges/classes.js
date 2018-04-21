023bb6c2ea6f523e6b631ffbf0bf6cb474f83eee
calvintirrell <calvintirrell@gmail.com>
2018-04-20 17:07:53 -0700

    completed the classes.js stretch goal

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,38 +1,42 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 class CuboidMaker {
-  constructor (dimensions) {
-    this.length = dimensions.length;
-    this.width = dimensions.width;
-    this.height = dimensions.height;
+  constructor (len, wid, hei) {
+    this.len = len;
+    this.wid = wid;
+    this.hei = hei;
   }
   volume() {
-    return `Volume: ${this.length * this.width * this.height}`;
+    return this.len * this.wid * this.hei;
   }
   surfaceArea() {
-    return `Surface area: ${2 * (this.length * this.width + this.length * this.height + this.width * this.height)}`;
+    return 2 * (this.len * this.wid + this.len * this.hei + this.wid * this.hei);
   }
 }
 
-// CuboidMaker.prototype.volume = function volume() {
-//   return `Volume: ${this.length * this.width * this.height}`;
-// };
-
-// CuboidMaker.prototype.surfaceArea = function surfaceArea() {
-//   return `Surface area: ${2 * (this.length * this.width + this.length * this.height + this.width * this.height)}`;
-// };
-
-let cuboid = new CuboidMaker({
-  "length": 4,
-  "width": 5,
-  "height": 5
-});
+let cuboid = new CuboidMaker(4, 5, 5);
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
- 
+
 /* Stretch Task:
-
-  Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods as well.  Create a new cube object and log out the results of your new cube.
-
+  Extend the base class CuboidMaker with a sub class called CubeMaker.
+  Find out the formulas for volume and surface area for cubes and create those methods as well.
+  Create a new cube object and log out the results of your new cube.
 */
+class CubeMaker extends CuboidMaker {
+  constructor(measure) {
+    super(measure, measure, measure);
+    this.measure = measure;
+  }
+  cubeVolume() {
+    return Math.pow(this.measure, 3);
+  }
+  cubeSurfaceArea() {
+    return 6 * Math.pow(this.measure, 2);
+  }
+};
+
+let cube = new CubeMaker(5);
+console.log(cube.cubeVolume()); // 125
+console.log(cube.cubeSurfaceArea()); // 150
