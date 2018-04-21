884d30895e9c93e6840cae70f8f753a6052ebae1
Andrew Kolumbic <andrewkolumbic@Andrews-Mac-mini.local>
2018-04-20 17:23:16 -0700

    fixed all issues, everything 100% done!

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,48 +7,39 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-let consume = function(x, y, callback) {
-  return callback(x, y);          // return cb(x, y); broke stuff, the callback function doesn't work.
+function consume(x, y, callback) {
+  return callback(x, y);
 };
- let foo = function(b, c) {return console.log(c)};
-    consume(1, 3, foo);
-    consume
-    
+
+// let consume = function(x, y, callback) {
+//   return callback(x, y);          
+// };
+// //  let foo = function(b, c) {return console.log(c)};
+// //     consume(1, 3, foo);
+// //     consume
+
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
-// let add = (x, y) => {
-// //   return cb(x+y);
-// // };
+function add(x, y){
+  return x + y;
+}
 
-// function add(x,y){
-//   return x+y;
-// }
+let multiply = function (x, y){
+  return x * y;
+}
 
-// // let multiply = (x, y) => {
-// //   return cb(x*y);
-// // };
-
-// function multiply(x, y){
-//   return x*y;
-// }
-
-
-// let greeting = (first_name, last_name) => {
-//   return cb(`Hello ${first_name} ${last_name}, nice to meet you!`);
-// }
-
-// function greeting(first_name, last_name) {
-//   return `Hello ${first_name} ${last_name}, nice to meet you!`;
-// }
+function greeting(first_name, last_name) {
+  return `Hello ${first_name} ${last_name}, nice to meet you!`;
+}
 
 // /* Step 3: Check your work by uncommenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // // ==== Closures ==== 
@@ -58,17 +49,17 @@ let consume = function(x, y, callback) {
 
 // // Question 2: Given the example below, what scope is the external variable in?
 
-// let external = "I'm outside!";
+let external = "I'm outside!";
 
-// function myFunction() {
-//   let internal = "Hello! I'm inside the function";
-//   console.log(external);
+function myFunction() {
+  let internal = "Hello! I'm inside the function";
+  console.log(external);
 
-//   function nestedFunction() {
-//     console.log(internal);
-//   };
-//   nestedFunction();
-// }
-// myFunction();
+  function nestedFunction() {
+    console.log(internal);
+  };
+  nestedFunction();
+}
+myFunction();
 
 // // Answer: Window/Global 
