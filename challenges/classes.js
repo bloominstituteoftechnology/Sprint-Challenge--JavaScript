d9b4010c3e9af324f4efb38260c764c670e0c42d
Sukhada Gholba <sukhadagholba@gmail.com>
2018-05-25 10:18:12 -0700

    refactored into class syntax and tested for results

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,54 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
+function CuboidMaker(attributes){
+  this.length = attributes.length;
+  this.width = attributes.width;
+  this.height = attributes.height;
+}
+
+//volume method
+
+CuboidMaker.prototype.volume = function(){
+  return (this.length)*(this.width)*(this.height);
+};
+
+
+//surfaceAcrea method
+
+CuboidMaker.prototype.surfaceArea = function(){
+    return 2*((this.length)*(this.width)+(this.length)*(this.height)+(this.width)*(this.height));
+};
+
+
+//new cuboid object
+const cuboid = new CuboidMaker({length: 4, width: 5,height: 5});
+
+
+//refactored into class syntax.
+
+class CuboidMaker{
+  constructor(attributes){
+  this.length = attributes.length;
+  this.width = attributes.width;
+  this.height = attributes.height;    
+  }
+  volume() {
+  return (this.length)*(this.width)*(this.height);
+  }  
+
+  surfaceArea() {
+    return 2*((this.length)*(this.width)+(this.length)*(this.height)+(this.width)*(this.height));
+  }
+}
+  
+const cuboid = new CuboidMaker({length: 4, width: 5,height: 5}); 
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+ console.log(cuboid.volume()); // 100
+ console.log(cuboid.surfaceArea()); // 130
+
+Tested and logged correct results
 
 /* Stretch Task:
 
