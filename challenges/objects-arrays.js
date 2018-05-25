7fc90258538a66eea5a40ddee3efc16ab0098068
Mariana Cerda <raymond_grc@yahoo.com>
2018-05-25 12:16:41 -0500

    Raymond Garcia; Js Sprint Challenge

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -1,19 +1,47 @@
 // ==== Objects ====
 
-/* 
-  Given the following information about dinosaurs, create 3 objects: 
-  Use this pattern to create your objects: 
+/*
+  Given the following information about dinosaurs, create 3 objects:
+  Use this pattern to create your objects:
   object name, diet, weight, length, period
 */
-
+// class Dinosaur {
+//   constructor(dinosaurOptions){
+//     this.name = dinosaurOptions.name;
+//     this.diet = dinosaurOptions.diet;
+//     this.weight = dinosaurOptions.weight;
+//     this.length = dinosaurOptions.length;
+//     this.period = dinosaurOptions.period;
+//   }
+// }
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
-
+var tyrannosaurus = {
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceious",
+  roar: function(){
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+};
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+var stegosaurus = {
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic",
+};
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
-
+var velociraptor = {
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceious"
+};
 // Using your dinosaur objects, log answers to these questions:
-
 // How much did tyrannosaurus weigh?
 console.log(tyrannosaurus.weight);
 // What was the diet of a velociraptor?
@@ -42,24 +70,36 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
+/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for (var i = 0; i < graduates.length; i++){
+  universities.push(graduates[i].university)
+}
+universities.sort();
 console.log(universities)
 
-/* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
+/* Request 2: Create a new array called contactInfo that contains both first name and email of each student.
 
-The resulting contact information should have a space between the first name and the email information like this: 
+The resulting contact information should have a space between the first name and the email information like this:
 Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (var i = 0; i < graduates.length; i++){
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`)
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (var i = 0; i < graduates.length; i++){
+  if(graduates[i].university.search("Uni") != "-1"){
+    uni.push(graduates[i]);
+  }
+}
 console.log(uni);
 
 
@@ -80,8 +120,14 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 let lowerCase = [];
-console.log(lowerCase); 
+for (var i = 0; i < zooAnimals.length; i++){
+  lowerCase.push(zooAnimals[i].animal_name.toLowerCase());
+}
+console.log(lowerCase);
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
-let populationTotal = [];
+let populationTotal = [0];
+for (var i = 0; i < zooAnimals.length; i++){
+  populationTotal[0] += zooAnimals[i].population;
+}
 console.log(populationTotal);
