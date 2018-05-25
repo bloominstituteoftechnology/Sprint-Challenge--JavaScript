5ad8da874827b51974e20d50673959015774b95d
snowcoding <vimalshah77@gmail.com>
2018-05-25 09:13:05 -0700

    completed

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,6 +5,19 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
+class CuboidMaker{
+  constructor(props){
+    this.length = props.length;
+    this.width = props.width;
+    this.height = props.height;
+  }
+  volume(){
+    return this.length*this.width*this.height;
+  }
+  surfaceArea(){
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
+}
 
 
 /* == Step 2: Volume Method ==
@@ -26,8 +39,14 @@
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
+let cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+})
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 
