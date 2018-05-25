e7b79354bdcb19455908dc07eaa534af18f88a19
Simon <Simon_Freelance@hotmail.com>
2018-05-25 11:41:10 -0400

    Zoo animal problems completed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -112,8 +112,12 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 let lowerCase = [];
+zooAnimals.map(animal => {
+  lowerCase.push(animal["animal_name"].toLowerCase());
+});
 console.log(lowerCase); 
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
-let populationTotal = [];
+let populationTotal = zooAnimals.reduce((accumulator, animalObj) => accumulator += animalObj.population, 0);
+
 console.log(populationTotal);
