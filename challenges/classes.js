f3ba6f022b970ead5ea8dba643fd6bfe52a1535d
TangledTessellations <mcevoya@oregonstate.edu>
2018-05-25 09:24:24 -0700

    Completed classes challenges

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,36 @@
 // Copy and paste your prototype in here and refactor into class syntax.
+// function CuboidMaker (length, width, height) {
+//   this.length = length;
+//   this.width = width;
+//   this.height = height;
+// }
+//
+// CuboidMaker.prototype.volume = function () {
+//   return this.length * this.width * this.height;
+// }
+//
+// CuboidMaker.prototype.surfaceArea = function () {
+//   return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
+// }
 
+class CuboidMaker {
+  constructor(length, width, height) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea() {
+    return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
+  }
+}
+
+let cuboid = new CuboidMaker(4,5,5);
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 /* Stretch Task:
 
