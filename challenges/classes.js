d884a731d30a3dfbdb708ae3bc553d462485ed7e
Nicky Chen <nicky.chen0@gmail.com>
2018-05-25 09:38:00 -0700

    finished sprint challenge + stretch

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,39 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMaker {
+  constructor(length, width, height) {
+    this.length = length;
+    this.width = width;
+    this.height = height;
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea() {
+    return (2 * (this.length * this.width + this.length * this.height + this.width * this.height));
+  }
+}
+
+class CubeMaker extends CuboidMaker {
+  constructor(length) {
+    super(length);
+  }
+  volume() {
+    return this.length ** 3;
+  }
+  surfaceArea() {
+    return 6 * this.length ** 2;
+  }
+}
+
+const cuboid = new CuboidMaker(4, 5, 5);
+const cube = new CubeMaker(3);
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+console.log(cube.volume())
+console.log(cube.surfaceArea())
 
 /* Stretch Task:
 
