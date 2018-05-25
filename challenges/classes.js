356ccce70b576485e3edc087745188b2e9f5b466
Katie Gorbell <katiegorbell@gmail.com>
2018-05-25 10:19:38 -0500

    initial push

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,8 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 /* Stretch Task:
 
