3085a228c90149b045dd0c5fb1b3a001694f7a52
Noah-Amar <ne.amar@outlook.com>
2018-05-25 12:59:19 -0500

    stretch task syntax

--- challenges/classes.js
+++ challenges/classes.js
@@ -32,3 +32,11 @@ const cuboidObject= new CuboidMaker({
   Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods as well.  Create a new cube object and log out the results of your new cube.
 
 */
+
+class CubeMaker extends CuboidMaker{
+
+}
+
+const cube=new CubeMaker{
+
+}
