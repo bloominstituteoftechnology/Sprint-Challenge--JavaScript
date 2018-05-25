4000c84a9241c6e9a6e08b7a80702ccce69096be
Mark Hermansen <markwilliams.aus@gmail.com>
2018-05-25 11:01:40 -0500

    completed challenge

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,9 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
+function consume (param1, param2, cb) {
+  return cb(param1, param2);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,20 +16,31 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add (param1, param2) {
+  return param1 + param2;
+}
 
+function multiply (param1, param2) {
+  return param1 * param2;
+}
+
+function greeting (firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation: The nested funtion is reaching into the scope of the outer function to find the data it needs to correctly log the value stored inside the variable 'internal'.
 
 // Question 2: Given the example below, what scope is the external variable in?
+// The external variable is in the global scope.
 
 let external = "I'm outside!";
 
