fe290d56a3e3ca91e5e1ab2a8ad142c9eea9863f
emaquino44 <prexy44@yahoo.com>
2018-05-25 09:49:44 -0700

    finsihed function.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -1,19 +1,46 @@
 // ==== Objects ====
 
-/* 
-  Given the following information about dinosaurs, create 3 objects: 
-  Use this pattern to create your objects: 
+/*
+  Given the following information about dinosaurs, create 3 objects:
+  Use this pattern to create your objects:
   object name, diet, weight, length, period
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
 
+const tyrannosaurus = {
+	"name": "tyrannosaurus",
+	"diet": "carnivorous",
+	"weight": "7000kg",
+	"length": "12m",
+	"period": "Late Cretaceious",
+	roar() {return "RAWERSRARARWERSARARARRRR!"}
+};
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const stegosaurus = {
+	"name": "stegosaurus",
+	"diet": "herbivorous",
+	"weight": "2000kg",
+	"length": "9m",
+	"period": "Late Jurassic"
+};
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
-
+const velociraptor = {
+	"name": "velociraptor",
+	"diet": "carnivorous",
+	"weight": "15kg",
+	"length": "1.8m",
+	"period": "Late Cretaceious"
+};
 // Using your dinosaur objects, log answers to these questions:
-
+const emmazingsaurus = {
+	"name": "emmazingsaurus",
+	"diet": "whatever she finds",
+	"weight": "unknown",
+	"length": "human height",
+	"period": "Late 90's"
+};
 // How much did tyrannosaurus weigh?
 console.log(tyrannosaurus.weight);
 // What was the diet of a velociraptor?
@@ -42,24 +69,38 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
-
+/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+		for (let i = 0; i < graduates.length; i++) {
+			universities.push(graduates[i].university);
+		};
 
-/* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
+console.log(universities);
 
-The resulting contact information should have a space between the first name and the email information like this: 
+/* Request 2: Create a new array called contactInfo that contains both first name and email of each student.
+
+The resulting contact information should have a space between the first name and the email information like this:
 Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push(graduates[i].first_name) + contactInfo.push(graduates[i].email);
+	};
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+	for (let i = 0; i < graduates.length; i++) {
+  	let value = graduates[i].university;
+  		if (value.includes("Uni")) {
+    		uni.push(graduates[i].university);
+  		}  else
+		};
+
+
 console.log(uni);
 
 
@@ -80,8 +121,16 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 let lowerCase = [];
-console.log(lowerCase); 
+
+zooAnimals.map(function(letters){
+  lowerCase.push(letters.animal_name.toLowerCase());
+});
+console.log(lowerCase);
+
+
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
 let populationTotal = [];
+
+
 console.log(populationTotal);
