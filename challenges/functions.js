ef902a1d80b246f929c5c0359a25514f707afc0f
Kairi <kairijb@gmail.com>
2018-05-25 10:23:34 -0500

    functions is done

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume (x,y,cb) {
+  return cb(x,y);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,21 +18,36 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add (x,y) {
+  console.log(x+y);
+  return x + y;
+}
+
+function multiply (x,y) {
+  console.log (x * y);
+  return x * y;
+}
+
+function greeting (x,y) {
+  console.log(`Hello ${x} ${y}, nice to meet you!`);
+  return `Hello ${x} ${y}, nice to meet you!`;
+}
+
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation: There is a function nested inside a function
 
 // Question 2: Given the example below, what scope is the external variable in?
 
-let external = "I'm outside!";
+let external = "I'm outside!"; //Global
 
 function myFunction() {
   let internal = "Hello! I'm inside the function";
