30136c65d21ca8d569994b1315c937a56bc2a404
Arthur Bates <aabatesj@gmail.com>
2018-06-29 14:07:12 -0400

    before lunch PR
    finished all but functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,26 +7,44 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(a, b, cb) {
+  cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(a, b, cb) {
+  return cb(a + b);
+}
 
+function multiply(a, b, cb) {
+  return cb(a * b);
+}
+
+function greeting(a, b, cb) {
+  return cb(`Hello ${a} ${b}, nice to meet you`);
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2, 2, add)); // 4
+consume(10, 16, multiply); // 160
+consume("Mary", "Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation: the nestedfunction does not have to go outside of myFunction to find the value of the variable 'internal'.
+// 'External' however, has to climb outside of the function scope to find it's value. this is the idea of closure, that each function has it's own scope
+// and variable can be accessed from outside of the scope but outside variables cant access values inside the function scope.
+
 
 // Question 2: Given the example below, what scope is the external variable in?
+
+//The external variable is in the global scope since the console.log has to reach outside of the function scope to find the value.
 
 let external = "I'm outside!";
 
