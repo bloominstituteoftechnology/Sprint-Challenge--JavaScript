529f0b680bd72feb58d5fc08175603fd3bf46045
bri79319 <bri79319@gmail.com>
2018-06-29 12:53:53 -0400

    prototype.js
    have prototypes working, it was a misspelling which caused an error

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -18,9 +18,9 @@ function CuboidMaker(properties) {
   Formula for cuboid volume: length * width * height
 */
 
-CuboidMaker.prototype.volumne = function(L, W, H) {
-  return (L * W * H);
-}
+CuboidMaker.prototype.volume = function() {
+  return this.length * this.width * this.height;
+};
 
 
 /* == Step 3: Surface Area Method ==
@@ -29,19 +29,19 @@ CuboidMaker.prototype.volumne = function(L, W, H) {
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
-CuboidMaker.prototype.surfaceArea = function(L, W, H) {
-  return (2 * (L*W) + (L*H) + (W*H));
-}
+CuboidMaker.prototype.surfaceArea = function() {
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+};
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
-const cuboid = new CuboidMaker({
-  length: 4,
-  width: 5,
-  height: 5
+const cuboid = new CuboidMaker ({
+  "length": 4,
+  "width": 5,
+  "height": 5,
 });
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
