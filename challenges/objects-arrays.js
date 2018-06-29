156d8544a8cf9469c6379bd7537d58338d0e2c11
keegn <keegan.t.burkett@gmail.com>
2018-06-29 11:04:42 -0500

    halfway through objects arrays js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,10 +7,30 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
+let tyrannosaurus = {
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceious',
+  roar: function() {
+    return `RAWERSRARARWERSARARARRRR!`;
+  }
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+let stegosaurus = {
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic',
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+let velociraptor = {
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceious',
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
@@ -46,7 +66,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+for (let uni of graduates) {
+  universities.push(uni.university);
+}
+
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -55,11 +79,19 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (let grad of graduates) {
+  contactInfo.push(grad.first_name, grad.email);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (let grad of graduates) {
+  if (grad.university.includes('Uni')) {
+    uni.push(grad.university)
+  }
+} 
 console.log(uni);
 
 
