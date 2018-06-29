c2256dac0e624bbcc6e0405599ff1527c9cea1d0
bri79319 <bri79319@gmail.com>
2018-06-29 12:15:41 -0400

    function.js
    did the closure questions
    
    did to work on the callbacks

--- challenges/functions.js
+++ challenges/functions.js
@@ -8,8 +8,7 @@
 */
 
 function consume(param1, param2, cb) {
-  let food = (param1 + param2);
-  cb(food);
+  return cb(param1 + param2);
 }
 
 
@@ -19,34 +18,35 @@ function consume(param1, param2, cb) {
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
-function add(num1, num2, consume) {
+function add(num1, num2, cb) {
   let sum = num1 + num2;
-  consume(sum);
 }
 
-function multiply(x, y, consume) {
-  let product = x * y;
-  consume(product);
+function multiply(x, y) {
+  return x * y;
 }
 
-function greeting(firstName, lastName, consume) {
-  let fullName = firstName + '' + lastName;
-  return `Hello ${fullName}, nice to meet you!`;
+function greeting(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
 }
 
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation: You have a nested function or in other words a function inside of
+// another function. The internal function is logging a value which isn't inside 
+// of it so it needs to check outside until it finds that value.
 
 // Question 2: Given the example below, what scope is the external variable in?
+
+// Explanation: Global scope. So any function may access it. No matter how nested.
 
 let external = "I'm outside!";
 
