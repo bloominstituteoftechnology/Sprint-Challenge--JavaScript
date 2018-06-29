02908d71d24854665cdba8d6283c805ea0dabe8a
Naaz Shaikh <Naaz@Naazs-MacBook-Pro.local>
2018-06-29 10:16:20 -0700

    completed should go over consume with PM

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,8 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function createCB (par1,par2,cb){
+return (cb(par1, par2))}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -15,18 +16,33 @@
 */
 
 
+let adds = function(x, y) {
+  return (x+y);
+}
+let multiply=function(x, y) {
+  return (x*y);
+}
+
+let greeting = function(x, y){
+  return (`Hello ${x} ${y}, nice to meet you!`);
+} 
+let consume = function(x,y,cb) {
+  return cb(x,y);
+}
+
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,adds); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
 // Explanation: 
-
+closure gives access to an outer functions scope from an inner function
 // Question 2: Given the example below, what scope is the external variable in?
+
 
 let external = "I'm outside!";
 
@@ -40,3 +56,6 @@ function myFunction() {
   nestedFunction();
 }
 myFunction();
+
+
+external is within the myFunction scope
