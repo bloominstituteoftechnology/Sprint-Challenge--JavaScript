e881fdc7c76bd1ae1a0d80106f4f51ca636b88cb
szincone <sawyerzincone@gmail.com>
2018-06-29 13:42:52 -0600

    Rewrote array challenge.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -89,8 +89,10 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
-console.log(contactInfo.push(graduates.map(x => x.first_name + " " + x.email)));
-
+// console.log(contactInfo.push(graduates.map(x => x.first_name + " " + x.email)));
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push(graduates[i].first_name + " " + graduates[i].email)
+}
 console.log(contactInfo);
 
 
