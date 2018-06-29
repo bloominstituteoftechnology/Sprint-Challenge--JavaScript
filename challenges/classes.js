00d9c7f941856f1ce3bf7d8f344194967633d974
keegn <keegan.t.burkett@gmail.com>
2018-06-29 11:49:48 -0500

    completed classes js
    working on classes stretch

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,36 @@
 // Copy and paste your prototype in here and refactor into class syntax.
+/* == Step 1: Base Constructor ==
+  Create a constructor function named CuboidMaker that accepts properties for length, width, and height
+*/
+class CuboidMaker {
+  constructor(props) {
+    this.length = props.length;
+    this.width = props.width;
+    this.height = props.height;
+  }
+  volume() {
+    return this.length * this.width * this.height; //cuboid volume: length * width * height
+  }
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height); //surface area of a cube: 2 * (length * width + length * height + width * height)
+  }
+}
+
+
+/* == Step 4: Create a new object that uses CuboidMaker ==
+  Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
+  Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
+*/
+let cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5,
+})
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+
 
 /* Stretch Task:
 
