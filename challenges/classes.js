e3ba7fc25f4e505583decbfec8cffc8ab3f461a0
AI2EJAY <JHammett1314@gmail.com>
2018-06-29 12:49:29 -0500

    Done with everything but my blockers which have comments around them.

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,11 +1,58 @@
 // Copy and paste your prototype in here and refactor into class syntax.
+class CuboidMaker {
+  constructor(properties) {
+    this.length = properties.length;
+    this.width = properties.width;
+    this.height = properties.height;
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
+}
+
+
+/* == Step 4: Create a new object that uses CuboidMaker ==
+  Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
+  Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
+*/
+const thomas = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+});
+
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(thomas.volume()); // 100
+console.log(thomas.surfaceArea()); // 130
 
 /* Stretch Task:
 
   Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods as well.  Create a new cube object and log out the results of your new cube.
 
+
 */
+class CubeMaker extends CuboidMaker {
+  constructor(CubeMakerproperties) {
+  super(CubeMakerproperties);
+  this.area = CubeMakerproperties.area;
+  }
+  cubeVolume () {
+    return (this.area * this.area * this.area);
+  }
+  cubeSurfaceArea () {
+    return (6 * Math.pow(this.area, 2));
+  }
+};
+
+const cubie = new CubeMaker({
+  area: 7
+});
+
+console.log(cubie.cubeVolume())
+console.log(cubie.cubeSurfaceArea())
+
+
--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,14 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+// =================================> Not Done/ Help needed on Call-backs. I understand the idea of what they are but I am struggling on writing them.  
+function consume(food1, food2, cb) {
+  return cb(food1, food2)
+}
+function eatFood (food1, food2) {
+  return `I grabbed my ${food1} and ${food2} and munched out of control`;
+}
+console.log(consume("Pizza", "T-ravs", eatFood()))
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -23,10 +30,10 @@
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
-
-// Explanation: 
+// Explanation: It is closure because the console.log of external is able to grab outside of the function for i'm outside as well as the nested function can grab items from above it in the parent function. 
 
 // Question 2: Given the example below, what scope is the external variable in?
+// Explanation: Global
 
 let external = "I'm outside!";
 
