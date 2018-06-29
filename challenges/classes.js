e2158fe0f1755593184f130c92d42b05d5f6e3bd
luis salazar <luissalazar@luiss-MacBook-Pro.local>
2018-06-29 09:57:56 -0700

    Major changes - Classes.js
    Finished all objectives except the stretch task.

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,32 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMaker {
+  constructor(props) {
+    this.length = props.length;
+    this.width = props.width;
+    this.height = props.height;
+  }
+  volume(cuboidObj) {
+    let volume = cuboidObj.length * cuboidObj.width * cuboidObj.height;
+    return volume;
+  }
+  surfaceArea(cuboidObj) {
+    let surfaceArea = 2 * (cuboidObj.length * cuboidObj.width + cuboidObj.length * cuboidObj.height + cuboidObj.width * cuboidObj.height);
+    return surfaceArea;
+  }
+} // end of CuboidMaker
+
+
+const cuboid = new CuboidMaker ({
+  length: 4,
+  width: 5,
+  height: 5
+});
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume(cuboid)); // 100
+console.log(cuboid.surfaceArea(cuboid)); // 130
 
 /* Stretch Task:
 
