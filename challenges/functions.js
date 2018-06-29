18de97103eae7d8730b7f3823b63f52788917105
Sean <sean47900@gmail.com>
2018-06-29 12:25:04 -0500

    functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+const consume = (param1, param2, cb) => {
+  return cb(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,19 +17,23 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
++add=(x,y)=>x+y;
++multiply=(x,y)=>x*y;
++greeting=(firstName, lastName)=>`Hello ${firstName} ${lastName}, nice to meet you!`;
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
-
+myFunctionhas access to code that is in the global scope
 // Explanation: 
 
 // Question 2: Given the example below, what scope is the external variable in?
+Global scope
 
 let external = "I'm outside!";
 
