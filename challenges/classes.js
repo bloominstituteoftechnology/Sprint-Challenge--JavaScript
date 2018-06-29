93b3f4fa3d08c316d3737ed1c7feb3c742782470
Brandon White <bwhite06@stac.edu>
2018-06-29 18:51:50 -0400

    boo yah
    all done

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,11 +1,45 @@
 // Copy and paste your prototype in here and refactor into class syntax.
+class CuboidMaker {
+  constructor(properties) {
+    this.length=properties.length;
+    this.width=properties.width
+    this.height=properties.height;
 
-// Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+  }
+  volume (length, width, height) {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea(length, width, height){
+    return 2 * (this.length * this.width + this.length * this.height +this.width * this.height)
+  }
+}
 
-/* Stretch Task:
 
-  Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods as well.  Create a new cube object and log out the results of your new cube.
+/* == Step 2: Volume Method ==
+  Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
 
+  Formula for cuboid volume: length * width * height
 */
+// Methods
+
+
+/* == Step 3: Surface Area Method ==
+  Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height.
+
+  Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
+*/
+const cuboid = new CuboidMaker(
+  {
+  length: 4,
+  width: 5,
+  height: 5,
+}
+)
+// == Step 4: Create a new object that
+// Test your volume and surfaceArea methods by uncommenting the logs below:
+ console.log(cuboid.volume()); // 100
+ console.log(cuboid.surfaceArea()); // 130
+
+// Stretch Task:
+
+//  Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods as well.  Create a new cube object and log out the results of your new cube.
