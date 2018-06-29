4fa6f0fa34b9d34e1af2f4ff9dd328bd1e9983c9
sliw <slioweng@gmail.com>
2018-06-30 01:09:42 +0800

    Steven L

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(a, b, callback) {
+  return callback(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,19 +17,37 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(x, y) {
+  return x + y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function greeting(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation:
+// After calling the nestedFunction within myFunction,
+// the local variable in the execution context of myFunction should be deleted,
+// which implies that the nestedFunction cannot access the value of internal.
+// However, because of closure, which is created when the nestedFunction first created,
+// the closure has already stored the value of internal and hence,
+// nestedFunction can find the value of internal.
 
 // Question 2: Given the example below, what scope is the external variable in?
+// Global scope.
 
 let external = "I'm outside!";
 
@@ -37,6 +58,8 @@ function myFunction() {
   function nestedFunction() {
     console.log(internal);
   };
+
   nestedFunction();
 }
+
 myFunction();
