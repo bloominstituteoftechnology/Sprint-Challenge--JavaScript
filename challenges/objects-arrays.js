1515166b38c2cb104b4dc549beca16cc8ec888f6
Fred <sohnf90@yahoo.com>
2018-06-29 08:31:52 -0700

    First commit for pull request

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,7 +7,7 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
-
+ 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
