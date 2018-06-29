59c71e3409f9c82a60832c82342558b9f7a0c9e8
Joseph Stossmeister <joseph.stossmeister@gmail.com>
2018-06-29 11:58:51 -0400

    Classes complete
    -added a semi-colon to prototypes

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -31,7 +31,7 @@ CuboidMaker.prototype.volume = function(){
 */
 
 CuboidMaker.prototype.surfaceArea = function(){
-  return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height))
+  return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
 }
 
 
@@ -44,7 +44,7 @@ const cuboid = new CuboidMaker({
   length: 4,
   width: 5,
   height: 5,
-})
+});
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
