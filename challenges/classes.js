3403027149584cf136b2e891d638f1650e360656
szincone <sawyerzincone@gmail.com>
2018-06-29 11:00:16 -0600

    Finished classes.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,27 @@
 // Copy and paste your prototype in here and refactor into class syntax.
+class CuboidMaker {
+  constructor(baseAttributes) {
+      this.length = baseAttributes.length;
+      this.width = baseAttributes.width;
+      this.height = baseAttributes.height;
+  }
+  volume () {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea () {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
+}// End of Parent
+
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5,
+});
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 /* Stretch Task:
 
