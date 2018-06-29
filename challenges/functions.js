337b8233fb40ffbeed4299fb1cb8741d5a78c99e
bri79319 <bri79319@gmail.com>
2018-06-29 13:41:12 -0400

    function.js
    debugging callbacks

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,10 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-function consume(param1, param2, cb) {
-  return cb(param1 + param2);
-}
-
+let consume = function(param1, param2, calc) {
+  return calc(param1, param2);
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -18,17 +17,17 @@ function consume(param1, param2, cb) {
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
-function add(num1, num2, cb) {
-  let sum = num1 + num2;
-}
+function add(num1, num2) {
+  return num1 + num2;
+};
 
 function multiply(x, y) {
   return x * y;
-}
+};
 
 function greeting(firstName, lastName) {
   return `Hello ${firstName} ${lastName}, nice to meet you!`;
-}
+};
 
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
