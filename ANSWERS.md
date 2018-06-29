6e2ac05b33f96e603b7d7ecbab3adb9b2567e602
Katherine Gilligan <katia.gilligan88@gmail.com>
2018-06-29 09:22:01 -0600

    finished functions.js

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,6 +1,16 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 1. Describe the biggest difference between `.forEach` & `.map`.
+
+    ForEach iterates through a array but does not return any values. With a forEach function, values need to be pushed into an existing array or changed in the existing array. The higher order function, map, returns a new array of the same size as the original array. 
+
 2. What is the difference between a function and a method?
+
+    A method is a type of function that can be called from a key inside of an object. Methods are found inside of objects and classes.
+
 3. What is closure?
+
+
 4. Describe the four rules of the 'this' keyword.
 5. Why do we need super() in an extended class?
+
+
--- challenges/functions.js
+++ challenges/functions.js
@@ -7,26 +7,41 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(param1, param2, cb){
+  return cb(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(param1, param2){
+  return param1 + param2;
+}
 
+function multiply(param1, param2){
+  return param1 * param2;
+}
+
+function greeting(first, last){
+  return `Hello ${first} ${last}, nice to meet you!`
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation: The example is a closure because the internal function(s) have access to the outer function's and global variables.
 
 // Question 2: Given the example below, what scope is the external variable in?
+
+//Explanation: The external variable is in the global scope. 
 
 let external = "I'm outside!";
 
