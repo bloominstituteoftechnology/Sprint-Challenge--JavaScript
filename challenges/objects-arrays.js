c9231c6016659945ada9a0c3cf07c0daabe4f7e4
johnoro <jjcummins102@gmail.com>
2018-06-29 15:24:12 -0400

    Fixed arrays in objects-arrays.js
    I used array methods where we weren't supposed to; my bad!

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -69,8 +69,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = graduates.map(grad => grad.university).sort();
-console.log(universities);
+const universities = [];
+for (let grad in graduates) {
+  universities.push(graduates[grad].university);
+}
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -78,16 +81,21 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = graduates.map(grad => `${grad.first_name} ${grad.email}`);
+const contactInfo = [];
+for (let grad in graduates) {
+  contactInfo.push(`${graduates[grad].first_name} ${graduates[grad].email}`);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = graduates.filter(grad => {
-  if(/Uni/.test(grad.university)) { // returns array of grad. objects with 'Uni' in their university's name
-    return grad.university;
+const uni = [];
+for(let grad in graduates) {
+  if(/Uni/.test(graduates[grad].university)) {
+    uni.push(graduates[grad].university);
   }
-}).map(grad => grad.university); // returns array of universities only
+}
+
 console.log(uni);
 
 
