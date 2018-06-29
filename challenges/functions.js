a7f906a9aac150f0e40a7532df1ad85c1dfa0480
francistse23 <wtse.ucsd@gmail.com>
2018-06-29 09:49:07 -0700

    Stuck on callbacks.js. Completed prototypes.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,13 +6,27 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(any1, any2, cb){
+  return cb(any1, any2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+
+function add(x,y){
+  return x + y;
+}
+
+function multiply(x,y){
+  return x * y ;
+}
+
+function greeting(firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
 
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
@@ -24,9 +38,11 @@
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation: The variable internal is saved in the lexical scope. When nestedFunction is called, it will search upwards for a variable that matches the console.log, which is internal. Upon finding the data, it will be stored after the function is called.
 
-// Question 2: Given the example below, what scope is the external variable in?
+// Question 2: Given the example below, what scope is the external variable in? 
+
+//Global Scope
 
 let external = "I'm outside!";
 
