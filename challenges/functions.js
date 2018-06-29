edf4b43184e8d0ed1357f6748dbde23ea9f1c68c
farste <sfargali@gmail.com>
2018-06-29 15:43:19 -0400

    Stephen Fargali
    Base objectives really complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,31 +6,39 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-function hey(name) { return 'Hey ' + name;}
 
-function callyou(name) {
-  callback(name);
+
+function consume(a, b, callback){
+  return callback(a, b)
 }
-
-console.log(callyou('daug'))
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(a,b){
+  return a+b;
+}
 
+let multiply = function(a, b) {
+  return a * b;
+}
+
+function greeting(a , b) {
+  return 'Hello ' + a + " " + b + ", nice to meet you!"
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2,2,add)); // 4
+ console.log(consume(10,16,multiply)); // 160
+ console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation: It accesses a variable outside of the function's local scope
 
 // Question 2: Given the example below, what scope is the external variable in?
 
