251caaee209dff380b6381d03626daf9eb8fcaee
Anthony <anthony@Anthonys-MacBook-Pro.local>
2018-07-27 16:03:28 -0700

    fixed map

--- src/challenges.js
+++ src/challenges.js
@@ -5,7 +5,7 @@ const each = (elements, cb) => {
   // Iterates over a list of elements, yielding each in turn to the `cb` function.
   // This only needs to work with arrays.
   for (let i = 0; i < elements.length; i++) {
-    elements[i] = cb(elements[i]);
+    elements[i] = cb(elements[i], i);
   }
   return elements;
 };
