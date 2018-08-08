57ba6b238e2706fa4daa48d026b319892aa8b305
Tramane Hall <tramanedhall@gmail.com>
2018-08-08 17:14:21 -0400

    commented the functions.js file

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,7 +7,7 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-consume = (param1, param2, callback) => {return callback(param1, param2);}
+consume = (param1, param2, callback) => {return callback(param1, param2);} //the consume function takes three argument, the third of which is a callback function
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -15,30 +15,31 @@ consume = (param1, param2, callback) => {return callback(param1, param2);}
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-add = (num1, num2) => {return num1 + num2;}
-multiply = (num1, num2) => {return num1 * num2;}
-greeting = (firstName, lastName) => {return `Hello ${firstName} ${lastName}, nice to meet you!`;}
+add = (num1, num2) => {return num1 + num2;} //the add function passes in two parameters num1 and num2 and returns the sum of the two parameters
+multiply = (num1, num2) => {return num1 * num2;} //the multiply function passes two parameters and returns the product of those two parameters
+greeting = (firstName, lastName) => {return `Hello ${firstName} ${lastName}, nice to meet you!`;} //the greeting function passes two parameters and returns the parameters as first and last name in template string format
 
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-consume(2,2,add); // 4
-consume(10,16,multiply); // 160
-consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4 //4 is returned due to adding 2 and 2 as the arguments and invoking the add callback function
+consume(10,16,multiply); // 160 // 160 is returned by multiplying 10 and 16 as the arguments and invoking the multiply callback function
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you! //the greeting is returned by passing the two strings and calling the greeting callback function
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
 // Explanation: 
-//The example below is a closure because the external is outside the scope of myFunction but is being called and logged from inside of myFunction.
+//The example below is a closure because the external variable is outside the scope of myFunction but is being called and logged from inside of myFunction.
+
 // Question 2: Given the example below, what scope is the external variable in?
 //Global Scope
 
-let external = "I'm outside!";
+let external = "I'm outside!"; //global variable
 
 function myFunction() {
   let internal = "Hello! I'm inside the function";
-  console.log(external);
+  console.log(external); //this is a closure because the external variable is called from within the scope of the internal function
 
   function nestedFunction() {
     console.log(internal);
