cb81a74fcb8bdc759c5f0b7e21260268aec19d0e
Kevin Sooter <kevin.m.sooter@gmail.com>
2018-08-10 11:13:31 -0500

    Completed Array Methods(task 3)

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -216,9 +216,13 @@ zooAnimals = [
 ];
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
-const lowerCase = [];
-console.log(lowerCase);
+const lowerCase = zooAnimals.map(element => {
+  return element.animal_name.toLowerCase();
+});
+console.log(lowerCase.sort());
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
-const populationTotal = [];
+const populationTotal = zooAnimals.reduce((acc, pop) => {
+  return (acc += pop.population);
+}, 0);
 console.log(populationTotal);
