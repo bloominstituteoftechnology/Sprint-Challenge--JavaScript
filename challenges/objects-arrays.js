7ef5abb8cacbed3e6c79738ac91687b9a1254ba2
Ray Lomeli <raylom96@gmail.com>
2018-08-10 14:36:24 -0700

    methods complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -136,11 +136,11 @@ const graduates = [{
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-for (var i = 0; i <= university.length; i++) {
+for (let i = 0; i < graduates.length; i++) {
   universities.push(graduates[i].university)
-  return universities
+
 }
-console.log(universities)
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -149,11 +149,21 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push(graduates[i].first_name + " " + " " + graduates[i].email)
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (let i = 0; i < graduates.length; i++) {
+  if (graduates[i].university.includes("Uni")) {
+    uni.push(graduates[i].university)
+  } else {
+
+  }
+}
 console.log(uni);
 
 
@@ -224,9 +234,15 @@ zooAnimals = [{
 ];
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
-const lowerCase = [];
+
+let lowerCase = zooAnimals.map((animalName) => {
+  return animalName.animal_name.toLowerCase();
+});
 console.log(lowerCase);
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
-const populationTotal = [];
+let populationTotal = zooAnimals.reduce((animal, totalPopulation) => {
+  return animal + totalPopulation.population;
+}, 0);
+
 console.log(populationTotal);
