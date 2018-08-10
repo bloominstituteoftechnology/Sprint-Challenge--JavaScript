e2292af08f9438aa2cbd94c1c7d1a2a4763ef72c
Emily McClanahan <emily.f.mcclanahan@gmail.com>
2018-08-10 12:30:49 -0500

    classes complete. started on stretch.

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,29 @@
 // Copy and paste your prototype in here and refactor into class syntax.
+class CuboidMaker{
+  constructor(props) {
+  this.length = props.length;
+  this.width = props.width;
+  this.height = props.height;
+  }
+  volume() {
+    return (this.length * this.width * this.height);
+  }
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
+  
+}
 
+class CubeMaker extends CuboidMaker{
+  constructor(property);
+  super(property);
+}
+
+const cuboid = new CuboidMaker({length: 4, width: 5, height: 5});
+const cube = new CubeMaker({})
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 /* Stretch Task:
 
