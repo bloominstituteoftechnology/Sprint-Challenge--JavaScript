e614dcbc44b65f18e8ca5310602ba3df8f423bc9
MikeRGarrity <mikergarrity@gmail.com>
2018-08-10 11:13:28 -0500

    finished with main part of sprint, working on stretch

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,25 +6,40 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(x, y, cb) {
+  return cb(x, y);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(x, y) {
+  console.log (x + y);
+  // return(x + y);
+};
 
+function multiply(x, y) {
+  console.log (x * y);
+  // return(x * y);
+} 
+
+function greeting(first_name, last_name) {
+  // return `Hello ${first_name} ${last_name}, nice to meet you!`;
+  console.log(`Hello ${first_name} ${last_name}, nice to meet you!`);
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: `nestedfunction()` can access the variable `internal` because it can search outside of it's local scope to find variables it needs.  In this case `nestedfunction()` looks up into the function that it's nested in to find the `internal` variable. Javascript has the ability to look outside of the local scope and all the way into the global scope to find the variables that it may need.
 
 
 const external = "I'm outside the function";
