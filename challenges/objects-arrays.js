1db216fdf72cf143afb172b3ca0f9eddeb20e03f
Luisan <luisangelsc@gmail.com>
2018-08-10 09:48:09 -0600

    objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -1,30 +1,53 @@
 // ==== Objects ====
 
-/* 
-  Given the following information about dinosaurs, create 3 objects: 
-  Use this pattern to create your objects: 
+/*
+  Given the following information about dinosaurs, create 3 objects:
+  Use this pattern to create your objects:
   object name, diet, weight, length, period
 */
 
+class Dinosaour {
+  constructor(object) {
+    this.name = object.name
+    this.diet = object.diet
+    this.weight = object.weight
+    this.length = object.length
+    this.period = object.period
+  }
+  roar() {
+    return "RAWERSRARARWERSARARARRRR!"
+  }
+}
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
+
+tyr_obj = {'name': 'tyrannosaurus', 'diet': 'carnivorous', 'weight': '7000kg', 'length': '12m', 'period': 'Late Cretaceious'}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+vel_obj = {'name': 'stegosaurus', 'diet': 'herbivorous', 'weight': '2000kg', 'length': '9m', 'period': 'Late Jurassic'}
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+
+steg_obj = {'name': 'velociraptor', 'diet': 'carnivorous', 'weight': '15kg', 'length': '1.8m', 'period': 'Late Cretaceious'}
 
 // Using your dinosaur objects, log answers to these questions:
 
-// How much did tyrannosaurus weigh?
-console.log(tyrannosaurus.weight);
-// What was the diet of a velociraptor?
-console.log(velociraptor.diet);
-// How long was a stegosaurus?
-console.log(stegosaurus.length);
-// What time period did tyrannosaurus live in?
-console.log(tyrannosaurus.period);
+const tyrannosaurus = new Dinosaour(tyr_obj)
+const velociraptor = new Dinosaour(vel_obj)
+const stegosaurus = new Dinosaour(steg_obj)
 
-// Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log(tyrannosaurus.roar());
+// How much did tyrannosaurus weigh?
+// console.log(tyrannosaurus.weight);
+// // What was the diet of a velociraptor?
+// console.log(velociraptor.diet);
+// // How long was a stegosaurus?
+// console.log(stegosaurus.length);
+// // What time period did tyrannosaurus live in?
+// console.log(tyrannosaurus.period);
+//
+// // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
+// console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -42,15 +65,20 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
+/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for (i=0; i<graduates.length; i++){
+  universities.push(graduates[i].university)
+  universities.sort()
+}
+
 console.log(universities)
 
-/* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
+/* Request 2: Create a new array called contactInfo that contains both first name and email of each student.
 
-The resulting contact information should have a space between the first name and the email information like this: 
+The resulting contact information should have a space between the first name and the email information like this:
 Name email@example.com
 
 Log the result of your new array. */
@@ -80,7 +108,7 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 const lowerCase = [];
-console.log(lowerCase); 
+console.log(lowerCase);
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
 const populationTotal = [];
