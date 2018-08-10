e0cc698ffe60f2af805923723581f36feab4ec13
IlanL75 <anatoly.leytman@gmail.com>
2018-08-10 09:28:47 -0700

    Adding functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(parameter1, parameter2, callback) {
+  return callback(parameter1, parameter2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +18,24 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(parameter1, parameter2) {
+  return parameter1 + parameter2;
+}
+
+function multiply(parameter1, parameter2) {
+  return parameter1 * parameter2;
+}
+
+function greeting(parameter1, parameter2) {
+  return `Hello ${parameter1} ${parameter2}, nice to meet you!`
+}
+
+
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,7 +43,16 @@
 
 // Explanation: 
 
+nestedFunction defined inside myFunction.
+nestedFunction(inner function) has access to the variable let internal in the
+myFunction scope(outer function). 
+
+
 // Question 2: Given the example below, what scope is the external variable in?
+
+global(Window) scope.
+
+
 
 let external = "I'm outside!";
 
