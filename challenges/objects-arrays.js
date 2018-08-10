44ca775806a8e8be8e1cce5a6b3d3633d76861cc
Melanie Barth <mbarth1967@gmail.com>
2018-08-10 11:50:01 -0600

    objects-array.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,10 +7,31 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
+let tyrannosaurus = new Object();
+  tyrannosaurus.name = "tyrannosaurus";
+  tyrannosaurus.diet = "carnivorous";
+  tyrannosaurus.weight = "7000kg";
+  tyrannosaurus.length = "12m";
+  tyrannosaurus.period = "Late Cretaceious";
 
+function roar() {
+  return "RAWERSRARARWERSARARARRRR!";
+} 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+let stegosaurus = new Object();
+  stegosaurus.name = "stegosaurus";
+  stegosaurus.diet = "herbivorous";
+  stegosaurus.weight = "2000kg";
+  stegosaurus.length = "9m";
+  stegosaurus.period = "Late Jurassic"
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+let velociraptor = new Object();
+  velociraptor.name = "velociraptor";
+  velociraptor.diet = "carnivorous";
+  velociraptor.weight = "15kg";
+  velociraptor.length = "1.8m";
+  velociraptor.period = "Late Cretaceious"
 
 // Using your dinosaur objects, log answers to these questions:
 
@@ -24,7 +45,7 @@ console.log(stegosaurus.length);
 console.log(tyrannosaurus.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log(tyrannosaurus.roar());
+// console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -42,11 +63,14 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
-
+/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array. 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+function newArray() {
+  universities = graduates.slice(2,3);
+  
+};
+console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -55,6 +79,9 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+function newArray2() {
+  contactInfo = graduates.slice(0);
+}
 console.log(contactInfo);
 
 
