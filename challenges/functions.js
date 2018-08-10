250b37e41b9f2d8bc7189397add063353652d761
Ray Lomeli <raylom96@gmail.com>
2018-08-10 10:44:18 -0700

    functions complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,23 +1,39 @@
 // ==== Callbacks ====  
 
 /* Step 1: Create a function that accepts a callback
-  * Create a function named consume that can take 3 parameters.
-  * The first two parameteres can accept any argument
-  * The last parameter accepts a callback 
-  * In the body of the function return the callback with the two parameters that you created
-*/
+ * Create a function named consume that can take 3 parameters.
+ * The first two parameteres can accept any argument
+ * The last parameter accepts a callback 
+ * In the body of the function return the callback with the two parameters that you created
+ */
+function consume(x, y, cb) {
 
+  return cb(x + y);
+}
 
 /* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+ * Create a function named add that returns the sum of two numbers
+ * Create a function named multiply that returns the product of two numbers 
+ * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+ */
 
+function add(x, y, cb) {
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x + y);
+}
 
+function multiply(firstName, lastName, cb) {
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(x * y);
+}
+
+function greeting(firstName, lastName, cb) {
+  // sumNums adds two numbers (x, y) and passes the result to the callback.
+  cb(`Hello ${firstName} ${lastName}, nice to meet you!`);
+}
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
+consume(2, 2, add); // 4
+consume(10, 16, multiply); // 160
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
@@ -25,7 +41,7 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+// Closures have access to the variables in the outer function
 
 const external = "I'm outside the function";
 
