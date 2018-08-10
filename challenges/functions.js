42c87e0d8fbcfde63c090b8e202e5683ba3bfec0
Anthony Greb <agreb255@gmail.com>
2018-08-10 13:01:37 -0400

    committing functions and prototypes

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,18 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(a, b, cb) {
+  return cb(a, b);
+}
+function add(x, y) {
+  return x + y;
+}
+function multiply(x, y) {
+  return x * y;
+}
+function greeting(first, last) {
+  return `Hello ${first} ${last}, nice to meet you!`;
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -16,15 +27,17 @@
 
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation:Because a nested object can access anything inside of its block. 
+// Function nestedFunction() can access it because it is also inside the scope of function myFunction().
+// I hope I explained that clearly enough. lol
 
 
 const external = "I'm outside the function";
@@ -35,7 +48,8 @@ function myFunction() {
 
   function nestedFunction() {
     console.log(internal);
-  };
+};
+  console.log(nestedInternal);
   nestedFunction();
 }
 myFunction();
--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,7 +5,19 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
-
+class CuboidMaker {
+  constructor(details) {
+    this.length = details.length;
+    this.width = details.width;
+    this.height = details.height;
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
+}
 
 /* == Step 2: Volume Method ==
   Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
@@ -25,9 +37,13 @@
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
-
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+});
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 
