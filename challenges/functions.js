28de72683522fd605186610b1897621cec5291f4
Joseph Bradley <josephbradley@Josephs-MacBook-Air.local>
2018-08-10 09:51:35 -0700

    added to functions....finished cbs

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(a, b, cb) {
+  return cb(a,b)
+};
+///Kept getting an error
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +18,23 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a,b) {
+  return a + b;
+}
+
+function multiply(a,b) {
+  return a * b;
+}
+
+function greeting (firstname, lastname) {
+  return `Hello ${firstname} ${lastname}, nice to meet you`;
+}
+
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add));
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
