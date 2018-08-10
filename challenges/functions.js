737b292d7e104bb76749d5a94efa39917444f5c2
mparedes003 <michelle.g.paredes@gmail.com>
2018-08-10 12:36:19 -0700

    Reworked functions.js to be called by console.log(arr1, arr2, cb)

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,7 +7,7 @@
   * In the body of the function return the callback with the two parameters that you created
 */
  function consume(param1, param2, cb) {
-   cb(param1, param2);
+   console.log(cb(param1, param2))
 }
 
 consume('burgers', 'pizza', function(param1, param2) {
@@ -20,36 +20,22 @@ consume('burgers', 'pizza', function(param1, param2) {
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-function add(param1, param2, cb) {
-  cb(param1 + param2);
+function add(param1, param2) {
+  return param1 + param2;
 }
 
-add(300, 564, function(total) {
-  console.log(total);
-});
-
-function multiply(param1, param2, cb) {
-  cb(param1 * param2);
+function multiply(param1, param2) {
+  return param1 * param2;
 }
 
-multiply(300, 564, function(total) {
-  console.log(total);
-});
-
-function greeting(param1, param2, cb) {  //GO BACK TO THIS
-  console.log(cb(param1 + param2));
+function greeting(first_name, last_name) {
+  console.log('Hello ' + first_name + ' ' + last_name + ', nice to meet you!');
 }
-
-greeting('Michelle', 'Paredes',function(first_name, last_name) {
- console.log('Hello '+ first_name + ' ' + last_name + ', nice to meet you!');
-});
-// greeting(name);
-
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
