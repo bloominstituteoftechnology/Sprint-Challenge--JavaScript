9a8649d7317e4066c117d6a852e42e5909bf54a0
Vera Simon <ghjkl>
2018-08-10 10:42:06 -0700

    classes.js
    Finished stretch task
    Added isCube method to CubeMaker constructor to ensure that object being passed is in fact a cube
    Attempted to check that object type being passed was a cube using instanceof or typeof, but was unsuccessful

--- challenges/classes.js
+++ challenges/classes.js
@@ -32,7 +32,27 @@ class CubeMaker extends CuboidMaker {
   constructor(cubeAttr) {
     super(cubeAttr);
   }
-  
+  isCube() {
+    if( (this.length === this.width) && (this.length === this.height) ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+  cubeVolume() {
+    if(this.isCube()) {
+      return this.length**3;
+    } else {
+      return 'Not a cube';
+    }
+  }
+  cubeSurfaceArea() {
+    if(this.isCube()) {
+      return 6 * this.length**2;
+    } else {
+      return 'Not a cube';
+    }
+  }
 }
 
 // ----------Objects---------- //
@@ -40,6 +60,18 @@ const cuboid = new CuboidMaker({
   'length': 4,
   'width': 5,
   'height': 5
+});
+
+const cube = new CubeMaker({
+  'length': 3,
+  'width': 3,
+  'height': 3
+});
+
+const notACube = new CubeMaker({
+  'length': 3,
+  'width': 4,
+  'height': 3
 });
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
@@ -51,3 +83,8 @@ console.log(cuboid.surfaceArea()); // 130
   Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods as well.  Create a new cube object and log out the results of your new cube.
 
 */
+console.log(cube.cubeVolume());
+console.log(cube.cubeSurfaceArea());
+
+console.log(notACube.cubeVolume());
+console.log(notACube.cubeSurfaceArea());
