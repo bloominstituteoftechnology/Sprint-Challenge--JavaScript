f075f67aae67edbbbc24c5af2015530210adb04f
Joannah Laluces <joannahlaluces@Joannahs-MacBook-Air.local>
2018-08-10 09:07:16 -0700

    Finished functions and object-arrays.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(arg1, arg2, cb) {
+  return cb(arg1, arg2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,17 +18,31 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(arg1, arg2) {
+  return arg1 + arg2;
+}
+
+function multiply(arg1, arg2) {
+  return arg1 * arg2;
+}
+
+function greeting(arg1, arg2) {
+  return `Hello ${arg1} ${arg2}, nice to meet you!`;
+}
+
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+
+// The nested function can access myFunction's 'internal' variable due to the way lexicle scoping works. To put it the easy way, nested functions have full access to variables declared in their OUTER SCOPE. Any functions nested inside the outer function can use their parent function's variables. Thus the nestedFunction() is looking 'up' at its parent and finding the internal variable to use it in the console.log statement.
 
 
 const external = "I'm outside the function";
