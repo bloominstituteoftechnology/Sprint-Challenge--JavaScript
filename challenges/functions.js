a0137634d4e7b0bb6c54ef20d6dd201a6aed6e77
Jordan Marsaw <37964165+blokboy@users.noreply.github.com>
2018-08-10 11:40:01 -0500

    issa wrap

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,30 +1,47 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====
 
 /* Step 1: Create a function that accepts a callback
   * Create a function named consume that can take 3 parameters.
   * The first two parameteres can accept any argument
-  * The last parameter accepts a callback 
+  * The last parameter accepts a callback
   * In the body of the function return the callback with the two parameters that you created
 */
+function callbackFunc(arg1, arg2, cb) {
+  return cb[arg1, arg2];
+}
 
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named multiply that returns the product of two numbers
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(x, y) {
+  return x + y;
+}
 
+function multiply(x, y) {
+  return x * y;
+}
+
+function greeting(first, last) {
+  return(`Hello ${first}${last}, nice to meet you!`);
+}
+
+function consume(a, b, cb) {
+  return cb(a, b);
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
-// ==== Closures ==== 
+// ==== Closures ====
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: It's within the block scope of the function. The outside function passes down everything to inner functions.
 
 
 const external = "I'm outside the function";
