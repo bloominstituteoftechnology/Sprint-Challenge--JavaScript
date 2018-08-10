242b884a838e891f01ebae7e56964d4b5d2a3a9c
Andrew <ghobrial.andrew@gmail.com>
2018-08-10 13:06:29 -0400

    functions.js complete but needs some revisions

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,14 +6,28 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(param1, param2, callback){
+  return callback(`${param1} and ${param2}!`);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add (param1, param2, callback){
+  return callback (param1 + param2);
+}
 
+add(2, 3);
+
+function multiply (param1, param2, callback){
+  return callback (param1 * param2);
+}
+
+function greeting (first_name, last_name, callback){
+  return callback (`Hello ${first_name} ${last_name}, nice to meet you!`);
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
 //consume(2,2,add); // 4
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -73,17 +73,34 @@ universities.sort();
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
-
 The resulting contact information should have a space between the first name and the email information like this: 
+
 Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+for (i = 0; i < graduates.length; i++) {
+  contactInfo.push(
+    {
+      name: graduates[i].first_name,
+      email: graduates[i].email
+    }
+  );
+}
+
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+
+for (i = 0; i < graduates.length; i++) {
+  if (graduates[i].university.includes("uni")){
+    uni.push(graduates[i].university);
+  }
+}
+
 console.log(uni);
 
 
@@ -104,6 +121,7 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 const lowerCase = [];
+let lowerCase = zooAnimals.map(function(x){ return x.toLowerCase() })
 console.log(lowerCase); 
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
