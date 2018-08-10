cb15a71917c57700f1a2bc8875c3134951cecbe0
easyra <ezra.rr.davis@gmail.com>
2018-08-10 10:41:54 -0700

    update 3 final

--- challenges/classes.js
+++ challenges/classes.js
@@ -23,9 +23,35 @@ const cuboid = new CuboidMaker ({
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
-
 /* Stretch Task:
 
-  Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods as well.  Create a new cube object and log out the results of your new cube.
-
+  Extend the base class CuboidMaker with a sub class called CubeMaker.
+  Find out the formulas for volume and surface area for cubes and create those methods as well.  
+  Create a new cube object and log out the results of your new cube.
 */
+
+
+class CubeMaker extends CuboidMaker {
+  constructor(attributes) {
+    super(attributes) 
+    this.side = attributes.side
+    this.width - attributes.side
+    this.height = attributes.side
+    this.length = attributes.side
+
+     console.log(this);
+
+  }
+  volume(){         
+    return Math.pow( this.side,3)
+  }
+  surfaceArea(){
+    return 6*Math.pow(this.side,2)
+  }
+}
+
+const cube = new CubeMaker ({
+  side: 5,  
+})
+console.log(cube.volume());
+console.log(cube.surfaceArea());
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -70,10 +70,12 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-graduates.forEach(element => {
-  universities.push(element.university)
-  universities.sort();
-});
+
+for (let i = 0; i < graduates.length; i++) {
+  universities.push(graduates[i].university)  
+}
+universities.sort();
+
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -82,15 +84,25 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
+const contactInfo = []
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push( `${graduates[i].first_name} ${graduates[i].email}`)
+  
+}
 
-const contactInfo = graduates.map(element => element = `${element.first_name} ${element.email}`);
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 
+const uni =[];
 
-const uni = graduates.filter(element => element['university'].indexOf('Uni') != -1);
+for (let i = 0; i < graduates.length; i++) {
+  if(graduates[i]['university'].indexOf('Uni') != -1){
+    uni.push(graduates[i]['university'])
+  }  
+}
+
 console.log(uni);
 
 
