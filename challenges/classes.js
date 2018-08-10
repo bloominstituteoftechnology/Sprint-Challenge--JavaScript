8248d12bc3a099fe1099c0f5562e20f48e2d9dda
lndubose <lndubose13@gmail.com>
2018-08-10 11:14:18 -0500

    Task 5: Protoypes complete

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,29 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
+function CuboidMaker(attributes) {
+  this.length = attributes.length;
+  this.width = attributes.width;
+  this.height = attributes.height;
+}
+
+CuboidMaker.prototype.volume = function() {
+  return this.length * this.width * this.height;
+}
+
+
+CuboidMaker.prototype.surfaceArea = function() {
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+}
+
+
+const cuboid = new CuboidMaker({
+  'length': 4,
+  'width': 5,
+  'height': 5
+});
+
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
