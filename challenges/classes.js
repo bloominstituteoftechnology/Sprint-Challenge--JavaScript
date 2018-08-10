90888f95dc5fcc9c720f194853085bd645a9558a
Vera Simon <ghjkl>
2018-08-10 10:09:51 -0700

    classes.js
    Finished CuboidMaker conversion
    Starting CubeMaker extension

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,50 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
+// function CuboidMaker(cuboidAttr) {
+//   this.length = cuboidAttr.length;
+//   this.width = cuboidAttr.width;
+//   this.height = cuboidAttr.height;
+// }
+
+// CuboidMaker.prototype.volume = function() {
+//   return this.length * this.width * this.height;
+// }
+
+// CuboidMaker.prototype.surfaceArea = function() {
+//   return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+// }
+
+class CuboidMaker {
+  constructor(cuboidAttr) {
+    this.length = cuboidAttr.length;
+    this.width = cuboidAttr.width;
+    this.height = cuboidAttr.height;
+  }
+  volume() {
+    return this.length * this.width * this.height;
+  }
+  surfaceArea() {
+    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+  }
+}
+
+class CubeMaker extends CuboidMaker {
+  constructor(cubeAttr) {
+    super(cubeAttr);
+  }
+  
+}
+
+// ----------Objects---------- //
+const cuboid = new CuboidMaker({
+  'length': 4,
+  'width': 5,
+  'height': 5
+});
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 /* Stretch Task:
 
