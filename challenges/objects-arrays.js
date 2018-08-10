7b583e721740372872497167eb9b7f2486138786
m-j-hacker <mjhacker@gmail.com>
2018-08-10 11:10:13 -0500

    Completed request 3 of objects-arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -95,6 +95,12 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+
+for (let i = 0; i < graduates.length; i++) {
+  if (graduates[i]["university"].includes('Uni')) {
+    uni[i] = graduates[i]["university"];
+  }
+}
 console.log(uni);
 
 
