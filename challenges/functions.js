1ac1319d834157dec8dbeba22e3ad8105743c8b4
Joseph Chretien-Golden <joechre@gmail.com>
2018-08-10 13:02:18 -0500

    new

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,11 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
+function consume(a, b, cb) {
+  
+  return cb(a-b);
+}
+
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,18 +18,30 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(num1, num2, cb) {
+ return cb(num1 + num2);
+}
+
+function multiply(num1, num2, cb) {
+  return cb(num1 * num2);
+ }
+
+ function greeting(firstName, lastName, cb) {
+  return cb(`Hello ${firstName} ${lastName}, nice to meet you!`);
+ }
 
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: because the closure structure allows us to have access 
+//to the outter function's scope from within the nestedfunction.
 
 
 const external = "I'm outside the function";
