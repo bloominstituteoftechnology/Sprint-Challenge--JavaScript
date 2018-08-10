25b99ce5e540c260bf9b3b520a308cbfaa0424d9
jeanfern5 <jeanfern555@gmail.com>
2018-08-10 10:57:12 -0500

    Completed array methods section in the objects-arrays.js file, this file has been completed.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -70,7 +70,7 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-let universities = [];
+const universities = [];
   for (i=0; i<graduates.length; i++) {
     universities.push(graduates[i]['university']);
   }
@@ -81,35 +81,46 @@ console.log(universities.sort());
 The resulting contact information should have a space between the first name and the email information like this: 
 Name email@example.com
 
-// Log the result of your new array. */
-// const contactInfo = [];
-// console.log(contactInfo);
+Log the result of your new array. */
+const contactInfo = [];
+for (i=0; i<graduates.length; i++) {
+  contactInfo.push(`${graduates[i]['first_name']} ${graduates[i]['email']}`)
+}
+console.log(contactInfo);
 
 
-// /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-// const uni = [];
-// console.log(uni);
+/* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+const uni = [];
+for (i=0; i<graduates.length; i++) {
+  if (graduates[i]['university'].includes('Uni')){
+    uni.push(graduates[i]['university']);
+  }
+}
+console.log(uni);
 
 
-// // ==== Array Methods ====
+// ==== Array Methods ====
 
-// // Given this zoo data from around the United States, follow the instructions below
+// Given this zoo data from around the United States, follow the instructions below
 
-// zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":"Canis aureus","state":"Kentucky"},
-// {"animal_name":"Screamer, southern","population":1,"scientific_name":"Chauna torquata","state":"Alabama"},
-// {"animal_name":"White spoonbill","population":8,"scientific_name":"Platalea leucordia","state":"Georgia"},
-// {"animal_name":"White-cheeked pintail","population":1,"scientific_name":"Anas bahamensis","state":"Oregon"},
-// {"animal_name":"Black-backed jackal","population":2,"scientific_name":"Canis mesomelas","state":"Washington"},
-// {"animal_name":"Brolga crane","population":9,"scientific_name":"Grus rubicundus","state":"New Mexico"},
-// {"animal_name":"Common melba finch","population":5,"scientific_name":"Pytilia melba","state":"Pennsylvania"},
-// {"animal_name":"Pampa gray fox","population":10,"scientific_name":"Pseudalopex gymnocercus","state":"Connecticut"},
-// {"animal_name":"Hawk-eagle, crowned","population":10,"scientific_name":"Spizaetus coronatus","state":"Florida"},
-// {"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
+zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":"Canis aureus","state":"Kentucky"},
+{"animal_name":"Screamer, southern","population":1,"scientific_name":"Chauna torquata","state":"Alabama"},
+{"animal_name":"White spoonbill","population":8,"scientific_name":"Platalea leucordia","state":"Georgia"},
+{"animal_name":"White-cheeked pintail","population":1,"scientific_name":"Anas bahamensis","state":"Oregon"},
+{"animal_name":"Black-backed jackal","population":2,"scientific_name":"Canis mesomelas","state":"Washington"},
+{"animal_name":"Brolga crane","population":9,"scientific_name":"Grus rubicundus","state":"New Mexico"},
+{"animal_name":"Common melba finch","population":5,"scientific_name":"Pytilia melba","state":"Pennsylvania"},
+{"animal_name":"Pampa gray fox","population":10,"scientific_name":"Pseudalopex gymnocercus","state":"Connecticut"},
+{"animal_name":"Hawk-eagle, crowned","population":10,"scientific_name":"Spizaetus coronatus","state":"Florida"},
+{"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
 
-// // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
-// const lowerCase = [];
-// console.log(lowerCase); 
+// The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the result.
+const lowerCase = [];
+  zooAnimals.map((animal)=>{
+    return(lowerCase.push(animal["animal_name"].toLowerCase()));
+  });
+console.log(lowerCase); 
 
-// // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
-// const populationTotal = [];
-// console.log(populationTotal);
+// The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
+const populationTotal = [];
+console.log(populationTotal);
