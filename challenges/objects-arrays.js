bd834179577c1f86d7b931f60bd01146ebe3550f
Michael Agard <mdagard@gmail.com>
2018-08-10 12:19:15 -0400

    finished request 3

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -74,13 +74,17 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 Log the result of your new array. */
 const contactInfo = [];
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push(graduates[i]['first_name'] + ' ' + graduates[i]['email'])
+}
 console.log(contactInfo);
-
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (let i = 0; i < graduates.length; i++) {
+  if (graduates[i]['university'].includes('Uni')) {uni.push(graduates[i]['university'])}
+}
 console.log(uni);
-
 
 // ==== Array Methods ====
 
