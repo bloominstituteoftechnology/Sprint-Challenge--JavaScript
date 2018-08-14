7d34f0cfe26a2e97cf360b9a6b17d84bdc06e515
Marious Davis <mlancelot88@gmail.com>
2018-08-13 22:44:59 -0500

    converted prototypes from Task 5 into class syntax. Finished with Task 6

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,26 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMaker {
+  constructor(properties) {
+    this.length = properties.length;
+    this.width = properties.width;
+    this.height = properties.height;
+}
+volume() {
+  return `Volume ${this.length * this.width * this.height}`;
+}
+surfaceArea() {
+  return `Surface Area: ${2 * (this.length * this.width + this.length * this.height + this.width * this.height)}`;
+  }
+}
+
+const cuboidObj = new CuboidMaker({
+  'length': 4,
+  'width': 5,
+  'height': 5,
+  });
+
+  
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
