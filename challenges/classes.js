64e2d3120f4866739cef0312bf3d8a021201cdc3
unknown <jerrardsmith215@gmail.com>
2018-08-22 21:42:41 -0400

    Jerrard Smith Sprint-JavaScript 100% completed

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,43 @@
-// Copy and paste your prototype in here and refactor into class syntax.
+/* Copy and paste your prototype in here and refactor into class syntax.
+function CuboidMaker (cuboid) {
+  this.length=cuboid.length;
+  this.width=cuboid.width;
+  this.height=cuboid.height;
+}
 
-// Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+CuboidMaker.prototype.volume = function () {
+  var vol = (this.height*this.length*this.width);
+  return vol;
+  console.log(vol);
+};
+CuboidMaker.prototype.surfaceArea = function() {
+  var sarea = 2*( (this.length*this.width) + (this.length*this.height) + (this.width * this.height) );
+  return sarea;
+  console.log(sarea);
+}
+*/
+class CuboidMaker {
+  constructor (cuboid) {
+    this.length=cuboid.length;
+    this.width=cuboid.width;
+    this.height=cuboid.height;
+  };
+  volume () {
+    var vol = (this.height*this.length*this.width);
+    return vol;
+    console.log(vol);
+  };
+  surfaceArea () {
+    var sarea = 2*( (this.length*this.width) + (this.length*this.height) + (this.width * this.height) );
+    return sarea;
+    console.log(sarea);
+  };
+}
+
+var cuboid = new CuboidMaker ({length:4, width:5, height:5});
+//Test your volume and surfaceArea methods by uncommenting the logs below:
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 /* Stretch Task:
 
