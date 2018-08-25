f3fd3c816411a8265256ec15b4d50e653c9119dc
Jason Hedrick <jasonleehedrick@gmail.com>
2018-08-25 10:41:19 -0700

    Mostly done now to refactor objects-arrays and get rid of builtins

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,6 +6,34 @@
   object name, diet, weight, length, period
 */
 
+
+const tyrannosaurus = {
+	diet: "carnivorous",
+	weight: "7000kg",
+	length: "12m",
+	period: "Late Cretaceious",
+};
+
+const stegosaurus = {
+	diet: "herbivorous",
+	weight: "2000kg",
+	length: "9m",
+	period: "Late Jurassic",
+};
+
+const velociraptor = {
+	diet: "carnivorous",
+	weight: "15kg", 
+	length: "1.8m",
+	period: "Late Cretaceious",
+};
+
+
+tyrannosaurus.roar = function() {
+	return "RAWERSRARARWERSARARARRRR!";
+}
+
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -42,11 +70,15 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
+/* FIX THIS */ 
+
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities)
+const universities = graduates.map((item) => {
+	return item.university;
+});
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -54,12 +86,18 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map((item) => {
+	return `${item.first_name} ${item.email}`;
+});
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = [];
+const uni = graduates.filter((item) => {
+	if (item.university.includes('Uni')) {
+		return item.university;
+	}
+});
 console.log(uni);
 
 
@@ -79,9 +117,15 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 {"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
-const lowerCase = [];
+const lowerCase = zooAnimals.map((item) => {
+	return item.animal_name.toLowerCase();
+});
+
 console.log(lowerCase); 
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
-const populationTotal = [];
+const populationTotal = zooAnimals.reduce((accumulator, item) => {
+	return accumulator += item.population;
+}, 0);
+
 console.log(populationTotal);
