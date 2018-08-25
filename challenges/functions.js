78042eff62d9284dcf97571c37259c6beeb55534
Joe Mercado <jryel1970@gmail.com>
2018-08-25 19:10:04 -0400

    Completed sprint challenge

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(argument1, argument2, callback){
+  return callback(argument1, argument2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,6 +17,17 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function addition(x,y){
+  return x + y;
+}
+
+function multiplicatio(x,y){
+  return x*y;
+}
+
+function greeting (firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
 //consume(2,2,add); // 4
