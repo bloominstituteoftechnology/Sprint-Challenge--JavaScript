adf3886a47db8d101a5b9d74d4eed180ff5ea02a
hraya <hraya@lssu.edu>
2018-08-25 12:21:29 -0500

    completed functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,24 +7,40 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+let consume = function(a, b, cb){
+  return cb(a,b);
+}
+
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+let add = function(a,b){
+  console.log( a + b);
+}
 
+let multiply =function(a,b){
+  console.log(a * b);
+}
+
+let greeting = function(firstName, lastName, cb){
+  console.log(firstName + " " + lastName + ' nice to meet you!');
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: This is because a function can go outside of its scope to look for a variable that it might need and
+//also internal is also within the scope of myFunction which nestedfunction is inside of. Functions are greedy.
 
 
 const external = "I'm outside the function";
