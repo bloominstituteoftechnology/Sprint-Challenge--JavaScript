7323074539557d03ba8692cb4fdfa88e70e23094
Courtney Buratto <you@example.com>
2018-08-25 11:28:46 -0600

    objects complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,13 +7,33 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
-
+const tyrannosaurus = {
+  objectName: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceious',
+  roar () {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+}
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const stegosaurus = {
+  objectName: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic',
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
-
+const velociraptor = {
+  objectName: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceious',
+}
 // Using your dinosaur objects, log answers to these questions:
-
 // How much did tyrannosaurus weigh?
 console.log(tyrannosaurus.weight);
 // What was the diet of a velociraptor?
