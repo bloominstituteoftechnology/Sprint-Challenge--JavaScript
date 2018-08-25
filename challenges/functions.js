f967a74c8f0f6a04c9ae2f06cc599f30f2979785
Kendra Williams <kmonique.williams@gmail.com>
2018-08-25 13:27:10 -0400

    Task 4: functions.js completed

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume (x, y, cb){
+  console.log(cb(x, y));
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,17 +17,28 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add (num1, num2) {
+  return num1 + num2;
+}
+
+function multiply (num1, num2) {
+  return num1 * num2;
+}
+
+function greeting (first_name, last_name) {
+  return `Hello ${first_name} ${last_name}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: functions are greedy.  Everything inside them are scoped only to that function; however, variables declared outside of a nestest function can be grabbed and pulled into a greedy function.
 
 
 const external = "I'm outside the function";
