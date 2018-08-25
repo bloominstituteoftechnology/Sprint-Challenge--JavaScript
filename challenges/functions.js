1a03991ee1d45247a6de9978fc6fc49a68ac68c5
stevekutz <stkutz@gmail.com>
2018-08-25 12:38:53 -0500

    completed functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,24 +7,43 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume( val1, val2, cb) {
+  return cb(val1, val2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  * Create a function named greeting that accepts a first and last name and
+  *         returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(x, y) {
+  return x + y;
+}
+
+function multiply(x,y) {
+  return x * y;
+}
+
+function greeting(first, last) {
+  return `Hello ${first} ${last}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting);
+ // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+// Closures have access to the local environment in which they are created. The closure can access internal, even after
+// myFunction is completed.
+
 
 
 const external = "I'm outside the function";
