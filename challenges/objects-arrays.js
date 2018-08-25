da3e946250b31aa53acf2f6b5495792681ab5923
Jason Hedrick <jasonleehedrick@gmail.com>
2018-08-25 12:46:33 -0700

    All completed except stretch goals. Will get to those. Working on removing the .sort call

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -73,12 +73,121 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* FIX THIS */ 
 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
-
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
+/* Didn't really need to do all this.. self challenge? */
+let alphabetWeight = {
+	// English alphabet weighting
+	'A': 1,
+	'a': 1,
+	'B': 2,
+	'b': 2,
+	'C': 3,
+	'c': 3,
+	'D': 4,
+	'd': 4,
+	'E': 5,
+	'e': 5,
+	'F': 6,
+	'f': 6,
+	'G': 7,
+	'g': 7,
+	'H': 8,
+	'h': 8,
+	'I': 9,
+	'i': 9,
+	'J': 10,
+	'j': 10,
+	'K': 11,
+	'k': 11,
+	'L': 12,
+	'l': 12,
+	'M': 13,
+	'm': 13,
+	'N': 14,
+	'n': 14,
+	'O': 15,
+	'o': 15,
+	'P': 16,
+	'p': 16,
+	'Q': 17,
+	'q': 17,
+	'R': 18,
+	'R': 18,
+	'S': 19,
+	's': 19,
+	'T': 20,
+	't': 20,
+	'U': 21,
+	'u': 21,
+	'V': 22,
+	'v': 22,
+	'W': 23,
+	'w': 23,
+	'X': 24,
+	'x': 24,
+	'Y': 25,
+	'y': 25,
+	'Z': 26,
+	'z': 26,
+};
+
+/* Now I know how to arrange letters in a string I just have to compare all letters in a string until I have a clear winner */
+const alphabetSoup = function(sortString) {
+	let sortedString = [];
+	for(let i = 0; i < sortString.length; i++) {
+		sortedString.push(sortString[i]);
+		for (let x = 0; x < sortedString.length; x++) {
+			if (x == i) { break; }
+			if (alphabetWeight[sortedString[i]] < alphabetWeight[sortedString[x]]) {
+				let curItem = sortedString[i];
+				let replacedItem = sortedString[x];
+				sortedString[i] = replacedItem;
+				sortedString[x] = curItem;
+			}
+		}
+	}
+	return sortedString;
+}
+/* END self challenge */
+/*
+for (let item = 0; item < universities.length; item++) {
+	if 
+}
+*/
+
+let universities = [];
+const arrayLength = function(arrayToCheck) {
+	let item;
+	let arrayLength = 0;
+	for(let i = 0; i <= arrayLength; i++) {
+		item = arrayToCheck[i];
+		if (item === undefined) {
+			return arrayLength;
+		}
+		else {
+			arrayLength += 1;
+		}
+	}
+}
+
+let theArraylength = arrayLength(graduates);
+for(let i = 0; i < theArraylength; i++) {
+	// arrayLength needs some fixin' so a quick error check here
+	if (graduates[i] === undefined) {
+		break;
+	}
+	let university = graduates[i].university;
+	universities.push(graduates[i].university);
+}
+
+console.log(universities.sort()); // I'm coming after you sort method!
+/*
 const universities = graduates.map((item) => {
 	return item.university;
 });
 console.log(universities.sort());
+*/
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -86,18 +195,21 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = graduates.map((item) => {
-	return `${item.first_name} ${item.email}`;
-});
+let contactInfo = [];
+for (let i = 0; i < graduates.length; i++) {
+	contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = graduates.filter((item) => {
-	if (item.university.includes('Uni')) {
-		return item.university;
+let uni = [];
+for (let i = 0; i < graduates.length; i++) {
+	if (graduates[i].university.includes('Uni')) {
+		uni.push(graduates[i].university);
 	}
-});
+}
+
 console.log(uni);
 
 
