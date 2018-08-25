5e3e2b634bef2856be370fb6ad6b55b13efc8d11
Colin Dismuke <colin.dismuke@gmail.com>
2018-08-25 11:46:04 -0500

    Finished classes.js stretch task.

--- challenges/classes.js
+++ challenges/classes.js
@@ -39,3 +39,47 @@ console.log(cuboid.surfaceArea()); // 130
   Create a new cube object and log out the results of your new cube.
 
 */
+
+class CubeMaker extends CuboidMaker {
+  constructor(cubeParams) {
+    super(cubeParams);
+  }
+
+  volume() {
+    // Formula for cube volume: length ^ 3
+    if (this.length != this.width || this.length != this.height) {
+      return "You have not created a cube!"
+    } else {
+      return Math.pow(this.length, 3);
+    }
+    
+  }
+  
+  surfaceArea() {
+    // Formula for cube surface area: 
+    // 6 * (width * height)
+    if (this.length != this.width || this.length != this.height) {
+      return "You have not created a cube!"
+    } else {
+      return 6 * (this.width * this.height);
+    }
+  }
+}
+
+const cube = new CubeMaker({
+  length: 5,
+  width: 5,
+  height: 5,
+})
+
+console.log(cube.volume()); // 125
+console.log(cube.surfaceArea()); // 150
+
+const notACube = new CubeMaker({
+  length: 5,
+  width: 4,
+  height: 5,
+})
+
+console.log(notACube.volume()); // 
+console.log(notACube.surfaceArea()); //
