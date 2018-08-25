2b5928acd0f714a5c045bc06955f70e285c52c62
Ken Barlow <kenbrlow@gmail.com>
2018-08-25 10:04:01 -0700

    Logging all of the universities in alphabetical order.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -68,6 +68,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for (var i = 0;  i < graduates.length; i++) {
+  universities.push(graduates[i].university)
+}
+universities.sort();
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
