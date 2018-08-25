f71843b34632d1f819ab9fa5e91a64c23789050b
Abigayle Franklin <abifanklin@gmail.com>
2018-08-25 13:31:18 -0500

    Task 6 + stretch goal complete

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,57 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMaker{
+  constructor(attr) {
+    this.length = attr.length;
+    this.width = attr.width;
+    this.height = attr.height;
+  }
+
+  volume() {
+    return (this.length * this.width * this.height);
+  }
+
+  surfaceArea() {
+    return (2 * (this.length * this.width + this.length * this.height + this.width * this.height));
+  }
+};
+
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+});
+
+
+class CubeMaker extends CuboidMaker {
+  constructor(cubeAttr) {
+    super(cubeAttr);
+    this.side = cubeAttr.side;
+  }
+
+  cubeVolume() {
+    return Math.pow(this.side, 3);
+  }
+
+  cubeSA() {
+    return 6 * Math.pow(this.side, 2);
+  }
+}
+
+const cube = new CubeMaker({
+  length: 5,
+  width: 5,
+  height: 5,
+  side: 5
+})
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+console.log(cube.volume());
+console.log(cube.surfaceArea());
+console.log(cube.cubeVolume());
+console.log(cube.cubeSA());
 
 /* Stretch Task:
 
