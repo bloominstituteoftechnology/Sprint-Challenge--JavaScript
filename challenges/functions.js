66245b816f916949e095b227bcd159dc18d903d3
Jennifer King <jenbellpgh@gmail.com>
2018-08-25 13:53:39 -0400

    Finished closure section of functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -38,7 +38,7 @@ console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: nestedFunction is a closure within myFunction(), and inherits locally defined variables, which includes 'internal'.
 
 
 const external = "I'm outside the function";
