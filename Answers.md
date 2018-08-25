27c3661cb4fdbb1622ef5c147cb88c1b8b214617
TexaCaliCoder <c.roth.lambda@gmail.com>
2018-08-25 12:12:10 -0500

    task 4 functions completed update to Answers: I forgot to answer the last question. Added answer 5

--- Answers.md
+++ Answers.md
@@ -9,5 +9,7 @@ explicit: having to physically call a binding of :
         -.call : taking in the first parameter to change the context and the following parameters to be passed in that are separated by commas (C-all = Commas). 
         - .apply: taking in only two parameters. The first assigning context the second being an array.
         - .bind: specifically used to change the context and essentially creating a new function within the context that will allow you to pass in different parameters.
-New: a new binding is created when you use the new Keyword to       create a function and uses ``this`` to "describe" that          function.
-Default/Window: ``this `` will check through the "layers" of        your scope to see if any of the other rules (listed above)      apply and if not, it will default all the way back to the       window. 
+New: a new binding is created when you use the new Keyword to       create a function and uses          ``this`` to "describe" that          function.
+        Default/Window: ``this `` will check through the "layers" of        your scope to see if any    of the other rules (listed above)      apply and if not, it will default all the way back to       the       window. 
+
+The super keyword is the ES6 way of binding ``this`` to a child from a parent and keeping us   with DRY code as well as from making us physically make a copy of the parent Prototype.
--- challenges/functions.js
+++ challenges/functions.js
@@ -1,31 +1,41 @@
 // ==== Callbacks ====  
-
+let cl = console.log;
 /* Step 1: Create a function that accepts a callback
   * Create a function named consume that can take 3 parameters.
   * The first two parameteres can accept any argument
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+const consume = (a, b, cb) =>{
+  return cb(a,b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const add = (a,b) =>{
+  return a + b;
+}
+const multiply = (a, b) =>{
+  return a * b;
+}
+const greeting =(fName, lName) =>{return `Hello ${fName} ${lName}, nice to meet you! ` }
+
 
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+// A function can remember the variables in the scope of which it was created and `nestedFunction()` is created within `myFunction()`
 
 const external = "I'm outside the function";
 
