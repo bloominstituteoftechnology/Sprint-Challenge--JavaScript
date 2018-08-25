19fa978cc983c0ee2d827e57a7ef9bba582ef41d
FBT <flaming.bunny.tomb@gmail.com>
2018-08-25 12:01:47 -0700

    finished as I'm going to get for now.

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,38 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
+/* ===== Prototype Practice ===== */
+
+class CuboidMaker{
+  constructor (obj) {
+this.length = obj.length;
+this.width = obj.width;
+this.height = obj.height;
+};
+volume(){
+  return `${this.length}` * `${this.width}` * `${this.height}`;
+};
+surfaceArea(){
+  return `2` * (`${this.length}` * `${this.width}` + `${this.length}` * `${this.height}` + `${this.width}` * `${this.height}`);
+}
+};
+
+let cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5,
+  });
+// Test your volume and surfaceArea methods by uncommenting the logs below:
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+
+
+
+
+
+
+
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
