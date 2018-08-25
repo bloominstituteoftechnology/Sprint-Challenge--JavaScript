cf93846210eb5b44943b1812f1d6b76531fc72cf
Pascale Pierre <pascaleee@gmail.com>
2018-08-25 12:08:34 -0500

    worked on object challenge

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -1,14 +1,25 @@
 // ==== Objects ====
 
-/* 
-  Given the following information about dinosaurs, create 3 objects: 
-  Use this pattern to create your objects: 
+/*
+  Given the following information about dinosaurs, create 3 objects:
+  Use this pattern to create your objects:
   object name, diet, weight, length, period
 */
+const tyrannosaurus = {name:'tyrannosaurus', diet:'carnivorous', weight: '7000kg', length: '12m', period:'Late Cretaceious',
+roar: function () {
+       return 'RAWERSRARARWERSARARARRRR!';
+   }
+   };
+
+
+
+const stegosaurus =  {name:'stegosaurus', diet: 'stegosaurus', weight:'2000kg', length:'9m', period:'Late Jurassic'};
+
+const velociraptor =  {name:'velociraptor', diet:'carnivorous', weight:'15kg', length:'1.8m', period:'Late Cretaceious'};
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
 
-// stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+// stegosaurus, stegosaurus, 2000kg, 9m, Late Jurassic
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
 
@@ -42,24 +53,56 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
+/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.
+
+
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
+
+var i;
+for (i = 0; i < graduates.length; i++) {
+    universities.push(graduates[i]);
+}
+
+function compare(a,b) {
+  if (a.university < b.university)
+    return -1;
+  if (a.university > b.university)
+    return 1;
+  return 0;
+}
+
+universities.sort(compare);
+
 console.log(universities)
 
-/* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
-The resulting contact information should have a space between the first name and the email information like this: 
+/* Request 2: Create a new array called contactInfo that contains both first name and email of each student.
+
+The resulting contact information should have a space between the first name and the email information like this:
 Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+
+var i;
+for (i = 0; i < graduates.length; i++) {
+    contactInfo.push(graduates.first_name + ' ' + graduates.email);
+}
+
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+// var i;
+// for (i = 0; i < graduates.length; i++) {
+//   if(graduates['university'].includes('Uni')){
+//     uni.push(graduates[i])};
+// }
 console.log(uni);
 
 
@@ -80,7 +123,9 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 const lowerCase = [];
-console.log(lowerCase); 
+
+
+console.log(lowerCase);
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
 const populationTotal = [];
