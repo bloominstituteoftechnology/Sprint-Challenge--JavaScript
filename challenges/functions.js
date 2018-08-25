6adebf27de932cb8bfa40a74d1a554e27b0e1df0
Andres Rivera <andres.rivera.toro@gmail.com>
2018-08-25 12:36:40 -0400

    completed functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -30,16 +30,20 @@ function greeting(firstName, lastName) {
 }
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
 
+// nestedFunction() has access to the scope outside of it just like myFunction() has access to `external`, a variable in the global scope.
+// Functions first look to their own local scope to access a variable, and if it isn't found, then they continue to the scope above them.
+// Let's say I want to use the variable `external` in nestedFunction(). It'll look to itself first, then myFunction(), and then the global scope.
+// Outer functions do not have access to inner functions. 
 
 const external = "I'm outside the function";
 
