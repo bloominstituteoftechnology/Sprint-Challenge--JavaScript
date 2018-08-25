1a5bfad02b5dac41c2510e06fc5bd2a5b48ec1b4
TheGameFreak720 <luis.alvarez@commithub.com>
2018-08-25 12:16:41 -0400

    finished arrays in array task

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -70,6 +70,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+
+for(let i = 0; i < graduates.length; i++) {
+  universities.push(graduates[i].university);
+}
+
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -79,11 +84,23 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+for(let i = 0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
+
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+
+for(let i = 0; i < graduates.length; i++) {
+  if (graduates[i].indexOf('Uni') > -1) {
+    uni.push(graduates[i].university);
+  }
+}
+
 console.log(uni);
 
 
