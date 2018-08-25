6a827cb9c25c50d344e16cf06151b9bfa6ca4591
meganfontenot <meganfontenot517@yahoo.com>
2018-08-25 11:43:57 -0500

    finshed with functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,40 +6,42 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-function consume(param1, param2, cb) {
-  cb(param1, param2);
-  }
+function consume(arg1, arg2, cb) {
+  return cb(arg1, arg2);
+    //code
+}
+
+/* Step 2: Create several functions to callback with consume();
+  * Create a function named add that returns the sum of two numbers
+  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+*/
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function greeting(first, last) {
+     return `Hello ${first} ${last}, nice to meet you!`;
+}
+
+/* Step 3: Check your work by uncommenting the following calls to consume(): */
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
   
-  /* Step 2: Create several functions to callback with consume();
-    * Create a function named add that returns the sum of two numbers
-    * Create a function named multiply that returns the product of two numbers 
-    * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-  */
-  function add(x,y,cb){
-    cb(x + y);
-  }
-  function multiply(x,y,cb){
-    return(x*y);
-  }
-  function greeting(param1, param2, cb){
-    return 'Hello param1 param2 nice to meet you!';
-  }
   
+// ==== Closures ==== 
+// Question 1: Explain in your own words why the example below is a closure.
   
-  /* Step 3: Check your work by uncommenting the following calls to consume(): */
-  consume(2,2,add); // 4
-  consume(10,16,multiply); // 160
-   consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+// Explanation: 
   
+// Question 2: Given the example below, what scope is the external variable in?
   
-  // ==== Closures ==== 
-  // Question 1: Explain in your own words why the example below is a closure.
-  
-  // Explanation: 
-  
-  // Question 2: Given the example below, what scope is the external variable in?
-  
-  let external = "I'm outside!";
+let external = "I'm outside!";
   
   function myFunction() {
     let internal = "Hello! I'm inside the function";
