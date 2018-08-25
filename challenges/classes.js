9dfb89db7ec8128bd5d6592c19a216e54ffb9d5b
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-08-25 13:05:09 -0400

    Complete stretch goals and then some

--- challenges/classes.js
+++ challenges/classes.js
@@ -4,7 +4,7 @@ class CuboidMaker {
     constructor(info)   {
         this.length = info.length;
         this.width = info.width;
-        this.height = info.height;    
+        this.height = info.height;
     }
     volume() {
         return this.length * this.width * this.height;
@@ -30,3 +30,35 @@ console.log(cuboid.surfaceArea()); // 130
   Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods as well.  Create a new cube object and log out the results of your new cube.
 
 */
+class Cube extends CuboidMaker  {
+    constructor(info)   {
+        super({
+            length: info.length,
+            width: info.length,
+            height: info.length,
+        })  //Used super to inherit length, width, height. However didn't pass the same object
+            //as I would to CuboidMaker since cubes should be equal on all sides.
+
+    }
+    volume()    {
+        return this.length*this.length*this.length;
+    }   //even though Cube would inherit volume I adjusted the volume method to better fit a cube
+    surfaceArea()   {
+        return 6*(this.length*this.length)
+    }   //Again, Cube would inherit surfaceArea from CuboidMaker, but I adjusted it to be more simple
+        //for the cube.
+    //I finished with two hours left to go, so I decided to add in a more complex method. This one is not shared by CuboidMaker.
+    spaceNotFilledBySphere()    {
+        let radius = this.length/2
+        return this.volume() - (4/3)*(Math.PI*radius*radius*radius);
+    }
+    //this is essentially the volume of the cube that would not be taken up by the
+    //volume of a sphere that sits in the center of a cube with sides that are tangent
+    //to the center of the cubes sides.
+}
+const cube = new Cube({
+    length: 4,
+})
+console.log(cube.volume());
+console.log(cube.surfaceArea());
+console.log(cube.spaceNotFilledBySphere())
