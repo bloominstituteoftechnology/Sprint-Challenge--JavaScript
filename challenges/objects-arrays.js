277971f6c985ed9165b5dad50c946f66b9437cb1
Bruce Cabanayan <bcabanayan@Bruces-MBP.domain>
2018-08-25 09:48:38 -0700

    Completed arrays and array methods exercises

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -70,6 +70,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for (i = 0; i < graduates.length; i++) {
+  universities.push(graduates[i].university);
+}
+universities.sort();
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -79,13 +83,20 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (i = 0; i < graduates.length; i++) {
+  contactInfo.push(graduates[i].first_name + ' ' + graduates[i].email);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (i = 0; i < graduates.length; i++) {
+  if (graduates[i].university.includes('Uni')) {
+    uni.push(graduates[i].university);
+  }
+}
 console.log(uni);
-
 
 // ==== Array Methods ====
 
@@ -104,8 +115,18 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 const lowerCase = [];
+let convertToLower = (animal) => {
+  return animal.animal_name.toLowerCase();
+}
+lowerCase.push(zooAnimals.map(convertToLower));
 console.log(lowerCase); 
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
 const populationTotal = [];
-console.log(populationTotal);
+for (i = 0; i < zooAnimals.length; i++) {
+  populationTotal.push(zooAnimals[i].population);
+}
+const addTotal = (total, num) => {
+    return total + num;
+}
+console.log(populationTotal.reduce(addTotal));
