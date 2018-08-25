e21374896872f6847a1c0386906542eec8efc8ff
Courtney Buratto <you@example.com>
2018-08-25 11:01:56 -0600

    functions.js complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,25 +6,36 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(param1, param2, cb) {
+  cb(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(param1, param2) {
+  return param1 + param2;
+}
 
+function multiply(param1, param2) {
+  return param1 * param2;
+}
 
+function greeting(firstName, lastName) {
+  return "Hello firstName + lastName, nice to meet you!"
+}
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
-
-// Explanation: 
+  
+// Explanation: Children have access to all of their parent's variables because of closure and function scope.
 
 
 const external = "I'm outside the function";
