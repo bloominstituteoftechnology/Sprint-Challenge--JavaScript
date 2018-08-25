7e3bf39895d1c146cb0fea370b795638a6e2580f
meganfontenot <meganfontenot517@yahoo.com>
2018-08-25 11:48:15 -0500

    finished prototypes

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -30,7 +30,7 @@ CuboidMaker.prototype.surfaceArea=function(){
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
-  Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
+  Add properties and values of length: 4, width: 5, and height: 5 to cuboid   
 */
 const cuboid = new CuboidMaker({"length":4,"width":5,"height":5});
 
