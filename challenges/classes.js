1d89754a45f7f9d3fb3dfc5dd1a384d87af5e1e1
paulapivat <paul.apivat@gmail.com>
2018-08-26 01:16:29 +0700

    complete classes.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,53 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMaker {
+  constructor(cubeAttribute) {
+    this.length = cubeAttribute.length;
+    this.width = cubeAttribute.width;
+    this.height = cubeAttribute.height;
+  }
+
+  volume(){
+    return `${this.length*this.width*this.height}`;
+  }
+
+  surfaceArea(){
+    return `${2*(this.length*this.width+this.length*this.height+this.width*this.height)}`
+  }
+}
+
+
+const cuboid = new CuboidMaker({length: 4, width: 5, height: 5});
+
+
+//Test your volume and surfaceArea methods by uncommenting the logs below:
+console.log(cuboid.volume()); 
+console.log(cuboid.surfaceArea()); 
+
+
+// function CuboidMaker(cubeAttribute) {
+//   this.length = cubeAttribute.length;
+//   this.width = cubeAttribute.width;
+//   this.height = cubeAttribute.height;
+// }
+
+// CuboidMaker.prototype.volume = function() {
+//   return `${this.length}*${this.width}*${this.height}`;
+// }
+
+// CuboidMaker.prototype.surfaceArea = function() {
+//   return `2*(${this.length}*${this.width}+${this.length}*${this.height}+${this.width}*${this.height})`
+// }
+
+// const cuboid = new CuboidMaker({length: 4, width: 5, height: 5});
+
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
+
+
+
 
 /* Stretch Task:
 
