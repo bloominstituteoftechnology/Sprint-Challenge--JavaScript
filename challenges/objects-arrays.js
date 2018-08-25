9051e71e09f55b415183dfad93335e9e8646123b
Kendra Williams <kmonique.williams@gmail.com>
2018-08-25 14:15:32 -0400

    clean up line 83

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -80,8 +80,7 @@ const universities = [];
 for (let i =0; i < graduates.length; i++){
   universities.push(graduates[i].university);
 }
-universities.sort();
-console.log(universities)
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
