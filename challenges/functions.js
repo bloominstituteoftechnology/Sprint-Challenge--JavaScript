12603622f5d2655557c7b0de84ac7fa9e5668c5c
Lorenzo Evans <lorenzo.evans94@gmail.com>
2018-08-25 12:54:09 -0400

    Completed functions challenges.

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,11 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+let consume = function (par1, par2, cb){
+
+  return cb(par1, par2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,17 +19,28 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+let adderBot = function (par1, par2){
+  return par1 + par2;
+}
 
+let timesBot = function (par1, par2){
+  return par1 * par2 ;
+}
+
+let greetBot = function (par1, par2) {
+  return `Hello ${par1} ${par2}, nice to meet you`;
+}
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,adderBot)); // 4
+console.log(consume(10,16,timesBot)); // 160
+console.log(consume("Mary","Poppins", greetBot)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: nestedFunction is a closure, or closure function, meaning that it has access to the state recorded in the 
+// scope surrounding it, state being the variables declared and their values.
 
 
 const external = "I'm outside the function";
