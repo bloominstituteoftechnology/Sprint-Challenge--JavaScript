d27d4af28b6a82030a53f7efb63a5ecda880fc21
jbierly1 <james.bierly@gmail.com>
2018-08-25 14:38:18 -0400

    finished classes

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,43 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMaker {
+	constructor(data) {
+		this.length = data.length;
+		this.width = data.width;
+		this.height = data.height;
+	}
+	volume() {
+		let anw = this.length * this.width * this.height;
+		return anw;
+		console.log(anw);
+	}
+	surfaceArea() {
+		let anw =
+			2 *
+			(this.length * this.width +
+				this.length * this.height +
+				this.width * this.height);
+		return anw;
+		console.log(anw);
+	}
+}
+
+/* == Step 2: Volume Method ==
+  Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
+  
+  Formula for cuboid volume: length * width * height
+*/
+const cuboidData = {
+	length: 4,
+	width: 5,
+	height: 5
+};
+
+const cuboid = new CuboidMaker(cuboidData);
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 /* Stretch Task:
 
