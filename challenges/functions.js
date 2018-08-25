c3885de9c71027f5f9ce7575d3cca6065547accb
Oliver Abreu <oliverabreu@Olivers-MacBook.local>
2018-08-25 14:18:03 -0400

    Completed: Callbacks

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,30 +1,46 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====
 
 /* Step 1: Create a function that accepts a callback
   * Create a function named consume that can take 3 parameters.
   * The first two parameteres can accept any argument
-  * The last parameter accepts a callback 
+  * The last parameter accepts a callback
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume (param1, param2, cb) {
+  return cb(param1, par2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named multiply that returns the product of two numbers
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function cbAdd (a, b) {
+     return a+b;
+}
+function cbMultiply (a, b) {
+     return a*b;
+}
+function cbGreeting (first, last) {
+     return `Hello, ${first} ${last}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
+console.log(`
+${consume(2,2,cbAdd)}
+${consume(10,16,cbMultiply)}
+${consume("Mary","Poppins", cbGreeting)}`)
 
-// ==== Closures ==== 
+// ==== Closures ====
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation:
 
 
 const external = "I'm outside the function";
