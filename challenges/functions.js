4a951d99dfd1f364bb93ae7fccefb535048089f6
Rob Salzberg <robsalzberg@gmail.com>
2018-08-25 13:32:43 -0400

    Finished Task 4, functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,35 +7,45 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-
-/* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
-
-
-/* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
-
-
-// ==== Closures ==== 
-// Explain in your own words why `nestedfunction()` can access the variable `internal`.
-
-// Explanation: 
-
-
-const external = "I'm outside the function";
-
-function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
-
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
-}
-myFunction();
+function consume (x, y, cb) {
+    return cb(x, y);
+  }
+  
+  /* Step 2: Create several functions to callback with consume();
+    * Create a function named add that returns the sum of two numbers
+    * Create a function named multiply that returns the product of two numbers 
+    * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  */
+  
+  function add (a, b) {
+      return a+b;
+    }
+  function multiply (a, b) {
+      return a*b;
+    }
+  function greeting (first, last) {
+      return `Hello ${first} ${last}, nice to meet you.`;
+    }
+  
+  /* Step 3: Check your work by uncommenting the following calls to consume(): */
+  console.log(consume(2,2,add)); // 4
+  console.log(consume(10,16,multiply)); // 160
+  console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+  
+  // ==== Closures ==== 
+  // Explain in your own words why `nestedfunction()` can access the variable `internal`.
+  
+  // Explanation: Nested function can access "internal" because nestedFunction is able to
+  // utilize any variables declared in the global scope or it's "parent scope," which is "myFunction". 
+  const external = "I'm outside the function";
+  
+  function myFunction() {
+    console.log(external);
+    const internal = "Hello! I'm inside myFunction!";
+  
+    function nestedFunction() {
+      console.log(internal);
+    };
+    nestedFunction();
+  }
+  myFunction();
