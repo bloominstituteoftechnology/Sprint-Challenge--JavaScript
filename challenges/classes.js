cf4da4236ddecaf1a947547dcc7326bc3ab62233
rlmclaughlin <rlmclaughlin@yahoo.com>
2018-08-25 13:13:25 -0500

    answered the comprehension questions

--- challenges/classes.js
+++ challenges/classes.js
@@ -23,12 +23,6 @@ console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 
-
-
-// Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
-
 /* Stretch Task:
 
   Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods as well.  Create a new cube object and log out the results of your new cube.
