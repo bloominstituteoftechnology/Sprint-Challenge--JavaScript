10ba988617ffd6138222377b52cfbe0237a00449
MAbbas <mabbashm110@gmail.com>
2018-08-25 10:41:53 -0500

    completed functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(paramOne, paramTwo, operation) {
+  return operation(paramOne, paramTwo);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +17,32 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+//THE LONG WAY - BUT ASKED IN CHALLENGE
+function add (param1, param2) {
+  return param1 + param2;
+}
+
+function multiply (param1, param2) {
+  return param1 * param2;
+}
+
+function greeting (firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
+
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+// Due to scope, nested function is within the parent function scope and can access all parent variables, but not its child's variables
+// As Dan said, parents are unaware of their children but children know about the parents. :-) 
 
 const external = "I'm outside the function";
 
