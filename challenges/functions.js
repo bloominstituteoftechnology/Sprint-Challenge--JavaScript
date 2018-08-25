e33cc6fa777de3d09f737e9d61ff462cbfa4661f
Marguel Gutierrez <marguel.gtz@gmail.com>
2018-08-25 12:18:57 -0500

    add, multiply and greet functions created

--- challenges/functions.js
+++ challenges/functions.js
@@ -16,10 +16,22 @@ function consume(param1, param2, cb) {
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = (param1, param2) => {
+  return param1 + param2;
+};
+
+const multiply = (param1, param2) => {
+  return param1 * param2;
+};
+
+const greeting = (param1, param2) => {
+  return `Hello ${param1} ${param2}, nice to meet you`;
+};
+
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ====
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
