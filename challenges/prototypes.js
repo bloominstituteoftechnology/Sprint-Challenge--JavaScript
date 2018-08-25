807c33902648c0a8ed45177cd470013c8c4f9c5f
Pascale Pierre <pascaleee@gmail.com>
2018-08-25 12:13:20 -0500

    worked on prototypes

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -16,7 +16,9 @@ function CuboidMaker(options) {
 
   Formula for cuboid volume: length * width * height
 */
-
+CuboidMaker.prototype.volume = function() {
+  return length * width * height ;
+};
 
 /* == Step 3: Surface Area Method ==
   Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height.
@@ -24,12 +26,19 @@ function CuboidMaker(options) {
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
 
+CuboidMaker.prototype.surfaceArea = function() {
+  return 2 * (length * width + length * height + width * height) ;
+};
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.
 */
-
+const cuboid = CuboidMaker({
+length: 2,
+width: 1,
+height: 1,
+});
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
