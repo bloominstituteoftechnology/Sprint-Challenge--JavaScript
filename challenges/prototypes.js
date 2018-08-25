7b1ae4809bd575e9d1159517b926243c9d22093b
Trevor Lewis <tenzingtitan@gmail.com>
2018-08-25 11:26:43 -0700

    Finished Sprint Challenge

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -20,21 +20,6 @@ CuboidMaker.prototype.volume = function() {
   return this.length * this.width * this.height;
 }
 
-function CuboidMaker(cuboid){
-  this.length = cuboid.length;
-  this.width = cuboid.width;
-  this.height = cuboid.height;
-};
-
-/* == Step 2: Volume Method ==
-Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
-
-Formula for cuboid volume: length * width * height
-*/
-CuboidMaker.prototype.volume = function() {
-return this.length * this.width * this.height;
-}
-
 /* == Step 3: Surface Area Method ==
 Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
 
