9fe38245a55aad608971ef12fe5edf5ee87503a3
amadison17 <angelmadison2015@gmail.com>
2018-08-25 14:34:45 -0400

    prototypes.js

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,7 +5,11 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
-
+function CuboidMaker(length, width, height) {
+  this.length = length;
+  this.width = width;
+  this.height - height;
+}
 
 /* == Step 2: Volume Method ==
   Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
