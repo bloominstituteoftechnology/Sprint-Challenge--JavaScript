ca801eb4311194ee699a2de1c1a330a9e3b252c1
Marguel Gutierrez <marguel.gtz@gmail.com>
2018-08-25 12:35:31 -0500

    cuboid object created, all test pass

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -38,7 +38,12 @@ CuboidMaker.prototype.surfaceArea = function() {
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+});
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
