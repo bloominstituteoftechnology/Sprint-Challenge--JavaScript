5e91e8e5a047eac917a9452d28de4810339e8a7a
Usman Javed <ujaved931@gmail.com>
2018-08-27 03:36:32 +0500

    added ans

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,6 +1,14 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 1. Describe the biggest difference between `.forEach` & `.map`.
+ans. ForEach does not return 
 2. What is the difference between a function and a method?
+Ans. Functions are defined outside classes btu methods are defined inside a class. 
 3. What is closure?
+ans. closure is when a function inside another function has access to values from outside
 4. Describe the four rules of the 'this' keyword.
+ans. explicit = .call .apply. bind
+    implicit = object inside another object
+    window = when called outside 
+    new = new key:value pairs
 5. Why do we need super() in an extended class?
+ans. to have access to parent functions 
--- challenges/classes.js
+++ challenges/classes.js
@@ -1,4 +1,33 @@
 // Copy and paste your prototype in here and refactor into class syntax.
+/* ===== Prototype Practice ===== */
+
+// Task: You are to build a cuboid maker that can return values for a cuboid's volume or surface area. Cuboids are similar to cubes but do not have even sides. Follow the steps in order to accomplish this challenge.
+
+function CuboidMaker(cuboidAttributes) {
+  this.length = cuboidAttributes.length;
+  this.width = cuboidAttributes.width;
+  this.height = cuboidAttributes.height
+}
+
+CuboidMaker.prototype.volume = function() {
+  return (this.length*this.width*this.height);
+}
+
+CuboidMaker.prototype.surfaceArea = function() {
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+}
+
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+})
+
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+
+
+
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
