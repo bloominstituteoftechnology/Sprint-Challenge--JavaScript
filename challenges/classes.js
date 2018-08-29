2f40cc9a0e2158d669214dbf83dab82f0cb5e0b4
alasalle <angela.lasalle91@gmail.com>
2018-08-28 21:52:22 -0400

    applied Prettier to all files declared variables i with let in object-arrays.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,24 +1,27 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 class CuboidMaker {
-  constructor (cuboid) {
+  constructor(cuboid) {
     this.length = cuboid.length;
     this.width = cuboid.width;
     this.height = cuboid.height;
   }
 
   volume() {
-  let vol = (this.height * this.length * this.width);
-  return vol
-  };
+    let vol = this.height * this.length * this.width;
+    return vol;
+  }
 
   surfaceArea() {
-  let sArea = (2 *((this.length * this.width) + (this.length * this.height) + (this.width * this.height)));
-  return sArea;
-  };
-
+    let sArea =
+      2 *
+      (this.length * this.width +
+        this.length * this.height +
+        this.width * this.height);
+    return sArea;
+  }
 }
 
-cuboid = new CuboidMaker ({length: 4, width: 5, height: 5});
+cuboid = new CuboidMaker({ length: 4, width: 5, height: 5 });
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
@@ -26,27 +29,29 @@ console.log(cuboid.surfaceArea()); // 130
 
 /* Stretch Task:
 
-  Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods as well.  Create a new cube object and log out the results of your new cube.
+  Extend the base class CuboidMaker with a sub class called CubeMaker. 
+  Find out the formulas for volume and surface area for cubes and create 
+  those methods as well.  Create a new cube object and log out the results 
+  of your new cube.
 
 */
 
 class CubeMaker extends CuboidMaker {
-  constructor (cube) {
+  constructor(cube) {
     super(cube);
   }
 
   cubeVolume() {
-    let cVol = (this.length * this.length * this.length);
+    let cVol = this.length * this.length * this.length;
     return cVol;
-  };
+  }
 
   cubeSurfaceArea() {
-    let cSA = (6 * (this.length * this.length));
+    let cSA = 6 * (this.length * this.length);
     return cSA;
-  };
-
+  }
 }
 
-cube = new CubeMaker ({length: 5});
+cube = new CubeMaker({ length: 5 });
 console.log(cube.cubeSurfaceArea());
 console.log(cube.cubeVolume());
