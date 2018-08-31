f0e307d1befb5f4c2623faaf81ac52e2d579ab28
shannonhealy <sahealy525@gmail.com>
2018-08-30 22:57:00 -0400

    Changed objects-arrays.js so that the functions are written in ES6 arrow notation.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -14,7 +14,7 @@ let tyrannosaurus = {
   weight: `7000kg`,
   length: `12m`,
   period: `Late Cretaceious`,
-  roar: function() {
+  roar: () => {
     return "RAWERSRARARWERSARARARRRR!";
   },
 };
@@ -73,7 +73,7 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-let createUniversities = function() {
+let createUniversities = () => {
   for(let i = 0; i < graduates.length; i++){
     universities.push(graduates[i].university);
   }
@@ -88,7 +88,7 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
-let createContactInfo = function() {
+let createContactInfo = () => {
   for(let i = 0; i < graduates.length; i++){
     contactInfo.push(graduates[i].first_name + ' ' + graduates[i].email);
   }
@@ -99,7 +99,7 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
-let findUni = function() {
+let findUni = () => {
   for(let i = 0; i < universities.length; i++){
     if(universities[i].includes("Uni")){
       uni.push(universities[i]);
@@ -127,7 +127,7 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 const lowerCase = [];
-let makeLowerCase = function(str) {
+let makeLowerCase = (str) => {
   for(let i = 0; i < zooAnimals.length; i++){
     let strArray = zooAnimals[i].animal_name.split("");
     for(let i = 0; i < strArray.length; i++){
@@ -144,7 +144,7 @@ console.log(lowerCase);
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
 const populationTotal = [];
-let createPopulationTotal = function() {
+let createPopulationTotal = () => {
   for(let i = 0; i < zooAnimals.length; i++){
     populationTotal.push(zooAnimals[i].population);
   }
