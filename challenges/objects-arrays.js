b985f85125f2563bb8bf31d8b53339e5a741ddf7
Gannon <gkleblanc@gmail.com>
2018-09-14 15:29:03 -0400

    forgot to push the change to request three on object-array change so it fills the array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -94,7 +94,7 @@ console.log(contactInfo);
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
 for (let i = 0; i < graduates.length; i++) {
-  if (graduates[i].university === 'Uni') {
+  if (graduates[i].university.includes('Uni')) {
     uni.push(graduates[i])
   }
 }
