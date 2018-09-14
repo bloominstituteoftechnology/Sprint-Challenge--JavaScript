d8f72005b7c2933c906326946eb4288f08a9def8
Carlo Clamucha <carlo.clamucha@gmail.com>
2018-09-14 10:17:30 -0500

    tested with node and no errors

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(param1,param2,cb){
+return cb(param1,param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,6 +17,17 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(var1,var2){
+  console.log(var1 + var2);
+}
+
+function multiply(var1,var2){
+  console.log(var1 * var2);
+}
+
+function greeting(firstName,lastName){
+  console.log(`Hello ${firstName} ${lastName} nice to meet you`);
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
 //consume(2,2,add); // 4
@@ -25,6 +39,8 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+// Nested function can access the variable `internal` because the variable `internal` is a closure which JavaScript stores in memory 
+//for future use hence why it is a closure.
 
 
 const external = "I'm outside the function";
