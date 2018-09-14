bb5582e9fe4a49fd981f4f3bfc1e520cca80858a
Mindysueasaurus <mymindyyeager@gmail.com>
2018-09-14 10:44:06 -0500

    fun fun functions are finished

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,24 +6,34 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(par1, par2, cb) {
+  return cb(par1, par2)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+function add(num1, num2) {
+  return num1 + num2;
+}
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+function greeting(first_name, last_name) {
+  return `Hello ${first_name} ${last_name}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
-
+//because nested function has access to the variables in its own scope and outside of it all the way up to the global scope. In fact thats why it's a closure. It's because it retains data for  variables outside of it. 
 // Explanation: 
 
 
@@ -35,7 +45,7 @@ function myFunction() {
 
   function nestedFunction() {
     console.log(internal);
-  };
-  nestedFunction();
+     };
+   nestedFunction();
 }
-myFunction();
+ myFunction();
