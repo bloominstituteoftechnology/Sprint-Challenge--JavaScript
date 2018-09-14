0f0fda20ff3c32fcde3b386090ca3cd84c4bead0
csdisciple <oleksstasyev@gmail.com>
2018-09-14 16:20:43 -0400

    Fixed object-arrays.js to use for loops instead of built in methods

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -66,11 +66,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = graduates.map(universityName => {
-    return universityName.university;
-});
-
-console.log(universities)
+const universities = [];
+for(let i = 0; i < graduates.length; i++){
+    universities.push(graduates[i].university);
+}
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -78,18 +78,22 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = graduates.map(info => {
-    return `${info.first_name} ${info.email}`
-});
+const contactInfo = [];
+for(let i = 0; i < graduates.length; i++){
+    contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
 let shouldContain = 'Uni';
-    graduates.filter(item => {
-    if(item.university.includes(shouldContain)){return uni.push(item.university);}
-});
+
+for(let i = 0; i < graduates.length; i++){
+    if(graduates[i].university.includes(shouldContain)){
+        uni.push(graduates[i].university);
+    }
+}
 console.log(uni);
 
 
