8c405a480dfee1a97752ccec4cb1358ae68f7d64
samsisle <samko9522@gmail.com>
2018-09-14 09:27:06 -0700

    Update functions.js challenge (#1)

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(param1, param2, cb) {
+  return cb(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +17,33 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2) {
+  return num1 + num2; 
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function greeting(first, last) {
+  return `Hello ${first} ${last}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
-
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+/*
+  Because the nestedFunction() does not have the internal variable defined within its own
+  scope, it will look outside the scope chain and look for the internal variable referenced
+  in the outer scope. This is closure. Closure basically gives a function body access to the
+  scope outside of it. 
+*/
 
 const external = "I'm outside the function";
 
