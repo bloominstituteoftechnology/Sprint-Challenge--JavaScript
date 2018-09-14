d7dee7109e91ee894815dafb606b3c237635abaf
brianmgre <brian.gregorius.com>
2018-09-14 14:17:33 -0400

    completed sprint

--- Answers.md
+++ Answers.md
@@ -1,1 +1,24 @@
+1. Describe the biggest difference between `.forEach` & `.map`.
 
+foreach searches through the entire array. Map also searches but creates a new array.
+
+2. What is the difference between a function and a method?
+
+a  function is defined outside of an object. A method is defined inside of an object.
+
+3. What is closure?
+
+Closure is scope and where a block of code ends.  
+
+4. Describe the four rules of the 'this' keyword.
+
+1. Without a scope this tries to go global
+2. Implicit binding: it ties the object to inside the function or passed into it (block of scope)
+3. New binding: to build new opjects
+4. Explicit binding:holds value with a variable which can be called at a later time
+
+5. Why do we need super() in an extended class?
+
+To access and call functions on an object. 
+
+.
--- challenges/functions.js
+++ challenges/functions.js
@@ -7,24 +7,58 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+const animals = ['dog', 'cat', 'bird', 'fish'];
+
+function contains(item, list, cb) {
+  for (let i =0; i< list.length; i++){
+    if( list[i] === item){
+      return cb(true);
+    }
+  }
+  return cb(false);
+}
+
+contains('dog', animals, function(dog){
+console.log(dog)
+});
+
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function consume(x, y, cb) {
+  return cb(x,y)
+}
 
 
-/* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+let add = function(x,y){
+  return (x+y);
+}
+
+let multiply = function(x,y){
+  return (x*y);
+}
+
+let greeting = function(x,y){
+  return (`Hello ${x} ${y}, nice to meet you!`);
+}
+
+
+// /* Step 3: Check your work by uncommenting the following calls to consume(): */
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+
+
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: Because of scope. nested function is still inside of myFunction's scope.
 
 
 const external = "I'm outside the function";
