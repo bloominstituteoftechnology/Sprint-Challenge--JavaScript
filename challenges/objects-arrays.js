e478f7d7206377f8bd78cbcfdfeb33cbed33f752
TRIF3X <thewizardofcrypto@gmail.com>
2018-09-14 12:37:12 -0500

    arrays-finished

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -84,10 +84,10 @@ Name email@example.com
 Log the result of your new array. */
 const contactInfo = [];
 for (let i = 0; i < graduates.length; i++) {
- contactInfo.push(graduates[i].first_name, graduates[i].email);
+ let x = (graduates[i].first_name && graduates[i].email);
+ contactInfo.push(`${graduates[i].first_name} ${x}`)
 }
 console.log(contactInfo);
-
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
