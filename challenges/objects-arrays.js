c34847d89990aeb661227b455fae0592715b8339
TRIF3X <thewizardofcrypto@gmail.com>
2018-09-14 11:04:14 -0500

    partial done array methods

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -70,6 +70,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for (let i = 0; i < graduates.length; i++) {
+  universities.push(graduates[i].university)
+}
+universities.sort();
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -79,11 +83,17 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (let i = 0; i < graduates.length; i++) {
+ contactInfo.push(graduates[i].first_name, graduates[i].email);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (let i = 0; i < graduates.length; i++) {
+   uni.push(graduates[i].university);    
+}
 console.log(uni);
 
 
@@ -105,8 +115,12 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 const lowerCase = [];
+const newMap = zooAnimals.map(x => x['animal_name'].toLowerCase());
+lowerCase.push(newMap);
 console.log(lowerCase); 
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
 const populationTotal = [];
+const sum = zooAnimals.reduce((pop, animal) => pop + animal.population, 0);
+populationTotal.push(sum);
 console.log(populationTotal);
