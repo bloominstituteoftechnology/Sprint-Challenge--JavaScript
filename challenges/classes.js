0c9dea6bc912fba8a200b424198ebdd7000f552a
Rahul Desai <rd825@georgetown.edu>
2018-09-14 12:02:51 -0400

    Cleaning up some formatting

--- challenges/classes.js
+++ challenges/classes.js
@@ -24,12 +24,10 @@ const cuboid = new CuboidMaker({
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
-<<<<<<< HEAD
-/* Stretch Task:
 
+/* Stretch Task:
   Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods as well.  Create a new cube object and log out the results of your new cube.
 */
-
 class CubeMaker extends CuboidMaker {
   constructor(attributes) {
     super(attributes);
@@ -52,6 +50,3 @@ const cube = new CubeMaker({
 
 console.log(cube.cubeVolume()); // 64
 console.log(cube.cubeSurfaceArea()); // 96 
-=======
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
->>>>>>> d8ebbccdbd17e98156a2dd72eb778de1d6ef190e
