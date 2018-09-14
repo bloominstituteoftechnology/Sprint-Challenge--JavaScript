3dd44765c5eb9555799e9679903ac822139324bc
Edward J <edjeong99@gmail.com>
2018-09-14 11:47:16 -0400

    completed array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -123,5 +123,5 @@ const lowerCase = zooAnimals.map(item => item.animal_name.toLowerCase());
 console.log(lowerCase); 
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
-const populationTotal = zooAnimals.reduce((total, current) => total + current.population);
+const populationTotal = zooAnimals.reduce(((total, current) => total + current.population),0);
 console.log(populationTotal);
