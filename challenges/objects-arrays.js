76378dd8bf81be6614c0fb556797e596352f621a
Yusuf Nafey <yusufnafey@yahoo.com>
2018-09-14 08:40:22 -0700

    finished objects portion of objects-arrays.js file

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -1,19 +1,39 @@
 // ==== Objects ====
-
 /* 
   Given the following information about dinosaurs, create 3 objects: 
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
-
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
-
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
 
-// Using your dinosaur objects, log answers to these questions:
+const tyrannosaurus = {
+  "name": "tyrannosaurus",
+  "diet": "carnivorous",
+  "weight": "7000kg",
+  "length": "12m",
+  "period": "Late Cretaceious",
+  "roar": function() {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+}
+const stegosaurus = {
+  "name": "stegosaurus",
+  "diet": "herbivorous",
+  "weight": "2000kg",
+  "length": "9m",
+  "period": "Late Jurassic"
+}
+const velociraptor = {
+  "name": "velociraptor",
+  "diet": "carnivorous",
+  "weight": "15kg",
+  "length": "1.8m",
+  "period": "Late Cretaceious"
+}
 
+// Using your dinosaur objects, log answers to these questions:
 // How much did tyrannosaurus weigh?
 console.log(tyrannosaurus.weight);
 // What was the diet of a velociraptor?
@@ -22,13 +42,14 @@ console.log(velociraptor.diet);
 console.log(stegosaurus.length);
 // What time period did tyrannosaurus live in?
 console.log(tyrannosaurus.period);
-
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
 console.log(tyrannosaurus.roar());
 
 
-// ==== Arrays ====
 
+
+
+// ==== Arrays ====
 // Given an array of college graduates.  Complete the following requests WITHOUT using any array methods like .forEach(), .map(), .reduce(), .filter()
 
 const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
