bdd1e76cdb01246dd62c0c4fa58c4a2ff7902497
ThomasUtsey <tomutsey@yahoo.com>
2018-09-14 15:14:04 -0400

    didnt see callbacks file its late but done

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,4 +1,3 @@
-// ==== Callbacks ====  
 
 /* Step 1: Create a function that accepts a callback
   * Create a function named consume that can take 3 parameters.
@@ -7,25 +6,38 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume (num1,num2,callback){
+  return callback(num1,num2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  
 */
+function add (x,y){
+  return (x+y);
+  
+};
+function multiply (x,y){
+  return (x*y)
+};
+function greeting (firstName,lastName){
+  return 'Hello' + ' ' + firstName + " " + lastName +" "+ 'nice to meet you!'
+};
 
-
-/* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+ /* Step 3: Check your work by uncommenting the following calls to consume(): */
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+//The external is on the global scope and can be accessed by any function whereas th internal can only be accessed within its local scope and its childred ( nested functions). The nested function cannot be accessed by its parent but the nested function can access the external function scope.
 
 const external = "I'm outside the function";
 
