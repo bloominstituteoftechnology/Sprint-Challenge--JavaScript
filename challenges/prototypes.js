58253fd3e8a8ca01413ad7c0f12d2abdeffeed42
Brandon Vilaychith <blvilaychith@outlook.com>
2018-09-14 10:52:44 -0500

    added volume method to CuboidMaker prototype

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -16,7 +16,10 @@ function CuboidMaker(cuboidAttrs) {
   
   Formula for cuboid volume: length * width * height
 */
-
+CuboidMaker.prototype.volume = function() {
+  const volume;
+  volume = this.length * this.width * this.height;
+}
 
 /* == Step 3: Surface Area Method ==
   Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
