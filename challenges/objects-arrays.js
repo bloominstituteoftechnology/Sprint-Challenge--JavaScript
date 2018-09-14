fe9399fb6abbdb659b924c1ef89fadfb0f37a711
unknown <quan.nguyen199712@gmail.com>
2018-09-14 12:27:02 -0400

    finished with object array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -71,6 +71,9 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for( let i= 0; i<graduates.length;i++){
+  universities.push(graduates[i].university);
+}
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
@@ -80,11 +83,19 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for( let i= 0; i<graduates.length;i++){
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for( let i= 0; i<graduates.length;i++){
+  if(graduates[i].university.includes('Uni')){
+    uni.push(graduates[i].university);
+  }
+}
 console.log(uni);
 
 
@@ -106,8 +117,14 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 const lowerCase = [];
+zooAnimals.forEach(function(lowName){
+  lowerCase.push(lowName.animal_name.toLowerCase() );
+}
+);
 console.log(lowerCase); 
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
-const populationTotal = [];
+const populationTotal = zooAnimals.reduce(function(totalPopulation, item) {
+  return totalPopulation + item.population;
+  }, 0);;;
 console.log(populationTotal);
