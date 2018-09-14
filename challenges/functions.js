dc9cb01f584d3ed7a750976ca1f0a8de410342e1
Joe Bugajski <yogurtmystery@gmail.com>
2018-09-14 12:20:17 -0500

    completed functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(param1, param2, callback) {
+  return callback(param1, param2)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +17,33 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(x, y) {
+  return x + y
+}
+
+function multiply(x, y) {
+  return x * y
+}
+
+function greeting(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add) 
+// 4
+consume(10,16,multiply); 
+// 160
+// 
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
-
+/* Explanation: 
+    A nested function will first look inside of its own local scope to define a variable. It will then go up to its parent scope to see if it's defined there, then the grandparent(if there is a next parent) all the way up to global scope. It has access to all of its ancestor functions, all the way up to global. But it does not have access to unconnected function chains. 
+*/
 
 const external = "I'm outside the function";
 
