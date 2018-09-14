5f5b081e830ee26f93ae70b467c031c854d06810
Stublink <chrisrey95@gmail.com>
2018-09-14 12:06:08 -0400

    Classes + Stretch

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,44 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-// Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+class CuboidMaker {
+    constructor(dimensions) {
+        this.length = dimensions.length;
+        this.width = dimensions.width;
+        this.height = dimensions.height;
+    }
+    volume() { return this.length * this.width * this.height }; 
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+    surfaceArea() { 
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height) 
+    }; 
+}
+
+const cuboid = new CuboidMaker({ length: 4, width: 5, height: 5 });
+
+// Test your volume and surfaceArea methods by uncommenting the logs below:
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  
+// Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  
+// Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker {
+    constructor(dimensions) {
+        super({length: dimensions, width: dimensions, height: dimensions});
+    }
+
+    cubeVolume() {
+        return this.length * this.width * this.height;
+    };
+
+    cubeSurface() {
+        return this.length * 6;
+    };
+}
+
+const cube = new CubeMaker(4);
+
+// Test your volume and surfaceArea methods by uncommenting the logs below:
+console.log(cube.cubeVolume()); // 100
+console.log(cube.cubeSurface()); // 130
