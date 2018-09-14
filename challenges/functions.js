2c2996c6c3627288c3e6a3374a9ba5759182493a
Steve Smodish <smodish@hotmail.com>
2018-09-14 13:00:24 -0400

    Answers.md objects-arrays.js functions.js complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,4 +1,4 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====
 
 /* Step 1: Create a function that accepts a callback
   * Create a function named consume that can take 3 parameters.
@@ -6,26 +6,42 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(arg1, arg2, callback) {
+  return callback(arg1, arg2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(arg1, arg2) {
+  return arg1 + arg2;
+}
 
+function multiply(arg1, arg2) {
+  return arg1 * arg2;
+}
+
+function greeting(arg1, arg2) {
+  return `Hello ${arg1} ${arg2}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
-
-// ==== Closures ==== 
+// ==== Closures ====
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation:
+/*
 
+      Javascript's closure property allows a function's methods to access it's data AFTER the function has been executed.
+      (I now feel this isn't a great example because this seems like a simple scope demonstration and doesn't fully show what closures are about)
+
+*/
 
 const external = "I'm outside the function";
 
@@ -35,7 +51,7 @@ function myFunction() {
 
   function nestedFunction() {
     console.log(internal);
-  };
+  }
   nestedFunction();
 }
 myFunction();
