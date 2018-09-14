3649aa2f53d84ab7a38477bd7f51d76adc655cef
Brandon Vilaychith <blvilaychith@outlook.com>
2018-09-14 10:25:21 -0500

    Finished zoo animals request 1

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -77,12 +77,13 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
-graduates.forEach(students => contactInfo.push(`${students.first_name} ${students.email}`))
+graduates.map(students => contactInfo.push(`${students.first_name} ${students.email}`))
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+// graduates.filter(students => )
 console.log(uni);
 
 
@@ -103,6 +104,7 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 const lowerCase = [];
+zooAnimals.map(animal => lowerCase.push(animal.animal_name.toLowerCase()));
 console.log(lowerCase); 
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
