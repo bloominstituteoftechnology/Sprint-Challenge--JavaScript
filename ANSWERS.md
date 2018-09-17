df4b581afc82dd84cff57b97a6c30882f7a2d86e
Ellen Nitchals <ellen.nitchals@gmail.com>
2018-09-17 11:03:41 -0700

    sprint complete

--- ANSWERS.md
+++ ANSWERS.md
@@ -1,7 +1,16 @@
 # Your responses to the short answer questions should be laid out here using Mark Down.
 change here
 1. Describe the biggest difference between `.forEach` & `.map`.
+Map creates a new array from the results of calling a function on each item in an array. forEach executes a function, but does not build an array.
 2. What is the difference between a function and a method?
+A method is a function that is the value corresponding to a key on an object.
 3. What is closure?
+A function plus that function's local environment. Variables can be declared inside the closure without being accessible from outside the closure.
 4. Describe the four rules of the 'this' keyword.
+These questions/tests are applied in order to determine what 'this' refers to:
+ - was the function called using `new`?
+ - are call(), apply(), or bind() being used?
+ - Is the function called directly as a method on the object?
+ - Is the function being called from the global scope?
 5. Why do we need super() in an extended class?
+super() gives the child access to the parent's method definitions.
--- challenges/functions.js
+++ challenges/functions.js
@@ -6,27 +6,29 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+const consume = (arg1, arg2, cb) => {return cb(arg1, arg2)}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+const add = (arg1, arg2) => {return (arg1 + arg2)};
+const multiply = (arg1, arg2) => {return (arg1 * arg2)};
+const greeting = (arg1, arg2) => {return `Hello ${arg1} ${arg2}, nice to meet you!`}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Question 1: Explain in your own words why the example below is a closure.
 
-// Explanation: 
+// Explanation: It's a combination of a function and some data that is defined inside the function.
 
-// Question 2: Given the example below, what scope is the external variable in?
+// Question 2: Given the example below, what scope is the external variable in? GLOBAL (as far as we know!)
 
 let external = "I'm outside!";
 
