df4d49f2e15db22a39ade696e0314014cee8c66e
carlos <cruz1124902gmail.com>
2018-09-19 15:46:44 -0400

    challenges/functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,3 +1,4 @@
+
 // ==== Callbacks ====  
 
 /* Step 1: Create a function that accepts a callback
@@ -6,25 +7,37 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
+function consume ( any1 , any2 , callback){
 
-
-/* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
-
-
-/* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
-
+  return callback(any1,any2);
+  }
+  
+  /* Step 2: Create several functions to callback with consume();
+    * Create a function named add that returns the sum of two numbers
+    * Create a function named multiply that returns the product of two numbers 
+    * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  */
+  function add(num1 , num2){
+     console.log(num1 + num2);
+  }
+  function multiply (num1,num2){
+  console.log (num1 * num2 );
+  }
+  function greeting(firstname, lastname){
+  console.log( "Hello, "+ firstname +' ' +lastname + ",nice to meet you! ");
+  }
+  
+  
+  /* Step 3: Check your work by uncommenting the following calls to consume(): */
+  consume(2,2,add); // 4
+  consume(10,16,multiply); // 160
+  consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation:Becuase its inside the body of the main function.
+
 
 
 const external = "I'm outside the function";
