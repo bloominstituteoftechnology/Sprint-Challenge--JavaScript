26c60f8e8c4fb9ebe5fad9742419a0f471a215b0
Aiveteran <42783946+Aiveteran@users.noreply.github.com>
2018-09-21 15:14:07 -0700

    objects and arrays push

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,13 +5,42 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
-
+class Dinosaur {
+  constructor(attributes={}) {
+    this.name = attributes.name || 'generic dinosaur' ;
+    this.diet = attributes.diet || 'herbivorous';
+    this.weight = attributes.weight || 'weight unkown';
+    this.length = attributes.length || 'length unkown';
+    this.period = attributes.period || 'period unkown';
+  }
+  roar(){
+      return "RAWERSRARARWERSARARARRRR!";
+  }
+}
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
-
+const tyrannosaurus = new Dinosaur({
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg', 
+  length: '12m',
+  period: 'Late Cretaceious'
+})
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const stegosaurus = new Dinosaur({
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+})
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
-
+const velociraptor = new Dinosaur({
+  name:'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceious'
+})
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
