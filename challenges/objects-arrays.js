d30eb22d288cd7c9ee3d23ca2c0b11bfb751d6e6
Trai Lynne <trailynne@outlook.com>
2018-10-19 10:22:30 -0600

    Obj & Arr: Defined roar() method for tyrannosaurus object

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,7 +12,10 @@ const tyrannosaurus = {
     diet: 'carnivorous',
     weight: '7000kg',
     length: '12m',
-    period: 'Late Cretaceious'
+    period: 'Late Cretaceious',
+    roar: function() {
+        return 'RAWERSRARARWERSARARARRRR!';
+    }
 }
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
