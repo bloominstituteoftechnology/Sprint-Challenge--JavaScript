b2435ddc6263976dee31f8f82d727c956f9004c2
Addison-Stavlo <AddieStavlo@gmail.com>
2018-10-19 12:23:35 -0400

    'stretch' complete

--- challenges/classes.js
+++ challenges/classes.js
@@ -24,7 +24,7 @@ class CuboidMaker {
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
 
 
-// ^^^ what sense does this make?  A cube is a cuboid where length = width = height... so all the methods defined in cuboid will still give the proper volume and surfaceArea for a cube....
+// ^^^ what sense does this make?  A cube is a cuboid where length = width = height... so all the methods defined in cuboid will still give the proper volume and surfaceArea for a cube....  below are redundantly defined methods as asked?  am i interpereting these directions wrong??
 
 class CubeMaker extends CuboidMaker{
     constructor(attributes){
