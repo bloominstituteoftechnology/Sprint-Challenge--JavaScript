329f7f3eed8b34bbcdfff1a524722c94edb65fb3
Trai Lynne <trailynne@outlook.com>
2018-10-19 10:14:39 -0600

    Obj & Arr: Created tyrannosaurus object

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,6 +7,13 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
+const tyrannosaurus = {
+    objectName: 'tyrannosaurus',
+    diet: 'carnivorous',
+    weight: '7000kg',
+    length: '12m',
+    period: 'Late Cretaceious'
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
@@ -31,16 +38,67 @@ console.log(tyrannosaurus.roar());
 
 // Given an array of college graduates.  Complete the following requests WITHOUT using any array methods like .forEach(), .map(), .reduce(), .filter()
 
-const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
-{"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
-{"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
-{"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
-{"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
-{"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
-{"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
-{"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
-{"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
-{"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
+const graduates = [{
+        "id": 1,
+        "first_name": "Cynde",
+        "university": "Missouri Southern State College",
+        "email": "ctorry0@macromedia.com"
+    },
+    {
+        "id": 2,
+        "first_name": "Saundra",
+        "university": "The School of the Art Institute of Chicago",
+        "email": "swhal1@state.gov"
+    },
+    {
+        "id": 3,
+        "first_name": "Lambert",
+        "university": "Marian College",
+        "email": "lparham2@techcrunch.com"
+    },
+    {
+        "id": 4,
+        "first_name": "Modestine",
+        "university": "International Medical & Technological University",
+        "email": "mdolder3@symantec.com"
+    },
+    {
+        "id": 5,
+        "first_name": "Chick",
+        "university": "Sultan Salahuddin Abdul Aziz Shah Polytechnic",
+        "email": "camorts4@google.com.au"
+    },
+    {
+        "id": 6,
+        "first_name": "Jakob",
+        "university": "Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft",
+        "email": "jharken5@spiegel.de"
+    },
+    {
+        "id": 7,
+        "first_name": "Robbi",
+        "university": "Salem University",
+        "email": "rbrister6@redcross.org"
+    },
+    {
+        "id": 8,
+        "first_name": "Colline",
+        "university": "Coastal Carolina University",
+        "email": "cbrosh7@alibaba.com"
+    },
+    {
+        "id": 9,
+        "first_name": "Michail",
+        "university": "Universidad Católica de Ávila",
+        "email": "mrome8@shinystat.com"
+    },
+    {
+        "id": 10,
+        "first_name": "Hube",
+        "university": "Universitat Rovira I Virgili Tarragona",
+        "email": "hlethbrig9@foxnews.com"
+    }
+]
 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
@@ -68,16 +126,67 @@ console.log(uni);
 // Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
 
 
-zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":"Canis aureus","state":"Kentucky"},
-{"animal_name":"Screamer, southern","population":1,"scientific_name":"Chauna torquata","state":"Alabama"},
-{"animal_name":"White spoonbill","population":8,"scientific_name":"Platalea leucordia","state":"Georgia"},
-{"animal_name":"White-cheeked pintail","population":1,"scientific_name":"Anas bahamensis","state":"Oregon"},
-{"animal_name":"Black-backed jackal","population":2,"scientific_name":"Canis mesomelas","state":"Washington"},
-{"animal_name":"Brolga crane","population":9,"scientific_name":"Grus rubicundus","state":"New Mexico"},
-{"animal_name":"Common melba finch","population":5,"scientific_name":"Pytilia melba","state":"Pennsylvania"},
-{"animal_name":"Pampa gray fox","population":10,"scientific_name":"Pseudalopex gymnocercus","state":"Connecticut"},
-{"animal_name":"Hawk-eagle, crowned","population":10,"scientific_name":"Spizaetus coronatus","state":"Florida"},
-{"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
+zooAnimals = [{
+        "animal_name": "Jackal, asiatic",
+        "population": 5,
+        "scientific_name": "Canis aureus",
+        "state": "Kentucky"
+    },
+    {
+        "animal_name": "Screamer, southern",
+        "population": 1,
+        "scientific_name": "Chauna torquata",
+        "state": "Alabama"
+    },
+    {
+        "animal_name": "White spoonbill",
+        "population": 8,
+        "scientific_name": "Platalea leucordia",
+        "state": "Georgia"
+    },
+    {
+        "animal_name": "White-cheeked pintail",
+        "population": 1,
+        "scientific_name": "Anas bahamensis",
+        "state": "Oregon"
+    },
+    {
+        "animal_name": "Black-backed jackal",
+        "population": 2,
+        "scientific_name": "Canis mesomelas",
+        "state": "Washington"
+    },
+    {
+        "animal_name": "Brolga crane",
+        "population": 9,
+        "scientific_name": "Grus rubicundus",
+        "state": "New Mexico"
+    },
+    {
+        "animal_name": "Common melba finch",
+        "population": 5,
+        "scientific_name": "Pytilia melba",
+        "state": "Pennsylvania"
+    },
+    {
+        "animal_name": "Pampa gray fox",
+        "population": 10,
+        "scientific_name": "Pseudalopex gymnocercus",
+        "state": "Connecticut"
+    },
+    {
+        "animal_name": "Hawk-eagle, crowned",
+        "population": 10,
+        "scientific_name": "Spizaetus coronatus",
+        "state": "Florida"
+    },
+    {
+        "animal_name": "Australian pelican",
+        "population": 5,
+        "scientific_name": "Pelecanus conspicillatus",
+        "state": "West Virginia"
+    }
+];
 
 /* Request 1: .forEach()
 
@@ -94,7 +203,7 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 */
 
 const lowerCase = [];
-console.log(lowerCase); 
+console.log(lowerCase);
 
 /* Request 3: .filter() 
 
@@ -118,4 +227,3 @@ console.log(populationTotal);
 Stretch: If you haven't already, convert your array methods callbacks into arrow functions.
 
 */
-
