9183486a95af0eae10fc247bd3defdd4b5169b15
Civoremo <omerovic_n@hotmail.com>
2018-10-19 12:18:07 -0400

    prototypes.js complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,14 +6,18 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(param1, param2, cb) {
+  return cb(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+consume("My Name", 24, function(callback){
+  console.log(callback);
+});
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
 //consume(2,2,add); // 4
