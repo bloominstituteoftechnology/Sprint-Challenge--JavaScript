45fcf83e2551e8a74d232b83d9ee90c01b053cae
jheinz1999 <jonathan.heinz@aol.com>
2018-10-19 09:09:02 -0700

    finished stretch in classes.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,32 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-class CuboidMaker {
+class IShapeMaker3d { // Interface for making shapes
+
+  constructor() {
+
+
+
+  }
+
+  getVolume() {
+
+
+
+  }
+
+  getSurfaceArea() {
+
+
+
+  }
+
+}
+
+class CuboidMaker extends IShapeMaker3d {
 
   constructor(length, width, height) {
+
+    super();
 
     this.length = length;
     this.width = width;
@@ -31,3 +55,33 @@ console.log(cuboid.getVolume()); // 100
 console.log(cuboid.getSurfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+// this makes no sense, because a cube is not a derivative of a cuboid. These classes would not share anything.
+class CubeMaker extends IShapeMaker3d {
+
+  constructor(sideLength) {
+
+    super();
+
+    this.sideLength = sideLength;
+
+  }
+
+  getVolume() {
+
+    return Math.pow(this.sideLength, 3);
+
+  }
+
+  getSurfaceArea() {
+
+    return 6 * this.sideLength * this.sideLength;
+
+  }
+
+}
+
+const cube = new CubeMaker(3);
+
+console.log(cube.getVolume());
+console.log(cube.getSurfaceArea());
