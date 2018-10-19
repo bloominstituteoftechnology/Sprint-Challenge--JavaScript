263b458ebeba47f666d2c1e9ec54b6ec97ce1409
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-10-19 11:13:56 -0500

    add solution to closure tasks in functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,9 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
+function consume(param1, param2, callback) {
+  return callback(param1, param2);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,18 +16,28 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(n1, n2) {
+  return n1 + n2;
+}
 
+function multiply(n1, n2) {
+  return n1 * n2;
+}
+
+function greeting(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: Because in JavaScript, functions form closure. So eventhough variable internal does not exist in the local-scope of nestedFunction, its closure gives access to outer function's variable where variable internal does exist and use it as a fallback.
 
 
 const external = "I'm outside the function";
