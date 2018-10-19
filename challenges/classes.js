217d97bbf6918595ffa0ea7884c16fce43e51349
David Situ <davidsitu626@gmail.com>
2018-10-19 13:13:20 -0700

    David Situ Javascript Sprint Challenge stretch class.js stretch completed

--- challenges/classes.js
+++ challenges/classes.js
@@ -14,13 +14,32 @@ class CuboidMaker{
   }
 }
 
-const cuboid = new CuboidMaker({
+class CubeMaker extends CuboidMaker{
+  constructor(cubeDimensions){
+    super(cubeDimensions);
+    this.side = cubeDimensions.side;
+  }
+  cubeVolume(){
+    return this.side ** 3;
+  }
+  cubeSA(){
+    return 6*(this.side ** 2);
+  }
+}
+
+const cuboid = new CubeMaker({
   length: 4,
   width: 5,
-  height: 5
+  height: 5,
+  side: 4
 });
 
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+console.log(cuboid.cubeVolume()); //64
+console.log(cuboid.cubeSA()); //96
+// // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.
+//  Find out the formulas for volume and surface area for cubes and create those
+//  methods using the dimension properties from CuboidMaker.
+//  Test your work by logging out your volume and surface area.
