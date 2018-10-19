7d5a94920c53fae13a8fd0b1627cf195550446c7
Trai Lynne <trailynne@outlook.com>
2018-10-19 11:44:22 -0600

    Adv Arrays Req  4: .reduce total up animal population

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -264,7 +264,9 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal;
+const populationTotal = zooAnimals.reduce((total, element) => {
+    return total + element.population;
+}, 0);
 console.log(populationTotal);
 
 
