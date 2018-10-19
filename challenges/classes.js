a5228f55b3dc0c17db71884567ffffb607f7eff6
Civoremo <omerovic_n@hotmail.com>
2018-10-19 12:25:51 -0400

    classes.js complete w/o stretch

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,30 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMakers {
+    constructor (cubAtrib) {
+        this.length = cubAtrib.length;
+        this.width = cubAtrib.width;
+        this.height = cubAtrib.height;
+    }
+    volume() {
+        return this.length * this.width * this.height;
+    }
+    surfaceArea() {
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    }
+}
+
+
+const box = new CuboidMakers({
+    length: 4,
+    width: 5,
+    height: 5,
+});
+
+
+console.log(box.volume() + " classes"); // 100
+console.log(box.surfaceArea() + " classes"); // 130
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -44,7 +44,7 @@ const cuboid = new CuboidMaker({
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume() + " prototype"); // 100
+console.log(cuboid.surfaceArea() + " prototype"); // 130
 
 
