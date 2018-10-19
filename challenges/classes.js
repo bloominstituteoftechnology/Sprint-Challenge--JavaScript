8f2ac4258944c6081cf559035a38c5f4be4c37a2
Jarrad Miller <jm020801@gmail.com>
2018-10-19 09:51:32 -0700

    finished sprint

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,19 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
-
+class CuboidMaker{
+    constructor(param){
+      this.length = param.length;
+      this.width = param.width;
+      this.height = param.height;
+    }
+    volume(){
+      return this.length * this.width * this.height;
+    }
+    surfaceArea(){
+      return  2*((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
+    }
+  }
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
--- challenges/functions.js
+++ challenges/functions.js
@@ -6,25 +6,36 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume (param1, param2, callback){
+  return callback (param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+function add(param1, param2){
+  return (param1 + param2);
+  }
+  function multiply(param1, param2){
+  return ( param1 * param2);
+  }
+  function greeting(param1,param2){
+  return ( `Hello ${param1} ${param2}, nice to meet you!`);
+  }
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: nestedFunction will have access to myFunction and the external variable even though my function is called on the bottom of the page, it chains up,
+// The inner function has access to the variables in the outer function scope, even after the outer function has returned.
 
 
 const external = "I'm outside the function";
