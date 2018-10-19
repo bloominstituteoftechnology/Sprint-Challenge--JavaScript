6e39c006b1e8ba6510c10005152688089e369ed1
Trai Lynne <trailynne@outlook.com>
2018-10-19 11:40:19 -0600

    Adv Arrays Req  2: .map collect animal names as all lowercase letters

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -242,7 +242,10 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map(element => {
+    return element.animal_name.toLowerCase();
+});
+
 console.log(lowerCase);
 
 /* Request 3: .filter() 
