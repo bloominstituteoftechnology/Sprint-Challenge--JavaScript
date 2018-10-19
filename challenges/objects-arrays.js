7de990d5c54712b3f9402783c42597d869284cb4
Trai Lynne <trailynne@outlook.com>
2018-10-19 10:18:11 -0600

    Obj & Arr: Created velociraptor object

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -27,6 +27,14 @@ const stegosaurus = {
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
 
+const velociraptor = {
+    objectName: 'velociraptor',
+    diet: 'carnivorous',
+    weight: '15kg',
+    length: '1.8m',
+    period: 'Late Cretaceious'
+}
+
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
