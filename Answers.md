39f3f5d1186851942fd055bcb68d5813f145d83b
David Situ <davidsitu626@gmail.com>
2018-10-19 12:46:19 -0700

    David Situ Javascript Sprint Challenge complete, stretch pending

--- Answers.md
+++ Answers.md
@@ -1,1 +1,34 @@
+1. Describe the biggest difference between `.forEach` & `.map`.
 
+While both methods are able to apply some function to each element of the array,
+.forEach mutates the calling array, but .map returns the new values to a new array.
+
+2. What is the difference between a function and a method?
+
+Functions and methods are both functions. The only difference is that functions are global and methods are tied to an object as a property.
+
+3. What is closure?
+
+Closure describes the accessibility of a chain of outer and inner functions. Inner functions (nested functions) have access to the global scope variables, its "parent" function's variable and its own. But it does not have access to use its own inner function's variables. In this way, there is a cascading effect of accessibility  from outer function to inner function to inner-inner function, with the inner-inner function having the most accessibility and the outer function having no accessibility to its nested function's variables.
+
+4. Describe the four rules of the 'this' keyword.
+
+a. window/global binding
+
+  If 'this' is not called with a method or property, it will be tied to the global scope automatically.
+
+b. implicit binding
+
+  When 'this' is called with a method or property (this.name), it automatically binds/refers to the object it was called in.
+
+c. explicit binding
+
+  When 'this' is bind/referenced to an object elsewhere using .call, .apply, or .bind.
+
+d. 'new' binding
+
+  'new' creates a new object which inherits from a prototype of another object. However, 'this' used within the new object is bind to the new object.
+
+5. Why do we need super() in an extended class?
+
+  The keyword super is used to access functions on an object's parent. It must be used in an extended class so that it can inherit the functions and prototype methods of the parent.
--- challenges/functions.js
+++ challenges/functions.js
@@ -1,30 +1,43 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====
 
 /* Step 1: Create a function that accepts a callback
   * Create a function named consume that can take 3 parameters.
   * The first two parameteres can accept any argument
-  * The last parameter accepts a callback 
+  * The last parameter accepts a callback
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consumed(x, y, cb){
+  return cb(x, y);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named multiply that returns the product of two numbers
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+const add = function(x,y){
+  console.log(x+y);
+}
+const multiply = function(x,y){
+  console.log(x*y);
+}
+const greeting = function(x, y){
+  console.log(`Hello, ${x} ${y}, nice to meet you!`);
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
-// ==== Closures ==== 
+// ==== Closures ====
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+ // The nestedFunction() has access to global scope variables, its 'parent' function's variable, and its own.
+ // Because its parent function is myFunction(), nestedFunction() has access to variable 'internal'.
+
+// Explanation:
 
 
 const external = "I'm outside the function";
