aaa6089f862a1c679457a145a0e6fd04ca945665
Alex <alexander.t.carter@gmail.com>
2018-10-19 12:16:00 -0700

    vanilla final

--- Answers.md
+++ Answers.md
@@ -1,1 +1,9 @@
+1. Map creates a new array by applying the same function to every element in the originating array whereas forEach applies any amount of custo logic to each element in the originating array.
 
+2. In javascript everything function is an object and an object is a collection of key:value pairs. If a value is primitive (integar, string, etc.) it's considered a property, if it's a function it's considered a method. You can say methods accept functions as parameters but you wouldn't generally say that a function can accept a method as a parameter. (TBH my understanding of this is still a little shakey).
+
+3. Closure is the ability of javascript's 2-pass compiler to reference variables that would otherwise be out of the local scope. They are accesible, however, because they are within a larger family of nested objects/functions.
+
+4. The four rules of the 'this' keyword refer to how values for this are assigned. #1 default binding (Window), #2 Implicit Binding, #3 Explicit binding, #4 new binding (instantiating a new object)
+
+5. The super keyword is used to access and call functions on an object's parent. By calling the parent constructor we can assign values to the parent's attributes.
--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(a, b, callback){
+  return callback(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,17 +17,28 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a, b){
+  return a+b;
+}
+
+function multiply(a, b){
+  return a*b;
+}
+
+function greeting(first_name, last_name){
+  return `Hello ${first_name} ${last_name}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: the 2-pass compiler grants access to variables that would not normally be accessed locally by looking at the nested function chain and incorporaitng umstream variables
 
 
 const external = "I'm outside the function";
