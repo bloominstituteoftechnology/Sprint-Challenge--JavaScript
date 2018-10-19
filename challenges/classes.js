be25fbdf00aef0557b3ac73a020accc3e6cd1288
tryingtokeepup <kl.hawaii@gmail.com>
2018-10-19 10:39:57 -0700

    added some clever math, put new variable for new constructor CubeMaker, good to go

--- challenges/classes.js
+++ challenges/classes.js
@@ -32,4 +32,26 @@ const cuboid = new CuboidMaker({
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  
+//Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  
+//Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker {
+    constructor(param) {//screw it call it param
+        super(param);
+        this.side = param.side
+    }
+    volume() {
+        return this.side**3
+    }
+    surfaceArea() {
+        return 6* (this.side**2);
+    }
+}
+
+const cube = new CubeMaker({
+    side: 3
+  });
+  
+console.log(cube.volume());
+console.log(cube.surfaceArea());
