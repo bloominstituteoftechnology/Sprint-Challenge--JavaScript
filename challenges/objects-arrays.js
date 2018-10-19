ba532d4dd4abfdff7b753adcb86cdf618572bddf
Kevin Cornea <kpcornea@gmail.com>
2018-10-19 09:42:00 -0700

    objects-arrays completed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,9 +8,30 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
 
+const tyrannosaurus = {
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceious"
+}
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stegosaurus = {
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic"
+}
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+
+const velociraptor = {
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceious"
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
@@ -24,6 +45,11 @@ console.log(stegosaurus.length);
 console.log(tyrannosaurus.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
+
+tyrannosaurus.roar = function() {
+  return "RAWERSRARARWERSARARARRRR!";
+}
+
 console.log(tyrannosaurus.roar());
 
 
@@ -45,8 +71,19 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
-console.log(universities)
+
+for (let i = 0; i < graduates.length; i++) {
+  let student = graduates[i];
+  let uni = student.university;
+
+  universities.push(uni);
+}
+
+const sortedUni = universities.sort();
+
+console.log(sortedUni);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -55,11 +92,30 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+for (let i = 0; i < graduates.length; i++) {
+  let student = graduates[i];
+  let first = student.first_name;
+  let email = student.email;
+
+  contactInfo.push(`${first} ${email}`);
+}
+
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+
+for (let i = 0; i < graduates.length; i++) {
+  let student = graduates[i];
+  let school = student.university;
+
+  if (school.indexOf("Uni") > -1) {
+    uni.push(school);
+  }
+}
+
 console.log(uni);
 
 
@@ -85,6 +141,11 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+
+zooAnimals.forEach(ele => {
+  animalNames.push(`Name: ${ele.animal_name}, Scientific: ${ele.scientific_name}.`);
+});
+
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -92,8 +153,8 @@ console.log(animalNames);
 The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 
 */
+let lowerCase = zooAnimals.map(ele => ele.animal_name.toLowerCase());
 
-const lowerCase = [];
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -101,7 +162,10 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+let largerPopulation = []; // smaller population?
+
+largerPopulation = zooAnimals.filter(ele => ele.population < 5);
+
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -109,9 +173,13 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal;
-console.log(populationTotal);
+let populationTotal = 0;
 
+const reducer = (acc, curVal) => acc + curVal.population;
+
+populationTotal = zooAnimals.reduce(reducer, 0);
+
+console.log(populationTotal);
 
 /* 
 
