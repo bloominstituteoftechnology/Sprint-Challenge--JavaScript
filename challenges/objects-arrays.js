8d3eeee54ddef6810860c8c7bfd8a3907cb67543
Yasirah Krueng <yasirah.krueng@gmail.com>
2018-10-19 11:02:14 -0500

    calculate population total of zooAnimal in objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -147,7 +147,7 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal;
+const populationTotal = zooAnimals.reduce((acc, e) => acc + e.population, 0);
 console.log(populationTotal);
 
 
