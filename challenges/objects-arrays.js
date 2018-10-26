f4c689f136da43dc5656c2830cd77a8e6377824c
chrisgins <christiansgina@gmail.com>
2018-10-26 10:28:05 -0600

    create dino roar

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -48,7 +48,10 @@ console.log(stegosaurus.length);
 console.log(tyrannosaurus.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
+
+tyrannosaurus.roar = function(){return 'RAWERSRARARWERSARARARRRR!';}
 console.log(tyrannosaurus.roar());
+
 
 
 // ==== Arrays ====
