ba0e41a15e90eaa15498dfd3de4012665ee12967
chrisgins <christiansgina@gmail.com>
2018-10-26 10:39:03 -0600

    request 2 - .map to lower case

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -145,8 +145,9 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
-console.log(lowerCase); 
+const lowerCase = zooAnimals.map(animal => animal.animal_name.toLowerCase());
+console.log(lowerCase);
+ 
 
 /* Request 3: .filter() 
 
