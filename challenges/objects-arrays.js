93d29a4f953f440c58e03d00241e6200fb858a2d
chrisgins <christiansgina@gmail.com>
2018-10-26 10:35:21 -0600

    request 3 - # of schools with Uni in name

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -99,8 +99,17 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+
 const uni = [];
+for(let i = 0; i < graduates.length; i++){
+  if (graduates[i].university.includes("Uni")){
+    uni.push(graduates[i].university);
+  }
+}
 console.log(uni);
+console.log(`There are ${uni.length} universities with \"Uni\" in their name`);
+
+
 
 
 // ==== ADVANCED Array Methods ====
