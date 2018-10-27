05b0ee91ff2387fa683345cd5cfefee9896ba01a
Arpita Sinha <sinha.arpita@gmail.com>
2018-10-27 11:45:40 -0700

    MVP and strech done

--- challenges/classes.js
+++ challenges/classes.js
@@ -4,4 +4,61 @@
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.
+//  Find out the formulas for volume and surface area for cubes and create those methods
+// using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+/* == Step 1: Base Constructor ==
+  Create a constructor function named CuboidMaker that accepts properties for length, width, and height
+*/
+class CuboidMaker{
+   constructor(attributes) {
+       this.length = attributes.length;
+       this.width = attributes.width;
+       this.height = attributes.height;
+   }
+   volume(){
+       const value =this.length*this.width*this.height;
+       return(`The volume of the cuboid is ${value} `);
+   }
+
+   surfaceArea(){
+       const value = 2 * (this.length *this.width + this.length * this.height + this.width * this.height);
+       return(`The surfaceArea  of the cuboid is ${value} `);
+
+   }
+}
+
+
+
+const cuboid = new CuboidMaker({
+    length:4,
+    width:5,
+    height:5
+
+})
+
+// Test your volume and surfaceArea methods by uncommenting the logs below:
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
+
+class CubeMaker extends CuboidMaker{
+    constructor(cubeAttributes){
+        super(cubeAttributes);
+    }
+    volume(){
+        const value =this.length*this.length*this.length;
+        return(`The volume of the cuboid is ${value} `);
+    }
+
+    surfaceArea(){
+        const value=6*this.length*this.length;
+        return(`The volume of the cuboid is ${value} `);
+    }
+}
+const cube = new CubeMaker({
+    length:7,
+    width:7,
+    height:7
+})
+console.log(cube.volume());
+console.log(cube.surfaceArea());
--- challenges/functions.js
+++ challenges/functions.js
@@ -7,24 +7,51 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume (x,y,callback){
+  return callback(x,y);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  * Create a function named greeting that accepts a first and last name and returns
+  * "Hello first-name last-name, nice to meet you!"
 */
+function add(x,y){
+   return x+y;
+}
+
+function multiply(x,y){
+    return x*y;
+
+}
+
+function greeting(firstName,lastName){
+    return (`Hello ${firstName} ${lastName},nice to meet you!`);
+
+}
 
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation:
+//The "nestedfunction()" can access the variable "internal" because of "closures".In
+// JavaScript "closures" works in a function's scope.when a function is declared and created ,a
+// new scope is created.Any variables declared within that function's scope will be enclosed in
+// a lexical/private scope that belongs to that function.If some variable isn't defined in a function's
+// scope,the function looks outside the scope chain and search for a variable being referenced in the
+// outer scope.
+//Here, in the below example nestedFunction() doesn't have the variable "internal" ,so the function
+// looks outward for context and gets it in its parent function.
+
 
 
 const external = "I'm outside the function";
