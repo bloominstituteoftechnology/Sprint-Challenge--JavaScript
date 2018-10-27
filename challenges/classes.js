264575df0a6f2d5ede9cca9fd0ea8d0bed41949a
Alando Appling <alandoappling@gmail.com>
2018-10-27 11:06:31 -0700

    Finished MVP

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,46 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
+class CuboidMaker{
+    constructor(attr){
+        this.length = attr.length;
+        this.height = attr.height;
+        this.width = attr.width;
+    }
+    volume(){
+        const volume = this.length * this.width * this.height
+        return volume;
+    }
+    surfaceArea(){
+        const surfaceArea = 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+        return surfaceArea;
+    }
+}
 
+const cuboid = new CuboidMaker({
+    length: 4,
+    width: 5,
+    height: 5
+})
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker{
+    constructor(cubeAttr){
+        super(cubeAttr)
+    }
+    cubeSurfaceArea(){
+        const squareSurfaceArea = 6 * this.height * this.width
+        return squareSurfaceArea;
+    }
+}
+
+const squared = new CubeMaker({
+    length:6,
+    width:6,
+    height:6
+})
+
+console.log(squared.volume());
+console.log(squared.cubeSurfaceArea());
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -63,7 +63,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = graduates.map(x => x.university);
+const universities = [];
+for(let i = 0; i < graduates.length; i++){
+  universities.push(graduates[i].university)
+}
 universities.sort();
 console.log(universities)
 
@@ -73,12 +76,18 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-const contactInfo = graduates.map(x => `${x.first_name} ${x.email}`);
+const contactInfo = [];
+for(let i = 0;i < graduates.length; i++){
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`)
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
-const uni = graduates.filter(x => x.university.includes('Uni'));
+const uni = [];
+for(let i = 0;i < graduates.length; i++){
+  uni.push(graduates[i].university.includes('Uni').length)
+}
 console.log(uni);
 
 
