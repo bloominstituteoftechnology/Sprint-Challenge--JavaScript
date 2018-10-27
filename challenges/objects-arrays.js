0c9ca16284483b5b97f11e1b38c3adb4673747cc
Jonathan Dorety <kyullog@gmail.com>
2018-10-27 13:00:20 -0400

    Complete array Request 3 in objects-array.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -84,6 +84,9 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (let i of graduates) {
+  if (i.university.includes("Uni")) uni.push(i.university);
+}
 console.log(uni);
 
 
