aec2197642ad073e461de28c24a494812110c929
Jonathan Dorety <kyullog@gmail.com>
2018-10-27 12:57:39 -0400

    Complete array Request 2 in objects-array.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -76,6 +76,9 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (let i of graduates) {
+  contactInfo.push(`${i.first_name} ${i.email}`)
+}
 console.log(contactInfo);
 
 
