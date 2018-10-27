1fcd0b9bb257359eb7b62826b99fbe6af6385ae1
Leila Berrouayel <nb.leila10@gmail.com>
2018-10-27 12:04:15 -0500

    Complete Request 3 in ADVANCED Array methods section
    Create largerPopulation that contains animals that have a population less than 5 using filter method

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -141,10 +141,11 @@ console.log(lowerCase);
 
 /* Request 3: .filter() 
 
-The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
+The zoos are concerned about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+let largerPopulation = [];
+largerPopulation = zooAnimals.filter(animal => animal.population < 5);
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
