8f55fdefd8add14e904c818c1cbfe2a4b9393e9a
iradsanchez <ira.d.sanchez@gmail.com>
2018-10-27 11:05:21 -0700

    Sprint Complete

--- Answers.md
+++ Answers.md
@@ -1,1 +1,25 @@
+1. Describe the biggest difference between `.forEach` & `.map`.
+* _.forEach_ iterates over an array while _.map_ makes a copy of that array using the output of a function performed on the original array's elements. 
+
+2. What is the difference between a function and a method?
+* A method is an function that is part of an object or class; it is non-global. Regular functions, if you will, are essentially methods in the global scope. 
+
+3. What is closure?
+* A closure is the combination of a function and the lexical environment within which that function was declared. It means that the location of a variable's declaration does determine its usability.
+
+4. Describe the four rules of the 'this' keyword.
+
+* In general, _this_ in Javascript has a meaning very similar to 
+_this_ in English. It points to something. 
+
+* 1 - In global scope, _this_ is pointing to the whole window. 
+
+* 2 - object.function(); functions on the right of . _this_ on the left. It remains true if you stack objects in the notation like so: parentObject.childObject.function(); _this_ is childObject here.
+
+* 3 - In a constructor function, _this_ refers to the product of the function, not the function itself. 
+
+* 4 - Just like in English, _this_ is a pronoun, a shortcut used instead of directly naming something, used to save one from repetition. Normally, _this_ is used to indirectly, or implicitly, point to something. You can explicitly point to things with _this_, and one uses call and apply to do so.
+
+5. Why do we need super() in an extended class?
+* _super()_ is how the attributes and methods are inherited by the child class from the parent class.
 
--- challenges/functions.js
+++ challenges/functions.js
@@ -4,28 +4,48 @@
   * Create a function named consume that can take 3 parameters.
   * The first two parameteres can accept any argument
   * The last parameter accepts a callback 
-  * In the body of the function return the callback with the two parameters that you created
+  * In the body of the function return the callback
+  *  with the two parameters that you created
 */
+consume = (param1, param2, cb) => {
+  return cb(param1, param2);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  * Create a function named greeting that accepts a first and last 
+  * name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+add= (param1, param2) => {
+  return param1 + param2;
+}
+console.log(consume(7, 2, add));
+
+multiply = (param1, param2) => {
+  return param1*param2;
+}
+console.log(consume(7, 2, multiply));
+
+greeting = (firstName, lastName) => {
+  return `Hello, ${firstName} ${lastName}, nice to meet you!`;
+}
+console.log(consume('Ira', 'Sanchez', greeting));
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add));
+console.log(consume(10,16,multiply)); 
+console.log(consume("Mary","Poppins", greeting)); 
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+// Nested functions can access variables that are declared in their parent functions.
+// This is due to lexical scoping, where the parser determines the validity of a variable name call based on where it was originally declared.
 
 const external = "I'm outside the function";
 
