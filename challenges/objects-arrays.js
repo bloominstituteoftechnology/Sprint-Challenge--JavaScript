085c67760d19f402ef9e5bb6177909fc7318167b
Andrew <acarmstrong98@gmail.com>
2018-10-27 14:25:08 -0400

    objects-arrays.js and prototypes.js finished.

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,9 +8,38 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
 
+let tyrannosaurus = {
+  'name': 'tyrannosaurus',
+  'diet': 'carnivorous',
+  'weight': '7000kg',
+  'length': '12m',
+  'period': 'Late Cretaceious',
+  'speak': 'RAWERSRARARWERSARARARRRR!'
+}
+
+function roar (){
+  console.log(this.speak)
+}
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+let stegosaurus = {
+  'name': 'stegosaurus',
+  'diet': 'herbivorous',
+  'weight': '2000kg',
+  'length': '9m',
+  'period': 'Late Jurassic',
+}
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+
+let velociraptor = {
+  'name': 'velociraptor',
+  'diet': 'carnivorous',
+  'weight': '15kg',
+  'length': '1.8m',
+  'period': 'Late Cretaceious',
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
@@ -43,9 +72,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
-
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for (let i = 0; i < graduates.length; i++){
+  if (graduates[i] = university)
+}
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
