a1d90eaf22356311e5a94e790c73fb6dbe8d0f0d
dickwinters <coding1965@gmail.com>
2018-10-27 11:44:01 -0500

    first draft of array methods

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,10 +7,35 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
+const tyrannosaurus = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceious'
+  roar: function() {
+    return "RAWERSRARARWERSARARARRRR!"; 
+  }
+}
+
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stegosaurus = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+const velociraptor = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceious'
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
@@ -42,9 +67,19 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
 {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
-/* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
+//  Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
+//
 
-Once you have the new array created, sort the universities alphabetically and log the result. */
+const universitiyArray = (graduates) => {
+  for (let i = 0; i < graduates.length; i++) {
+    universities.push(graduates[i].university)
+    universities = universities.sort();
+  }
+}
+
+// Once you have the new array created, sort the universities alphabetically and log the result. |)}>#
+
+
 const universities = [];
 console.log(universities)
 
@@ -54,11 +89,27 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
+
+const contactArray = (graduates) => {
+  for (let i = 0; i < graduates.length; i++) {
+    contactInfo.push(`${first_name} ${email}`)
+  }
+}
+
+
 const contactInfo = [];
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+
+const containUni = (graduates) {
+  for (let i = 0; i < graduates.length; i++) {
+    if (graduates[i].university.includes("Uni")) {
+      uni.push(graduates[i])
+    }
+  }
+}
 const uni = [];
 console.log(uni);
 
