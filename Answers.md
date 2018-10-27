4b945a771105fda1a0893706e8b92721489b81ac
rberriz <rberriz@icloud.com>
2018-10-27 13:53:48 -0400

    Final Sprint Submission w/ Stretch Goals

--- Answers.md
+++ Answers.md
@@ -1,1 +1,25 @@
+1. Describe the biggest difference between `.forEach` & `.map`.
+Map creates a new array. forEach doesnt
 
+
+
+2. What is the difference between a function and a method?
+
+A function can be global scope and is called to perform a task on other variables. A method is a function within a function that cannot be accessed elsewhere.
+
+
+3. What is closure?
+Closure is when nested functions can reference varaibles set in its parents.
+
+
+
+4. Describe the four rules of the 'this' keyword.
+    1. When it is assigned to something in global scope, this refers to the global scope.
+    2. Whenever 'this' has to be called by a dot, it is implicitly bound to the .function
+    3. When used in constructor functions this refers to the specific instance of that object that was created. 
+    4. If we use call or apply than this is explicitly bound.
+
+
+
+5. Why do we need super() in an extended class?
+Super allows us to access the functions of the parent
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,10 +7,38 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
-
+const tyrannosaurus = {
+  "name": "tyrannosaurus",
+  "diet": "carnivorous",
+  "weight": "7000kg",
+  "length": "12m",
+  "period":  "Late Cretaceious",
+  roar: function() {
+     return "RAWERSRARARWERSARARARRRR!";
+  }
+};
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stegosaurus = {
+  "name": "stegosaurus",
+  "diet": "herbivorous",
+  "weight": "2000kg",
+  "length": "9m",
+  "period":  "Late Jurassic",
+  speak: function() {
+     return "RAWERSRARARWERSARARARRRR!";
+  }
+};
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+
+const velociraptor = {
+  "name": "velociraptor",
+  "diet": "carnivorous",
+  "weight": "15kg",
+  "length": "1.8m",
+  "period":  "Late Cretaceious"
+};
 
 // Using your dinosaur objects, log answers to these questions:
 
@@ -46,7 +74,11 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+for (let i=0; i < graduates.length; i++) {
+  universities.push(graduates[i].university);
+}
+universities.sort();
+console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -55,11 +87,22 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+for (let i=0; i < graduates.length; i++) {
+  contactInfo.push(graduates[i].first_name + ' ' + graduates[i].email);
+}
 console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+
+for (let i=0; i < graduates.length; i++) {
+  if (graduates[i].university.includes('Uni')) {
+    uni.push(graduates[i].university)
+  }
+}
+
 console.log(uni);
 
 
@@ -85,6 +128,7 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+zooAnimals.forEach(x => animalNames.push(x.animal_name + ' ' + x.scientific_name))
 console.log(animalNames);
 
 /* Request 2: .map()    
@@ -93,7 +137,9 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+
+let lowerCase = zooAnimals.map(x => x.animal_name.toLowerCase())
+
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -101,7 +147,10 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+
+
+let largerPopulation = zooAnimals.filter(x => x.population < 5);
+
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
@@ -109,7 +158,10 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal;
+
+
+let populationTotal = zooAnimals.reduce((a,b) => a + b.population, 0)
+
 console.log(populationTotal);
 
 
