53fd21b13ada0f32311a681a8b77752be7555197
Jason Cook <the.root.user@gmail.com>
2018-10-27 13:55:33 -0400

    Complete functions challenge

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,13 +7,19 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+const consume = (param1, param2, callback) => {
+
+  return callback(param1, param2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+const add = (x, y) => x + y;
+const multiply = (x, y) => x * y;
+const greeting = (first, last) => `Hello ${first} ${last}, nice to meet you!`
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
 //consume(2,2,add); // 4
@@ -25,7 +31,7 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-
+// Because it is within the function scope, which the inner function has access to via the scope chain. More or less it's an encapsulation of data that is accesible and then readmissible to the original context from which it was referred, potentially in a transformative way.
 
 const external = "I'm outside the function";
 
