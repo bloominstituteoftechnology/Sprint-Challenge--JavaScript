fc0771fa0104d94d9e212d69ec10d1efd0091b43
Angie Lawrence <lawrenceangie45@gmail.com>
2018-10-27 14:42:00 -0400

    Completed all of objects-arrays.js but .forEach

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,74 +7,74 @@
 */
 
 //tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
-// const tyrannosaurus = {
-//   name: 'tryannosaurus',
-//   diet: 'carnivorous',
-//   weight: '7000kg',
-//   length: '12m',
-//   period: 'Late Cretaceious',
-//   roar: function () {
-//     return 'RAWERSRARARWERSARARARRRR!';
-//   }
-// };
+const tyrannosaurus = {
+  name: 'tryannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceious',
+  roar: function () {
+    return 'RAWERSRARARWERSARARARRRR!';
+  }
+};
 
-// // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-// const stegosaurus = {
-//   name: 'stegosaurus',
-//   diet: 'herbivorous',
-//   weight: '2000kg',
-//   length: '9m',
-//   period: 'Late Jurassic',
-// };
-// // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
-// const velociraptor = {
-//   name: 'velociraptor',
-//   diet: 'carnivorous',
-//   weight: '15kg',
-//   lenght: '1.8m',
-//   period: 'Late Cretaceious',
-// };
-// // Using your dinosaur objects, log answers to these questions:
+// stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stegosaurus = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic',
+};
+// velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+const velociraptor = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  lenght: '1.8m',
+  period: 'Late Cretaceious',
+};
+// Using your dinosaur objects, log answers to these questions:
 
-// // How much did tyrannosaurus weigh?
-// console.log(tyrannosaurus.weight);
-// // What was the diet of a velociraptor?
-// console.log(velociraptor.diet);
-// // How long was a stegosaurus?
-// console.log(stegosaurus.length);
-// // What time period did tyrannosaurus live in?
-// console.log(tyrannosaurus.period);
+// How much did tyrannosaurus weigh?
+console.log(tyrannosaurus.weight);
+// What was the diet of a velociraptor?
+console.log(velociraptor.diet);
+// How long was a stegosaurus?
+console.log(stegosaurus.length);
+// What time period did tyrannosaurus live in?
+console.log(tyrannosaurus.period);
 
-// // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-// console.log(tyrannosaurus.roar());
+// Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
+console.log(tyrannosaurus.roar());
 
 
-// ==== Arrays ====
+//==== Arrays ====
 
-// Given an array of college graduates.  Complete the following requests WITHOUT using any array methods like .forEach(), .map(), .reduce(), .filter()
+//Given an array of college graduates.  Complete the following requests WITHOUT using any array methods like .forEach(), .map(), .reduce(), .filter()
 
-// const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
-// {"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
-// {"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
-// {"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
-// {"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
-// {"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
-// {"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
-// {"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
-// {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
-// {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
+const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
+{"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
+{"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
+{"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
+{"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
+{"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
+{"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
+{"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
+{"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
+{"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}]
 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-// const universities = [];
+const universities = [];
 
-// for (i = 0; i < graduates.length; i++) {
-//   graduates[i].university;
-//   universities.push(graduates[i].university);
-//   universities.sort();
-// }
-// console.log(universities)
+for (i = 0; i < graduates.length; i++) {
+  graduates[i].university;
+  universities.push(graduates[i].university);
+  universities.sort();
+}
+console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -82,18 +82,18 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-// const contactInfo = [];
+const contactInfo = [];
 
-// for (i = 0; i < graduates.length; i++) {
-//   contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
-// }
-// console.log(contactInfo);
+for (i = 0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
+console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
 
-for (var i = 0; i<graduates.length; i++) {
+for (i = 0; i < graduates.length; i++) {
   if (graduates[i].university.indexOf('Uni') > -1) {
     uni.push(graduates[i].university);
   }
@@ -123,7 +123,7 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 // /* Request 1: .forEach()
 
 // The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
-
+  
 // */
 
 
