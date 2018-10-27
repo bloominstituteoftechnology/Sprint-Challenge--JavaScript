f96e17a8f86592742573fcc398a3f1fe6df444be
Jonathan Dorety <kyullog@gmail.com>
2018-10-27 13:27:50 -0400

    Complete Step 3 in functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -20,7 +20,7 @@ function add(x, y) {
 }
 
 function multiply(x, y) {
-  return x + y;
+  return x * y;
 }
 
 function greeting(firstName, lastName) {
@@ -29,9 +29,9 @@ function greeting(firstName, lastName) {
 
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ====
