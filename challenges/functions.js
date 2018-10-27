da87c0022b6671645117255707f28031fd41f99b
Tevvin <Tevvin7395@icloud.com>
2018-10-27 13:56:56 -0400

    Finished functions.js task

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,30 +1,46 @@
 // ==== Callbacks ====  
 
 /* Step 1: Create a function that accepts a callback
-  * Create a function named consume that can take 3 parameters.
-  * The first two parameteres can accept any argument
-  * The last parameter accepts a callback 
-  * In the body of the function return the callback with the two parameters that you created
-*/
+ * Create a function named consume that can take 3 parameters.
+ * The first two parameteres can accept any argument
+ * The last parameter accepts a callback 
+ * In the body of the function return the callback with the two parameters that you created
+ */
+
+function consume(x, y, cb) { // cb = callback.
+  return cb(x, y);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+ * Create a function named add that returns the sum of two numbers
+ * Create a function named multiply that returns the product of two numbers 
+ * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+ */
+
+function add(x, y) {
+  console.log(x + y);
+}
+
+function multiply(x, y) {
+  console.log(x * y);
+}
+
+function greeting(x, y) {
+  console.log(`Hello ${x} ${y}, nice to meet you!`);
+}
 
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2, 2, add); // 4
+consume(10, 16, multiply); // 160
+consume("Mary", "Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: Basically because nestedFunction is within the scope my MyFunction. So if I were to add const example = "example"; to myFunction nestedFunction would have access my example variable as well.
 
 
 const external = "I'm outside the function";
