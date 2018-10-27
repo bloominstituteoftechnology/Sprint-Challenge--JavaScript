7a20cacd39c9a2d43db8536da6e9ab66e3835e50
Jason Cook <the.root.user@gmail.com>
2018-10-27 14:29:22 -0400

    Complete tretch CubeMaker extender class

--- challenges/classes.js
+++ challenges/classes.js
@@ -33,3 +33,22 @@ class CuboidMaker {
 
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker {
+    constructor(length, width, height) {
+        super(length, width, height);
+
+    }
+    cubeVolume(length) {
+    
+        return Math.pow(this.length, 3);
+    }
+    cubeSurfaceArea(length) {
+        return 6 * (Math.pow(this.length, 2));
+    }
+}
+
+const cube = new CubeMaker(7, 7, 7);
+
+console.log(cube.cubeVolume())
+console.log(cube.cubeSurfaceArea())
