6b4de452d9f447c900b610b9a7daf05597d2107e
Leila Berrouayel <nb.leila10@gmail.com>
2018-10-27 11:56:06 -0500

    Complete Request 1 in ADVANCED Array Methods
    Create animalNames using forEach method and log result

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -120,10 +120,12 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 
 /* Request 1: .forEach()
 
-The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
+The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  
+The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
 const animalNames = [];
+zooAnimals.forEach(animal => animalNames.push(`name: ${animal.animal_name}, Scientific: ${animal.scientific_name}.`));
 console.log(animalNames);
 
 /* Request 2: .map()    
