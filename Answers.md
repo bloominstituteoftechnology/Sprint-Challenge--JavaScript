ebb9367b2e3490ecacb5606e4d95a3738929d3cb
Alando Appling <alandoappling@gmail.com>
2018-10-27 09:24:29 -0700

    Finished Prototype.js

--- Answers.md
+++ Answers.md
@@ -1,1 +1,18 @@
+Describe the biggest difference between .forEach & .map.
 
+    .forEach() iterates through each item in an array and mutates it permanantly while .map() iterates through and creates a new array all together.
+What is the difference between a function and a method?
+
+    A function exists on a global scale while a method only exists within an object.
+What is closure?
+
+    A closure is a variable that is passed down inside of a function.
+Describe the four rules of the 'this' keyword.
+
+    Window Binding: when in the global scope, the value of `this` will be the window Object.
+    Implicit Binding: Whenver a function is called by a preceding dot, the object before that dot is `this`
+    New Binding: Whenever a constructor function is used, this refers to the specific instance of the object that is created and returned by the constructor function.
+    Explicit Binding: Whenever JavaScript's call or apply method is used, this is explicitly defined.
+Why do we need super() in an extended class?
+
+    So that the parameters of that class are still being used and not just the parent objects.
--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -5,29 +5,43 @@
 /* == Step 1: Base Constructor ==
   Create a constructor function named CuboidMaker that accepts properties for length, width, and height
 */
-
+function CuboidMaker(attr){
+  this.length = attr.length;
+  this.height = attr.height;
+  this.width = attr.width;
+}
 
 /* == Step 2: Volume Method ==
   Create a method using CuboidMaker's prototype that returns the volume of a given cuboid's length, width, and height
-  
+
   Formula for cuboid volume: length * width * height
 */
-
+CuboidMaker.prototype.volume = function(){
+  const volume = this.length * this.width * this.height
+  return volume;
+}
 
 /* == Step 3: Surface Area Method ==
-  Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height. 
+  Create another method using CuboidMaker's prototype that returns the surface area of a given cuboid's length, width, and height.
 
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
-
+CuboidMaker.prototype.surfaceArea = function(){
+  const surfaceArea = 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
+  return surfaceArea;
+}
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
-  Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
+  Add properties and values of length: 4, width: 5, and height: 5 to cuboid.
 */
-
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+})
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 
