87b82451931aff70f29b55f09117865120fbc3e6
Leila Berrouayel <nb.leila10@gmail.com>
2018-10-27 13:01:33 -0500

    Complete Stretch Task in classes.js
    Created class CubeMaker that extends CuboidMaker
    Add two new methods to CubeMaker for calculating volume and surface area
    Test the methods on a new cube object.

--- challenges/classes.js
+++ challenges/classes.js
@@ -16,11 +16,33 @@ class CuboidMaker2 {
 
 const cuboid2 = new CuboidMaker2({
   length: 4,
-  width: 5,
+  width : 5,
   height: 5
 });
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid2.volume()); // 100
 console.log(cuboid2.surfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and 
+// create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+class CubeMaker extends CuboidMaker {
+  constructor(cubeProperties) {
+    super(cubeProperties);
+  }
+  volumeCube() {
+    return Math.pow(this.length, 3);
+  }
+  surfaceAreaCube() {
+    return 6 * Math.pow(this.length, 2);
+  }
+}
+
+const cube = new CubeMaker({
+  length: 2,
+  width : 2,
+  height: 2
+});
+
+console.log(cube.volumeCube());
+console.log(cube.surfaceAreaCube());
+// Could use the methods volume() and surfaceArea from CuboidMaker2, because cube is a special case of a cuboid, it has equal length, width, and height
