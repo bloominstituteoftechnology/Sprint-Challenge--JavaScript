bcc18bc2ca9f8d719834d4ec734519521015130c
Leila Berrouayel <nb.leila10@gmail.com>
2018-10-27 11:45:19 -0500

    Complete Request 2 in objects-arrays.js file
    Create contactInfo array with name and email of each student and log the result

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -75,8 +75,6 @@ for(let i = 0; i < graduates.length; i ++) {
   universities.push(graduates[i].university);
 }
 
-console.log(universities);
-
 /*Once you have the new array created, sort the universities alphabetically and log the result. */
 const universitiesSorted = universities.sort();
 console.log(universitiesSorted)
@@ -88,6 +86,9 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push(graduates[i].first_name + " " + graduates[i].email);
+}
 console.log(contactInfo);
 
 
