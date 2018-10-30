4a00361dcf7aadf2b6999aeffa67d594d57ab4aa
amadison17 <angelmadison2015@gmail.com>
2018-10-30 01:29:59 -0400

    functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,8 +6,8 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-function consume(param1,param2,cb) {
-  return cb(param1,param2);
+function consume(a,b,cb) {
+  return cb(a,b);
 }
 
 /* Step 2: Create several functions to callback with consume();
@@ -15,34 +15,31 @@ function consume(param1,param2,cb) {
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-function consume(2,2,add){
-  add = (2,2) = {
-  return add(2+2);
+function add(a,b){
+  return a + b;
   }
-}
 
-function consume(10,16,multiply){
-  multiply = (10,16) = {
-  return multiply(10 * 16);
+function multiply(a,b){
+  return a * b;
   }
-}
 
-function consume(first-name,last-name,greeting){
-  greeting = (first-name, last-name) = {
-    return "Hello ${first-name} + ${last-name}, nice to meet you!";
-  }
+
+function greeting(firstName,lastName){
+  return (`Hello ${firstName} ${lastName}, nice to meet you!`);
 }
 
 /* Step 3: Check your work by uncommenting the following calls to consume(): */
-//consume(2,2,add); // 4
+// consume(2,2,add); // 4
 // consume(10,16,multiply); // 160
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
-
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ==== 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: It can access the variable 'internal' because its still within myFunction scope.
+// Explanation: The variables are within that closure scope.
 
 
 const external = "I'm outside the function";
