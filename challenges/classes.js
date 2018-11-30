8fd8073e8e2fb295f8a2d683907ae275f594b6d2
Nathan Thomas <nwthomas@me.com>
2018-11-30 09:14:53 -0800

    Add stretch goals for classes.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -32,3 +32,26 @@ console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker {
+  constructor(makerProps) {
+    super(makerProps);
+  }
+
+  cubeVolume() {
+    return this.length * this.length * this.length; // Cube volume = length^3, where it is assumed that it's a perfect cube and that each side of the cube is the same
+  }
+
+  cubeSurfaceArea() {
+    return 6 * this.length * this.length;
+  }
+}
+
+const cube = new CubeMaker({
+  length: 5,
+  width: 5,
+  height: 5
+});
+
+console.log(cube.cubeVolume());
+console.log(cube.cubeSurfaceArea());
