3f696858042d364b329b78adc589e0545eb79db1
ills5771 <ilyayelizarov1@gmail.com>
2018-11-30 10:51:37 -0800

    Added stretch task in classes.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,9 +1,5 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
-// function CuboidMaker(properties) {
-//     this.length = properties.length;
-//     this.width = properties.width;
-//     this.height = properties.height;
-//   }
+
 
 class CuboidMaker {
     constructor(properties) {
@@ -18,14 +14,6 @@ class CuboidMaker {
         return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
     }
 } 
-//   CuboidMaker.prototype.volume = function() {
-//     return (this.length * this.width * this.height)
-//   }
-
-
-//   CuboidMaker.prototype.surfaceArea = function() {
-//     return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
-//   }
 
   const cuboid = new CuboidMaker({
     length: 4,
@@ -36,4 +24,24 @@ class CuboidMaker {
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  
+//Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.Test your work by logging out your volume and surface area
+//.
+class CubeMaker extends CuboidMaker {
+    constructor(cubeMakerProperties) {
+        super(cubeMakerProperties);
+
+    }
+    cubeVolume() {
+        return Math.pow(this.length, 3)
+    }
+    cubeSurfaceArea() {
+        return 6 * Math.pow(this.length, 2)
+    }
+}
+const cube = new CubeMaker({
+    length: 7,
+})
+
+console.log(cube.cubeVolume())
+console.log(cube.cubeSurfaceArea())
