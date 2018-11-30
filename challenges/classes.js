feb1c9b9969871b5fc642f71c57008dbfcf05924
yannafaith <yannafaith97@gmail.com>
2018-11-30 10:45:46 -0700

    finished project MVP

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,8 +1,33 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker. 
+// Test your work by logging out your volume and surface area.
 
-// Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+/* ========================== prototype copy and paste  =======================
+function CuboidMaker(attributes) {
+    this.length = attributes.length;
+    this.width = attributes.width;
+    this.height = attributes.height;
+};
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+CuboidMaker.prototype.volume = function() {return (this.length*this.width*this.height)};
+CuboidMaker.prototype.surfaceArea = function() {return 2*(this.length*this.width*this.length*this.height*this.width*this.height)}; 
+ ========================== prototype copy and paste  ======================= */
 
+ class Cuboidmaker {
+     constructor(attriubutes) {
+        this.length = attributes.length;
+        this.width = attributes.width;
+        this.height = attributes.height;
+     };
+     volume() {console.log(this.length*this.width*this.height)};
+     surfaceArea() {console.log(this.length*this.width*this.length*this.height*this.width*this.height)};
+ };
+
+const newCuboid = new CuboidMaker({
+    length: 5,
+    width: 6,
+    height: 7
+});
+
+newCuboid.volume();
+newCuboid.surfaceArea();
