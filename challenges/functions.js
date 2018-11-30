69a0f91580fb5dc8a4a924c6fa20a123d24ca596
Matt Basile <matthewbasile2@gmail.com>
2018-11-30 12:03:10 -0500

    Added stpes 1 and 2 to function.js LINES: step 1: 9-11, step 2: 17-27.

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,13 +6,25 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
-
+function consume (x, y, cb){
+  return cb(x,y);
+}
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const add = function (x,y){
+  return x+y;
+}
+const multiply = function (x,y){
+  return x*y;
+}
+const greeting = function (fName,lName){
+  return `Hello ${fName} ${lName}, nice to meet you!`
+}
+
+console.log(consume("Matt", "Basile", greeting));
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
