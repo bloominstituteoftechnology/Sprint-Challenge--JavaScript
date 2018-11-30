1bb004b06f181616aba77fc26d2dfdaa839b9b29
David Flack <davidscottflack@gmail.com>
2018-11-30 10:17:24 -0600

    (objects-arrays.js) Complete dinosaur objects challenge

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -41,20 +41,21 @@ const velociraptor = new Dinosaur({
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+tyrannosaurus.roar = function() {return "RAWERSRARARWERSARARARRRR!"};
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
