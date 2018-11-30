6bf686b271b14340bc98f4d579573ca6b40621d8
KieranVieira <kieranvieira@live.com>
2018-11-30 11:51:54 -0500

    Adjusted objects-arrays.js to include normal object syntax removed classes

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,46 +6,36 @@
   object name, diet, weight, length, period
 */
 
-class Dinosaur {
-  constructor(props){
-    this.name = props.name;
-    this.diet = props.diet;
-    this.weight = props.weight;
-    this.length = props.length;
-    this.period = props.length;
-  }
-  roar(){
-    return "RAWERSRARARWERSARARARRRR!";
-  }
-}
-
 //tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
-const tyrannosaurus = new Dinosaur({
+const tyrannosaurus ={
   'name': 'tyrannosaurus',
   'diet': 'carnivorous',
   'weight': '7000kg',
   'length': '12m',
   'period': 'Late Cretaceious',
-})
+  'roar': () => {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
-const stegosaurus = new Dinosaur({
+const stegosaurus ={
   'name': 'stegosaurus',
   'diet': 'herbivorous',
   'weight': '2000kg',
   'length': '9m',
   'period': 'Late Jurassic',
-})
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
-const velociraptor = new Dinosaur({
+const velociraptor = {
   'name': 'velociraptor',
   'diet': 'carnivorous',
   'weight': '15kg',
   'length': '1.8m',
   'period': 'Late Cretaceious',
-})
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
