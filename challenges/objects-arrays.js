3702a23d6e273652c5a79164c0669f2b765dd61b
Olivia Osborn <olioz95@gmail.com>
2018-11-30 11:21:29 -0500

    logged answers to dino questions, and created roar method for trex

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,12 +12,15 @@ const trex = {
   diet: "carnivorous",
   weight: "7000kg",
   length: "12m",
-  period: "Late Cretaceious"
+  period: "Late Cretaceious",
+  roar: function() {
+    return "RAWERSRARARWERSARARARRRR!"
+  }
 };
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-const stegasaurus = {
-  name: "Stegasaurus",
+const stegosaurus = {
+  name: "Stegosaurus",
   diet: "herbivorous",
   weight: "2000kg",
   length: "9m",
@@ -36,20 +39,20 @@ const velociraptor = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(trex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(trex.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(trex.roar());
 
 
 // ==== Arrays ====
