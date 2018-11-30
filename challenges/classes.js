124b48834948551aeea81eceb3af9bf6eeeef45e
byronholmes2018 <lshopeful2018@mgmail.com>
2018-11-30 11:42:03 -0600

    Completed last stretch in classes.js = created subclass CubeMaker with ES6 extends syntax.

--- challenges/classes.js
+++ challenges/classes.js
@@ -46,8 +46,16 @@ console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
 
+class CubeMaker extends CuboidMaker{
+  constructor(cubeProps){
+    super(cubeProps);
+    this.width = cubeProps.length;
+    this.height = cubeProps.length;
+    this.length = cubeProps.length;
+  }
 
+}
 
-
-
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+const cube = new CubeMAker({length:2});
+console.log(cube.volume());
+console.log(cube.surfaceArea());
