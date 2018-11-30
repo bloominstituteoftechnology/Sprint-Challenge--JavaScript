1e344423fb65e483d646964bd14dd3c4072243eb
spinon187 <spinon187@gmail.com>
2018-11-30 11:33:40 -0600

    SC-JS MVP

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,9 +6,8 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
 function consume(x, y, cb){
-  return (x, y, cb);
+  console.log(cb(x, y));
 };
 
 /* Step 2: Create several functions to callback with consume();
@@ -16,34 +15,46 @@ function consume(x, y, cb){
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-function add(x, y, cb){
-  return cb(x + y);
-}
-console.log(add(consume));
+function add(x, y){
+  return x + y;
+};
 
+function multiply(x, y){
+  return x * y;
+};
+
+function greeting(x, y){
+  return `Hello ${x} ${y}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: The internal variable is declared outside the scope of nestedFunction() so nestedFunction() is able to see internal and use it because there's nothing protecting it
+// Put another way, the scope is tiered as noted below. Everything in tier 2 can access tier 1, and everything in tier 3 can access tiers 1 and 2, but tiers 1 and 2 cannot access tier 3
 
-
+//start tier 1
 const external = "I'm outside the function";
 
 function myFunction() {
+  //start tier 2
   console.log(external);
   const internal = "Hello! I'm inside myFunction!";
 
   function nestedFunction() {
+    //start tier 3
     console.log(internal);
+    //end tier 3
   };
   nestedFunction();
+  //end tier 2
 }
 myFunction();
+//end tier 1
