2f0421a68d0e2f9696530d06611ee12550d808ff
umbur <work1415@gmail.com>
2018-11-30 11:32:23 -0700

    completed classes.js

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -39,7 +39,7 @@ CuboidMaker.prototype.surfaceArea = function () {
 const cuboid = new CuboidMaker(4, 5, 5);
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+// console.log(cuboid.volume()); // 100
+// console.log(cuboid.surfaceArea()); // 130
 
 
