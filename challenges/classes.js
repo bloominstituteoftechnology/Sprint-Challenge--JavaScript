720a8a03007df55763279794b6bfd2a27c41fce7
Leigh-Ann Friedel <leigh.ann.friedel@gmail.com>
2018-11-30 13:04:06 -0500

    Created pyramid object and updated methods in classes.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -2,10 +2,10 @@
 
 
   class NewCuboidMaker {
-      constructor(length,width,height) {
-        this.length = length;
-        this.width = width;
-        this.height = height;
+      constructor(cuboid) {
+        this.length = cuboid.length;
+        this.width = cuboid.width;
+        this.height = cuboid.height;
       }
       volume() {
         return this.length * this.width * this.height;
@@ -17,7 +17,11 @@
   }
 
 
-  const cuboidClass = new NewCuboidMaker(4, 5, 5);
+  const cuboidClass = new NewCuboidMaker({
+    length:4,
+    width: 5,
+    height: 5
+  });
   
   // Test your volume and surfaceArea methods by uncommenting the logs below:
   console.log(cuboidClass.volume()); // 100
@@ -26,15 +30,41 @@
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
 
 class CubeMaker extends NewCuboidMaker {
-    constructor(length,width,height) {
-        super(length,width,height);
+    constructor(cube) {
+        super(cube);
     }
     surfaceArea() {
         return 6 * Math.pow(this.length, 2);
     }
 }
 
-const cube = new CubeMaker(2, 2, 2);
+const cube = new CubeMaker({
+  length: 2, 
+  width: 2,
+  height: 2
+});
 
 console.log(cube.volume());
 console.log(cube.surfaceArea());
+
+class PyramidMaker extends NewCuboidMaker {
+  constructor(cyl) {
+    super(cyl);
+  }
+  volume() {
+    return (this.length * this.width * this.height) / 3;
+  }
+  surfaceArea() {
+    let sa = this.length * this.width + this.length * Math.sqrt(Math.pow(this.width*.5, 2) + Math.pow(this.height, 2))  + this.width * Math.sqrt(Math.pow(this.length*.5, 2) + Math.pow(this.height, 2));
+    return sa.toFixed(2);
+  }
+}
+
+const pyramid = new PyramidMaker({
+  length: 6,
+  width: 4,
+  height: 6
+})
+
+console.log(pyramid.volume());
+console.log(pyramid.surfaceArea());
