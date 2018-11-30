b1788e80369395fe5983629519ef56c537ed2179
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-11-30 10:31:24 -0600

    logged dinosaur objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -39,16 +39,16 @@ const velociraptor = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
