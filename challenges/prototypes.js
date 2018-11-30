cf86e06130d570296e99433e3894e87b1031531d
KieranVieira <kieranvieira@live.com>
2018-11-30 12:05:34 -0500

    completed classes.js MVP

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -40,7 +40,7 @@ CuboidMaker.prototype.surfaceArea = function(){
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
-const cuboid = ({
+const cuboid = new CuboidMaker({
   'length': 4, 'width': 5, 'height': 5
 })
 
