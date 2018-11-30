d2c80577b49fb4fc5cd5f4908955801cff88e027
RachelKolk <rach.m.obrien@gmail.com>
2018-11-30 13:03:51 -0500

    prototype tasks done

--- challenges/functions.js
+++ challenges/functions.js
@@ -32,9 +32,9 @@ function greeting(firstName, lastName) {
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-consume(2,2,add); // 4
-consume(10,16,multiply); // 160
-consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
