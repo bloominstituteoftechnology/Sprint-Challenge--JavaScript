7acbab5bcf761f907d667993ced794d277588b60
KieranVieira <kieranvieira@live.com>
2018-11-30 12:13:48 -0500

    Completed classes.js stretch

--- challenges/classes.js
+++ challenges/classes.js
@@ -13,6 +13,20 @@ class CuboidMaker{
         return 2 * (this.length * this.width + this.length * this.height + this.width * this.height)
     }  
 }
+
+class CubeMaker extends CuboidMaker{
+    constructor(props){
+        super(props)
+        this.volume = this.volume();
+        this.surfaceArea = this.surfaceArea();
+        //lines below are return the same result
+        //this.volume = props.length * props.width * props.height;
+        //this.surfaceArea = 2 * (props.length * props.width + props.length * props.height + props.width * props.height);
+    }
+}
+const cube = new CubeMaker ({
+    'length': 6, 'width': 2, 'height': 4
+  })
   
 const cuboid = new CuboidMaker({
     'length': 4, 'width': 5, 'height': 5
