f31568ead43223f037b6f1ae977256c2cbb83dc8
leiannel <leilouis112@gmail.com>
2018-11-30 10:59:09 -0500

    initial push

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,7 +7,7 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-
+d
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
