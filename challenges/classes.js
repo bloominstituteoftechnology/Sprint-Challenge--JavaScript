7345388111da23e66513e1287ef7a6a3dcac7bcf
joepound <14357797+joepound@users.noreply.github.com>
2018-11-30 12:15:09 -0700

    Fixed missing entries for Arrays Request 3 in objects-arrays.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -18,7 +18,7 @@ const cuboid = new CuboidMaker({
     length: 4, 
     width: 5, 
     height: 5
-  });
+});
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
@@ -46,7 +46,7 @@ const cube = new CubeMaker({
     length: 4, 
     width: 4, 
     height: 4
-  });
+});
 
 console.log(cube.volume()); // 64
 console.log(cube.surfaceArea()); // 96
