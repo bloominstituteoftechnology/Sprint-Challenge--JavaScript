488510a78ba92944383ecab3a1ebddc171056140
croy <roy.christie95@gmail.com>
2018-11-30 12:59:01 -0500

    Finished functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,10 +7,13 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
-// function consume(x, y, callBack) {
-//   return callBack();
+// const consume = function(x,y,cb){
+//  console.log(cb());
 // }
 
+function consume (a,b,cb){
+  console.log(cb(a, b));
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -18,16 +21,16 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
-function add(x, y, cb) {
-  return cb(x + y);
+function add(x, y) {
+  return (x + y);
 }
 
-function multiply(x, y, cb){
-  return cb(x * y);
+function multiply(x, y) {
+  return (x * y);
 }
 
-function greeting(firstName, lastName, cb){
-  return cb(`Hello ${firstName} ${lastName}, nice to meet you!`);
+function greeting(firstName, lastName){
+  return (`Hello ${firstName} ${lastName}, nice to meet you!`);
 }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
@@ -41,7 +44,10 @@ consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
-// nestedFunction() is able to access the variable 'internal' because it is able to reach outside of its scope.
+// nestedFunction() is an example of the concept of closure. When a function is declared, a new functional scope is created.
+// Variables or functions within that functional scope are able to reach outward for context. This is true no matter how
+// deeply functions are nested. In this specific example, nestedFunction() is able to access the variable 'internal' 
+// because nestedFunction() is able to reach outside of its scope.
 
 
 const external = "I'm outside the function";
