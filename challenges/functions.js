5b6c474d93370779a360e30f7ea0805037e3ff17
Nate Boyette <nateboyette@icloud.com>
2018-11-30 09:23:51 -0800

    prototypes.js complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -33,9 +33,9 @@ function greeting(firstName, lastName) {
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
