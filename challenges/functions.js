9baae701b6b8fe7204f935dbf2933429090320d8
linkel <ironcarbide@gmail.com>
2018-11-30 08:48:43 -0800

    Functions.js completed.

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(arg1, arg2, callback) {
+  return callback(arg1, arg2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +18,31 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function greeting(first, last) {
+  return `Hello ${first} ${last}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: Both nestedfunction() and const internal were declared within the outer function, and thus have the same scope.
+// Because of Javascript's closures, nestedFunction() "remembers" the environment that it was declared in, and when it is called in 
+// the line after it is declared within myFunction(), it executes and looks for the variable one scope up. 
 
 
 const external = "I'm outside the function";
