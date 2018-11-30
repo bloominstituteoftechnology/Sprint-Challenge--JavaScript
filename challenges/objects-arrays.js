2afd5f4f5e332f478159165c606edc585a7cd44c
Olivia Osborn <olioz95@gmail.com>
2018-11-30 11:44:50 -0500

    finshed request 3 of objects, and request 1 for arrays in objects-arrays.js file

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -95,6 +95,11 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (i=0; i<graduates.length; i++) {
+  if (graduates[i].university.includes("Uni")) {
+    uni.push(graduates[i].university)
+  }
+}
 console.log(uni);
 
 
@@ -119,9 +124,9 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 The zoo wants to display both the scientific name and the animal name in front of the habitats.  Return an array with only the animal and scientific names in it.  The individual values in the array should look like this "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const animalNames = [];
-console.log(animalNames);
-
+const animalNames = zooAnimals.forEach(animal => 
+  console.log("Name: " + animal.animal_name + ", Scientific: " + animal.scientific_name));
+  
 /* Request 2: .map()    
 
 The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
