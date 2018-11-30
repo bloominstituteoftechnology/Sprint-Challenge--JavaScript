e69d30b9ea8de191688fe5641a00cdf281f4bd54
Matt Basile <matthewbasile2@gmail.com>
2018-11-30 12:10:40 -0500

    Answered question on Closures. LINES: 40-43.

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,7 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-function consume (x, y, cb){
+const consume =function(x, y, cb){
   return cb(x,y);
 }
 /* Step 2: Create several functions to callback with consume();
@@ -27,10 +27,10 @@ const greeting = function (fName,lName){
 console.log(consume("Matt", "Basile", greeting));
 
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+// /* Step 3: Check your work by un-commenting the following calls to consume(): */
+// console.log(consume(2,2,add)); // 4
+// console.log(consume(10,16,multiply)); // 160
+// console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
