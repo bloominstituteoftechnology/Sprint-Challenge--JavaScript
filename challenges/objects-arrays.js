0f52a2396ea0043744b1c00b8f981656bcf9b686
jjamaltwin <43683321+jjamaltwin@users.noreply.github.com>
2018-11-30 14:34:17 -0600

    finished arrays and objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -166,8 +166,15 @@ console.log(largerPopulation);
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+const populationTotal = zooAnimals.reduce((popTotal, sum) => {
+  return popTotal + sum.population;
+ 
+ }, 0);
+ 
+ 
+ 
+ 
+ console.log(populationTotal);
 
 
 /* 
