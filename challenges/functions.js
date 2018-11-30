125d62065ce062f7e208ec42bba924438a2ac50d
samjeffcoat <samjeffcoat@gmail.com>
2018-11-30 12:29:25 -0600

    functions done

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,18 +7,31 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(x, y, cb){
+  return cb (x,y)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const add = function(x,y){
+  console.log(x+y)
+}
 
+const multiply = function (x,y,){
+  console.log(x*y)
+}
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+const greeting = function (x,y){
+  console.log(`Hello ${x} ${y}, nice to meet you!`)
+}
+
+Step 3: Check your work by un-commenting the following calls to consume(): */
+ consume(2,2,add); // 4
+ consume(10,16,multiply); // 160
+ consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -27,7 +40,7 @@
 
 // Explanation: 
 
-
+its part of closure. closure accesses the whole function
 const external = "I'm outside the function";
 
 function myFunction() {
