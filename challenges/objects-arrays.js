b50f957217dc33f58d323480b3208c3c7e3ab3bd
Lambda_School_Loaner_40 <lambda_school_loaner_40@Lambda-School-Loaner-40s-MacBook-Pro.local>
2018-11-30 14:55:52 -0500

    finished stretch question for classes

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -90,11 +90,18 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
-console.log(contactInfo);
+for (let i = 0; i < graduates.length; i++){
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
+console.log(contactInfo.sort());
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
+for (let i = 0; i < graduates.length; i++){
+  graduates[i] === 'uni'
+    uni.push(graduates[i].university);
+}
 console.log(uni);
 
 
