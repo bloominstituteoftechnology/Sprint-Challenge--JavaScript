552e8b69ad42098e16d86fd443a38f2045416b5b
Sheila <s.hopper01110@gmail.com>
2018-11-30 13:41:21 -0500

    JS SC - Update

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -121,15 +121,23 @@ The zoo wants to display both the scientific name and the animal name in front o
 
 */
 const animalNames = [];
+const scientificAnimals = zooAnimals.forEach(function(i){
+  animalNames.push(i.animal_name + " " + i.scientific_name);
+});
 console.log(animalNames);
+
+
 
 /* Request 2: .map()    
 
-The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
+The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  
+//Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map(function(nameAnimals){
+  return nameAnimals.animal_name.toLowerCase();
+});
 console.log(lowerCase); 
 
 /* Request 3: .filter() 
@@ -137,21 +145,32 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const largerPopulation = [];
+const largerPopulation = zooAnimals.filter(function(fiveOrLess){
+  return fiveOrLess.population <= 5
+});
+
 console.log(largerPopulation);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
+The zoos need to know their total animal population across the United States. 
+// Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
 
 
-/* 
+const zooPopulation = zooAnimals.reduce(function(zoosPopulation, zoosAmount){
+  return zoosPopulation + zoosAmount.population;
+}, 0);
 
-Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
-*/
+ console.log(zooPopulation);
+
+
+
+
+
+// Stretch: If you haven't already, convert your array method callbacks into arrow functions.
+
+// */
 
