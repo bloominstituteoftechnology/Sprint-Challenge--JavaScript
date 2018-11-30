e72fcd92b2fac96bead7e39a5d53f80cec3e2221
Alex Bott <Abott1222@gmail.com>
2018-11-30 09:26:56 -0800

    finished closures

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(arg1, arg2, callback) {
+  return callback(arg1, arg2);
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +18,26 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = (x,y) => x+y;
+const multiply = (x,y) => x*y;
+const greeting = (firstName,lastName) => `Hello ${firstName} ${lastName}, nice to meet you!`;
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
 // consume(10,16,multiply); // 160
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
+console.log(consume(2,2,add));
+console.log(consume(10,16,multiply));
+console.log(consume("Mary","Poppins", greeting));
+
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: nestedFunction can access the variable internal because it is included in the lexical scope of myFunction. The reason internal is not garbage collected is because nestedFunct has a reference to it
 
 
 const external = "I'm outside the function";
