40d375b32f4123b471f06e5b6f554784f7a2a1ed
ajfrierson <alvinfrierson@fuller.edu>
2018-12-01 13:06:47 -0800

    finished with .reduce() method of object-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -172,16 +172,22 @@ The zoos are concenred about animals with a lower population count. Find out whi
 
 */
 const largerPopulation = [];
-console.log(largerPopulation);
+zooAnimals.filter((zooPop) =>{
+  return zooPop.population < 5;
+});
 
 /* Request 4: .reduce() 
 
 The zoos need to know their total animal population across the United States.  Find the total population from all the zoos using the .reduce() method.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+const populationTotal = zooAnimals.reduce(function(totalPop, currentValue) {
+  // debugger;
+  return (totalPop + currentValue.population);
+  
+},0);
 
+console.log(populationTotal);
 
 /* 
 
