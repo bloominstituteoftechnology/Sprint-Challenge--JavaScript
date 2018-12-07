71bbd3ac2a436df5df52b9c502b083cf6c6bf2c3
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-12-07 10:05:51 -0600

    made dinosaur objects on objects-arrays.js

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,4 +1,4 @@
-// 1. hi. Copy and paste your prototype in here and refactor into class syntax.
+// 1. Copy and paste your prototype in here and refactor into class syntax.
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
