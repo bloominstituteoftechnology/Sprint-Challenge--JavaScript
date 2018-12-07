bee32488406b4eb704c87cdd91ecc349f85ae405
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-12-07 10:18:14 -0600

    sorted universities 86 on arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -76,7 +76,13 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
+for (let i = 0; i < graduates.length; i++) {
+  universities[i] = graduates[i]["university"];
+}
+
+universities.sort();
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
