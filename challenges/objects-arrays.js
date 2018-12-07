3b7ec097b172667be63521c9fb849fb393866849
Kelli Rae Hanson <kelliraehanson@gmail.com>
2018-12-07 10:11:59 -0600

    tyrannosaurus roar on arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -14,6 +14,9 @@ const tyrannosaurus = {
   weight: "7000kg",
   length: "12m",
   period: "Late Cretaceious",
+  roar: function(){
+    return ("RAWERSRARARWERSARARARRRR!");
+  }
 }
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -52,7 +55,7 @@ console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
