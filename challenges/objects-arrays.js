83e7e97b2ba9d921df2972973e77ad8dfd94266e
Junior Dugue <jrdugue@gmail.com>
2019-01-26 14:09:15 -0500

    still working on objects arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,29 +6,74 @@
   object name, diet, weight, length, period
 */
 
+const dinosaurs = [{"name": "tyrannosaurus", "diet": "carnivorous", "weight": "7000kg", "length": "12m", "period": "Late Cretaceious"}, //?
+                  {"name": "stegosaurus", "diet":"herbivorous", "weight":"2000kg", "length":"9m", "period":"Late Jurassic"}, 
+                  {"name": "velociraptor", "diet":"carnivorous", "weight":"15kg", "length": "1.8m", "period":"Late Cretaceious"}]; //?
+
+function Dinosaur(object){
+  this.name = object.name,
+  this.diet = object.diet,
+  this.weight = object.weight,
+  this.length = object.length,
+  this.period = object.period
+}
+
+Dinosaur.prototype.roar = function () {
+  if (this.name === 'tyrannosaurus') {
+    return 'RAWERSRARARWERSARARARRRR!';
+  }
+}
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceious
+const tyrannosaurus = new Dinosaur({
+  name: 'tyrannosaurus', 
+  diet: 'carnivorous', 
+  weight: '7000kg', 
+  length: '12m', 
+  period: 'Late Cretaceious'
+})
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stegosaurus = new Dinosaur({
+  name: 'stegosaurus', 
+  diet: 'herbivorous', 
+  weight: '2000kg', 
+  length: '9m', 
+  period: 'Late Jurassic'
+})
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceious
+const velociraptor = new Dinosaur({
+  name: 'velociraptor', 
+  diet: 'carnivorous', 
+  weight: '15kg', 
+  length: '1.8m', 
+  period: 'Late Cretaceious'
+})
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+
+console.log(dinosaurs[0].weight); 
+console.log(tyrannosaurus.weight)
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinosaurs[2].diet); 
+
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinosaurs[1].weight);
+
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinosaurs[0].period);
+
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
+
 
 
 // ==== Arrays ====
