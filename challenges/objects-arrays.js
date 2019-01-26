d9958f7f4fc54d1a8c60f8c3d4eafc8e6caf0ce3
Wes-White <wesleyrwhite86@gmail.com>
2019-01-26 09:21:52 -0800

    added last method for dino

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -17,7 +17,11 @@ const ty = {
   diet: 'carnivorius',
   weight: '7000kg',
   length:  '12m', 
-  period: 'Late Cretaceious'
+  period: 'Late Cretaceious', 
+  
+  roar: function(){ 
+    return "RAWERSRARARWERSARARARRRR!"
+  }
 
 }
 
@@ -55,7 +59,7 @@ console.log(ty.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(ty.roar())
 
 
 // ==== Arrays ====
