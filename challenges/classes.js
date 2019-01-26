b2ce220f3fc3216abcd2c37e4031fcad3c27bc28
Scott Grobe <gitgrobe@gmail.com>
2019-01-26 14:13:42 -0500

    classes.js complete

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,73 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
+console.log('hey')
+
+
+
+
+// function CuboidMaker(attr){
+//     this.length = attr.length;
+//     this.width = attr.width;
+//     this.height = attr.height; 
+// }
+
+// CuboidMaker.prototype.volume = function() {
+//     return this.length * this.width * this.height;
+// }
+
+
+// CuboidMaker.prototype.surfaceArea = function () {
+//     return 2 * (this.length *this.width + this.length * this.height + this.width * this.height); 
+// }
+
+
+class CuboidMaker {
+    constructor(attr) {
+        this.length = attr.length;
+        this.width = attr.width;
+        this.height = attr.height;
+    }
+
+    volume() {
+        return this.length * this.width * this.height;
+    }
+
+    surfaceArea() {
+        return 2 * (this.length *this.width + this.length * this.height + this.width * this.height); 
+    }
+}
+
+  
+const cuboid = new CuboidMaker ({
+    length: 4,
+    width: 5, 
+    height: 5
+})  
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+// // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+
+class CubeMaker extends CuboidMaker {
+    constructor(attr){
+      super(attr);
+    }
+
+    volume() {
+      return(this.length * this.width * this.height);
+    }
+
+    surfaceArea() {
+      return 6 * (this.length * this.width * this.height);
+    }
+  }
+const cube = new CubeMaker ({
+    length: 2,
+    height: 2,
+    width: 2
+})
+
+console.log(cube.volume()); // 100
+console.log(cube.surfaceArea()); // 130
