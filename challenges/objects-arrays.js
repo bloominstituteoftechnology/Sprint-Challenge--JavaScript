ec7f93b7d4a4e384640f7a3ca0cf1eb61374b348
Junior Dugue <jrdugue@gmail.com>
2019-01-28 21:09:18 -0500

    added example

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,9 +7,9 @@
 */
 
 const dinosaurs = [{"name": "tyrannosaurus", "diet": "carnivorous", "weight": "7000kg", "length": "12m", "period": "Late Cretaceious"}, //?
-                  {"name": "stegosaurus", "diet":"herbivorous", "weight":"2000kg", "length":"9m", "period":"Late Jurassic"}, 
+                  {"name": "stegosaurus", "diet":"herbivorous", "weight":"2000kg", "length":"9m", "period":"Late Jurassic"}, //?
                   {"name": "velociraptor", "diet":"carnivorous", "weight":"15kg", "length": "1.8m", "period":"Late Cretaceious"}]; //?
-
+// example
 function Dinosaur(object){
   this.name = object.name,
   this.diet = object.diet,
