ab0a1df363009fb7646a5a608c480622ea978745
jkaddu <jkaddu@yahoo.com>
2019-02-01 10:02:42 -0800

    prototypes done

--- challenges/functions.js
+++ challenges/functions.js
@@ -31,9 +31,9 @@ const greeting = function(first, last,) {
   return `Hello ${first} ${last}, nice to meet you!`;
 }
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
