867b7640d7749ad034f24f50fa0f5dcc18c4cafd
Maynard J. Hartman, Jr <mjhartman@brighthouse.com>
2019-02-01 18:49:31 -0500

    Reprise

--- challenges/classes.js
+++ challenges/classes.js
@@ -5,3 +5,16 @@
 // console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+class CuboidMaker {
+      constructor(length, width, height) {
+      this.length =  length,
+      this.width  =  width,
+      this.height =  height
+    }
+
+}  
+ const cuboid = Object.create(CuboidMaker.prototype);
+ 
+  // Test your volume and surfaceArea methods by uncommenting the logs below:
+  console.log(cuboid.volume(5, 5, 5)); // 100
+  console.log(cuboid.surfaceArea(5, 5, 5)); // 130
--- challenges/functions.js
+++ challenges/functions.js
@@ -13,10 +13,17 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const consume = function(arg1, arg2, add) {
+  return((`Sum: ${arge} + ${arg2}`) + (arg1 + arg2));
+}
+
+const multiply =  function (arg1, arg2, callback) {
+
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
+consume(2,2,add); // 4
 // consume(10,16,multiply); // 160
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
@@ -26,7 +33,7 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 // Because internal is declared inside of myFunction
 // Explanation: 
-
+// Because internal is Implicit
 
 const external = "I'm outside the function";
 
