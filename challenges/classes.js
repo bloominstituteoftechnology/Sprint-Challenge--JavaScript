8d75712aa512f66f4fc5be7fad9d4407ce3509a3
jackgipson <jackjgipson@gmail.com>
2019-02-09 11:55:09 -0800

    updated -objects-arrays

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,23 +1,24 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 class CuboidMaker {
-    constructor(props) {
-    this.length = props.length;
-    this.width = props.width;
-    this.height = props.height;
+  constructor(cubeProps) {
+    this.length = cubeProps.length;
+    this.width = cubeProps.width;
+    this.height = cubeProps.height;
   }
-    volume() {
-       console.log(`${this.length * this.width * this.height}`);
-     }
-     surfaceArea() {
-       console.log(2 * (`${this.length * this.width + this.length * this.height + this.width * this.height}`));
-     }
+
+  volume() {
+    return `${this.length * this.width * this.height}`;
   }
-  const cuboid = new CuboidMaker({
-    length: 4,
-    width: 5,
-    height: 5
-  });
-  
-  console.log(cuboid.volume()); // 100
-  console.log(cuboid.surfaceArea()); // 130
-  
+
+  surfaceArea() {
+    return `${2 * (this.length * this.width + this.length * this.height + this.width * this.height)}`;
+  }
+}
+
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5,
+});
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
