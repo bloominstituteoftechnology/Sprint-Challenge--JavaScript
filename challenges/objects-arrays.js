3c0764706412b6201ea2f30b9bd8ff5c3187a69d
Mylynh Nguyen <nmylynh@gmail.com>
2019-04-05 08:53:20 -0700

    Update objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -102,7 +102,14 @@ console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+
 const uni = [];
+
+for(var i = 0; i < graduates.length; i++) {
+	if(graduates[i].university.includes("uni")) {
+		uni.push(graduates[i].university);
+	}
+}
 console.log(uni);
 
 
