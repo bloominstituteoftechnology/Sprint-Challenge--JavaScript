e24596d29e3b240d58fd3606b380b7f36c007b83
Thomas-Tuttle <48684599+Thomas-Tuttle@users.noreply.github.com>
2019-04-05 12:01:46 -0400

    Functions
    Finished function page

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume (item1, item2, cb) {
+  return cb(item1,item2);
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,11 +17,22 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2) {
+  return num1 + num2;
+};
+
+function multiply(num1, num2) {
+  return num1 * num2;
+};
+
+function greeting(firstname, lastname) {
+  return `Hello ${firstname} ${lastname}, nice to meet you!`;
+};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log('Functions Step 3a:', consume(2,2,add)); // 4
+console.log('Functions Step 3b:', consume(10,16,multiply)); // 160
+console.log('Functions Step 3c:', consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -28,11 +42,11 @@
 // Explanation: 
 
 
-const external = "I'm outside the function";
+const external = "External: I call on the Global.";
 
 function myFunction() {
   console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+  const internal = "Internal: I call on the parent";
 
   function nestedFunction() {
     console.log(internal);
