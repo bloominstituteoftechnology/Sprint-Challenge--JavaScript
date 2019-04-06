4107b5417ce4b3a8b3131c77352d9e9c09e02bc5
JimmyMcBride <mcbride967@gmail.com>
2019-04-06 11:25:37 -0500

    finished the functions.js file

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,32 +1,40 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====
 
 /* Step 1: Create a higher-order function that accepts a callback
   * Create a higher-order function named consume that can take 3 parameters.
   * The first two parameters can accept any argument
-  * The last parameter accepts a callback 
+  * The last parameter accepts a callback
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(x, y, cb){
+  return cb(x, y)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named multiply that returns the product of two numbers
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+const add = (x, y) => consume(x, y, function(x, y) {console.log(x + y)})
+const multiply = (x, y) => consume(x, y, function(x, y) {console.log(x * y)})
+const greeting = (x, y) => consume(x, y, function(x, y) {console.log(`Hello ${x} ${y}, nice to meet you.`)})
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
-// ==== Closures ==== 
+// ==== Closures ====
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
-
+// Explanation:
+/* nestedFunction was created inside myFunction. Since it was created inside 
+the scope it can remember the variables up the chain in the enviroment it was 
+created in. */
 
 const external = "I'm outside the function";
 
