aa1f097cfa6d3c53b13c4c98cbb99366b15e1708
gaetanodaidone <gaetanodaidone@gmail.com>
2019-04-07 22:45:37 -0400

    MVP complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,24 +7,46 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(unos,dos,cb) {
+  return cb(unos,dos);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, 
+  * nice to meet you!"
 */
 
+let add = function(a,b) {
+  console.log(a+b)
+  return a+b;
+}
+
+function multiply(a,b) {
+  console.log(a*b)
+  return a*b;
+}
+
+let greeting = function(first,last) {
+  console.log (`Hello ${first} ${last}, nice to meet you!`)
+  return `Hello ${first} ${last}, nice to meet you!`
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+
+
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
-
+/* 
+In Javascript functions are functions and closures, the function body has access to variables that are defined outside of the function body.
+*/
 // Explanation: 
 
 
