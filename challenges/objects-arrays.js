aa72fd6b9a942c742fa78211bef0c320d62fa470
Sir-Williams <sirjohnwilliams@live.com>
2019-04-26 16:56:52 -0700

    arrays complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -74,7 +74,12 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities)
+
+for(let i = 0; i < graduates.length; i++){
+  universities.push(graduates[i]["university"])
+}
+
+console.log(universities.sort())
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
 
@@ -83,9 +88,12 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+for(let i = 0; i < graduates.length; i++){
+  contactInfo.push(graduates[i]["first_name"] + " " + graduates[i]["email"])
+}
+
 console.log(contactInfo);
-
-
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
 console.log(uni);
