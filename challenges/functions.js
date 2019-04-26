1359de458aa7e5539c611cba64df9609618b907a
Elvis Gonzalez <ElvisJGonzalez@gmail.com>
2019-04-26 19:09:09 -0400

    Finished closures question

--- challenges/functions.js
+++ challenges/functions.js
@@ -42,7 +42,7 @@ console.log(consume('Mary', 'Poppins', greeting)); // Hello Mary Poppins, nice t
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation:
+// Explanation: closure, internal is within the lexical scope of nestedfunction()
 
 const external = "I'm outside the function";
 
