78f2ad2a1d5016e20813a3c7c258be454f075c5a
Tatianna-Gilliam <gilliamtatiannam@gmail.com>
2019-05-24 09:10:45 -0700

    Objects-arrays
    Have some errors in console, don't know why...

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,28 +8,60 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+let tyrannosaurus = {
+  "name": "tyrannosaurus",
+  "diet": "carnivorous",
+  "weight": "7000kg",
+  "length": "12m",
+  "period": "Late Cretaceious",
+  "speak": function() {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+}
+
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+let stegosaurus = {
+  "name": "stegosaurus",
+  "diet": "herbivorous",
+  "weight": "2000kg",
+  "length": "9m",
+  "period": "Late Jurassic"
+}
+
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+let velociraptor = {
+  "name": "velociraptor",
+  "diet": "carnivorous",
+  "weight": "15kg",
+  "length": "1.8m",
+  "period": "Late Cretaceious"
+}
+
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
 console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
 console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
 console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
 console.log();
-
+console.log(tyrannosaurus.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
 console.log();
-
+console.log(tyrannosaurus.speak());
 
 // ==== Arrays ====
 
@@ -49,6 +81,19 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
+const graduates = [
+  {"id":1,"first_name":"Cynde","university":"Missouri Southern State College","email":"ctorry0@macromedia.com"},
+  {"id":2,"first_name":"Saundra","university":"The School of the Art Institute of Chicago","email":"swhal1@state.gov"},
+  {"id":3,"first_name":"Lambert","university":"Marian College","email":"lparham2@techcrunch.com"},
+  {"id":4,"first_name":"Modestine","university":"International Medical & Technological University","email":"mdolder3@symantec.com"},
+  {"id":5,"first_name":"Chick","university":"Sultan Salahuddin Abdul Aziz Shah Polytechnic","email":"camorts4@google.com.au"},
+  {"id":6,"first_name":"Jakob","university":"Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft","email":"jharken5@spiegel.de"},
+  {"id":7,"first_name":"Robbi","university":"Salem University","email":"rbrister6@redcross.org"},
+  {"id":8,"first_name":"Colline","university":"Coastal Carolina University","email":"cbrosh7@alibaba.com"},
+  {"id":9,"first_name":"Michail","university":"Universidad Católica de Ávila","email":"mrome8@shinystat.com"},
+  {"id":10,"first_name":"Hube","university":"Universitat Rovira I Virgili Tarragona","email":"hlethbrig9@foxnews.com"}
+]
 const universities = [];
 console.log(universities)
 
@@ -59,6 +104,10 @@ Name email@example.com
 
 Log the result of your new array. */
 const contactInfo = [];
+
+for (let i = 0; i < graduates.length; i++) {
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`);
+}
 console.log(contactInfo);
 
 
