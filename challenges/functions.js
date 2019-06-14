2adfbed65872796fb45d9fb1203cb5762d29315d
unknown <kenneth.austin93@gmail.com>
2019-06-14 13:22:08 -0400

    2nd commit arrays

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,7 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-dfd
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
