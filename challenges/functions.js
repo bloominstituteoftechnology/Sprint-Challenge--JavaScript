d259de87c1011dded6009a3a342db8d2a5823afd
dangolbeeker <scottb305@outlook.com>
2019-06-14 15:40:33 -0400

    fixed error

--- challenges/functions.js
+++ challenges/functions.js
@@ -15,41 +15,41 @@ function consume(agg1, agg2, cb) {
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-const add = function(a, b){
+const add = function (a, b) {
   return a + b;
 }
-const multiply = function(a, b){
+const multiply = function (a, b) {
   return a * b;
 }
-const greeting = function(firstName, lastName){
-return `Hello ${firstName} ${lastName}, nice to meet you!`;
-
-// /* Step 3: Check your work by un-commenting the following calls to consume(): */
-consume(2,2,add); // 4
-consume(10,16,multiply); // 160
-consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
-
-// ==== Closures ==== 
-
-// Explain in your own words why `nestedfunction()` can access the variable `internal`.
-
-// Explanation: since nestedFunction is essential an object of it's parent myFunction, it inherits all of the variables within myFunction. A way I visualize it in my own words is myFunction is the `main scope` of all code within the function. all variables declared within the `main scope` can be accessed from what I call a `child scope`(a function that is nested in another function).
-
-
-const external = "I'm outside the function";
-
-function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
-
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
+const greeting = function (firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
 }
-myFunction();
+  // /* Step 3: Check your work by un-commenting the following calls to consume(): */
+  consume(2, 2, add); // 4
+  consume(10, 16, multiply); // 160
+  consume("Mary", "Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
-function newFunction() {
+  // ==== Closures ==== 
+
+  // Explain in your own words why `nestedfunction()` can access the variable `internal`.
+
+  // Explanation: since nestedFunction is essential an object of it's parent myFunction, it inherits all of the variables within myFunction. A way I visualize it in my own words is myFunction is the `main scope` of all code within the function. all variables declared within the `main scope` can be accessed from what I call a `child scope`(a function that is nested in another function).
+
+
+  const external = "I'm outside the function";
+
+  function myFunction() {
+    console.log(external);
+    const internal = "Hello! I'm inside myFunction!";
+
+    function nestedFunction() {
+      console.log(internal);
+    };
+    nestedFunction();
+  }
+  myFunction();
+
+  function newFunction() {
     consume(2, 2, add); // 4
     consume(10, 16, multiply); // 160
     consume("Mary", "Poppins", greeting);
