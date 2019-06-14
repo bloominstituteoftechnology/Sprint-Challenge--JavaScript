2e2b9738ed478eedde9fcd7dbfad5f95d104281a
johnschneider1 <johnlawschneider@gmail.com>
2019-06-14 12:01:44 -0500

    functions

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -240,7 +240,7 @@ The zoos need a list of all their animal's names (names only, not scientific) co
 
 */
 
-const lowerCase = [];
+const lowerCase = zooAnimals.map(animal => animal.animal_name.toLowerCase());
 console.log(lowerCase);
 
 /* Request 3: .filter() 
@@ -248,7 +248,7 @@ console.log(lowerCase);
 The zoos are concenred about animals with a lower population count. Find out which animals have a population less than 5.
 
 */
-const lowerPopulation = [];
+const lowerPopulation = zooAnimals.filter(animal => animal.population < 5);
 console.log(lowerPopulation);
 
 /* Request 4: .reduce() 
