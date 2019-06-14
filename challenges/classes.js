1da10c45203fb2fe1f309d7a52d966f6bb1a965e
noorosce <077068@yahoo.com>
2019-06-14 18:03:11 -0500

    Answered the qustions in README.md and did small changes on all files

--- challenges/classes.js
+++ challenges/classes.js
@@ -21,11 +21,11 @@ const cuboid = new CuboidMaker ({ length: 4, width: 5, height: 5,})
 // Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker. 
 // Test your work by logging out your volume and surface area.
 
-class CubeMaker extends CuboidMaker {
-    constructor (att){
-    super(att)
+// class CubeMaker extends CuboidMaker {
+//     constructor (att){
+//     super(att)
 
-    }
-};
+//     }
+// };
 
-console.log(CubeMaker.volume());
+// console.log(CubeMaker.volume());
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -51,7 +51,7 @@ console.log(stegosaurus.length);
 // What time period did tyrannosaurus live in?
 console.log(tyrannosaurus.period);
 
-tyrannosaurus.roar = () =>  "RAWERSRARARWERSARARARRRR!";
+tyrannosaurus.roar = () => "RAWERSRARARWERSARARARRRR!";
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
 console.log(tyrannosaurus.roar());
 
