70f7a1b467345ffa9179fb8d494deb1862ededa8
Greg Johnston <johng3269@gmail.com>
2019-06-14 11:53:03 -0400

    Functions done

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,31 +1,44 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====
 
 /* Step 1: Create a higher-order function that accepts a callback
   * Create a higher-order function named consume that can take 3 parameters.
   * The first two parameters can accept any argument
-  * The last parameter accepts a callback 
+  * The last parameter accepts a callback
   * In the body of the function return the callback with the two parameters that you created
 */
 
+let consume = function (num1, num2, callback) {
+    return callback(num1, num2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
+  * Create a function named multiply that returns the product of two numbers
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+let add = function (a, b) {
+    return a+b;
+};
 
+let multiply = function (a, b) {
+    return a*b;
+};
+
+let greeting = function (first, last) {
+    return `Hello ${first} ${last}, nice to meet you!`
+};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2,2,add)); // 4
+console.log(consume(10,16,multiply)); // 160
+console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
-// ==== Closures ==== 
+// ==== Closures ====
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: Because they are both within the closure of myFunction().
 
 
 const external = "I'm outside the function";
