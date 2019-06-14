bcca57322dbd413f3c94590033fcfbdcbc2a909f
dangolbeeker <scottb305@outlook.com>
2019-06-14 12:26:32 -0400

    working thru arrays

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -52,6 +52,7 @@ console.log(trex.period);
 console.log(trex.roar());
 
 
+
 // ==== Arrays ====
 
 // Given an array of college graduates.  Complete the following requests using any array method you like
@@ -71,6 +72,10 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for (let university = 0; university < graduates.length; university++) {
+  universities.push(graduates[university].university);
+}
+universities.sort();
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
