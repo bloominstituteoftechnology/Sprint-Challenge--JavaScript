4860b6c71136c85059c5246012d713f0294ec877
dangolbeeker <scottb305@outlook.com>
2019-06-14 13:29:25 -0400

    prototypes completed

--- challenges/functions.js
+++ challenges/functions.js
@@ -24,11 +24,10 @@ const multiply = function(a, b){
 const greeting = function(firstName, lastName){
 return `Hello ${firstName} ${lastName}, nice to meet you!`;
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
+// /* Step 3: Check your work by un-commenting the following calls to consume(): */
 consume(2,2,add); // 4
 consume(10,16,multiply); // 160
 consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
-
 
 // ==== Closures ==== 
 
@@ -49,3 +48,9 @@ function myFunction() {
   nestedFunction();
 }
 myFunction();
+
+function newFunction() {
+    consume(2, 2, add); // 4
+    consume(10, 16, multiply); // 160
+    consume("Mary", "Poppins", greeting);
+  }
