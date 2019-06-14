b77eb7cc3a4417e8903424ad076eab03226e396f
Janet Azehko <Janet@projecterostribe.com>
2019-06-14 10:43:58 -0700

    objects section complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,24 +7,27 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+var dinosaur1 = { name: "tyrannosaurus", diet: " carnivorous", weight: "7000kg", length:"12m", period:"LateCretaceous"}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+var dinosaur2 = {name: "stegosaurus", diet:"herbivorous", weight: "2000kg", length:"9m", period: "LateJurassic"} 
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+var dinosaur3 = {name: "velociraptor", diet: "carnivorous", weight: "15kg", length: "1.8m", period: "LateCretaceous", }
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinosaur1.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinosaur3.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinosaur2.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinosaur1.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
