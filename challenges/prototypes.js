6b39732a8dd61946e2f5e7311d50407a98b485df
unknown <kenneth.austin93@gmail.com>
2019-06-14 15:22:03 -0400

    commit

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -42,10 +42,7 @@ const cuboid = new CuboidMaker({
   height: 5
 })
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
-
-console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130	console.log(cuboid.surfaceArea()); // 130
+ console.log(cuboid.volume()); // 100
+ console.log(cuboid.surfaceArea()); // 130
 
 
