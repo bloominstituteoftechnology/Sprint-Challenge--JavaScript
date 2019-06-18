f6b5302d86878cc7b1a5e927e6cf20993f34b31a
kevin <akolade4real@gmail.com>
2019-06-17 19:57:36 -0500

    adebola-aiyewumi

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,42 +1,63 @@
 // ==== Callbacks ====  
 
 /* Step 1: Create a higher-order function that accepts a callback
-  * Create a higher-order function named consume that can take 3 parameters.
-  * The first two parameters can accept any argument
-  * The last parameter accepts a callback 
-  * In the body of the function return the callback with the two parameters that you created
-*/
+ * Create a higher-order function named consume that can take 3 parameters.
+ * The first two parameters can accept any argument
+ * The last parameter accepts a callback 
+ * In the body of the function return the callback with the two parameters that you created
+ */
+function consume(thing1, thing2, cb) {
 
+    return cb(thing1, thing2);
+
+}
 
 /* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
+ * Create a function named add that returns the sum of two numbers
+ * Create a function named multiply that returns the product of two numbers 
+ * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+ */
+const add = (num1, num2) => {
+    return num1 + num2;
+}
 
+const multiply = (num1, num2) => {
+
+    return num1 * num2;
+}
+
+const greeting = (first, last) => {
+
+    return `Hello ${first} ${last}, nice to meet you!`
+
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
 // consume(10,16,multiply); // 160
 // consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
+console.log(consume(2, 2, add));
 
+console.log(consume(10, 16, multiply));
+
+console.log(consume("Mary", "Poppins", greeting));
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
 
-
+//'Internal' is within the scope of "nestedfunction()"" because "internal" is defined within "nestedfunction(), therefore they are within the same closure." 
 const external = "I'm outside the function";
 
 function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+    console.log(external);
+    const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
+    function nestedFunction() {
+        console.log(internal);
+    };
+    nestedFunction();
 }
 myFunction();
