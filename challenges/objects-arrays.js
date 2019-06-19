803466d8c5dd45186a8705dece78a37459e88de2
kstjohn93 <50053852+kstjohn93@users.noreply.github.com>
2019-06-19 18:45:58 -0500

    second commit

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -106,6 +106,7 @@ for (let i = 0; i < graduates.length; i++) {
 }
 
 console.log(contactInfo);
+
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
 const uni = [];
 
