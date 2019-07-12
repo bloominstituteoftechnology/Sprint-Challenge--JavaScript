e042faec367333fe205996101a00c8ff129df685
spettigrew <sara.pettigrew5@gmail.com>
2019-07-12 17:15:25 -0600

    ReadMe questions completed and objects completed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,23 +12,52 @@
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
 
+
+const dinosaurs = {
+  "name": 'Tyrannosaurus',
+  "diet": 'carnivourous',
+  "weight": '7000 kg',
+  "length": '12m',
+  "period": 'Late Cretaceous',
+  "roar": function () {
+    return "RAWERSRARARWERSARARARRRR!"
+  }
+}
+
+const dinosaurs1 = {
+  "name": 'stegosaurus',
+  "diet": 'herbivorous',
+  "weight": '2000 kg',
+  "length": '9m',
+  "period": 'Late Jurassic'
+}
+const dinosaurs2 = {
+  "name": 'velociraptor',
+  "diet": 'carnivourous',
+  "weight": '15 kg',
+  "length": '1.8m',
+  "period": 'Late Cretaceous'
+}
+
+
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinosaurs.name);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinosaurs2.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinosaurs1.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinosaurs.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+const tyrannosaurus = dinosaurs;
+console.log(dinosaurs.roar());
 
 
 // ==== Arrays ====
