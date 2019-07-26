9ec0ffaea7f0273ba0a0c300593b02b4e1c6c164
John Rojcewicz <52579412+rojcewiczj@users.noreply.github.com>
2019-07-26 12:59:16 -0500

    completed all but the stretch goal for class building

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -16,7 +16,7 @@ function CuboidMaker(attr) {
   
   Formula for cuboid volume: length * width * height
 */
-CuboidMaker.prototype.Volume = function() {
+CuboidMaker.prototype.volume = function() {
   return this.length * this.width * this.height;
 }
 
@@ -25,7 +25,7 @@ CuboidMaker.prototype.Volume = function() {
 
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
-CuboidMaker.prototype.SurfaceArea = function() {
+CuboidMaker.prototype.surfaceArea = function() {
   return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
 }
 
@@ -33,9 +33,15 @@ CuboidMaker.prototype.SurfaceArea = function() {
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
+const cuboid = new CuboidMaker ({
+  length: 4,
+  width: 5,
+  height: 5,
+})
+
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+ console.log(cuboid.volume()); // 100
+ console.log(cuboid.surfaceArea()); // 130
 
 
