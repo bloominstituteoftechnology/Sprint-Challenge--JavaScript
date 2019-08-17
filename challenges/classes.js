8440cda299244ccd701183bd13be49a20e946b91
emster7013 <52601501+emster7013@users.noreply.github.com>
2019-08-16 22:29:22 -0400

    finished classes and functions

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,22 +1,5 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-//function CuboidMaker (attr){
-   // this.length = attr.length;
-   // this.width = attr.width;
-   // this.height = attr.height;
- // }
-
-  
- // CuboidMaker.prototype.volume = function() {
-   // return this.length * this.width * this.height;
-  //}
-
-  
-  //CuboidMaker.prototype.surfaceArea = function() {
-   // return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
-  //} 
-
-  //CLASSSES START HERE -------------------------------------------------------------------------------------------------------------------------------
   class CuboidMaker {
       constructor(attr){
           this.length = attr.length;
