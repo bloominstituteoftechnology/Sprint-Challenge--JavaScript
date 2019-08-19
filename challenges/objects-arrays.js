bac92b132e89b2cb6e7b2528e90ff93e961992c9
Nolu <nrobynmac123@gmail.com>
2019-08-18 22:08:39 -0500

    finished class questions

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -74,7 +74,7 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = graduates[];
+const universities = graduatesF[];
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
