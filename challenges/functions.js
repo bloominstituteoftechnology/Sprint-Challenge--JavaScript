b2a3cb373cc51798aaa445abc07e9a69b3e98a99
Nolu <nrobynmac123@gmail.com>
2019-08-18 21:11:13 -0500

    Answered functions question in JavaScript

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,13 +7,45 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+ function consume(num1, num2, callback){
+  let divide = num1 / num2;
+ callback(divide);
+}
+
+consume(2, 2,function(divide){
+  console.log(divide);
+  })
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function consume(num1, num2, callback){
+  let add = num1 + num2;
+ callback(add);
+}
 
+consume(2, 2, function(add){
+  console.log(add);
+})
+
+function consume(num1, num2, callback){
+  let multiply = num1 * num2;
+ callback(multiply);
+}
+
+consume(10, 16, function(multiply){
+  console.log(multiply);
+})
+
+function consume(firstname, lastname, callback){
+  let greeting = `Hello ${firstname} ${lastname}, nice to meet you!`;
+  callback(greeting);
+}
+consume("Mary", "Poppins", function(greeting){
+  console.log(greeting);
+})
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
@@ -25,7 +57,7 @@
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: nested function can axcess the variablle internal because internal is defined within the scope of the the function called myFunction. The scope is local.
 
 
 const external = "I'm outside the function";
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,27 +8,52 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+tyrannosaurus ={
+   "diet": "carnivorous",
+   "weight": "7000kg",
+   "length": "12m",
+   "period": "Late Cretaceous",
+ }
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+stegosaurus ={
+  "diet": "herbivorous",
+  "weight": "2000kg",
+  "length": "9m",
+  "period": "Late Jurassic",
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+velociraptor ={
+  "diet": "carnivorous",
+  "weight": "15kg",
+  "length": "1.8m",
+  "period": "Late Cretaceous",
+}
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.carnivorous);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
-// Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+// Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result. FIXXXX
+tyrannosaurus ={
+  "diet": "carnivorous",
+  "weight": "7000kg",
+  "length": "12m",
+  "period": "Late Cretaceous",
+  "roar": funciton() {
+ alert(`RAWERSRARARWERSARARARRRR`)};
+};
+console.log(tyrannosaurus.roar);
 
 
 // ==== Arrays ====
@@ -49,7 +74,7 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array.  
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+const universities = graduates[];
 console.log(universities)
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. 
