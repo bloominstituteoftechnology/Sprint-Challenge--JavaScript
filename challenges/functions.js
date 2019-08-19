acf4277cf1ac7e2effc78dfb2fc66749c789ab26
mark stahl <markstahl0219@gmail.com>
2019-08-18 22:01:53 -0400

    functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(num1, num2, callback) {
+  return (callback(num1, num2));
+}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +18,30 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(num1, num2) {
+  return num1 + num2;
+}
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function greeting(first_name, last_name) {
+  return `Hello ${first_name} ${last_name}, nice to meet you!`
+}
+
+
+/* Step 3: Check your work by un-commenting the following calls to consume(): */;
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: Because nestedFunction is nested within myFunction and is also in the same scope as the constant internal, therefor being able to access it when called.
 
 
 const external = "I'm outside the function";
