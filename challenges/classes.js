ebc8bffd3acb4754ba0f75f81796f20817646892
Eric Foster <elitheroyalone@gmail.comgit config --global user.name eric-fostergit config --global user.email elitheroyalone@gmail.com>
2019-08-23 18:02:34 -0500

    MVP 2

--- challenges/classes.js
+++ challenges/classes.js
@@ -20,7 +20,7 @@ class CuboidMaker{
     
       const cuboid = new CuboidMaker({length:4, width:5, height:5}); 
 
-// Test your volume and surfaceArea methods by uncommenting the logs below:
+// Test your volume and surfaceArea methods by uncommenting the logs below:44
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
