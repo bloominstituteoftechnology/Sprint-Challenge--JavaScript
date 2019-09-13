dc850bf129977f0b9ba9a5e3831bc13e74f199ee
elsieAguilar <52051943+elsieAguilar@users.noreply.github.com>
2019-09-13 18:21:27 -0500

    Created 3 objects with the given info about dinosaurs, and created a method for one of them

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,7 +5,29 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
+const dinasourOne = {
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceous"
+};
 
+const dinasourTwo = {
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic"
+};
+
+const dinasourThree = {
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceous"
+};
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -15,20 +37,21 @@
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinasourOne.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinasourThree.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinasourTwo.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinasourOne.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+dinasourOne.rawr = "RAWERSRARARWERSARARARRRR!";
+console.log(dinasourOne.rawr);
 
 
 // ==== Arrays ====
