87e6f41c953d3e616e7cf8448fb85f08d78fcbae
Ray Aguilar <ray.aguilar15@yahoo.com>
2019-09-14 11:50:10 -0500

    higher-order function works

--- challenges/functions.js
+++ challenges/functions.js
@@ -16,12 +16,25 @@ function consume(param1, param2, cb) {
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(num1, num2) {
+  return num1 + num2;
+};
+
+
+function multiply(num1, num2) {
+  return num1 * num2;
+};
+
+function greeting(firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+};
+
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2,2,add)); // 4
+ console.log(consume(10,16,multiply)); // 160
+ console.log(consume("Mary","Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
