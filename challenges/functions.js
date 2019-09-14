672665317c4a1b9c5271105be781c0b099401abf
Samuel Kraus <S_read@aol.com>
2019-09-14 12:38:38 -0500

    completed stretch goals

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,9 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume (arg1, arg2, cb) {
+  return cb(arg1,arg2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -15,17 +17,35 @@
 */
 
 
+function add(num1,num2){
+  return num1+num2;
+};
+
+
+function multiply(num1,num2){
+  return num1*num2;
+};
+
+function greeting(firstname,lastname){
+  return `"Hello ${firstname} ${lastname}, nice to meet you!"`
+};
+
+
+
+
+
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: Because of the way closures work, they allow inner functions to access the outer scope of the functions they're in!
+
 
 
 const external = "I'm outside the function";
