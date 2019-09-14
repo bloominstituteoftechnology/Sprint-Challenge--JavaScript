f8391cb6adb7b78941621fbe00fc63f159ffaa19
elsieAguilar <52051943+elsieAguilar@users.noreply.github.com>
2019-09-13 20:59:44 -0500

    fixed the universities, contaInfo and found universities with Uni included in their names

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -73,12 +73,15 @@ const graduates = [
 
 //Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
-const universities = graduates.map((items) => {
- return items.university;
-});
+
 
 //Once you have the new array created, sort the universities alphabetically and log the result. */
-universities.sort();
+const universities = [];
+const univs = (item) => {
+  universities.push(`${item.university}`)
+  universities.sort()
+};
+graduates.map(univs);
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -88,6 +91,10 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach((item => {
+  contactInfo.push(`${item.first_name} ${item.email}`)
+}))
+console.log(contactInfo);
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
