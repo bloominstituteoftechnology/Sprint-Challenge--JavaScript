5ae729fc135578961efef3edfbd87563bb8d10d0
Michael Lowe <mrlowememphis@yahoo.com>
2019-09-16 10:23:38 -0500

    Second commit of JS Sprint Challenge all files modified

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,9 +1,41 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
+class CuboidMaker {
+    constructor(length, width, height) {
+      this.length = length;
+      this.width = width;
+      this.height = height;
+    }
+    volume() {
+      return this.length * this.width * this.height;
+    }
+    surfaceArea() {
+      return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    }
+  }
 
+  const cuboid = new CuboidMaker(4, 5, 5);
 
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+ console.log(cuboid.volume()); // 100
+ console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker {
+    constructor (side) {
+      super(side);
+      this.side = side;
+    }
+  
+    volume() {
+      return Math.pow(this.side, 3);
+    }
+    surfaceArea() {
+      return 6 * Math.pow(this.side, 2);
+    } 
+  }
+  
+  const cube = new CubeMaker (2);
+  console.log(cube.volume());
+  console.log(cube.surfaceArea()); 
--- challenges/functions.js
+++ challenges/functions.js
@@ -6,37 +6,40 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
-
+function consume (p1, p2, cb) {
+  return cb(p1, p2);
+}
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
-
+function add (num1, num2) {return num1 + num2;}
+function multiply(num1, num2) {return num1 * num2;}
+function greeting(first_name, last_name) {
+  return `Hello ${first_name} ${last_name}, nice to meet you!`
+}
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+ console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: The variable is within the block scope of the function. The outside function passes down everything to inner functions.
 
+//const external = "I'm outside the function";
 
-const external = "I'm outside the function";
-
-function myFunction() {
-  console.log(external);
+//function myFunction() {
+  //console.log(external);
   const internal = "Hello! I'm inside myFunction!";
 
   function nestedFunction() {
     console.log(internal);
   };
   nestedFunction();
-}
-myFunction();
+
+//myFunction()
