054377529bb07705a7020d77c01747e78c02c688
Michelle Tinajero <elletinajero11@gmail.com>
2019-09-15 20:21:30 -0500

    added to functions

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,14 +6,27 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume (a,b,cb) {
+  return cb(a, b);
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add (a, b) {
+  return a + b;
+};
 
+function multiply (a, b) {
+  return a * b;
+};
+
+function greeting (firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+};
+  
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
@@ -25,7 +38,9 @@
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: Well, the definition of a closure alone is that it will give you access to the outside scope of a function from INSIDE the function scope.
+            //  In the problem, we can see that internal is defined OUTSIDE of the nestedFunction function. Using closure, we are able to 
+            //  console log internal from inside the nestedFunction scope, even though we defined it outside of the scope.
 
 
 const external = "I'm outside the function";
