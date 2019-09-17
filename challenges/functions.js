01c067be1d51cd4f99dbc47262d3c6b64592e6d4
Joe Brown <leftnovbrown@gmail.com>
2019-09-17 19:39:35 -0400

    completed challenges for Sprint-Challenge--JavaScript.

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,54 +1,54 @@
-// ==== Callbacks ====  
+// // ==== Callbacks ====  
 
-/* Step 1: Create a higher-order function
-  * Create a higher-order function named consume with 3 parameters: a, b and cb
-  * The first two parameters can take any argument (we can pass any value as argument)
-  * The last parameter accepts a callback
-  * The consume function should return the invocation of cb, passing a and b into cb as arguments
-*/
-function consume (a, b, cb) {
-  return cb (a,b);
-}
+// /* Step 1: Create a higher-order function
+//   * Create a higher-order function named consume with 3 parameters: a, b and cb
+//   * The first two parameters can take any argument (we can pass any value as argument)
+//   * The last parameter accepts a callback
+//   * The consume function should return the invocation of cb, passing a and b into cb as arguments
+// */
+// function consume (a, b, cb) {
+//   return cb (a,b);
+// }
 
-/* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
-function add (num1, num2) {
-  return num1 + num2;
-}
+// /* Step 2: Create several functions to callback with consume();
+//   * Create a function named add that returns the sum of two numbers
+//   * Create a function named multiply that returns the product of two numbers 
+//   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+// */
+// function add (num1, num2) {
+//   return num1 + num2;
+// }
 
-function multiply (x,y) {
-  return x * y;
-}
+// function multiply (x,y) {
+//   return x * y;
+// }
 
-function greeting (firstName, lastName) {
-  return `Hello ${firstName} ${lastName}, nice to meet you!`;
-}
+// function greeting (firstName, lastName) {
+//   return `Hello ${firstName} ${lastName}, nice to meet you!`;
+// }
 
-/* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
-
-
-// ==== Closures ==== 
-
-// Explain in your own words why nestedfunction can access the variable internal.
-// Nested function can access the variable internal because they are a part of the same scope of the function.
-// Explanation: 
+// /* Step 3: Check your work by un-commenting the following calls to consume(): */
+// // console.log(consume(2, 2, add)); // 4
+// // console.log(consume(10, 16, multiply)); // 160
+// // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
-const external = "I'm outside the function";
+// // ==== Closures ==== 
 
-function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+// // Explain in your own words why nestedfunction can access the variable internal.
+// // Nested function can access the variable internal because they are a part of the same scope of the function.
+// // Explanation: 
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
-}
-myFunction();
+
+// const external = "I'm outside the function";
+
+// function myFunction() {
+//   console.log(external);
+//   const internal = "Hello! I'm inside myFunction!";
+
+//   function nestedFunction() {
+//     console.log(internal);
+//   };
+//   nestedFunction();
+// }
+// myFunction();
