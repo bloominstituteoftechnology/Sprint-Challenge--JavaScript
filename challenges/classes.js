ff9aca0d0ad505f74266bc82d612e141ff6950d6
Lambda_School_Loaner_160 <lambda_school_loaner_160@Lambda-School-Loaner-160s-MacBook-Pro.local>
2019-09-16 17:36:23 -0700

    completed classes

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,47 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+class CuboidMaker {
+    constructor(att) {
+      this.length = att.length;
+      this.width = att.width;
+      this.height = att.height;
+    }
+  
+    volume() {
+      return this.length * this.width * this.height;
+    }
+  
+    surfaceArea() {
+      return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    }
+  }
+  
+  class CubeMaker extends CuboidMaker {
+    constructor(att) {
+      super(att);
+    }
+  
+    volume() {
+      return this.length * this.width * this.height;
+    }
+  
+    surfaceArea() {
+      return 6 * (this.length * this.height);
+    }
+  }
+  
+  const cuboid = new CuboidMaker({
+    "length": 4,
+    "width": 5,
+    "height": 5,
+  });
+  
+  const newcuboid = new CubeMaker({
+    "length": 3,
+    "width": 2,
+    "height": 4,
+  });
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
