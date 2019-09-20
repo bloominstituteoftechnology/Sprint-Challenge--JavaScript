73905f8b84f3429a0c99ed5e2005612c6f4d22ca
Greg Hayes <gthelight@gmail.com>
2019-09-20 12:58:57 -0500

    finished stretch goals

--- challenges/classes.js
+++ challenges/classes.js
@@ -8,7 +8,7 @@ class CuboidMaker {
   volume() {
     return this.length * this.width * this.height;
   }
-  surfaceArea(length, width, height) {
+  surfaceArea() {
     return (
       2 *
       (this.length * this.width +
@@ -28,3 +28,24 @@ console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+class CubeMaker extends CuboidMaker {
+  constructor(attribute) {
+    super(attribute);
+  }
+  cubeVolume() {
+    return this.length * this.length * this.length;
+  }
+  cubeSurfaceArea() {
+    return 6 * (this.length * this.length);
+  }
+}
+
+const cube = new CubeMaker({
+  length: 5,
+  width: 5,
+  height: 5
+});
+
+console.log(cube.cubeVolume());
+console.log(cube.cubeSurfaceArea());
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -118,8 +118,15 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+// const universities = [];
+// graduates.forEach(function(item) {
+//   universities.push(item.university);
+//   universities.sort();
+// });
+// console.log(universities);
+
 const universities = [];
-graduates.forEach(function(item) {
+graduates.forEach(item => {
   universities.push(item.university);
   universities.sort();
 });
@@ -131,15 +138,21 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
+// const contactInfo = [];
+// graduates.forEach(function(item) {
+//   contactInfo.push(`${item.first_name} ${item.email}`);
+// });
+// console.log(contactInfo);
+
 const contactInfo = [];
-graduates.forEach(function(item) {
+graduates.forEach(item => {
   contactInfo.push(`${item.first_name} ${item.email}`);
 });
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
-graduates.filter(function(item) {
+graduates.filter(item => {
   if (item.university.includes("Uni")) {
     unisWithUni.push(item.university);
   }
@@ -218,8 +231,14 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
+// const displayNames = [];
+// zooAnimals.forEach(function(item) {
+//   displayNames.push(`Name: ${item.animal_name}, ${item.scientific_name}.`);
+// });
+// console.log(displayNames);
+
 const displayNames = [];
-zooAnimals.forEach(function(item) {
+zooAnimals.forEach(item => {
   displayNames.push(`Name: ${item.animal_name}, ${item.scientific_name}.`);
 });
 console.log(displayNames);
@@ -229,9 +248,14 @@ console.log(displayNames);
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 
 */
+// const lowCaseAnimalNames = [];
+// zooAnimals.map(function(item) {
+//   lowCaseAnimalNames.push(item.animal_name.toLowerCase());
+// });
+// console.log(lowCaseAnimalNames);
 
 const lowCaseAnimalNames = [];
-zooAnimals.map(function(item) {
+zooAnimals.map(item => {
   lowCaseAnimalNames.push(item.animal_name.toLowerCase());
 });
 console.log(lowCaseAnimalNames);
@@ -241,8 +265,16 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
+// const lowPopulationAnimals = [];
+// zooAnimals.filter(function(item) {
+//   if (item.population < 5) {
+//     lowPopulationAnimals.push(item);
+//   }
+// });
+// console.log(lowPopulationAnimals);
+
 const lowPopulationAnimals = [];
-zooAnimals.filter(function(item) {
+zooAnimals.filter(item => {
   if (item.population < 5) {
     lowPopulationAnimals.push(item);
   }
@@ -254,14 +286,20 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
-populationTotal = zooAnimals.reduce(function(accumulator, animal) {
-  return accumulator + animal.population;
-}, 0);
-console.log(populationTotal);
+// let populationTotal = 0;
+// populationTotal = zooAnimals.reduce(function(accumulator, animal) {
+//   return accumulator + animal.population;
+// }, 0);
+// console.log(populationTotal);
 
 /*
 
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
 */
+let populationTotal = 0;
+populationTotal = zooAnimals.reduce(
+  (accumulator, animal) => accumulator + animal.population,
+  0
+);
+console.log(populationTotal);
