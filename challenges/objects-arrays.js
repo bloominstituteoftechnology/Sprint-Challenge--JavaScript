4d87fb232cd5b91ef5cbe709ae8026118e8c7523
Norma Bunton <te_nj25@hotmail.com>
2019-09-20 12:36:11 -0400

    objects complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,30 +7,47 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-
+const dinosaurOne = {
+  name: "tyrannosaurus",
+  diet: "carnivorous",
+  weight: "7000kg",
+  length: "12m",
+  period: "Late Cretaceous",
+}
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const dinosaurTwo = {
+  name: "stegosaurus",
+  diet: "herbivorous",
+  weight: "2000kg",
+  length: "9m",
+  period: "Late Jurassic",
+  }
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+const dinosaurThree = {
+  name: "velociraptor",
+  diet: "carnivorous",
+  weight: "15kg",
+  length: "1.8m",
+  period: "Late Cretaceous",
+  }
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinosaurOne.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinosaurThree.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinosaurTwo.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
-
+console.log(dinosaurOne.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
-
-
+  dinosaurOne.speak=function(){  
+return "RAWERSRARARWERSARARARRRR!";}
+console.log(dinosaurOne.speak());
 // ==== Arrays ====
 
 // Given an array of college graduates.  Complete the following requests using any array method you like
