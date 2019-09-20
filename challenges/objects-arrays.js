339d29cc9b0f7eee04dd953c798e702f0cf96b6b
AMC <53350942+AMC-ai@users.noreply.github.com>
2019-09-20 12:29:07 -0400

    finished objects

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,29 +6,55 @@
   object name, diet, weight, length, period
 */
 
+
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+cosnt = dinoOne = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  speak: function () {
+    return 'RAWERSRARARWERSARARARRRR!'
+  }
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+cosnt = dinoTwo = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic',
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+cosnt = dinoThree = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous',
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinoOne.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinoThree.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinoTwo.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinoOne.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(dinoOne.speak());
 
 
 // ==== Arrays ====
