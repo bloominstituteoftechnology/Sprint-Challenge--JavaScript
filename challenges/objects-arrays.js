461cc4c7e1714a09e36e49c530359e7490164d46
spettigrew <sara.pettigrew5@gmail.com>
2019-10-02 20:44:06 -0600

    small change

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -140,7 +140,7 @@ console.log(bothNames);
 
 /* Request 2: .map()    
 
-The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
+The zoos need a list of all their animal's names (names only, not scientific) converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the result.
 
 */
 
