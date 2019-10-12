65fd93de3a805b4293fc6112600c8c3c7cafc734
Ana Tulea <anatulea@MacBook-Pro-de-Ana.local>
2019-10-12 10:24:03 -0700

    functions completed

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,19 +6,34 @@
   * The last parameter accepts a callback 
   * In the body of the function return the callback with the two parameters that you created
 */
-
+function consume(item1, item2, callback) {
+  return callback(item1, item2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(a, b, cb) {
+  return cb = a + b;
+}
+console.log(consume(6, 2, add));
 
+function multiply(a, b, cb) {
+  return cb = a * b;
+}
+console.log(consume(10, 16, multiply));
+
+const greeting = (firstName, lastName, cb) => {
+  return `${"Hello"} ${firstName} ${lastName} ${"nice to meet you!"}`
+}
+console.log(consume('Marry', 'Poppins,', greeting));
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -26,6 +41,10 @@
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+// Because nestedFunction() can reach outside its scope to find `internal` because it
+// does not exist inside nestedFunction's scope when `internal` is referenced. In other words
+//the child function - `nestedFunction()` has accsess to the parent's - `myFunction()`'s scope and to the global scope too.
+// While the parent function doesn't have accesss to the child's scope but only to the global scope.
 
 
 const external = "I'm outside the function";
