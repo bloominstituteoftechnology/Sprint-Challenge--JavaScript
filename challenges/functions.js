ca82733e9061419aa93a774de65fd59020bc5e1a
Anthony Moton <ysir09@gmail.com>
2019-10-12 12:50:25 -0500

    finished with closures and callbacks

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,9 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
+const consume = function(a, b, cb) {
+  return cb(a, b);
+};
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,19 +16,26 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
-
+function add(a, b){
+  return a + b;
+};
+function multiply(a, b){
+  return a * b;
+};
+function greeting(firstName, lastName) {
+  return (`Hello ${firstName} ${lastName}, nice to meet you!`);
+}
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: nestedfunction can access the variable internal because nestedfunction (which is inside the myFunction function) has the ability to reach outward for context (although never inward).
 
 
 const external = "I'm outside the function";
