c403c98c6ea8b2ea9e608aaa45e303f739d6a6c7
Anthony Moton <ysir09@gmail.com>
2019-10-14 23:16:20 -0500

    finally done with the sprint

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -79,6 +79,12 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+
+let schools = graduates.forEach(function (school) {
+  universities.push(school.university)
+})
+
+universities.sort();
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -88,10 +94,16 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+let contacts = graduates.forEach(function (students) {
+  contactInfo.push(`${students.first_name} ${students.email}`)
+})
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+const unisWithUni = graduates.filter((char) => {
+
+  return char.university.includes("Uni")
+  });
 console.log(unisWithUni);
 
 
@@ -118,6 +130,9 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+let zoo = zooAnimals.forEach(function (animals) {
+  displayNames.push(`Name: ${animals.animal_name}, Scientific: ${animals.scientific_name}`)
+})
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -126,7 +141,8 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(x => (x.animal_name).toLowerCase());
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -134,7 +150,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter(x => (x.population < 5));
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -142,7 +158,9 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((total, animals) => {
+  return total += animals.population;
+},0);
 console.log(populationTotal);
 
 
--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -19,8 +19,8 @@ function CuboidMaker(values) {
   
   Formula for cuboid volume: length * width * height
 */
-CuboidMaker.prototype.volume = function(volume) {
-  return length * width * height;
+CuboidMaker.prototype.volume = function() {
+  return this.length * this.width * this.height;
 }
 
 /* == Step 3: Surface Area Method ==
@@ -28,8 +28,8 @@ CuboidMaker.prototype.volume = function(volume) {
 
   Formula for cuboid surface area of a cube: 2 * (length * width + length * height + width * height)
 */
-CuboidMaker.prototype.surfaceArea = function(surfaceArea) {
-  return 2 * (length * width + length * height + width * height);
+CuboidMaker.prototype.surfaceArea = function() {
+  return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
 }
 
 
@@ -37,11 +37,14 @@ CuboidMaker.prototype.surfaceArea = function(surfaceArea) {
   Create a cuboid object that uses the new keyword to use our CuboidMaker constructor
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
+
+
 const cuboid = new CuboidMaker ({
   length: 4,
   width: 5,
   height: 5
 });
+
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
