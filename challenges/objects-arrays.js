7d9f915b00575ca024d980953b82bddc51fa958e
Anthony Moton <ysir09@gmail.com>
2019-10-14 23:49:49 -0500

    the sprint is finished!

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -163,7 +163,6 @@ const populationTotal = zooAnimals.reduce((total, animals) => {
 },0);
 console.log(populationTotal);
 
-
 /*
 
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
