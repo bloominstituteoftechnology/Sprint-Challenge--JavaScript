a43154a2bb4775a55a642b4c1007f9d7e3450408
Aeson Johnson <aeson.m.johnson@gmail.com>
2019-10-18 09:23:06 -0700

    aeson-johnson

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,8 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function consume(a, b, c){}
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -6,29 +6,58 @@
   object name, diet, weight, length, period
 */
 
+
+
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+
+const tyra={
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  roar: function(){
+    return 'RAWERSRARARWERSARARARRRR!'
+  }
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const steg={
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+}
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const velo={
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyra.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velo.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(steg.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyra.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyra.roar());
 
 
 // ==== Arrays ====
@@ -52,7 +81,11 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
-console.log(universities);
+for (let i = 0; i < graduates.length; i++){
+  universities[i] = graduates[i].university;
+}
+console.log(universities.sort());
+
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -63,10 +96,17 @@ Log the result of your new array. */
 const contactInfo = [];
 console.log(contactInfo);
 
+graduates.forEach(function(items){
+  return contactInfo.push (`${items.first_name} ${items.email}`)
+})
+
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
 console.log(unisWithUni);
 
+graduates.filter(function(items){
+  return unisWithUni.push(items.university > "Uni");
+});
 
 // ==== ADVANCED Array Methods ====
 
@@ -93,6 +133,10 @@ The zoos want to display both the scientific name and the animal name in front o
 const displayNames = [];
 console.log(displayNames);
 
+zooAnimals.forEach(function(items){
+  return displayNames.push(`Name: ${items.animal_name}, Scientific: ${items.scientific_name}`)
+})
+
 /* Request 2: .map()
 
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
@@ -109,6 +153,10 @@ The zoos are concerned about animals with a lower population count. Using filter
 */
 const lowPopulationAnimals = [];
 console.log(lowPopulationAnimals);
+
+zooAnimals.filter(function(items){
+  return lowPopulationAnimals.push(items.population < 5);
+})
 
 /* Request 4: .reduce() 
 
