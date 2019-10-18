64cc1b0e782d41392638d751f481cb3cdd788d95
Miguel Nicolas <miguelqnicolas@gmail.com>
2019-10-18 12:53:24 -0400

    completed task 1, 2, and 3

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -8,28 +8,59 @@
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
+const tyrannosaurus = {
+	name: 'tyrannosaurus',
+	diet: 'carnivorous',
+	weight: '7000kg',
+	length: '12m',
+	period: 'Late Cretaceous',
+	roar: function() {
+		return `RAWERSRARARWERSARARARRRR!`;
+	}
+}
+
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stegosaurus = {
+	name: 'stegosaurus',
+	diet: 'herbivorous',
+	weight: '2000kg',
+	length: '9m',
+	period: 'Late Jurassic'
+}
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+const velociraptor = {
+	name: 'velociraptor',
+	diet: 'carnivorous',
+	weight: '15kg',
+	length: '1.8m',
+	period: 'Late Cretaceous'
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+
+console.log(velociraptor['diet']);
 
 // How long was a stegosaurus?
-console.log();
+
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
 
+console.log(tyrannosaurus.roar());
 
 // ==== Arrays ====
 
@@ -51,7 +82,13 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+
+const universities = graduates.map(array => array.university);
+
+universities.sort();
+
+// could we have done this inside .map method?
+
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -60,13 +97,21 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+
+const contactInfo = graduates.map(array => `${array.first_name} ${array.email}`)
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
-console.log(unisWithUni);
 
+const unisWithUni = graduates.filter(function(array) {
+	return array.university.includes('Uni');
+	}
+);
+
+// ?
+
+console.log(unisWithUni);
 
 // ==== ADVANCED Array Methods ====
 
@@ -91,6 +136,13 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+
+zooAnimals.forEach(function(array) {
+	displayNames.push(`Name: ${array.animal_name}, Scientific: ${array.scientific_name}`);
+});
+
+// difference between this and putting forEach after = in line 138?
+
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -99,7 +151,13 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(function(array) {
+	return array.animal_name.toLowerCase();
+	}
+);
+
+// didn't work as arrow function? maybe because more than one thing is happening?
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -107,7 +165,11 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+
+const lowPopulationAnimals = zooAnimals.filter(function(array) {
+	return array.population < 5;
+});
+
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -115,9 +177,16 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+
+let populationTotal = 0;
+
+populationTotal = zooAnimals.reduce(function(accumulator, currentValue) {
+	return accumulator + currentValue.population;
+}, 0);
+
 console.log(populationTotal);
 
+// two ways to structure, wont let const this way, do you need = 0? or put function right after equal sign?
 
 /*
 
