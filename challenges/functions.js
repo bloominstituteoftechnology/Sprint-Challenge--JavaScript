69e28322a7125f2743d1a6260dfab0e05d78dc4d
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2019-10-18 03:52:15 -0700

    functions-step-three

--- challenges/functions.js
+++ challenges/functions.js
@@ -24,14 +24,14 @@ function multiply(a, b) {
     return (a * b)
 }
 
-function greeting(first - name, last - name) {
-    return (`Hello ${first-name} ${last-name}, nice to meet you!`)
+function greeting(firstName, lastName) {
+    return (`Hello ${firstName} ${lastName}, nice to meet you!`)
 }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
