7a795b454b8426e3eaf56eb739fbc18a15f0e456
o0WhiteTigerOo <supermanrobinhood1986@gmail.com>
2019-11-08 19:48:57 -0600

    Array finished

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -53,8 +53,10 @@ console.log(obj1.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
 const roar = function () {
-  console.log("RAWERSRARARWERSARARARRRR!");
+  return "RAWERSRARARWERSARARARRRR!";
 }
+
+console.log(roar());
 
 
 
@@ -79,6 +81,12 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+for( let i=0; i<graduates.length; i++){
+  universities.push(graduates[i].university);
+  universities.sort();
+}
+
+
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -88,10 +96,18 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+for(let i=0; i<graduates.length; i++){
+  contactInfo.push(graduates[i].first_name + ' ' + graduates[i].email);
+}
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+for(let i=0; i<graduates.length; i++){
+  if(graduates[i].university.includes('Uni')){
+    unisWithUni.push(graduates[i].university);
+  }
+}
 console.log(unisWithUni);
 
 
