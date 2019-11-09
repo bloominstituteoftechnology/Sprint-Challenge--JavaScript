ce1def38cb9ecda5e550eb77ba7e235bddfef268
rdowd <rjarrettdowd@yahoo.com>
2019-11-08 20:42:42 -0500

    finnished objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -102,6 +102,12 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+graduates.forEach(function(element){
+  if(element.university.includes('Uni')){
+    unisWithUni.push(element);
+  }
+
+});
 console.log(unisWithUni);
 
 
@@ -128,6 +134,9 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(function(element){
+  displayNames.push('Names: '+ element.animal_name + ' Scientific: ' + element.scientific_name );
+});
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -136,7 +145,12 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(function(element){
+  return element.animal_name.toLowerCase();
+})
+
+
+
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -144,7 +158,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter(a => a.population < 5);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -152,7 +166,12 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+newArr = [];
+zooAnimals.forEach(function(element){
+  newArr.push(element.population);
+});
+const populationTotal = newArr.reduce((acc, current)=> acc + current);
+
 console.log(populationTotal);
 
 
