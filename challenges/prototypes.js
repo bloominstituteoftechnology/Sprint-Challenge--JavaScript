92d7e3f478bd6262884920658a1d9981de65b0fc
o0WhiteTigerOo <supermanrobinhood1986@gmail.com>
2019-11-08 22:04:30 -0600

    classes completed

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -42,7 +42,7 @@ const cuboid = new CuboidMaker({
   length: 4,
   width: 5,
   height: 5
-})
+});
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
