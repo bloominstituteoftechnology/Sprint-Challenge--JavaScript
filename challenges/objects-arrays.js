87ff38dfe2896f976243f274f8eeda1afa0adc89
Anthony Streett <arjstreett@gmail.com>
2019-11-15 13:29:31 -0500

    Finished Sprint

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -98,8 +98,8 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
-if (graduates.length === 'Uni') {
-  unisWithUni.push(graduates);
+if (graduates.university === 'Uni') { 
+    unisWithUni.push(graduates.university);
 }
 console.log(unisWithUni);
 
