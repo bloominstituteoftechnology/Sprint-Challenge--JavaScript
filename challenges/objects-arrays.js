2b80f18d74f00b9a27c9ee2720ac953d143cd918
Kris Monroe <krisdmonroe@gmail.com>
2019-11-15 11:27:36 -0800

    Good now

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -88,10 +88,9 @@ console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array  
 called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
-if (graduates.university === "Uni"){
-  unisWithUni.push(graduates.university);
-}
+const unisWithUni = graduates.filter(function (uni) {
+    return uni.university.includes("Uni");
+});
 console.log(unisWithUni);
 
 
