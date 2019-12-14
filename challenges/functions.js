ed9f16bc5317ac1880c99770a243bf1f6642b427
Thomas Zieger <thomaszieger@Thomass-MacBook-Pro.local>
2019-12-13 20:18:23 -0700

    initial commit

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,7 +6,9 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume (a, b, cb) {
+  
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,7 +5,32 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
+let trex = new Object () {
+  name = tyrannosaurus,
+  diet = carnivorous,
+  weight = 7000kg,
+  length = 12m,
+  period = Late Cretaceous,
+  roar = function () {
+    return "RAWERSRARARWERSARARARRRR!";
+  }
+}
 
+let steg = new object () {
+  name = stegosaurus,
+  diet = herbivorous,
+  weight = 2000kg,
+  length = 9m,
+  period = Late Jurassic,
+}
+
+let Vel = new object () {
+  name = velociraptor,
+  diet = carnivorous,
+  weight = 15kg,
+  length = 1.8m,
+  period = Late Cretaceous,
+}
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
@@ -15,20 +40,20 @@
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(trex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(vel.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(steg.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(trex.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(trex.roar);
 
 
 // ==== Arrays ====
@@ -51,8 +76,12 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
+for (let i=0; i<graduates.length; i++) {
+universities.sort(graduates[i].university)}
 console.log(universities);
+
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -61,6 +90,7 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+for (let i=0; i<graduates.length; i++)
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
