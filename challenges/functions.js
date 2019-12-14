52b361e402d90f3dbd41b07ea8dea48b89245551
LeiMaya19 <nicholsjanae6@gmail.com>
2019-12-13 20:20:49 -0800

    classes.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,13 +6,26 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+ function consume (a, b, cd){
+   return cd(a, b);
+ }
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add (a, b){
+  return a + b;
+};
+
+function multiply (num1, num2){
+  return num1 * num2;
+};
+
+function greeting(firstName, lastName){
+  return (`Hello ${firstName} ${lastName}, nice to meet you!`);
+};
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
