b369e5509fbf7e2a74674f32ac1ad4691094a15e
blindbatman <emlundaj@gmail.com>
2019-12-15 21:01:13 -0700

    Hotfix cuboid class and cuboid prototype class were conflicting changed class to CuboidMaker2

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,14 +1,14 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
-// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  
-//Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  
+// Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.
+//Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.
 //Test your work by logging out your volume and surface area.
 
-class CuboidMaker {
+class CuboidMaker2 {
   constructor(length, width, height) {
     this.length = length;
     this.height = height;
@@ -27,8 +27,7 @@ class CuboidMaker {
   }
 }
 
-class CubeMaker extends CuboidMaker{
-    super(length,width,height);
+class CubeMaker extends CuboidMaker2 {
+  super(length, width, height) {}
 }
-var cuboid = new CuboidMaker(4, 5, 5);
-
+var cuboid = new CuboidMaker2(4, 5, 5);
