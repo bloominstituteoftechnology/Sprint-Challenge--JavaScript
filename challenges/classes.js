eed08f7b1a769c142a9f1da349d731eba847f2f8
Thomas Zieger <thomaszieger@Thomass-MacBook-Pro.local>
2019-12-15 22:24:48 -0700

    mvp

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,59 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
-// Test your volume and surfaceArea methods by uncommenting the logs below:
-// console.log(cuboid.volume()); // 100
-// console.log(cuboid.surfaceArea()); // 130
+
+
+class CuboidMaker {
+    constructor(properties){
+    (this.length = properties.length),
+    (this.width = properties.width),
+    (this.height = properties.height);
+    }
+    volume(){
+        return this.length * this.width * this.height;
+    }
+    surfaceArea(){
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    }
+
+  }
+
+
+ let cuboid = new CuboidMaker({
+    length: 4,
+    width: 5,
+    height: 5
+  });
+
+  console.log(cuboid);
+  
+
+
+
+
+  // Test your volume and surfaceArea methods by uncommenting the logs below:
+console.log(cuboid.volume()); // 100
+console.log(cuboid.surfaceArea()); // 130
 
 // Stretch Task: Extend the base class CuboidMaker with a sub class called CubeMaker.  Find out the formulas for volume and surface area for cubes and create those methods using the dimension properties from CuboidMaker.  Test your work by logging out your volume and surface area.
+
+
+
+    class CubeMaker extends CuboidMaker {
+    constructor(properties){
+        super(properties)
+    }
+    cube_volume(){
+        return Math.pow(this.length, 3);
+    }
+    cube_surfaceArea(){
+        return 6 * Math.pow(this.length, 2);
+    }
+    }
+
+    let Cube = new CubeMaker({
+        length:4,
+        width:4,
+        height:4
+    });
+
+    console.log(Cube)
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,31 +5,31 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
-let trex = new Object () {
-  name = tyrannosaurus,
-  diet = carnivorous,
-  weight = 7000kg,
-  length = 12m,
-  period = Late Cretaceous,
-  roar = function () {
+let trex = {
+  name : 'tyrannosaurus',
+  diet : 'carnivorous',
+  weight : '7000kg',
+  length : '12m',
+  period : "Late Cretaceous",
+  roar : function () {
     return "RAWERSRARARWERSARARARRRR!";
   }
 }
 
-let steg = new object () {
-  name = stegosaurus,
-  diet = herbivorous,
-  weight = 2000kg,
-  length = 9m,
-  period = Late Jurassic,
+let steg =  {
+  name : "stegosaurus",
+  diet : "herbivorous",
+  weight :"2000kg",
+  length :'9m',
+  period : 'Late Jurassic'
 }
 
-let Vel = new object () {
-  name = velociraptor,
-  diet = carnivorous,
-  weight = 15kg,
-  length = 1.8m,
-  period = Late Cretaceous,
+let vel = {
+  name : 'velociraptor',
+  diet : 'carnivorous',
+  weight : '15kg',
+  length : '1.8m',
+  period : 'Late Cretaceous'
 }
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
 
@@ -53,7 +53,7 @@ console.log(trex.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log(trex.roar);
+console.log(trex.roar());
 
 
 // ==== Arrays ====
@@ -79,7 +79,9 @@ Once you have the new array created, sort the universities alphabetically and lo
 
 const universities = [];
 for (let i=0; i<graduates.length; i++) {
-universities.sort(graduates[i].university)}
+    universities.push(graduates[i].university)
+}
+ universities.sort()
 console.log(universities);
 
 
@@ -91,12 +93,18 @@ The resulting contact information strings should have a space between the first 
 Log the result of your new array. */
 const contactInfo = [];
 for (let i=0; i<graduates.length; i++) {
-  contactInfo.find(graduates[i].university)}
+  contactInfo.push(`${graduates[i].first_name} ${graduates[i].email}`)
+}
 
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+for (let i=0; i<graduates.length; i++) {
+ if (graduates[i].university.includes("Uni")){
+    unisWithUni.push(graduates[i].university)
+ }
+}
 console.log(unisWithUni);
 
 
@@ -123,7 +131,7 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
-zooAnimals.forEach(object => displayNames.push(`${object.animal_name}, ${object.scientific_name}`) );
+zooAnimals.forEach(object => displayNames.push(`Name: ${object.animal_name}, Scientific: ${object.scientific_name}`) );
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -132,8 +140,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
-zooAnimals.map(animal_name.toLowerCase);
+const lowCaseAnimalNames = zooAnimals.map(object => object.animal_name.toLowerCase());
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -141,8 +148,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
-zooAnimals.filter(object => object.population === ? >5)
+const lowPopulationAnimals = zooAnimals.filter(object => object.population < 5)
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -150,8 +156,7 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-zooAnimals.reduce((total, animals) => total +animals.population, 0);
+const populationTotal = zooAnimals.reduce((total, animals) => total +animals.population, 0);
 
 console.log(populationTotal);
 
@@ -159,6 +164,7 @@ console.log(populationTotal);
 /*
 
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
+
 
 */
 
