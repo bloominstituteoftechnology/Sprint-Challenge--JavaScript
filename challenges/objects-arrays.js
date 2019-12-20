fbe0f9ccece6a18ee09a25c6b1e7eea604e446af
padgettriver <padgettcriver@gmail.com>
2019-12-20 11:55:20 -0500

    Half of objects-arrays completed

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,52 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const dinasourOne = {
+  name: 'tyrannosaurus',
+  diet: `carnivorous`,
+  weight: `700kg`,
+  length: `12m`,
+  period: `Late Cretaceous`,
+  roar: function(){
+    return `RAWERSRARARWERSARARARRRR!`
+  }
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const dinasourTwo = {
+  name: 'stegosaurus',
+  diet: `herbivorous`,
+  weight: `2000kg`,
+  length: `9m`,
+  period: `Late Jurassic`
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const dinasourThree = {
+  name: 'velociraptor',
+  diet: `carnivorous`,
+  weight: `15kg`,
+  length: `1.8m`,
+  period: `Late Cretaceous`
+}
+
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinasourOne.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinasourThree.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinasourTwo.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinasourOne.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(dinasourOne.roar());
 
 
 // ==== Arrays ====
@@ -51,8 +75,19 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
+const universities = [ 
+ "Missouri Southern State College" ,
+  "The School of the Art Institute of Chicago" ,
+  "Marian College" ,
+   "International Medical & Technological University" ,
+   "Sultan Salahuddin Abdul Aziz Shah Polytechnic" ,
+  "Fachhochschule Rosenheim, Hochschule für Technik und Wirtschaft" ,
+  "Salem University" ,
+  "Coastal Carolina University",
+ "Universidad Católica de Ávila",
+ "Universitat Rovira I Virgili Tarragona"
+];
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -60,11 +95,28 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = [  
+{  first_name: "Cynde", email: "ctorry0@macromedia.com" },
+{  first_name: "Saundra",  email: "swhal1@state.gov" },
+{  first_name: "Lambert",  email: "lparham2@techcrunch.com" },
+{  first_name: "Modestine",  email: "mdolder3@symantec.com" },
+{  first_name: "Chick",  email: "camorts4@google.com.au" },
+{  first_name: "Jakob",  email: "jharken5@spiegel.de" },
+{  first_name: "Robbi",  email: "rbrister6@redcross.org" },
+{ first_name: "Colline",  email: "cbrosh7@alibaba.com" },
+{  first_name: "Michail",  email: "mrome8@shinystat.com" },
+{  first_name: "Hube",  email: "hlethbrig9@foxnews.com" }];
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+const unisWithUni = [
+
+  "International Medical & Technological University" ,
+  "Salem University" ,
+  "Coastal Carolina University",
+ "Universidad Católica de Ávila",
+ "Universitat Rovira I Virgili Tarragona"
+];
 console.log(unisWithUni);
 
 
@@ -90,7 +142,7 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
+const displayNames = [`Name ${animal_name}, Scientific ${scientific_name}`];
 console.log(displayNames);
 
 /* Request 2: .map()
