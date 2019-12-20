a80a831ee99cd7ac38dfdd0702e2080e80f2b3ee
mastaryan <58410246+mastaryan@users.noreply.github.com>
2019-12-20 12:00:04 -0500

    Object-arrays object questions

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -33,16 +33,16 @@ const velociraptors = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptors.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
