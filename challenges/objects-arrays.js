99e71e9ea36e57e7ee7e7a73d7ea4cb1d5009ce3
webmaaster <wecodewetest@gmail.com>
2019-12-20 13:21:13 -0500

    Done with request 1 and request 2

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -82,9 +82,9 @@ console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-RangeError(){
+Roar(){
 
-  
+
 }
 
 console.log();
@@ -111,6 +111,12 @@ const graduates = [
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
 const universities = [];
+graduates.forEach(element =>{
+  universities.push(element.university);
+  
+
+});
+universities.sort();
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -119,11 +125,19 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
+
+
+
 const contactInfo = [];
+graduates.forEach(element =>{
+  contactInfo.push(element.first_name+ " " + element.email);
+});
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+
 console.log(unisWithUni);
 
 
