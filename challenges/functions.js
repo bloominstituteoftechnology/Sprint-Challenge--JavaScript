a8b300f87c8c1bca1e325b4c86992c3bcf8b962b
mastaryan <58410246+mastaryan@users.noreply.github.com>
2019-12-20 12:53:53 -0500

    Functions done

--- challenges/functions.js
+++ challenges/functions.js
@@ -35,16 +35,17 @@ console.log(consume('arg1', 'arg2', test))
  }
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
 
-// Explanation: 
+// Explanation: The variable is an ancestor inside the scope.
 
 
 const external = "I'm outside the function";
