c401b7a100ca37084c5276def704d198b7bb40a5
azrael3069 <cmrivera0004@student.stcc.edu>
2020-01-24 20:55:27 -0500

    almost done

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,6 +6,9 @@
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
+function consume (num1,num2,callback) {
+  return callback(num1,num2)
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,19 +16,31 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+function add (num1,num2) {
+  return num1 + num2
+}
+function multiply (num1,num2) {
+  return num1 * num2 
+}
+function greeting (lastName, firstName) {
+  this.lastName=lastName;
+  this.firstName=firstName;
+  return `Hello ${this.firstName} ${this.lastName}, nice to meet you"`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+ console.log(consume(2, 2, add)); // 4
+ console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
-
-// Explanation: 
+// Explanation:
+Nested functions are functions within functions, so they can access
+ the variables of other functions it is connected to .
+ 
 
 
 const external = "I'm outside the function";
