cc414b7848543c3560da42f882e31f5e2b00dc2a
Ahmad Wali Sayadi <sayadi@Ahmads-MacBook-Pro.local>
2020-01-26 19:33:52 -0800

    up-date

--- README.md
+++ README.md
@@ -29,6 +29,7 @@ Demonstrate your understanding of this week's concepts by answering the followin
 Edit this document to include your answers after each question. Make sure to leave a blank line above and below your answer so it is clear and easy to read by your team lead
 
 1. Describe the biggest difference between `.forEach` & `.map`.
+  foEach does not return any value
 
 2. What is the difference between a function and a method?
 
--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -127,16 +127,19 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
-
+const universities = graduates.map(function(str){
+	return str.university
+})
+console.log(universities.sort());
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
 The resulting contact information strings should have a space between the first name and the email, like this: 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map((info)=>{
+	return  `${info.first_name} ${email}`
+})
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
