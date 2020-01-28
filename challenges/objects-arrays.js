7b98b4c488cefdb94e8c062fb6088d19a9436253
Cassandra Dew <cpdew2@gmail.com>
2020-01-27 22:10:58 -0500

    Project Complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,7 +7,7 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-const tyrannasaurus = {
+const tyrannosaurus = {
   "name": "tyrannasaurus",
   "diet": "carnivorous",
   "weight": "7000kg",
@@ -156,7 +156,7 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = zooAnimals.reduce(function(total, currentValue){
+const populationTotal = zooAnimals.reduce((total, currentValue)=> {
   return total + currentValue.population;
 }, 0);
 console.log(populationTotal);
