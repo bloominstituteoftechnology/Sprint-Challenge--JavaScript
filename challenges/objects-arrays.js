d1e7c283471693e1eb59acd40671b7623be34e53
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-01-28 17:47:30 -0500

    Completed functions questions

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -83,11 +83,13 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map(nameEmail => `${nameEmail.first_name} ${nameEmail.email}`);
+
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+
 console.log(unisWithUni);
 
 
@@ -113,7 +115,8 @@ const zooAnimals = [
 The zoos want to display both the scientific name and the animal name in front of the habitats. Populate the displayNames array with only the animal_name and scientific_name of each animal. displayNames will be an array of strings, and each string should follow this pattern: "Name: Jackal, asiatic, Scientific: Canis aureus."
 
 */
-const displayNames = [];
+const displayNames = 
+zooAnimals.forEach(console.log(`${animalScientific.animal_name}, ${animalScientific.scientific_name}`))
 console.log(displayNames);
 
 /* Request 2: .map()
