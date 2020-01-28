8a2a64ba01ad496a552ab5a8a6ba546a9d39095b
Amy Davis <amydavis@Blakes-MBP-2.lan1>
2020-01-28 18:27:36 -0500

    Completed prototypes and classes questions

--- challenges/functions.js
+++ challenges/functions.js
@@ -30,7 +30,7 @@ function greeting (firstName, lastName) {
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 console.log(consume(2, 2, add)); // 4
- console.log(consume(10, 16, multiply)); // 160
+console.log(consume(10, 16, multiply)); // 160
 console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
