82c7028d859d6018fc7e49b7857fc97be420d904
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-02-21 18:48:12 -0800

    Advanced Array Methods Request 1 passing

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -121,6 +121,7 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(animal => displayNames.push(`Name: ${animal.animal_name} Scientific: ${animal.scientific_name}`));
 console.log(displayNames);
 
 /* Request 2: .map()
