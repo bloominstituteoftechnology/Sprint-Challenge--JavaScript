305bbf58f6199660e8531baf7987800d492ac6d1
fadygouda <fadygouda123@gmail.com>
2020-02-22 09:52:38 -0800

    objects-arrays and functions complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,10 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+consume (a,b,callback) {
+  return callback(a,b);
+};
+
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,9 +18,22 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a,b) {
+  return a+b;
+}
+
+function multiply(a,b) {
+  return a*b;
+}
+
+function greeting(a,b) {
+  return `Hello, ${a} ${b}, nice to meet you!`
+}
+
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
+ console.log(consume(2, 2, add)); // 4
 // console.log(consume(10, 16, multiply)); // 160
 // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
@@ -24,7 +41,7 @@
 // ==== Closures ==== 
 
 // Explain in your own words why nestedfunction can access the variable internal.
-
+  The nestedFunction is an inner function, meaning it can access outside variables functions.
 // Explanation: 
 
 
