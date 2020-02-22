30ad80aa39aa8f8ddfbb304801eba97dac41d321
Zack-Ballenger <zack.ballenger@tutanota.com>
2020-02-22 10:41:21 -0700

    Making progress on the object-arrays portion

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -71,8 +71,10 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
-console.log(universities);
+const universities = graduates.map(function (rubes) {
+  return rubes.university;
+});
+console.log(universities.sort());
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
@@ -80,13 +82,16 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map (function(rubes) {
+  return `${rubes.first_name} ${rubes.email}`
+});
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
-console.log(unisWithUni);
-
+const unisWithUni = graduates.filter(function (snippet){
+  return snippet.university.match(/Uni/g)
+});
+console.log(unisWithUni)
 
 // ==== ADVANCED Array Methods ====
 
@@ -111,16 +116,25 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+
+zooAnimals.forEach((naymes) =>{
+  displayNames.push(`Name: ${naymes.animal_name}, Scientific-Name ${naymes.scientific_name}`)
+})
 console.log(displayNames);
 
 /* Request 2: .map()
+
 
 The zoos need a list of all their animal's names (animal_name only) converted to lower case. Using map, create a new array of strings named lowCaseAnimalNames, each string following this pattern: "jackal, asiatic". Log the resut.
 
 */
 
 const lowCaseAnimalNames = [];
-console.log(lowCaseAnimalNames);
+
+zooAnimals.map((animals)=> {
+  lowCaseAnimalNames.push(animals.animal_name.toLowerCase())
+});
+
 
 /* Request 3: .filter() 
 
