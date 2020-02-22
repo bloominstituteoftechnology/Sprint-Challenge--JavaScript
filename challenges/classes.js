7ab1c556a91741647c3525f9a12a73e8af462e46
Zack-Ballenger <zack.ballenger@tutanota.com>
2020-02-22 12:15:04 -0700

    finally finished with the first problem in the classes section...

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,18 +1,18 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 class CuboidMaker {
     constructor(cubesy) {
- function (length, width, height) 
-    this.length = cubesy.length;
-    this.width = cubesy.cubesy.width;
-    this.height = cubesy.height;
+        this.length = cubesy.length;
+        this.width = cubesy.width;
+        this.height = cubesy.height;
+    }
+    volume() {
+        return this.length * this.width * this.height;
+    }
+    surfaceArea() {
+        return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
+    }
 }
-volume() {
-    return this.lengh * this.height * this.height;
-}
-surfaceArea {
-    return 2 * (this.length * this.width + this.length * this.height + this.width * this.height);
-}
-  }
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
