8d792c66e2c1b3a584f920d819eb0a9e29cd05e8
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-02-21 19:11:22 -0800

    Callbacks passing all

--- challenges/functions.js
+++ challenges/functions.js
@@ -28,9 +28,9 @@ function greeting(a, b) {
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
