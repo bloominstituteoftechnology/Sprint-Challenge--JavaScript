ae6d900e77b1e7267a607762d3867d206d3345b9
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-02-21 18:55:35 -0800

    Advanced Array Methods Request 2 passing

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -131,6 +131,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
+zooAnimals.map(animal => lowCaseAnimalNames.push(animal.animal_name.toLowerCase()));
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
