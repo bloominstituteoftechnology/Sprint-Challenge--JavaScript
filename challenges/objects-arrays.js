56c9cf2c08c0144299d5ed2035ac2fb559a407e7
JOliver23 <59593045+JOliver23@users.noreply.github.com>
2020-02-21 18:59:45 -0800

    Advanced Array Methods Request 3 passing

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -139,7 +139,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter(animal => animal.population < 5);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
