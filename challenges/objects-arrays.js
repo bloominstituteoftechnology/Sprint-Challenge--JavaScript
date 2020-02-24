9b9701ac0ab947740fc2f2f3ebbbdea64694eef4
Elijah Atkins <elijahorama@gmail.com>
2020-02-23 20:11:07 -0700

    univerity done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,51 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tyrannosaurus = {
+  "diet":"carnivorous", 
+  "weight":"7000kg", 
+  "length":"12m", 
+  "era": "Late Cretaceous",
+  "roar": function() {
+    return "RAWERSRARARWERSARARARRRR!"
+  }
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
 
+const stegosaurus = {
+  "diet":"herbivorous", 
+  "weight":"2000kg", 
+  "length":"9m", 
+  "era":"Late Jurassic"};
+
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+
+
+const velociraptor = {
+  "diet":"carnivorous", 
+  "weight":"15kg", 
+  "length":"1.8m", 
+  "era": "Late Cretaceous"};
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.era);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
@@ -51,7 +74,7 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+const universities = graduates.map(x => x.university).sort();
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -60,14 +83,17 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates.map(grad => `${grad.first_name} ${grad.email}`)
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+var count = 0;
+for(var i = 0; i < universities.length; ++i){
+    if(universities[i].includes("Uni"))
+        count++;
+}
+const unisWithUni = count;
 console.log(unisWithUni);
-
-
 // ==== ADVANCED Array Methods ====
 
 // Given this zoo data from around the United States, follow the instructions below.  Use the specific array methods in the requests below to solve the problems.
