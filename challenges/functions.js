5fbd3ec349ac857f36f980c0a54da8abb0fccc13
Steven Craig Moss <uxuiudev@gmail.com>
2020-02-24 03:42:31 -0500

    second commit at time limit

--- challenges/functions.js
+++ challenges/functions.js
@@ -1,42 +1,44 @@
-// ==== Callbacks ====  
+// ==== Callbacks ====
 
 /* Step 1: Create a higher-order function
-  * Create a higher-order function named consume with 3 parameters: a, b and cb
-  * The first two parameters can take any argument (we can pass any value as argument)
-  * The last parameter accepts a callback
-  * The consume function should return the invocation of cb, passing a and b into cb as arguments
-*/
-
+ * Create a higher-order function named consume with 3 parameters: a, b and cb
+ * The first two parameters can take any argument (we can pass any value as argument)
+ * The last parameter accepts a callback
+ * The consume function should return the invocation of cb, passing a and b into cb as arguments
+ */
+const name = function(a, b, cb) {
+     callback(cb);
+};
+const consume = function(callback, a, b) {};
 
 /* Step 2: Create several functions to callback with consume();
-  * Create a function named add that returns the sum of two numbers
-  * Create a function named multiply that returns the product of two numbers 
-  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
-*/
-
+ * Create a function named add that returns the sum of two numbers
+ * Create a function named multiply that returns the product of two numbers
+ * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
+ */
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
 // console.log(consume(10, 16, multiply)); // 160
 // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
-
-// ==== Closures ==== 
+// ==== Closures ====
 
 // Explain in your own words why nestedfunction can access the variable internal.
-
-// Explanation: 
-
+/*
+            because it is within the same lexical environment which always supports inner most functions having access to its parent function. And so thinking about heridity where a parent (outer most function) gives info to child it means child has access to those genes and parent wont have access to childs gene
+            */
+// Explanation:
 
 const external = "I'm outside the function";
 
 function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+     console.log(external);
+     const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
+     function nestedFunction() {
+          console.log(internal);
+     }
+     nestedFunction();
 }
 myFunction();
