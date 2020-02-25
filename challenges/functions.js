77b2a3afaa59d901e964311f26972854232afb19
Steven Craig Moss <uxuiudev@gmail.com>
2020-02-24 22:39:11 -0500

    last commit

--- challenges/functions.js
+++ challenges/functions.js
@@ -6,21 +6,32 @@
  * The last parameter accepts a callback
  * The consume function should return the invocation of cb, passing a and b into cb as arguments
  */
-const name = function(a, b, cb) {
-     callback(cb);
-};
-const consume = function(callback, a, b) {};
+function consume(a, b, cb) {
+     return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
  * Create a function named add that returns the sum of two numbers
  * Create a function named multiply that returns the product of two numbers
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
+// const name = function(a, b, cb) {
+const add = function(num1, num2) {
+     const sum = num1 + num2;
+     return sum;
+};
+const multiply = function(num1, num2) {
+     const product = num1 * num2;
+     return product;
+};
+const greeting = function(firstName, lastName) {
+     return `Hello ${firstName} ${lastName}, nice to meet you!`;
+};
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 // ==== Closures ====
 
