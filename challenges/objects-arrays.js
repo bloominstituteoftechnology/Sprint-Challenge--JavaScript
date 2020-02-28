6a19d63854e328e8760e3de60dac4ebb7d7a6396
Taja-House <ahlliahtaja@yahoo.com>
2020-02-28 15:36:24 -0800

    Completed request 1 of Array

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -92,8 +92,20 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = [];
-console.log(universities);
+graduates.sort((a,b)=>{
+  if (a.university < b.university)
+ return -1;
+}
+  if (a.university> b.university)
+  return 1;
+}
+return 0;
+});
+
+console.log(university);
+
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
 
