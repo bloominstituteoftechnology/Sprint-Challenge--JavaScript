7881f5c900315a319311cf5927f6c25a1168dbd4
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-02-28 09:06:05 -0800

    functions - consume test is passing

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,50 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
-
+const dinosaurOne ={
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg',
+  length: '12m',
+  period: 'Late Cretaceous',
+  //function === methods
+  roar: function(){
+    return '"RAWERSRARARWERSARARARRRR!"';
+  },
+};
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const dinosaurTwo ={
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+  };
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+const dinosaurThree ={
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+  };
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinosaurOne.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinosaurThree.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinosaurTwo.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinosaurOne.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(dinosaurOne.roar());
 
 
 // ==== Arrays ====
@@ -51,7 +73,20 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
-const universities = [];
+const universities = graduates[graduates.university]
+
+function sortUniversities(universities) {
+  universities.sort(function(university1, university2){
+    if ( university1.university < university2.university ){
+      return -1
+    }
+    if (university1.university > university2.university){
+      return 1
+    }
+    return 0
+  })
+  return universities
+};
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -60,7 +95,7 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
-const contactInfo = [];
+const contactInfo = graduates[first_name, email];
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
