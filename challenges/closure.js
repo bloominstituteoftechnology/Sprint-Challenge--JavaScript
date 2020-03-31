fc66d8e4661b38d944b01c32a71f1b931660c617
Zoe Ludwig <zoe.ludwig@lambdaschool.com>
2020-03-31 15:18:34 -0700

    added counter challenge

--- challenges/closure.js
+++ challenges/closure.js
@@ -1,8 +1,6 @@
 // ==== Closures ==== 
 
-// Explain in your own words why nestedfunction can access the variable internal.
-
-// Explanation: 
+/* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */
 
 
 const external = "I'm outside the function";
@@ -17,3 +15,10 @@ function myFunction() {
   nestedFunction();
 }
 myFunction();
+
+// Explanation: 
+
+
+/* Task 2: Counter */
+
+/* Create a function called `factorial` that accepts a parameter and uses a counter to return the factorial of that number. For example, `factorial(4)` should return 10 because 1+2+3+4 is 10. */
