a540dcbb8d4e5758ff793eae7909f9df58cb8533
Brit Hemming <hello@brittanyhemming.com>
2020-04-24 10:42:08 -0400

    updated closure.js
    changed factorial to summation (factorial is for multiplication)

--- challenges/closure.js
+++ challenges/closure.js
@@ -21,4 +21,4 @@ myFunction();
 
 /* Task 2: Counter */
 
-/* Create a function called `factorial` that accepts a parameter and uses a counter to return the factorial of that number. For example, `factorial(4)` should return 10 because 1+2+3+4 is 10. */
+/* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
