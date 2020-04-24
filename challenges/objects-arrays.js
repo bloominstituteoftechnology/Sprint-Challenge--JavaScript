fa3b10616869a751aa67a7f19d3e542b1f8c4c1a
Joey <joey@xm8.ninja>
2020-04-24 11:33:25 -0400

    dinos done

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -48,19 +48,23 @@ const dinoThree = new Dino(
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(dinoOne.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(dinoThree.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(dinoTwo.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(dinoOne.period);
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+Dino.prototype.roar = function () {
+  console.log("RAWERSRARARWERSARARARRRR!");
+};
+
+dinoOne.roar();
 
 // ==== Arrays ====
 
