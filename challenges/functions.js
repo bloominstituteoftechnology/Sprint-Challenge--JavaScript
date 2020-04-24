5dd28f0f106fe3c88dbba0752e924fe772e899fd
Joey <joey@xm8.ninja>
2020-04-24 12:50:40 -0400

    initial push for function-js

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,12 +7,32 @@
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
 
+function highOrderFunc(num1, num2, cb) {
+  return cb(num1, num2);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+
+const add = (num1, num2) => {
+  return num1 + num2;
+};
+const multiply = (num1, num2) => {
+  return num1 * num2;
+};
+
+function multiply = (num1,)
+
+function greeting (first, last, consume) {
+  return consume(first, last);
+}
+const personOne = (first, last) =>
+  return `Hello ${first} ${last}, nice to meet you!`);
+};
+console.log(greeting('joey', 'wilson', personOne));
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
