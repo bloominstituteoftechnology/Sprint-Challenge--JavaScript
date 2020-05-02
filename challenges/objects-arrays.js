8c1c44f09d48cc00c6936e38010edb8ef9ae41b4
Chris Westberg <chriswestberg@Chriss-MBP.hsd1.ut.comcast.net>
2020-05-02 12:56:15 -0600

    Did not finish reduce method or callbacks

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -7,28 +7,52 @@
 */
 
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tRex = {
+  name: 'tyrannosaurus',
+  diet: 'carnivorous',
+  weight: '7000kg', 
+  length: '12m', 
+  period: 'Late Cretaceous',
+  roar: function() {
+    return `RAWERSRARARWERSARARARRRR!`
+  }
+}
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const steg = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg', 
+  length: '9m', 
+  period: 'Late Jurassic'
+}
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
+const raptor = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg', 
+  length: '1.8m', 
+  period: 'Late Cretaceous'
+}
 
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tRex.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(raptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(steg.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tRex.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tRex.roar);
 
 
 // ==== Arrays ====
@@ -52,6 +76,7 @@ const graduates = [
 
 Once you have the new array created, log the result. */
 const universities = [];
+graduates.forEach(element => universities.push(element.university));
 console.log(universities);
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student. This will be an array of strings.
@@ -61,10 +86,16 @@ The resulting contact information strings should have a space between the first 
 
 Log the result of your new array. */
 const contactInfo = [];
+graduates.forEach(element => contactInfo.push(`${element.first_name} ${element.email}`));
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
 const unisWithUni = [];
+graduates.forEach(function(element) {
+  if(element.university.includes('Uni')) {
+    unisWithUni.push(element.university)
+  }
+})
 console.log(unisWithUni);
 
 
@@ -91,6 +122,7 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach(element => displayNames.push(`Name: ${element.animal_name}, Scientific: ${element.scientific_name}`))
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -99,7 +131,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(element => element.animal_name.toLowerCase());
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -108,6 +140,11 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 const lowPopulationAnimals = [];
+zooAnimals.filter(function(element) {
+  if (element.population < 5) {
+    lowPopulationAnimals.push(element)
+  }
+})
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -115,7 +152,7 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((accumulator, currentValue) => accumulator + currentValue.population);
 console.log(populationTotal);
 
 
@@ -124,4 +161,3 @@ console.log(populationTotal);
 Stretch: If you haven't already, convert your array method callbacks into arrow functions.
 
 */
-
