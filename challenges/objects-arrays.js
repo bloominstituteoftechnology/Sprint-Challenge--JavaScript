39fd4e6385b4ea0917a3f18cfbf539438813ea1e
JenVest2020 <jenvest2019@gmail.com>
2020-05-01 22:19:08 -0500

    passed Adv Array Meth request2

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -129,6 +129,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
+zooAnimals.map(zooAnimals => { lowCaseAnimalNames.push(zooAnimals.animal_name.toLowerCase()) });
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
