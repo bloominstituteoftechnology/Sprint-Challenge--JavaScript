73075f1b925e701c273d937f9d378fa038b7ef22
Tylar Washington-Beechum <tylay47@gmail.com>
2020-05-01 23:56:44 -0400

    5/1/20

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,10 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
-
+function CuboidMaker (attributes){
+    this.length = attributes.length
+    this.width = attributes.width
+    this.height = attributes.height
+  }
+  
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
