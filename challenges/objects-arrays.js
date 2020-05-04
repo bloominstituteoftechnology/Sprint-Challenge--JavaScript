1eccef68249c598c479af9d218d2b28439f268af
Robel Mengistu <62121236+robelv2020@users.noreply.github.com>
2020-05-03 19:46:09 -0600

    made the roar method for MR tyrannosaurus

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -12,7 +12,11 @@ const tyrannosaurus = {
   diet:'carnivorous',
   weight:'7000kg',
   length:'12m',
-  period:'Late Cretaceous'
+  period:'Late Cretaceous',
+  roar = function(){
+    return 'RAWERSRARARWERSARARARRRR!';
+  }
+  
 
 }
 
@@ -39,20 +43,20 @@ const velociraptor = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velociraptor.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+console.log(tyrannosaurus.roar());
 
 
 // ==== Arrays ====
