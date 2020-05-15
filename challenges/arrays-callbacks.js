b040fbdcfc8e581fee05d03d4ab6eed279e4d186
Mitchell Wright <mitchellbwright@gmail.com>
2020-05-15 17:18:02 -0600

    complete Request 2 and 3

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -30,7 +30,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(animal => animal.animal_name.toLowerCase());
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter()
@@ -38,7 +38,7 @@ console.log(lowCaseAnimalNames);
 The zoos are concerned about animals with a lower population count. Using filter, create a new array of objects called lowPopulationAnimals which contains only the animals with a population less than 5.
 
 */
-const lowPopulationAnimals = [];
+const lowPopulationAnimals = zooAnimals.filter(animal => animal.population < 5);
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce()
