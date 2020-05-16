244bddcc850afe2d23e7d35f0812160d5ec58c2b
User <sjvanpeursem@gmail.com>
2020-05-16 12:33:06 -0500

    Getting Closer

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -88,7 +88,7 @@ function multiply (cb)
 return a*b; 
 
 function greeting (cb)
-return "Hello ${first-name} ${last-name}, nice to meet you"
+return 'Hello ${first-name} ${last-name} , nice to meet you'
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
@@ -96,7 +96,9 @@ return "Hello ${first-name} ${last-name}, nice to meet you"
 // console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
-
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 /*
 
--- challenges/closure.js
+++ challenges/closure.js
@@ -1,6 +1,7 @@
 // ==== Closures ==== 
 
-/* Task 1: Study the code below and explain in your own words why nested function can access the variable internal. */
+/* Task 1: Study the code below and explain in your own words why nested function can access 
+the variable internal. */
 
 
 const external = "I'm outside the function";
@@ -16,9 +17,16 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
-
-
+//Explanation: 
+//Because it is inside the function rather than outside the function. 
 /* Task 2: Counter */
 
-/* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+/* Create a function called `sumation` that accepts a parameter and uses a counter to return the 
+summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation ()
+ { let add = (function () {
+  var counter = 0;
+  return function () {counter += 1; return counter}
+})();
+}
