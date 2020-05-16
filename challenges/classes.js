9dc003d01396627a9926d4c087e403ec02c88b65
Terry Edwards <terryedwardsjr113@gmail.com>
2020-05-16 13:44:53 -0400

    one left

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,5 +1,20 @@
 // 1. Copy and paste your prototype in here and refactor into class syntax.
 
+function CuboidMaker(length,width,height){
+    this.length=length;
+    this.width=width;
+    this.height=height;
+  }
+
+  CuboidMaker.prototype.volume=function(){
+    return(this.length*this.width*this.height);
+  }
+  CuboidMaker.prototype.surfaceArea=function(){
+        return(2 * (this.length * this.width + this.length * this.height + this.width * this.height))
+      }
+
+    const cuboid= new CuboidMaker(length=4, width=5, height=5);
+
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 // console.log(cuboid.volume()); // 100
 // console.log(cuboid.surfaceArea()); // 130
--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -41,9 +41,7 @@ CuboidMaker.prototype.surfaceArea=function(){
 // const cube = new Child({
 //   length:4, width:5, height:5,
 // });
-const cuboid=new CuboidMaker({
-    4, 5, 5
-  })
+const cuboid= new CuboidMaker(length=4, width=5, height=5);
 // Test your volume and surfaceArea methods by uncommenting the logs below:
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
