d76f409420bf39aa26078107e5ce9d88edeeac21
Terry Edwards <terryedwardsjr113@gmail.com>
2020-05-16 13:18:29 -0400

    halfway done

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,6 +21,8 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+let display=zooAnimals.forEach((displays)=>{
+  displayNames.push(`Name: ${displays.animal_name}, Scientific: ${displays.scientific_name}`)})
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -30,6 +32,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 */
 
 const lowCaseAnimalNames = [];
+let lowAnimals=zooAnimals.map((names)=>lowCaseAnimalNames.push(names.animal_name))
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -38,6 +41,7 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 const lowPopulationAnimals = [];
+let lowPop=zooAnimals.filter((pop)=>{if(pop.population<5){lowPopulationAnimals.push(`Name- ${pop.animal_name}, Population- ${pop.population}`)}})
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -45,8 +49,8 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
-console.log(populationTotal);
+const populationTotal=zooAnimals.reduce((total,finalPop)=>{return total+=finalPop.population;},0)
+console.log("The total animal population across the United States is "+populationTotal+" animals!");
 
 
 // ==== Callbacks ====  
@@ -57,7 +61,9 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+function consume(a,b,cb){
+  return cb(a,b)
+};
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -65,11 +71,16 @@ console.log(populationTotal);
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a,b,cb){return(a+b)}
+
+function multiply(a,b,cb){return(a*b)}
+
+function greeting(a,b,cb){return(`Hello ${a} ${b}, nice to meet you!`)}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
--- challenges/closure.js
+++ challenges/closure.js
@@ -22,3 +22,11 @@ myFunction();
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+function sumation(x){
+  let sum=0;
+  for(let i=1;i<=x;i++){
+    sum+=i;
+  }
+  return(sum)
+}
+console.log(sumation(4));
