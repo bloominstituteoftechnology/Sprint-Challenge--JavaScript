d369313581a466575c58a0c999f207450e3e9bc1
Lane <lanelovesugly@icloud.com>
2020-05-15 19:47:01 -0600

    finished

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -21,6 +21,9 @@ The zoos want to display both the scientific name and the animal name in front o
 
 */
 const displayNames = [];
+zooAnimals.forEach((arrItem) => {
+  return displayNames.push(`Name: ${arrItem.animal_name}, Scientific: ${arrItem.scientific_name}`);
+});
 console.log(displayNames);
 
 /* Request 2: .map()
@@ -29,7 +32,9 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map((arrItem) => {
+  return arrItem.animal_name.toLowerCase();
+});
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
@@ -38,6 +43,11 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 const lowPopulationAnimals = [];
+zooAnimals.filter((arrItem) => {
+  if(arrItem.population < 5) {
+    lowPopulationAnimals.push(arrItem);
+  }
+});
 console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
@@ -45,7 +55,9 @@ console.log(lowPopulationAnimals);
 The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-const populationTotal = 0;
+const populationTotal = zooAnimals.reduce((accum, item) => {
+  return accum + item.population;
+},0);
 console.log(populationTotal);
 
 
@@ -57,20 +69,32 @@ console.log(populationTotal);
   * The last parameter accepts a callback
   * The consume function should return the invocation of cb, passing a and b into cb as arguments
 */
-
+const consume = function(a, b, cb) {
+  return cb(a, b);
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+const add = function(num1, num2) {
+  return num1 + num2;
+}
+
+const multiply = function(num1, num2) {
+  return num1 * num2;
+}
+
+const greeting = function(firstName, lastName) {
+  return `Hello ${firstName} ${lastName}, nice to meet you!`
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
-
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 
--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,21 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: because of how scope works. Inside the scope of nestedFunction I can access everything outside of it. But I cannot access variables unique to nested Functions scope from outside
 
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+
+
+function sumation(num) {
+  let result = 0;
+  for (i = 0; i <= num; i++) {
+    result += i;
+  }
+  return result
+}
+
+console.log(sumation(4))
