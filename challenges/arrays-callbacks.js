0d960fb0c60a2203ef6e1e33139bc48726d875a9
Your Name <you@example.com>
2020-05-22 10:46:21 -0500

    second callback works!

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -81,6 +81,17 @@ function consume (a, b, cb){
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a, b){
+  return a + b;
+}
+
+function multiply(a, b){
+  return a * b;
+}
+
+function greeting(firstName, lastName){
+  return `Hello ${firstName} ${lastName}, nice to meet you!`;
+}
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // console.log(consume(2, 2, add)); // 4
