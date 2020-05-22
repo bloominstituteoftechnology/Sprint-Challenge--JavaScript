57b2df88cc79f55455296f2f76a311a46762f7e1
Your Name <you@example.com>
2020-05-22 10:59:51 -0500

    did the clossures.js tasks

--- challenges/closure.js
+++ challenges/closure.js
@@ -18,7 +18,18 @@ myFunction();
 
 // Explanation: 
 
+`A: nestedFunction can access the variable internal because they are both within the block level scope of myFunction.`
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+function summation(n){
+  let counter = 0;
+  for (let i=1; i<= n; i++){
+    counter += i;
+  }
+  return counter;
+}
+
+console.log(summation(4));
