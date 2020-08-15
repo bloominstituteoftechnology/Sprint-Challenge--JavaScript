9269091ba18bcc1f8c68d949435e6b6377954935
extrude575757 <nicholas.a.lacapria@gmail.com>
2020-08-14 21:44:46 -0700

    Onto Stretch goals

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -36,7 +36,7 @@ CuboidMaker.prototype.volume = function(){
 */
 CuboidMaker.prototype.surfaceArea = function(){
   return 2 * (this.length * this.width + this.length * this.height +
-    this.width * this.height)
+    this.width * this.height);
 }
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
