2eb6cbad2bad65f68bba491962ead7e1375fc7fd
drewmrodriguez <drewmrodriguez@gmail.com>
2020-08-15 10:48:39 -0700

    Closures

--- challenges/closure.js
+++ challenges/closure.js
@@ -16,9 +16,21 @@ function myFunction() {
 }
 myFunction();
 
-// Explanation: 
+// Explanation: nested function can access const external bc external is in the global scope, any function can reach up to the global scope and grab a defined variable.
 
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+console.log("===Task 2===")
+
+function sumation(num) {
+  let counter = 0;
+  for (let i = 0; i <= num; i++) {
+    counter += i;
+  }
+  return counter;
+}
+
+console.log(sumation(4));
