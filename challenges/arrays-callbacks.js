599cd97eacc08b2fe8c409d6cb589e8541828e7d
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-08-16 18:53:28 -0700

    task3arrays

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -50,7 +50,15 @@ The zoos are concerned about animals with a lower population count. Using filter
 
 */
 const lowPopulationAnimals = [];
+zooAnimals.filter((item) =>{
+  lowPopulationAnimals.push(item.population < 5);
+  return item
+
+});
 console.log(lowPopulationAnimals);
+
+//review
+
 
 /* Request 4: .reduce() 
 
