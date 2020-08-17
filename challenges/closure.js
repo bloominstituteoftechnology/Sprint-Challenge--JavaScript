a9eea358f4b5466db2feb358c73ef955488beceb
Austin Littlefield <austinlittlefield@MacBook-Pro.local>
2020-08-16 19:24:23 -0700

    task1closure

--- challenges/closure.js
+++ challenges/closure.js
@@ -17,8 +17,18 @@ function myFunction() {
 myFunction();
 
 // Explanation: 
-
+// Because the nestedFunction is able to use variables that are set on a higher order function. Functions can call upon variables in higher order functions, but can not call upon variables nested lower. If this variable was in the global scope it could also be called upon.
 
 /* Task 2: Counter */
 
 /* Create a function called `sumation` that accepts a parameter and uses a counter to return the summation of that number. For example, `summation(4)` should return 10 because 1+2+3+4 is 10. */
+
+
+function sumation(parameter){
+let counter = 0;
+for(let i = 0; i <= parameter; i++){
+  counter += i;
+}
+return counter;
+}
+console.log(sumation(4));
