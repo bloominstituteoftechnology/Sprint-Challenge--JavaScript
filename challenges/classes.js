353a93a65f2e999213e36d4bb8632b144cb04d9f
ChrisReilly024 <chris.reilly024@gmail.com>
2021-06-18 19:06:32 -0400

    class tasks complete

--- challenges/classes.js
+++ challenges/classes.js
@@ -1,7 +1,17 @@
 // Copy and paste your prototype in here and refactor into class syntax.
 
-
-
+class CuboidMaker{
+    constructor(obj) {
+    this.length = obj.length;
+    this.width = obj.width;
+    this.height = obj.height
+    }
+    volume() {return this.length * this.height * this.width};
+    surfaceArea() {return  2 * (this.length * this.width + this.length * this.height + this.width * this.height)};
+}
+  
+  
+let cuboid = new CuboidMaker({length: 4, width: 5, height: 5});
 
 console.log(cuboid.volume()); // 100
 console.log(cuboid.surfaceArea()); // 130
