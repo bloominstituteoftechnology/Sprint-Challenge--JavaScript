75666889a867a101b94c5e3f7131639ba105695f
ChrisReilly024 <chris.reilly024@gmail.com>
2021-06-18 18:11:42 -0400

    array tasks complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -25,19 +25,19 @@ let velociraptor = new Dino({name: 'Velociraptor',diet: "carnivorous", weight: '
 // console.log(velociraptor);
 // Using your dinosaur objects, log answers to these questions:
 // How much did tyrannosaurus weigh?
-console.log(tyrannosaurus.weight)
+// console.log(tyrannosaurus.weight)
 // What was the diet of a velociraptor?
-console.log(velociraptor.diet)
+// console.log(velociraptor.diet)
 // How long was a stegosaurus?
-console.log(stegosaurus.length)
+// console.log(stegosaurus.length)
 // What time period did tyrannosaurus live in?
-console.log(tyrannosaurus.period)
+// console.log(tyrannosaurus.period)
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
 Dino.prototype.roar = function() {
   return `RAWERSRARARWERSARARARRRR!`
 }
-console.log(tyrannosaurus.roar())
+// console.log(tyrannosaurus.roar())
 
 // ==== Arrays ====
 
@@ -56,7 +56,8 @@ const graduates = [{"id":1,"first_name":"Cynde","university":"Missouri Southern 
 
 /* Request 1: Create a new array called universities that contains all the univeristies in the graduates array.
 Once you have the new array created, sort the universities alphabetically and log the result. */
-
+let universities = graduates.map(grad => grad.university).sort();
+// console.log(universities);
 
 
 /* Request 2: Create a new array called contactInfo that contains both first name and email of each student.
@@ -65,10 +66,12 @@ The resulting contact information should have a space between the first name and
 Name email@example.com
 
 Log the result of your new array. */
-
+let contactInfo = graduates.map(grad => `${grad.first_name} ${grad.email}`);
+// console.log(contactInfo);
 
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called uni that contains them all. Log the result. */
+// console.log(graduates.filter(grad => grad.university.includes('Uni')).length);
 
 
 // ==== Array Methods ====
@@ -87,7 +90,12 @@ zooAnimals = [{"animal_name":"Jackal, asiatic","population":5,"scientific_name":
 {"animal_name":"Australian pelican","population":5,"scientific_name":"Pelecanus conspicillatus","state":"West Virginia"}];
 
 // The zoos need a list of all their animal's names converted to lower case.  Create a new array named lowerCase and map over each name to convert them all to lower case.  Log the resut.
+// console.log(Array.from(new Set(zooAnimals.map(animal => animal.name.toUpperCase() + animal.slice(1)))))
 
+// console.log(zooAnimals.
+//   map(animal => animal.animal_name.split(' ').
+//   map(name => name[0].toUpperCase() + name.slice(1)).join(' ')
+//   ))
 
 // The zoos need to know their total animal population across the United States.  Add up all the population numbers from all the zoos using the .reduce() method.
-
+console.log(zooAnimals.reduce((acc,cur) => acc + cur.population,0));
